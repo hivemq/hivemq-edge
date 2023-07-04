fb7fc4a060afd20f441d7c3105b03c51466ca61f
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-07-04 17:24:20 +0100

    refactor(15305): add controlled state for active tab

--- hivemq-edge/src/frontend/src/modules/ProtocolAdapters/ProtocolAdapterPage.tsx
+++ hivemq-edge/src/frontend/src/modules/ProtocolAdapters/ProtocolAdapterPage.tsx
@@ -1,8 +1,8 @@
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react'
 
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 
 import PageContainer from '@/components/PageContainer.tsx'
 import ProtocolAdapters from '@/modules/ProtocolAdapters/components/panels/ProtocolAdapters.tsx'
@@ -15,9 +15,22 @@ export enum ProtocolAdapterTabIndex {
 
 const ProtocolAdapterPage: FC = () => {
   const { t } = useTranslation()
+  const { state } = useLocation()
+  const [tabIndex, setTabIndex] = useState(0)
+
+  useEffect(() => {
+    if (state?.protocolAdapterTabIndex) {
+      setTabIndex(state.protocolAdapterTabIndex)
+    }
+  }, [state])
+
+  const handleTabsChange = (index: number) => {
+    setTabIndex(index)
+  }
+
   return (
     <PageContainer title={t('protocolAdapter.title') as string} subtitle={t('protocolAdapter.description') as string}>
-      <Tabs isLazy>
+      <Tabs onChange={handleTabsChange} index={tabIndex}>
         <TabList>
           <Tab fontSize="lg" fontWeight={'bold'}>
             {t('protocolAdapter.tabs.protocols')}
