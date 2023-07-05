b5079c425469cb81f9ca4707b65d41a3c8b3aeaa
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-07-05 13:43:53 +0100

    refactor(15305): refactor the auth context provider to support local storage - add isLoading flag - add validate/verify check for token

--- hivemq-edge/src/frontend/src/modules/Auth/AuthProvider.tsx
+++ hivemq-edge/src/frontend/src/modules/Auth/AuthProvider.tsx
@@ -1,10 +1,15 @@
-import { createContext, FunctionComponent, PropsWithChildren, useState } from 'react'
+import { createContext, FunctionComponent, PropsWithChildren, useEffect, useState } from 'react'
+
+import { parseJWT, verifyJWT } from '@/api/utils.ts'
+import { ApiBearerToken } from '@/api/__generated__'
+import { useLocalStorage } from '@/hooks/useLocalStorage/useLocalStorage.ts'
+
 import { fakeAuthProvider } from './fakeAuthProvider'
-import { ApiBearerToken } from '../../api/__generated__'
 
 interface AuthContextType {
   credentials: ApiBearerToken | null
   isAuthenticated: boolean
+  isLoading: boolean
   login: (user: ApiBearerToken, callback: VoidFunction) => void
   logout: (callback: VoidFunction) => void
 }
@@ -13,6 +18,28 @@ export const AuthContext = createContext<AuthContextType | null>(null)
 
 export const AuthProvider: FunctionComponent<PropsWithChildren> = ({ children }) => {
   const [credentials, setCredentials] = useState<ApiBearerToken | null>(null)
+
+  const [isAuthToken, setAuthToken] = useLocalStorage<string | undefined>('auth', undefined)
+  const [isLoading, setLoading] = useState(true)
+
+  useEffect(() => {
+    if (isAuthToken) {
+      const parsedToken = parseJWT(isAuthToken)
+      if (parsedToken) {
+        const isValid = verifyJWT(parsedToken)
+        if (isValid) {
+          login({ token: isAuthToken }, () => {
+            setLoading(false)
+          })
+        } else {
+          setAuthToken(undefined)
+          setLoading(false)
+        }
+      }
+    } else {
+      setLoading(false)
+    }
+  }, [])
 
   const login = (newUser: ApiBearerToken, callback: VoidFunction) => {
     return fakeAuthProvider.login(() => {
@@ -24,12 +51,15 @@ export const AuthProvider: FunctionComponent<PropsWithChildren> = ({ children })
   const logout = (callback: VoidFunction) => {
     return fakeAuthProvider.logout(() => {
       setCredentials(null)
+      setAuthToken(undefined)
       callback()
     })
   }
 
   return (
-    <AuthContext.Provider value={{ credentials, login, logout, isAuthenticated: fakeAuthProvider.isAuthenticated }}>
+    <AuthContext.Provider
+      value={{ credentials, login, logout, isLoading, isAuthenticated: fakeAuthProvider.isAuthenticated }}
+    >
       {children}
     </AuthContext.Provider>
   )
