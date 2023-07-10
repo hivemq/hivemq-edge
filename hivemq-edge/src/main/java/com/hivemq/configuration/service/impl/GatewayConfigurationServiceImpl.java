737aa91e7dc953b0e39f09645caffd630470d390
Simon Johnson <2924684+simon622@users.noreply.github.com>
2023-07-10 10:06:53 +0100

    Simple config examples

--- hivemq-edge/src/main/java/com/hivemq/configuration/service/impl/GatewayConfigurationServiceImpl.java
+++ hivemq-edge/src/main/java/com/hivemq/configuration/service/impl/GatewayConfigurationServiceImpl.java
@@ -24,7 +24,6 @@ public class GatewayConfigurationServiceImpl implements DynamicConfigurationServ
 
     private boolean mutableConfigurationEnabled;
     private boolean configurationExportEnabled;
-    private String externalHostname;
 
     @Override
     public boolean isMutableConfigurationEnabled() {
