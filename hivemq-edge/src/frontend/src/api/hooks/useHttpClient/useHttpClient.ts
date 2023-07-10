4def800b0e8826f3ee1ab2b012f98a11c309b0f9
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-07-10 14:45:54 +0100

    feat(15309): add axios interceptors to the http client - define global axios instance - define interceptor-compatible class

--- hivemq-edge/src/frontend/src/api/hooks/useHttpClient/useHttpClient.ts
+++ hivemq-edge/src/frontend/src/api/hooks/useHttpClient/useHttpClient.ts
@@ -1,17 +1,54 @@
 import { useState } from 'react'
+import axios, { AxiosError } from 'axios'
+
+import { BaseHttpRequest, CancelablePromise, HiveMqClient, OpenAPIConfig } from '@/api/__generated__'
+import { ApiRequestOptions } from '@/api/__generated__/core/ApiRequestOptions.ts'
+import { request as __request } from '@/api/__generated__/core/request.ts'
+
+import config from '@/config'
 import { useAuth } from '@/modules/Auth/hooks/useAuth.ts'
-import { HiveMqClient } from '../../__generated__'
-import config from '../../../config'
+
+const axiosInstance = axios.create()
+
+class AxiosHttpRequestWithInterceptors extends BaseHttpRequest {
+  constructor(config: OpenAPIConfig) {
+    super(config)
+  }
+
+  public override request<T>(options: ApiRequestOptions): CancelablePromise<T> {
+    return __request(this.config, options, axiosInstance)
+  }
+}
 
 export const useHttpClient = () => {
   const { credentials } = useAuth()
   const [client] = useState<HiveMqClient>(createInstance)
 
   function createInstance() {
-    return new HiveMqClient({
-      BASE: config.apiBaseUrl,
-      TOKEN: credentials?.token,
-    })
+    // Make sure to clear the interceptors, since axiosInstance is global
+    axiosInstance.interceptors.response.clear()
+    axiosInstance.interceptors.response.use(
+      function (response) {
+        // Any status code that lie within the range of 2xx cause this function to trigger
+        // Do something with response data
+
+        return response
+      },
+      function (error: AxiosError) {
+        // Any status codes that falls outside the range of 2xx cause this function to trigger
+        // Do something with response error
+
+        return Promise.reject(error)
+      }
+    )
+
+    return new HiveMqClient(
+      {
+        BASE: config.apiBaseUrl,
+        TOKEN: credentials?.token,
+      },
+      AxiosHttpRequestWithInterceptors
+    )
   }
 
   return client
