1ce4688afc68d547b8e8567d7dfc631bbc8ea4b2
Simon Johnson <2924684+simon622@users.noreply.github.com>
2023-07-10 11:17:50 +0100

    Added expose header (dont wildcard) - does not need to be on the allow list

--- hivemq-edge/src/main/java/com/hivemq/http/filters/CorsFilter.java
+++ hivemq-edge/src/main/java/com/hivemq/http/filters/CorsFilter.java
@@ -27,7 +27,7 @@ public class CorsFilter implements ContainerResponseFilter {
             ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
         responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
         responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
-        responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization, X-Bearer-Token-Reissue");
+        responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
         //-- Add non-standard headers to the expose list
         responseContext.getHeaders().add("Access-Control-Expose-Headers", "X-Bearer-Token-Reissue");
         responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
