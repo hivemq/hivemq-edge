1816e3a895300da4c2b0042d348578ed6287eb7b
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-07-14 12:24:07 +0100

    chore(alpha): add test config

--- hivemq-edge/src/frontend/src/__test-utils__/setup.ts
+++ hivemq-edge/src/frontend/src/__test-utils__/setup.ts
@@ -5,6 +5,7 @@ import { server } from './msw/mockServer.ts'
 // Establish API mocking before all tests.
 beforeAll(() => {
   server.listen()
+  console.log('XXXXXXXX', import.meta.env.MODE, import.meta.env.VITE_API_BASE_URL)
 })
 
 // Reset any request handlers that we may add during the tests,
