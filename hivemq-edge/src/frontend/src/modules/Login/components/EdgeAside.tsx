540e9b66ddfb1069db49d8331ac1cbdcb10207af
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-07-19 17:01:58 +0100

    Merge pull request #18
    refactor(15460): Refactor Login page to align with design specs
    
    * refactor(15460): update Edge assets
    
    * refactor(15460): add background image for brand panel
    
    * refactor(15460): refactor login container and page to align closer wi…
    
    * refactor(15460): update translations

--- hivemq-edge/src/frontend/src/modules/Login/components/EdgeAside.tsx
+++ hivemq-edge/src/frontend/src/modules/Login/components/EdgeAside.tsx
@@ -8,22 +8,57 @@ const EdgeAside: FC = () => {
 
   return (
     <aside>
-      <Stack direction={{ base: 'row', md: 'column' }} align={'center'} m={{ base: '1', md: '6' }}>
+      <Stack
+        direction={{ base: 'row', md: 'column' }}
+        align={{ base: 'center', md: 'flex-start' }}
+        m={{ base: '1', md: '6' }}
+      >
         <Box>
-          <Image src={logo} alt={'branding.company' as string} boxSize={{ base: '150px', md: '500px' }} />
+          <Heading
+            textAlign={{ base: 'center', md: 'left' }}
+            pt={{ base: '0', md: '20' }}
+            color={'white'}
+            lineHeight={1.1}
+            display={{ base: 'none', md: 'inherit' }}
+            fontSize={{ md: '6xl', lg: '7xl' }}
+          >
+            {t('branding.appName')}
+          </Heading>
+          <Image
+            src={logo}
+            alt={'branding.appName' as string}
+            boxSize={{ base: '120px', md: '500px' }}
+            display={{ base: 'inherit', md: 'none' }}
+          />
         </Box>
-        <Heading
-          textAlign={'center'}
-          pt={{ base: '0', md: '20' }}
-          color={'white'}
-          lineHeight={1.1}
-          fontSize={{ base: '1xl', sm: '2xl', md: '3xl' }}
-        >
-          {t('login.aside.header')}
-        </Heading>
-        <Text color={'white'} fontFamily={'heading'} textAlign={'center'}>
-          {t('login.aside.subheader')}
-        </Text>
+        <Box>
+          <Box>
+            <Heading
+              textAlign={{ base: 'center', md: 'left' }}
+              pt={{ base: '0', md: '20' }}
+              color={'white'}
+              lineHeight={1.1}
+              fontSize={{ base: 'md', sm: '2xl', md: '4xl', lg: '5xl' }}
+            >
+              {t('login.aside.header')}
+            </Heading>
+            <Text
+              fontSize={{ base: 'md', sm: '2xl', md: '4xl' }}
+              color={'white'}
+              textAlign={{ base: 'center', md: 'left' }}
+            >
+              {t('login.aside.subheader')}
+            </Text>
+          </Box>
+          <Text
+            fontSize={{ base: 'sm', md: 'lg' }}
+            color={'white'}
+            textAlign={{ base: 'center', md: 'left' }}
+            mt={{ base: 0, md: 10 }}
+          >
+            {t('login.aside.tagline')}
+          </Text>
+        </Box>
       </Stack>
     </aside>
   )
