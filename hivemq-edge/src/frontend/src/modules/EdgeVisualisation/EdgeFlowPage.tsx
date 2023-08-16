5e5c0e8128d02ca3202b671c00567bb902e7afbb
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-08-16 17:08:54 +0100

    feat(16216): update the workspace with basic rendering

--- hivemq-edge/src/frontend/src/modules/EdgeVisualisation/EdgeFlowPage.tsx
+++ hivemq-edge/src/frontend/src/modules/EdgeVisualisation/EdgeFlowPage.tsx
@@ -1,12 +1,20 @@
 import { FC } from 'react'
 import { useTranslation } from 'react-i18next'
+
 import PageContainer from '@/components/PageContainer.tsx'
+
+import { EdgeFlowProvider } from './hooks/FlowContext.tsx'
+import ReactFlowWrapper from './components/ReactFlowWrapper.tsx'
 
 const EdgeFlowPage: FC = () => {
   const { t } = useTranslation()
 
   return (
-    <PageContainer title={t('welcome.title') as string} subtitle={t('welcome.description') as string}></PageContainer>
+    <PageContainer title={t('welcome.title') as string} subtitle={t('welcome.description') as string}>
+      <EdgeFlowProvider>
+        <ReactFlowWrapper />
+      </EdgeFlowProvider>
+    </PageContainer>
   )
 }
 
