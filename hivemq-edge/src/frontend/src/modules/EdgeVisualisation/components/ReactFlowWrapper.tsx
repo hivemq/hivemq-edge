372221eda50b93c74185274c48e785aa248f0665
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-08-17 10:12:32 +0100

    feat(16216): add custom node types to the flow

--- hivemq-edge/src/frontend/src/modules/EdgeVisualisation/components/ReactFlowWrapper.tsx
+++ hivemq-edge/src/frontend/src/modules/EdgeVisualisation/components/ReactFlowWrapper.tsx
@@ -1,16 +1,27 @@
+import { useMemo } from 'react'
 import ReactFlow, { Background, Controls } from 'reactflow'
 import 'reactflow/dist/style.css'
 
 import useGetFlowElements from '../hooks/useGetFlowElements.tsx'
+import { NodeTypes } from '../types.ts'
+import NodeEdge from './NodeEdge.tsx'
+import NodeAdapter from './NodeAdapter.tsx'
 
 const ReactFlowWrapper = () => {
   const { nodes, edges, onNodesChange, onEdgesChange } = useGetFlowElements()
-
+  const nodeTypes = useMemo(
+    () => ({
+      [NodeTypes.EDGE_NODE]: NodeEdge,
+      [NodeTypes.ADAPTER_NODE]: NodeAdapter,
+    }),
+    []
+  )
   return (
     <ReactFlow
       deleteKeyCode={null}
       snapToGrid={true}
       nodes={nodes}
+      nodeTypes={nodeTypes}
       onNodesChange={onNodesChange}
       edges={edges}
       onEdgesChange={onEdgesChange}
