00eb77f87806a61f08ae1cb6548d098c2bd1851d
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-08-18 15:20:30 +0100

    feat(16216): add topics container for nodes

---
+++ hivemq-edge/src/frontend/src/modules/EdgeVisualisation/components/parts/TopicsContainer.tsx
@@ -0,0 +1,30 @@
+import { FC } from 'react'
+import { HStack, Tag, TagLabel, VStack } from '@chakra-ui/react'
+import Topic from '@/components/MQTT/Topic.tsx'
+
+import { TopicFilter } from '../../types.ts'
+
+interface NodeTopicsProps {
+  topics: TopicFilter[]
+}
+
+const MAX_TOPICS = 2
+
+const TopicsContainer: FC<NodeTopicsProps> = ({ topics }) => {
+  return (
+    <HStack spacing="4">
+      <VStack alignItems={'flex-start'}>
+        {topics.slice(0, MAX_TOPICS).map((e, i) => (
+          <Topic key={`local-${i}`} topic={e.topic} />
+        ))}
+      </VStack>
+      {topics.length > MAX_TOPICS && (
+        <Tag size={'sm'}>
+          <TagLabel>+{topics.length - MAX_TOPICS}</TagLabel>
+        </Tag>
+      )}
+    </HStack>
+  )
+}
+
+export default TopicsContainer
