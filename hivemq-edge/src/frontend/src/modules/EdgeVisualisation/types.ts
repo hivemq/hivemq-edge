a1547d485e59eacb440439db43c49f10ee298dbb
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-08-21 10:11:32 +0100

    feat(16216): add types for listeners

--- hivemq-edge/src/frontend/src/modules/EdgeVisualisation/types.ts
+++ hivemq-edge/src/frontend/src/modules/EdgeVisualisation/types.ts
@@ -3,12 +3,14 @@ export interface EdgeFlowOptions {
   showStatus: boolean
   showMetrics: boolean
   showHosts: boolean
+  showGateway: boolean
 }
 
 export enum NodeTypes {
   EDGE_NODE = 'EDGE_NODE',
   BRIDGE_NODE = 'BRIDGE_NODE',
   ADAPTER_NODE = 'ADAPTER_NODE',
+  LISTENER_NODE = 'LISTENER_NODE',
 }
 
 export enum IdStubs {
@@ -17,6 +19,7 @@ export enum IdStubs {
   ADAPTER_NODE = 'adapter',
   HOST_NODE = 'host',
   HOST_GROUP = 'group',
+  LISTENER_NODE = 'listener',
   CONNECTOR = 'connect',
 }
 
