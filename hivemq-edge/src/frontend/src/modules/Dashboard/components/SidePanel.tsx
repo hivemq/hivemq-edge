bf73dd7d0a3d058de7b0878aa3e12a3a845ad1c1
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-08-22 13:59:48 +0100

    feat(16309): add release version number to the side panel

--- hivemq-edge/src/frontend/src/modules/Dashboard/components/SidePanel.tsx
+++ hivemq-edge/src/frontend/src/modules/Dashboard/components/SidePanel.tsx
@@ -1,16 +1,19 @@
 import { FC } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useNavigate } from 'react-router-dom'
-import { Box, Button, Flex, Image } from '@chakra-ui/react'
+import { Box, Button, Flex, Image, Text } from '@chakra-ui/react'
+import { FiLogOut } from 'react-icons/fi'
 
 import NavLinksBlock from './NavLinksBlock.tsx'
 
+import { useGetConfiguration } from '@/api/hooks/useGatewayPortal/useGetConfiguration.tsx'
 import logo from '@/assets/edge/03-hivemq-industrial-edge-vert.svg'
 import { useAuth } from '@/modules/Auth/hooks/useAuth.ts'
+
 import useGetNavItems from '../hooks/useGetNavItems.tsx'
-import { FiLogOut } from 'react-icons/fi'
 
 const SidePanel: FC = () => {
+  const { data: configuration } = useGetConfiguration()
   const items = useGetNavItems()
   const auth = useAuth()
   const navigate = useNavigate()
@@ -21,6 +24,11 @@ const SidePanel: FC = () => {
       <Flex flexDirection="column" w={256} h={'100%'} bgColor={'#f5f5f5'}>
         <Box p={4} m={'auto'} mb={10}>
           <Image src={logo} alt={t('branding.company') as string} boxSize="200px" />
+          {configuration && (
+            <Text fontSize="xs" textAlign={'center'}>
+              [ {configuration.environment?.properties?.version} ]
+            </Text>
+          )}
         </Box>
 
         <Flex flexDirection="column" flex={1}>
