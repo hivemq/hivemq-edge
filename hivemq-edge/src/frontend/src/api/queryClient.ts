91828b72590ce385cb73c559350c07964ce89a8d
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-08-22 08:45:28 +0100

    fix(16308): add config for network mode - add Types for the config element

--- hivemq-edge/src/frontend/src/api/queryClient.ts
+++ hivemq-edge/src/frontend/src/api/queryClient.ts
@@ -1,4 +1,5 @@
 import { QueryClient } from '@tanstack/react-query'
+import config from '@/config'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -7,10 +8,10 @@ const queryClient = new QueryClient({
       refetchOnWindowFocus: false,
       refetchOnReconnect: false,
       refetchOnMount: true,
-      networkMode: 'always',
+      networkMode: config.httpClient.networkMode,
     },
     mutations: {
-      networkMode: 'always',
+      networkMode: config.httpClient.networkMode,
     },
   },
 })
--- hivemq-edge/src/frontend/src/config/index.ts
+++ hivemq-edge/src/frontend/src/config/index.ts
@@ -1,4 +1,30 @@
-const config = {
+import { NetworkMode } from '@tanstack/react-query'
+
+interface configType {
+  environment: string
+  apiBaseUrl: string
+
+  version: string
+  documentationUrl: string
+
+  httpClient: {
+    axiosTimeout: number
+    networkMode: NetworkMode
+    pollingRefetchInterval: number
+  }
+
+  features: {
+    PROTOCOL_ADAPTER_FACET: boolean
+    METRICS_SELECT_PANEL: boolean
+    METRICS_DEFAULTS: string[]
+  }
+
+  documentation: {
+    namespace: string
+  }
+}
+
+const config: configType = {
   environment: import.meta.env.MODE,
 
   apiBaseUrl: import.meta.env.VITE_API_BASE_URL,
@@ -7,7 +33,22 @@ const config = {
   documentationUrl: import.meta.env.VITE_APP_DOCUMENTATION,
 
   httpClient: {
+    /**
+     * Number of milliseconds before Axios times out the request
+     * @see  https://axios-http.com/docs/req_config
+     */
     axiosTimeout: 15000,
+
+    /**
+     * React Query network mode
+     * @see https://tanstack.com/query/v5/docs/react/guides/network-mode
+     */
+    networkMode: 'always',
+
+    /**
+     * https://tanstack.com/query/v5/docs/react/reference/useQuery
+     * @see https://tanstack.com/query/v5/docs/react/reference/useQuery
+     */
     pollingRefetchInterval: 5 * 1000,
   },
 
