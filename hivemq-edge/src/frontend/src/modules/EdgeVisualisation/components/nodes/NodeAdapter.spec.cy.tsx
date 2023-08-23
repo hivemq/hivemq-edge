bf0896e938d12c60db5a2fcd03a52b8e158001fc
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-08-23 13:32:10 +0100

    fix(16216): update mocks

--- hivemq-edge/src/frontend/src/modules/EdgeVisualisation/components/nodes/NodeAdapter.spec.cy.tsx
+++ hivemq-edge/src/frontend/src/modules/EdgeVisualisation/components/nodes/NodeAdapter.spec.cy.tsx
@@ -2,6 +2,7 @@
 
 import { mockReactFlow } from '@/__test-utils__/react-flow/providers.tsx'
 import { MOCK_NODE_ADAPTER } from '@/__test-utils__/react-flow/nodes.ts'
+import { MOCK_TOPIC_REF1, MOCK_TOPIC_REF2 } from '@/__test-utils__/react-flow/topics.ts'
 import { mockProtocolAdapter } from '@/api/hooks/useProtocolAdapters/__handlers__'
 
 import NodeAdapter from './NodeAdapter.tsx'
@@ -19,8 +20,8 @@ describe('NodeAdapter', () => {
     cy.getByTestId('connection-status').should('contain.text', 'Connected')
     cy.getByTestId('topics-container')
       .should('be.visible')
-      .should('contain.text', 'topic/test/1')
-      .should('contain.text', 'topic/test/2')
+      .should('contain.text', MOCK_TOPIC_REF1)
+      .should('contain.text', MOCK_TOPIC_REF2)
   })
 
   it('should be accessible', () => {
