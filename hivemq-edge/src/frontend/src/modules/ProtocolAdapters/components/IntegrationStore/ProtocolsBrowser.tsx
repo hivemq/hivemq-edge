142c310b08600f4190e2a321a26555f5cbbc3334
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-08-29 10:41:32 +0100

    refactor(16216): refactor filtered protocol component

---
+++ hivemq-edge/src/frontend/src/modules/ProtocolAdapters/components/IntegrationStore/ProtocolsBrowser.tsx
@@ -0,0 +1,43 @@
+import { FC, useMemo } from 'react'
+import { Button, Card, CardBody, CardFooter, SimpleGrid } from '@chakra-ui/react'
+import { ArrowForwardIcon } from '@chakra-ui/icons'
+import { useTranslation } from 'react-i18next'
+
+import { ProtocolAdapter } from '@/api/__generated__'
+
+import { ProtocolFacetType } from '../../types.ts'
+import { applyFacets } from '../../utils/facets-utils.ts'
+import AdapterTypeSummary from '../adapters/AdapterTypeSummary.tsx'
+
+interface ProtocolsBrowserProps {
+  items: ProtocolAdapter[]
+  facet: ProtocolFacetType | undefined
+  onCreate?: (adapterId: string | undefined) => void
+}
+
+const ProtocolsBrowser: FC<ProtocolsBrowserProps> = ({ items, facet, onCreate }) => {
+  const { t } = useTranslation()
+  const filteredAdapters = useMemo(() => {
+    if (!facet) return items
+    return items.filter(applyFacets(facet))
+  }, [items, facet])
+
+  return (
+    <SimpleGrid mt={8} spacing={4} templateColumns={{ base: 'repeat(1, 1fr)', xl: 'repeat(2, 1fr)' }} gap={6}>
+      {filteredAdapters?.map((e) => (
+        <Card key={e.id} minW={'300px'}>
+          <CardBody p={2}>
+            <AdapterTypeSummary key={e.id} adapter={e} searchQuery={facet?.search} />
+          </CardBody>
+          <CardFooter p={2}>
+            <Button variant={'outline'} size={'sm'} rightIcon={<ArrowForwardIcon />} onClick={() => onCreate?.(e.id)}>
+              {t('protocolAdapter.action.createInstance')}
+            </Button>
+          </CardFooter>
+        </Card>
+      ))}
+    </SimpleGrid>
+  )
+}
+
+export default ProtocolsBrowser
