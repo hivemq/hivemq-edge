3385f2f8fe8dffddf39287b7ccfd40ed38186aa4
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-08-29 10:44:28 +0100

    test(16216): add tests for facet search

---
+++ hivemq-edge/src/frontend/src/modules/ProtocolAdapters/components/IntegrationStore/FacetSearch.spec.cy.tsx
@@ -0,0 +1,65 @@
+/// <reference types="cypress" />
+
+import FacetSearch from '@/modules/ProtocolAdapters/components/IntegrationStore/FacetSearch.tsx'
+import { mockProtocolAdapter } from '@/api/hooks/useProtocolAdapters/__handlers__'
+
+describe('FacetSearch', () => {
+  beforeEach(() => {
+    cy.viewport(800, 900)
+    cy.intercept('/api/v1/management/protocol-adapters/types', { items: [mockProtocolAdapter] }).as('getProtocols')
+    // cy.intercept('api/v1/management/protocol-adapters/adapters', { items: [mockAdapter] }).as('getAdapters')
+    // cy.intercept('api/v1/management/protocol-adapters/status', { items: [] })
+  })
+
+  it('should render properly when initialised with a search term', () => {
+    const mockOnSubmit = cy.spy().as('onSubmit')
+
+    cy.mountWithProviders(<FacetSearch facet={{ search: '123', filter: undefined }} onChange={mockOnSubmit} />)
+    cy.get('#facet-search-input').should('contain.value', '123')
+    cy.getByTestId('facet-filter-clear').should('contain.text', 'No filter').should('have.attr', 'aria-pressed', 'true')
+    cy.getByTestId('facet-filter-category-industrial').should('have.attr', 'aria-pressed', 'false')
+    cy.getByTestId('facet-filter-tag-tag1').should('have.attr', 'aria-pressed', 'false')
+    cy.getByTestId('facet-filter-tag-tag2').should('have.attr', 'aria-pressed', 'false')
+    cy.getByTestId('facet-filter-tag-tag3').should('have.attr', 'aria-pressed', 'false')
+  })
+
+  it('should render properly when initialised with a filter', () => {
+    const mockOnSubmit = cy.spy().as('onSubmit')
+
+    cy.mountWithProviders(
+      <FacetSearch facet={{ search: undefined, filter: { key: 'tags', value: 'tag1' } }} onChange={mockOnSubmit} />
+    )
+    cy.get('#facet-search-input').should('not.contain.value')
+    cy.getByTestId('facet-filter-clear')
+      .should('contain.text', 'No filter')
+      .should('have.attr', 'aria-pressed', 'false')
+    cy.getByTestId('facet-filter-category-industrial').should('have.attr', 'aria-pressed', 'false')
+    cy.getByTestId('facet-filter-tag-tag1').should('have.attr', 'aria-pressed', 'true')
+    cy.getByTestId('facet-filter-tag-tag2').should('have.attr', 'aria-pressed', 'false')
+    cy.getByTestId('facet-filter-tag-tag3').should('have.attr', 'aria-pressed', 'false')
+  })
+
+  it('should change the search term', () => {
+    const mockOnSubmit = cy.spy().as('onSubmit')
+
+    cy.mountWithProviders(<FacetSearch facet={{ search: '123', filter: undefined }} onChange={mockOnSubmit} />)
+    cy.get('#facet-search-input').type('4')
+    cy.get('@onSubmit').should('have.been.calledWith', { search: '1234' })
+  })
+
+  it('should clear the search term', () => {
+    const mockOnSubmit = cy.spy().as('onSubmit')
+
+    cy.mountWithProviders(<FacetSearch facet={{ search: '123', filter: undefined }} onChange={mockOnSubmit} />)
+    cy.get('#facet-search-clear').click()
+    cy.get('@onSubmit').should('have.been.calledWith', { search: null })
+  })
+
+  it('should be accessible', () => {
+    cy.injectAxe()
+    cy.mountWithProviders(<FacetSearch facet={{ search: 'ny term', filter: undefined }} onChange={cy.stub()} />)
+
+    cy.checkAccessibility()
+    cy.percySnapshot('Component: FacetSearch')
+  })
+})
