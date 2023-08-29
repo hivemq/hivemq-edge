6d63e47dd310fd2ca839e20a0cd87ed2ffaf3fb0
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-08-29 10:42:17 +0100

    refactor(16216): refactor protocol summary

--- hivemq-edge/src/frontend/src/modules/ProtocolAdapters/components/adapters/AdapterTypeSummary.tsx
+++ hivemq-edge/src/frontend/src/modules/ProtocolAdapters/components/adapters/AdapterTypeSummary.tsx
@@ -2,46 +2,44 @@ import { FC } from 'react'
 import { Image, Badge, Box, Flex, Highlight, Link, Text, HighlightProps } from '@chakra-ui/react'
 import { ExternalLinkIcon } from '@chakra-ui/icons'
 import { useTranslation } from 'react-i18next'
-import { useGetAdapterTypes } from '@/api/hooks/useProtocolAdapters/useGetAdapterTypes.tsx'
+
+import { ProtocolAdapter } from '@/api/__generated__'
 
 interface AdapterTypeSummaryProps {
-  id?: string
-  searchQuery?: string
+  adapter: ProtocolAdapter
+  searchQuery?: string | null
 }
 
 const AdapterHighlight: FC<HighlightProps> = (props) => (
-  <Highlight query={props.query} styles={{ pl: 2, py: '1', rounded: 'full', bg: 'gray.100' }}>
+  <Highlight query={props.query} styles={{ rounded: 'full', bg: 'orange.100' }}>
     {props.children}
   </Highlight>
 )
 
-const AdapterTypeSummary: FC<AdapterTypeSummaryProps> = ({ id, searchQuery }) => {
+const AdapterTypeSummary: FC<AdapterTypeSummaryProps> = ({ adapter, searchQuery }) => {
   const { t } = useTranslation()
-  const { data } = useGetAdapterTypes()
-  const selectedType = data?.items?.find((e) => e.id === id)
 
-  if (!selectedType) return null
   return (
     <Flex m={0}>
-      <Image boxSize="100px" objectFit="scale-down" src={selectedType.logoUrl} aria-label={selectedType.id} />
+      <Image boxSize="100px" objectFit="scale-down" src={adapter.logoUrl} aria-label={adapter.id} />
       <Box ml="3">
         <Text fontWeight="bold">
-          <AdapterHighlight query={searchQuery || ''}>{selectedType.name || ''}</AdapterHighlight>
+          <AdapterHighlight query={searchQuery || ''}>{adapter.name || ''}</AdapterHighlight>
           <Badge ml="1" colorScheme="green">
-            {selectedType.version}
+            {adapter.version}
           </Badge>
         </Text>
         <Text fontSize="sm">
-          {t('protocolAdapter.overview.type')} {selectedType.protocol}
+          {t('protocolAdapter.overview.type')} {adapter.protocol}
         </Text>
         <Text fontSize="sm">
-          {t('protocolAdapter.overview.author')} {selectedType.author}
+          {t('protocolAdapter.overview.author')} {adapter.author}
         </Text>
         <Text fontSize="sm">
-          <AdapterHighlight query={searchQuery || ''}>{selectedType.description || ''}</AdapterHighlight>
+          <AdapterHighlight query={searchQuery || ''}>{adapter.description || ''}</AdapterHighlight>
         </Text>
         <Box mt={2}>
-          <Link href={selectedType.url} isExternal>
+          <Link href={adapter.url} isExternal>
             {t('protocolAdapter.overview.documentation')} <ExternalLinkIcon mx={1} mb={1} />
           </Link>
         </Box>
--- hivemq-edge/src/frontend/src/modules/ProtocolAdapters/components/drawers/AdapterTypeSelector.tsx
+++ hivemq-edge/src/frontend/src/modules/ProtocolAdapters/components/drawers/AdapterTypeSelector.tsx
@@ -4,9 +4,9 @@ import { FormControl, FormErrorMessage, FormLabel, Select } from '@chakra-ui/rea
 
 import { ProtocolAdapter } from '@/api/__generated__'
 import { useGetAdapterTypes } from '@/api/hooks/useProtocolAdapters/useGetAdapterTypes.tsx'
-
 import { AdapterType, GenericPanelType } from '@/modules/ProtocolAdapters/types.ts'
-import AdapterTypeSummary from '@/modules/ProtocolAdapters/components/adapters/AdapterTypeSummary.tsx'
+
+import AdapterTypeSummary from '../adapters/AdapterTypeSummary.tsx'
 
 const AdapterTypeSelector: FC<GenericPanelType<AdapterType>> = ({ form }) => {
   const { t } = useTranslation()
@@ -16,7 +16,8 @@ const AdapterTypeSelector: FC<GenericPanelType<AdapterType>> = ({ form }) => {
     formState: { errors },
   } = form
   const { data } = useGetAdapterTypes()
-  const watchShowAge = watch('adapterType') // you can supply default value as second argument
+  const selectedType = watch('adapterType')
+  const selectedAdapter = data?.items?.find((e) => e.id === selectedType)
 
   return (
     <FormControl isInvalid={!!errors.adapterType} isRequired>
@@ -35,7 +36,7 @@ const AdapterTypeSelector: FC<GenericPanelType<AdapterType>> = ({ form }) => {
         ))}
       </Select>
 
-      {watchShowAge && <AdapterTypeSummary id={watchShowAge} />}
+      {selectedAdapter && <AdapterTypeSummary adapter={selectedAdapter} />}
 
       <FormErrorMessage>{errors.adapterType && errors.adapterType.message}</FormErrorMessage>
     </FormControl>
