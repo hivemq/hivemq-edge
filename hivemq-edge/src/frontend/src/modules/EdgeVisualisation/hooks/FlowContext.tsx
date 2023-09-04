aac555d6043e20d402133b7df7fe956db8c9f7ed
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-09-04 11:35:49 +0100

    feat(16525): add clustering and configuration control to the context

--- hivemq-edge/src/frontend/src/modules/EdgeVisualisation/hooks/FlowContext.tsx
+++ hivemq-edge/src/frontend/src/modules/EdgeVisualisation/hooks/FlowContext.tsx
@@ -1,17 +1,28 @@
 import { createContext, Dispatch, FunctionComponent, PropsWithChildren, SetStateAction, useState } from 'react'
-import { EdgeFlowOptions } from '@/modules/EdgeVisualisation/types.ts'
+import { useDisclosure, UseDisclosureReturn } from '@chakra-ui/react'
+
+import { EdgeFlowGrouping, EdgeFlowOptions } from '../types.ts'
 
 export interface EdgeFlowContextType {
   options: EdgeFlowOptions
   setOptions: Dispatch<SetStateAction<EdgeFlowOptions>>
+  groups: EdgeFlowGrouping
+  setGroups: Dispatch<SetStateAction<EdgeFlowGrouping>>
+  optionDrawer: UseDisclosureReturn
 }
 
 const defaultEdgeFlowContext: EdgeFlowOptions = {
   showTopics: true,
   showStatus: true,
   showMetrics: false,
-  showGateway: true,
-  showHosts: true,
+  showGateway: false,
+  showHosts: false,
+  showObservabilityEdge: false,
+}
+
+const defaultEdgeFloeGrouping: EdgeFlowGrouping = {
+  keys: [],
+  showGroups: true,
 }
 
 export const EdgeFlowContext = createContext<EdgeFlowContextType | null>(null)
@@ -21,6 +32,12 @@ export const EdgeFlowProvider: FunctionComponent<PropsWithChildren<{ defaults?: 
   defaults,
 }) => {
   const [options, setOptions] = useState<EdgeFlowOptions>({ ...defaultEdgeFlowContext, ...defaults })
+  const [groups, setGroups] = useState<EdgeFlowGrouping>(defaultEdgeFloeGrouping)
+  const optionDrawer = useDisclosure()
 
-  return <EdgeFlowContext.Provider value={{ options, setOptions }}>{children}</EdgeFlowContext.Provider>
+  return (
+    <EdgeFlowContext.Provider value={{ options, setOptions, groups, setGroups, optionDrawer }}>
+      {children}
+    </EdgeFlowContext.Provider>
+  )
 }
