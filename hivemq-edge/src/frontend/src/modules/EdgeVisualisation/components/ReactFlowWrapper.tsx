c5ce683ba416e7affca3483ca9342f3056bbb501
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-09-04 11:43:11 +0100

    feat(16525): add routing for accessing node properties

--- hivemq-edge/src/frontend/src/modules/EdgeVisualisation/components/ReactFlowWrapper.tsx
+++ hivemq-edge/src/frontend/src/modules/EdgeVisualisation/components/ReactFlowWrapper.tsx
@@ -1,13 +1,18 @@
 import { useMemo } from 'react'
-import ReactFlow, { Background, Controls } from 'reactflow'
+import ReactFlow, { Background } from 'reactflow'
+import { Outlet } from 'react-router-dom'
+
 import 'reactflow/dist/style.css'
 
 import useGetFlowElements from '../hooks/useGetFlowElements.tsx'
-import { NodeTypes } from '../types.ts'
+
+import CanvasControls from './controls/CanvasControls.tsx'
 import NodeEdge from './nodes/NodeEdge.tsx'
 import NodeAdapter from './nodes/NodeAdapter.tsx'
 import NodeBridge from './nodes/NodeBridge.tsx'
-import NodeListener from '@/modules/EdgeVisualisation/components/nodes/NodeListener.tsx'
+import NodeListener from './nodes/NodeListener.tsx'
+import ReportEdge from './edges/ReportEdge.tsx'
+import NodeGroup from '@/modules/EdgeVisualisation/components/nodes/NodeGroup.tsx'
 
 const ReactFlowWrapper = () => {
   const { nodes, edges, onNodesChange, onEdgesChange } = useGetFlowElements()
@@ -31,7 +36,8 @@ const ReactFlowWrapper = () => {
       onEdgesChange={onEdgesChange}
     >
       <Background />
-      <Controls />
+      <CanvasControls />
+      <Outlet />
     </ReactFlow>
   )
 }
