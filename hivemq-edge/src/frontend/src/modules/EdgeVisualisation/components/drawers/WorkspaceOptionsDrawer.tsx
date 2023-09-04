d51d80c506ca4c547e71c2da7012817a057a5145
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-09-04 16:11:15 +0100

    feat(16525): add workspace configuration for layout

--- hivemq-edge/src/frontend/src/modules/EdgeVisualisation/components/drawers/WorkspaceOptionsDrawer.tsx
+++ hivemq-edge/src/frontend/src/modules/EdgeVisualisation/components/drawers/WorkspaceOptionsDrawer.tsx
@@ -11,16 +11,21 @@ import {
   FormControl,
   FormHelperText,
   FormLabel,
+  Radio,
+  RadioGroup,
   Stack,
+  VStack,
 } from '@chakra-ui/react'
 import { Select } from 'chakra-react-select'
 
 import { useEdgeFlowContext } from '@/modules/EdgeVisualisation/hooks/useEdgeFlowContext.tsx'
-import { EdgeFlowOptions } from '@/modules/EdgeVisualisation/types.ts'
+import { EdgeFlowOptions, EdgeFlowLayout } from '@/modules/EdgeVisualisation/types.ts'
 import { groupingAttributes } from '@/modules/EdgeVisualisation/utils/layout-utils.ts'
+import { useTranslation } from 'react-i18next'
 
 const WorkspaceOptionsDrawer: FC = () => {
-  const { optionDrawer, options, setOptions } = useEdgeFlowContext()
+  const { t } = useTranslation()
+  const { optionDrawer, options, setOptions, groups, setGroups } = useEdgeFlowContext()
   const optionKeys: (keyof EdgeFlowOptions)[] = [
     'showTopics',
     'showStatus',
@@ -38,43 +43,84 @@ const WorkspaceOptionsDrawer: FC = () => {
       <DrawerOverlay />
       <DrawerContent>
         <DrawerCloseButton />
-        <DrawerHeader>Workspace configuration</DrawerHeader>
+        <DrawerHeader>{t('workspace.configuration.header')}</DrawerHeader>
 
         <DrawerBody>
-          <FormControl as="fieldset">
-            <FormLabel as="legend">Topology Graph: Content</FormLabel>
-            <CheckboxGroup
-              colorScheme="brand"
-              defaultValue={initValues}
-              onChange={(options) => {
-                const newOptions = options.reduce((a, opt) => ({ ...a, [opt]: true }), {})
-                const oldOptions = optionKeys.reduce((a, opt) => ({ ...a, [opt]: false }), {})
+          <VStack gap={4}>
+            <FormControl as="fieldset" borderWidth="1px" p={2}>
+              <FormLabel as="legend">{t('workspace.configuration.content.header')}</FormLabel>
+              <CheckboxGroup
+                colorScheme="brand"
+                defaultValue={initValues}
+                onChange={(options) => {
+                  const newOptions = options.reduce((a, opt) => ({ ...a, [opt]: true }), {})
+                  const oldOptions = optionKeys.reduce((a, opt) => ({ ...a, [opt]: false }), {})
 
-                setOptions((old) => ({ ...old, ...oldOptions, ...newOptions }))
-              }}
-            >
-              <Stack direction={'column'}>
-                <Checkbox value="showTopics">Topics</Checkbox>
-                <Checkbox value="showStatus">Status</Checkbox>
-                <Checkbox value="showMonitoringOnEdge">Monitoring on links</Checkbox>
-                <Checkbox value="showHosts">Hosts</Checkbox>
-                <Checkbox value="showGateway">Gateway</Checkbox>
-              </Stack>
-            </CheckboxGroup>
-            <FormHelperText>Select the contents to be displayed on the workspace</FormHelperText>
-          </FormControl>
+                  setOptions((old) => ({ ...old, ...oldOptions, ...newOptions }))
+                }}
+              >
+                <Stack direction={'column'}>
+                  <Checkbox value="showTopics">{t('workspace.configuration.content.showTopics')}</Checkbox>
+                  <Checkbox value="showStatus">{t('workspace.configuration.content.showStatus')}</Checkbox>
+                  <Checkbox value="showMonitoringOnEdge">
+                    {t('workspace.configuration.content.showMonitoringOnEdge')}
+                  </Checkbox>
+                  <Checkbox value="showHosts">{t('workspace.configuration.content.showHosts')}</Checkbox>
+                  <Checkbox value="showGateway">{t('workspace.configuration.content.showGateway')}</Checkbox>
+                </Stack>
+              </CheckboxGroup>
+              <FormHelperText>{t('workspace.configuration.content.prompt')}</FormHelperText>
+            </FormControl>
 
-          <FormControl as="fieldset" mt={4}>
-            <FormLabel>Topology Graph: Adapter grouping</FormLabel>
-            <Select
-              isMulti
-              name="adapters"
-              options={groupingAttributes.map((e) => ({ value: e.key, label: e.key }))}
-              onChange={(e) => console.log('XXXXX val', e)}
-              placeholder="Select some keys"
-              closeMenuOnSelect={true}
-            />
-          </FormControl>
+            <FormControl as="fieldset" borderWidth="1px" p={2}>
+              <FormLabel as="legend">{t('workspace.configuration.layout.header')}</FormLabel>
+
+              <FormControl>
+                <FormLabel>Layout method</FormLabel>
+                <RadioGroup
+                  colorScheme="brand"
+                  value={groups.layout}
+                  onChange={(v) => setGroups((old) => ({ ...old, layout: v as EdgeFlowLayout }))}
+                >
+                  <Stack direction="row">
+                    <Radio value={EdgeFlowLayout.HORIZONTAL}>Horizontal</Radio>
+                    <Radio value={EdgeFlowLayout.CIRCLE_PACKING}>Cluster</Radio>
+                  </Stack>
+                </RadioGroup>
+              </FormControl>
+
+              <FormControl mt={4} isDisabled={groups.layout !== EdgeFlowLayout.CIRCLE_PACKING}>
+                <FormLabel>Clustering</FormLabel>
+                <Select
+                  colorScheme="brand"
+                  isMulti
+                  name="adapters"
+                  defaultValue={groups.keys.map((e) => ({
+                    value: e,
+                    label: t('workspace.grouping.by', { context: e }) as string,
+                  }))}
+                  options={groupingAttributes.map((e) => ({
+                    value: e.key,
+                    label: t('workspace.grouping.by', { context: e.key }) as string,
+                  }))}
+                  onChange={(v) => setGroups((old) => ({ ...old, keys: v.map((e) => e.value) }))}
+                  placeholder="Select some keys"
+                  closeMenuOnSelect={true}
+                />
+                <FormHelperText>{t('workspace.configuration.layout.prompt')}</FormHelperText>
+              </FormControl>
+
+              <Checkbox
+                mt={4}
+                isDisabled={groups.layout !== EdgeFlowLayout.CIRCLE_PACKING}
+                colorScheme="brand"
+                isChecked={groups.showGroups}
+                onChange={(v) => setGroups((old) => ({ ...old, showGroups: v.target.checked }))}
+              >
+                Show clusters as node
+              </Checkbox>
+            </FormControl>
+          </VStack>
         </DrawerBody>
       </DrawerContent>
     </Drawer>
