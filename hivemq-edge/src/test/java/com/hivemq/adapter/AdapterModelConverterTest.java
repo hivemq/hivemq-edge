0ef0c36b8ee01c5c16be9ba67562653b508d00f0
Simon Johnson <2924684+simon622@users.noreply.github.com>
2023-09-05 14:19:49 +0100

    Module -> Protocol conversion tests

--- hivemq-edge/src/test/java/com/hivemq/adapter/AdapterModelConverterTest.java
+++ hivemq-edge/src/test/java/com/hivemq/adapter/AdapterModelConverterTest.java
@@ -1,19 +1,25 @@
 package com.hivemq.adapter;
 
+import com.hivemq.api.model.adapters.ProtocolAdapter;
 import com.hivemq.api.model.adapters.ProtocolAdapterCategory;
+import com.hivemq.api.model.components.Module;
 import com.hivemq.api.resources.impl.ProtocolAdapterApiUtils;
-import com.hivemq.api.resources.impl.ProtocolAdaptersResourceImpl;
+import com.hivemq.client.mqtt.mqtt5.Mqtt5AsyncClient;
+import com.hivemq.configuration.service.ConfigurationService;
 import com.hivemq.edge.modules.adapters.ProtocolAdapterConstants;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.mock;
 
 /**
  * @author Simon L Johnson
  */
 public class AdapterModelConverterTest {
+
+    ConfigurationService configurationService = mock(ConfigurationService.class);
 
     @Test
     void testCategoriesConvertToTransportModel() {
@@ -25,5 +31,19 @@ public class AdapterModelConverterTest {
         assertNotNull(apiCategory.getDisplayName(), "Category Display Name should not be null");
         assertNotNull(apiCategory.getDescription(), "Category Description should not be null");
         assertNull(apiCategory.getImage(), "Category Image should be null");
+    }
+
+    @Test
+    void testProtocolAdapterModuleConversionUtils() {
+
+        Module testModule = ModuleModelTests.createTestModule();
+        ProtocolAdapter adapter = ProtocolAdapterApiUtils.convertModuleAdapterType(testModule, configurationService);
+        assertEquals(testModule.getName(), adapter.getName(), "Adapter name should match module name");
+        assertEquals(testModule.getDescription(), adapter.getDescription(), "Adapter description should match");
+        assertEquals(testModule.getId(), adapter.getId(), "Adapter id should match");
+        assertEquals(testModule.getId(), adapter.getProtocol(), "Adapter protocol should match");
+        assertEquals(false, adapter.getInstalled(), "Adapter should not be installed");
+        assertEquals(testModule.getAuthor(), adapter.getAuthor(), "Adapter author should match");
+        assertEquals(testModule.getVersion(), adapter.getVersion(), "Adapter version should match");
     }
 }
