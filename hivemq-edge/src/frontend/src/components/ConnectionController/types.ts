ca96035e71e56bc4c5b5d6857eb2c18a04d5d61b
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-09-27 12:11:53 +0100

    feat(123): add runtime controller types

---
+++ hivemq-edge/src/frontend/src/components/ConnectionController/types.ts
@@ -0,0 +1,7 @@
+import { StatusTransitionCommand } from '@/api/__generated__'
+
+export interface ConnectionElementProps {
+  id: string
+  isRunning: boolean
+  onChangeStatus?: (id: string, status: StatusTransitionCommand.command) => void
+}
