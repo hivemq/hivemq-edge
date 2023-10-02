2755cc81035c092b8b2abdb156791625bb2ca9ef
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-10-02 18:46:54 +0100

    feat(138): replace default JsonSchema input for topics

--- hivemq-edge/src/frontend/src/modules/ProtocolAdapters/components/templates/__internals/TopicInputTemplate.tsx
+++ hivemq-edge/src/frontend/src/modules/ProtocolAdapters/components/templates/__internals/TopicInputTemplate.tsx
@@ -1,6 +1,24 @@
 import { FC } from 'react'
 import { BaseInputTemplateProps } from '@rjsf/utils'
+import { FormControl, FormLabel } from '@chakra-ui/react'
+
+import TopicCreatableSelect from '@/components/MQTT/TopicCreatableSelect.tsx'
+import { useGetEdgeTopics } from '@/hooks/useGetEdgeTopics/useGetEdgeTopics.tsx'
 
 export const TopicInputTemplate: FC<BaseInputTemplateProps> = (props) => {
-  return null
+  const { chakraProps, label, id, disabled, readonly, onChange, required, rawErrors, value } = props
+  const { isLoading, data } = useGetEdgeTopics({ branchOnly: true })
+
+  return (
+    <FormControl
+      isDisabled={disabled || readonly}
+      isRequired={required}
+      {...chakraProps}
+      mb={1}
+      isInvalid={rawErrors && rawErrors.length > 0}
+    >
+      <FormLabel htmlFor={id}>{label}</FormLabel>
+      <TopicCreatableSelect isLoading={isLoading} options={data} id={id} value={value} onChange={onChange} />
+    </FormControl>
+  )
 }
