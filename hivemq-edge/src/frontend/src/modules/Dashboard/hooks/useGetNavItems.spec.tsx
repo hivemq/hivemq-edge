9b17dac7de7c235e95180c8e47c7b62751d62f18
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-10-05 12:17:02 +0100

    test(139): add tests

--- hivemq-edge/src/frontend/src/modules/Dashboard/hooks/useGetNavItems.spec.tsx
+++ hivemq-edge/src/frontend/src/modules/Dashboard/hooks/useGetNavItems.spec.tsx
@@ -28,7 +28,6 @@ describe('useSpringClient', () => {
 
   it('should retrieve all the menu items', async () => {
     server.use(...handlers)
-    server.printHandlers()
     const { result } = renderHook(useGetNavItems, { wrapper })
 
     await waitFor(() => {
@@ -37,5 +36,10 @@ describe('useSpringClient', () => {
 
     expect(result.current.data.map((e) => e.title)).toStrictEqual(['HiveMQ Edge', 'Extensions', 'External resources'])
     expect(result.current.data[0].items).toHaveLength(5)
+    expect(result.current.data[2].items.map((e) => e.href)).toStrictEqual([
+      'https://www.hivemq.com/articles/power-of-iot-data-management-in-smart-manufacturing/',
+      'https://github.com/hivemq/hivemq-edge',
+      'https://github.com/hivemq/hivemq-edge/wiki',
+    ])
   })
 })
