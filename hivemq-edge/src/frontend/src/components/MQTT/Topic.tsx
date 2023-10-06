d6f0f2ee3141d211e378b0b8304a0563a62e4752
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-10-06 13:44:55 +0100

    refactor(139): refactor topic component to allow custom styles and content

--- hivemq-edge/src/frontend/src/components/MQTT/Topic.tsx
+++ hivemq-edge/src/frontend/src/components/MQTT/Topic.tsx
@@ -1,17 +1,18 @@
-import { FC } from 'react'
-import { Tag, TagLabel } from '@chakra-ui/react'
+import { FC, ReactNode } from 'react'
+import { Tag, TagLabel, TagProps } from '@chakra-ui/react'
 
 import TopicIcon from '@/components/Icons/TopicIcon.tsx'
 
-interface TopicProps {
-  topic: string
+// TODO[NVL] Not sure adding ReactNode as possible children is a good move.
+interface TopicProps extends TagProps {
+  topic: string | ReactNode
 }
 
-const Topic: FC<TopicProps> = ({ topic }) => {
+const Topic: FC<TopicProps> = ({ topic, ...rest }) => {
   return (
-    <Tag data-testid={'topic-wrapper'}>
+    <Tag data-testid={'topic-wrapper'} {...rest}>
       <TopicIcon boxSize="12px" mr={2} />
-      <TagLabel>{topic}</TagLabel>
+      {typeof topic === 'string' ? <TagLabel>{topic}</TagLabel> : topic}
     </Tag>
   )
 }
