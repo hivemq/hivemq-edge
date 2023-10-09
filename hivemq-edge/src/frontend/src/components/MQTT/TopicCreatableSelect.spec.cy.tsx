2d5ce9c910b81a91a0719d360f758398cfd24958
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-10-09 15:26:57 +0100

    test(139): add tests for multiple topics

--- hivemq-edge/src/frontend/src/components/MQTT/TopicCreatableSelect.spec.cy.tsx
+++ hivemq-edge/src/frontend/src/components/MQTT/TopicCreatableSelect.spec.cy.tsx
@@ -1,10 +1,12 @@
 /// <reference types="cypress" />
 
-import { SingleTopicCreatableSelect } from './TopicCreatableSelect.tsx'
+import { MultiTopicsCreatableSelect, SingleTopicCreatableSelect } from './TopicCreatableSelect.tsx'
+import { mockAdapter, mockProtocolAdapter } from '@/api/hooks/useProtocolAdapters/__handlers__'
+import { mockBridge } from '@/api/hooks/useGetBridges/__handlers__'
 
 const MOCK_ID = 'my-id'
 
-describe('TopicCreatableSelect', () => {
+describe('SingleTopicCreatableSelect', () => {
   beforeEach(() => {
     cy.viewport(450, 250)
   })
@@ -57,5 +59,71 @@ describe('TopicCreatableSelect', () => {
     cy.get('#my-id').click()
     cy.checkAccessibility()
     cy.percySnapshot('Component: TopicCreatableSelect')
+  })
+})
+
+describe.only('MultiTopicsCreatableSelect', () => {
+  beforeEach(() => {
+    cy.viewport(450, 250)
+  })
+
+  it('should render an empty component', () => {
+    const mockOnChange = cy.stub().as('onChange')
+    cy.intercept('/api/v1/management/protocol-adapters/types', { items: [] }).as('getConfig1')
+    cy.intercept('/api/v1/management/protocol-adapters/adapters', { items: [] }).as('getConfig2')
+    cy.intercept('/api/v1/management/bridges', { items: [] }).as('getConfig3')
+
+    cy.mountWithProviders(<MultiTopicsCreatableSelect id={MOCK_ID} value={[]} onChange={mockOnChange} />)
+
+    cy.wait(['@getConfig1', '@getConfig2', '@getConfig3'])
+    cy.get('#my-id').click()
+    cy.get('#react-select-2-listbox').contains('No topic loaded')
+  })
+
+  it('should render a single topic', () => {
+    const mockOnChange = cy.stub().as('onChange')
+    cy.intercept('/api/v1/management/protocol-adapters/types', { items: [mockProtocolAdapter] }).as('getConfig1')
+    cy.intercept('/api/v1/management/protocol-adapters/adapters', { items: [mockAdapter] }).as('getConfig2')
+    cy.intercept('/api/v1/management/bridges', { items: [mockBridge] }).as('getConfig3')
+
+    cy.mountWithProviders(<MultiTopicsCreatableSelect id={MOCK_ID} value={[]} onChange={mockOnChange} />)
+    cy.wait(['@getConfig1', '@getConfig2', '@getConfig3'])
+
+    cy.get('#my-id').click()
+
+    cy.get('#react-select-3-listbox').contains('#')
+
+    cy.get('#my-id').type('123')
+    cy.get('#react-select-3-listbox').contains('Add the topic ... 123')
+    // cy.get('#my-id').type('{Enter}')
+
+    cy.get('#react-select-3-option-4').click()
+
+    cy.get('@onChange').should('have.been.calledWith', ['123'])
+  })
+
+  it('should render multiple topics', () => {
+    const mockOnChange = cy.stub().as('onChange')
+    cy.intercept('/api/v1/management/protocol-adapters/types', { items: [mockProtocolAdapter] }).as('getConfig1')
+    cy.intercept('/api/v1/management/protocol-adapters/adapters', { items: [mockAdapter] }).as('getConfig2')
+    cy.intercept('/api/v1/management/bridges', { items: [mockBridge] }).as('getConfig3')
+
+    cy.mountWithProviders(<MultiTopicsCreatableSelect id={MOCK_ID} value={['old topic']} onChange={mockOnChange} />)
+    cy.wait(['@getConfig1', '@getConfig2', '@getConfig3'])
+
+    cy.get('#my-id').contains('old topic')
+    cy.get('#my-id').click()
+
+    cy.get('#react-select-4-listbox').contains('#')
+
+    cy.get('#my-id').type('123')
+    cy.get('#react-select-4-listbox').contains('Add the topic ... 123')
+    // cy.get('#my-id').type('{Enter}')
+
+    cy.get('#react-select-4-option-4').click()
+
+    cy.get('@onChange').should('have.been.calledWith', ['old topic', '123'])
+
+    cy.clearInterceptList('@getConfig3')
   })
 })
