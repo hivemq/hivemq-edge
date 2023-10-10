5e43b797c4019c80fcd8699c2fc41ad7306b1db0
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-10-10 15:07:08 +0100

    test(139): fix test for topic rendering

--- hivemq-edge/src/frontend/src/components/MQTT/Topic.tsx
+++ hivemq-edge/src/frontend/src/components/MQTT/Topic.tsx
@@ -2,6 +2,7 @@ import { FC, ReactNode } from 'react'
 import { Tag, TagLabel, TagProps } from '@chakra-ui/react'
 
 import TopicIcon from '@/components/Icons/TopicIcon.tsx'
+import { formatTopicString } from '@/components/MQTT/topic-utils.ts'
 
 // TODO[NVL] Not sure adding ReactNode as possible children is a good move.
 interface TopicProps extends TagProps {
@@ -9,7 +10,7 @@ interface TopicProps extends TagProps {
 }
 
 const Topic: FC<TopicProps> = ({ topic, ...rest }) => {
-  const expandedTopic = typeof topic === 'string' ? topic.split('/').join(' / ') : topic
+  const expandedTopic = typeof topic === 'string' ? formatTopicString(topic) : topic
   return (
     <Tag data-testid={'topic-wrapper'} {...rest} letterSpacing={'-0.05rem'}>
       <TopicIcon boxSize="12px" mr={2} />
