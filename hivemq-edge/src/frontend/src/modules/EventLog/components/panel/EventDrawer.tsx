eafb9f702c12ba7e29c338de5d5357b45ef1b5c1
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-10-23 15:09:10 +0100

    feat(17144): add side panel for event visualisation

---
+++ hivemq-edge/src/frontend/src/modules/EventLog/components/panel/EventDrawer.tsx
@@ -0,0 +1,38 @@
+import { FC } from 'react'
+import { useTranslation } from 'react-i18next'
+import { Drawer, DrawerBody, DrawerHeader, DrawerContent, DrawerCloseButton } from '@chakra-ui/react'
+
+import { Event } from '@/api/__generated__'
+
+interface BridgeMainDrawerProps {
+  event: Event | undefined
+  isOpen: boolean
+
+  onClose: () => void
+}
+
+const EventDrawer: FC<BridgeMainDrawerProps> = ({ isOpen, onClose }) => {
+  const { t } = useTranslation()
+
+  return (
+    <>
+      <Drawer
+        variant={'hivemq'}
+        closeOnOverlayClick={true}
+        size={'lg'}
+        isOpen={isOpen}
+        placement="right"
+        onClose={onClose}
+      >
+        <DrawerContent aria-label={t('bridge.drawer.label') as string}>
+          <DrawerCloseButton />
+          <DrawerHeader id={'bridge-form-header'}>{t('eventLog.panel.title')}</DrawerHeader>
+
+          <DrawerBody></DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
+  )
+}
+
+export default EventDrawer
