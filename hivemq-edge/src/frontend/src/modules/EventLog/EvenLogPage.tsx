7514adc71038f0222856db1f5debfe6acc59b2e3
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-10-23 19:51:17 +0100

    refactor(17144): add support for selected event

--- hivemq-edge/src/frontend/src/modules/EventLog/EvenLogPage.tsx
+++ hivemq-edge/src/frontend/src/modules/EventLog/EvenLogPage.tsx
@@ -1,7 +1,8 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useDisclosure } from '@chakra-ui/react'
 
+import { Event } from '@/api/__generated__'
 import PageContainer from '@/components/PageContainer.tsx'
 
 import EventDrawer from './components/panel/EventDrawer.tsx'
@@ -10,19 +11,22 @@ import EventLogTable from './components/table/EventLogTable.tsx'
 const EvenLogPage: FC = () => {
   const { isOpen, onOpen, onClose } = useDisclosure()
   const { t } = useTranslation()
+  const [selectedEvent, setSelectedEvent] = useState<Event | undefined>(undefined)
 
   const handleEditorOnClose = () => {
+    setSelectedEvent(undefined)
     onClose()
   }
 
-  const handleEditorOpen = () => {
+  const handleEditorOpen = (evt: Event) => {
+    setSelectedEvent(evt)
     onOpen()
   }
 
   return (
     <PageContainer title={t('eventLog.title') as string} subtitle={t('eventLog.description') as string}>
       <EventLogTable onOpen={handleEditorOpen} />
-      <EventDrawer isOpen={isOpen} onClose={handleEditorOnClose} event={undefined} />
+      <EventDrawer isOpen={isOpen} onClose={handleEditorOnClose} event={selectedEvent} />
     </PageContainer>
   )
 }
