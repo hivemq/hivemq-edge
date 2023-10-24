ad65cab0f23662cc8e2ac6da53285863e5ff1152
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-10-24 18:40:38 +0100

    refactor(17144): add the attribute/value from the event into the panel - add tests

--- hivemq-edge/src/frontend/src/modules/EventLog/components/panel/EventDrawer.tsx
+++ hivemq-edge/src/frontend/src/modules/EventLog/components/panel/EventDrawer.tsx
@@ -1,11 +1,31 @@
 import { FC } from 'react'
 import { useTranslation } from 'react-i18next'
-import { Drawer, DrawerBody, DrawerHeader, DrawerContent, DrawerCloseButton } from '@chakra-ui/react'
+import { DateTime } from 'luxon'
 
-import { Event } from '@/api/__generated__'
+import {
+  Drawer,
+  DrawerBody,
+  DrawerHeader,
+  DrawerContent,
+  DrawerCloseButton,
+  Text,
+  Card,
+  CardHeader,
+  CardBody,
+  Grid,
+  GridItem,
+  VStack,
+  Code,
+  Badge,
+} from '@chakra-ui/react'
+
+import { Event, Payload } from '@/api/__generated__'
+import SourceLink from '../SourceLink.tsx'
+import SeverityBadge from '@/modules/EventLog/components/SeverityBadge.tsx'
+import { prettifyXml, prettyJSON } from '@/modules/EventLog/utils/payload-utils.ts'
 
 interface BridgeMainDrawerProps {
-  event: Event | undefined
+  event: Event
   isOpen: boolean
 
   onClose: () => void
@@ -13,6 +33,12 @@ interface BridgeMainDrawerProps {
 
 const EventDrawer: FC<BridgeMainDrawerProps> = ({ event, isOpen, onClose }) => {
   const { t } = useTranslation()
+
+  const isJSON = event.payload?.contentType === Payload.contentType.JSON
+  const isXML = event.payload?.contentType === Payload.contentType.XML
+
+  const JSONFormat = isJSON && prettyJSON(event.payload?.content)
+  const XMLFormat = isXML && prettifyXml(event.payload?.content)
 
   return (
     <>
@@ -28,7 +54,57 @@ const EventDrawer: FC<BridgeMainDrawerProps> = ({ event, isOpen, onClose }) => {
           <DrawerCloseButton />
           <DrawerHeader id={'bridge-form-header'}>{t('eventLog.panel.title')}</DrawerHeader>
 
-          <DrawerBody></DrawerBody>
+          <DrawerBody>
+            <VStack gap={2}>
+              <Card w={'100%'}>
+                <CardHeader>
+                  <SeverityBadge event={event} />
+                </CardHeader>
+                <CardBody>
+                  <Grid templateColumns="repeat(2, 1fr)" gap={6}>
+                    <GridItem data-testid={'event-title-created'}>
+                      <Text>{t('eventLog.table.header.created')}</Text>
+                    </GridItem>
+                    <GridItem data-testid={'event-value-created'}>
+                      <Text> {DateTime.fromISO(event?.created || '').toRelativeCalendar({ unit: 'minutes' })}</Text>
+                    </GridItem>
+                    <GridItem data-testid={'event-title-source'}>
+                      <Text>{t('eventLog.table.header.source')}</Text>
+                    </GridItem>
+                    <GridItem data-testid={'event-value-source'}>
+                      <SourceLink event={event?.source} />
+                    </GridItem>
+                    <GridItem data-testid={'event-title-associatedObject'}>
+                      <Text>{t('eventLog.table.header.associatedObject')}</Text>
+                    </GridItem>
+                    <GridItem data-testid={'event-value-associatedObject'}>
+                      <SourceLink event={event?.associatedObject} />
+                    </GridItem>
+                  </Grid>
+                </CardBody>
+              </Card>
+
+              <Card w={'100%'}>
+                <CardBody data-testid={'event-value-message'}>{event.message}</CardBody>
+              </Card>
+
+              <Card w={'100%'}>
+                <CardHeader data-testid={'event-title-payload'}>
+                  {t('eventLog.table.header.payload')}
+                  <Badge variant="outline" mx={2}>
+                    {event.payload?.contentType}
+                  </Badge>
+                </CardHeader>
+                <CardBody>
+                  <Code w={'100%'} p={2} whiteSpace={'pre-wrap'}>
+                    {JSONFormat && JSONFormat}
+                    {XMLFormat && XMLFormat}
+                    {!isJSON && !isXML && event.payload?.content}
+                  </Code>
+                </CardBody>
+              </Card>
+            </VStack>
+          </DrawerBody>
         </DrawerContent>
       </Drawer>
     </>
