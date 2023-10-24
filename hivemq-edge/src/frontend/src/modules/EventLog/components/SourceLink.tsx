fd8b0843da8dd9dd9176296882eb0df230448c60
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-10-24 18:20:09 +0100

    refactor(17144): refactor source component

--- hivemq-edge/src/frontend/src/modules/EventLog/components/SourceLink.tsx
+++ hivemq-edge/src/frontend/src/modules/EventLog/components/SourceLink.tsx
@@ -3,11 +3,11 @@ import { Link as ReactRouterLink } from 'react-router-dom'
 import { Box, Icon, Link as ChakraLink } from '@chakra-ui/react'
 import { PiBridgeThin, PiPlugsConnectedFill, PiUserFill } from 'react-icons/pi'
 
-import { Event, TypeIdentifier } from '@/api/__generated__'
+import { TypeIdentifier } from '@/api/__generated__'
 import { MdOutlineEventNote } from 'react-icons/md'
 
 interface SourceLinkProps {
-  event: Event
+  event: TypeIdentifier | undefined
 }
 
 const SourceLink: FC<SourceLinkProps> = ({ event }) => {
@@ -27,22 +27,22 @@ const SourceLink: FC<SourceLinkProps> = ({ event }) => {
     [TypeIdentifier.type.USER]: undefined,
   }
 
-  if (!event.associatedObject?.type || !navRoute[event.associatedObject?.type])
+  if (!event?.type || !navRoute[event?.type])
     return (
       <Box whiteSpace={'nowrap'} display={'inline-flex'}>
-        {event.associatedObject?.identifier}
+        {event?.identifier}
       </Box>
     )
 
   return (
     <ChakraLink
       as={ReactRouterLink}
-      to={navRoute[event.associatedObject?.type]?.(event.associatedObject?.identifier)}
+      to={navRoute[event.type]?.(event.identifier)}
       whiteSpace={'nowrap'}
       display={'inline-flex'}
     >
-      {event.source?.type && IconComponent[event.source?.type]}
-      {event.associatedObject?.identifier}
+      {event.type && IconComponent[event.type]}
+      {event.identifier}
     </ChakraLink>
   )
 }
