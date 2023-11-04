298e39c667b400ec3893795728d348e5efbab28d
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-11-04 11:42:52 +0000

    refactor(17445): replace filter by more robust select component - delete deprecated components

--- hivemq-edge/src/frontend/src/components/PaginatedTable/components/Filter.tsx
+++ hivemq-edge/src/frontend/src/components/PaginatedTable/components/Filter.tsx
@@ -1,9 +1,8 @@
 import { useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Column } from '@tanstack/react-table'
-// import { RangeSlider, RangeSliderFilledTrack, RangeSliderThumb, RangeSliderTrack } from '@chakra-ui/react'
-
-import { DebouncedInput } from './DebouncedInput.tsx'
+import { CreatableSelect } from 'chakra-react-select'
+import { Box } from '@chakra-ui/react'
 
 export interface FilterProps<T>
   extends Pick<
@@ -13,7 +12,6 @@ export interface FilterProps<T>
   firstValue: unknown
 }
 
-// /**/export const Filter = <T,>({ column, table }: { column: Column<T, unknown>; table: Table<T> }) => {
 export const Filter = <T,>({
   id,
   getFilterValue,
@@ -24,7 +22,6 @@ export const Filter = <T,>({
 }: FilterProps<T>) => {
   const { t } = useTranslation()
   // const firstValue = table.getPreFilteredRowModel().flatRows[0]?.getValue(id)
-
   const columnFilterValue = getFilterValue()
 
   const sortedUniqueValues = useMemo(
@@ -32,62 +29,25 @@ export const Filter = <T,>({
     [firstValue, getFacetedUniqueValues]
   )
 
-  // const FilterMulti = (
-  //   <div className="flex space-x-2">
-  //     <DebouncedInput
-  //       type="number"
-  //       min={Number(getFacetedMinMaxValues()?.[0] ?? '')}
-  //       max={Number(getFacetedMinMaxValues()?.[1] ?? '')}
-  //       value={(columnFilterValue as [number, number])?.[0] ?? ''}
-  //       onChange={(value) => setFilterValue((old: [number, number]) => [value, old?.[1]])}
-  //       placeholder={`Min ${getFacetedMinMaxValues()?.[0] ? `(${getFacetedMinMaxValues()?.[0]})` : ''}`}
-  //       className="w-24 border shadow rounded"
-  //     />
-  //     <DebouncedInput
-  //       type="number"
-  //       min={Number(getFacetedMinMaxValues()?.[0] ?? '')}
-  //       max={Number(getFacetedMinMaxValues()?.[1] ?? '')}
-  //       value={(columnFilterValue as [number, number])?.[1] ?? ''}
-  //       onChange={(value) => setFilterValue((old: [number, number]) => [old?.[0], value])}
-  //       placeholder={`Max ${getFacetedMinMaxValues()?.[1] ? `(${getFacetedMinMaxValues()?.[1]})` : ''}`}
-  //       className="w-24 border shadow rounded"
-  //     />
-  //   </div>
-  // )
-  //
-  // const FilterRangeSlider = (
-  //   <RangeSlider
-  //     onChangeEnd={(a) => console.log('XXXXX', a)}
-  //     aria-label={['min', 'max']}
-  //     defaultValue={[Number(getFacetedMinMaxValues()?.[0] ?? ''), Number(getFacetedMinMaxValues()?.[1] ?? '')]}
-  //     min={Number(getFacetedMinMaxValues()?.[0] ?? '')}
-  //     max={Number(getFacetedMinMaxValues()?.[1] ?? '')}
-  //   >
-  //     <RangeSliderTrack>
-  //       <RangeSliderFilledTrack />
-  //     </RangeSliderTrack>
-  //     <RangeSliderThumb index={0} />
-  //     <RangeSliderThumb index={1} />
-  //   </RangeSlider>
-  // )
-
   if (typeof firstValue === 'number') return null
 
   return (
-    <>
-      <datalist id={id + '-list'}>
-        {sortedUniqueValues.slice(0, 5000).map((value: string) => (
-          <option value={value} key={value} />
-        ))}
-      </datalist>
-      <DebouncedInput
-        type="text"
-        id={id}
-        value={(columnFilterValue ?? '') as string}
-        onChange={(value) => setFilterValue(value)}
+    <Box w={'100%'} textTransform={'none'} fontWeight={'initial'}>
+      <CreatableSelect
+        size={'sm'}
+        inputId={id}
+        value={{ value: columnFilterValue, label: columnFilterValue }}
+        onChange={(item) => setFilterValue(item?.value)}
+        options={sortedUniqueValues
+          .slice(0, 5000)
+          .map((value: string) => ({ value: value, label: value, group: 'DDD' }))}
         placeholder={t('components:pagination.filter.placeholder', { size: getFacetedUniqueValues().size }) as string}
-        list={id + '-list'}
+        isClearable={true}
+        isMulti={false}
+        components={{
+          DropdownIndicator: null,
+        }}
       />
-    </>
+    </Box>
   )
 }
