66c02f6ae532399d26fdab8b6e66f4c483a00b5c
A-Imal <abdullah.imal@hivemq.com>
2023-11-06 15:42:41 +0100

    Logging > The current TurboFilterList implementation of logback-classic which calls the TurboFilters is a CopyOnWriteArrayList. This means every time any logger method is called the array of TurboFilters is copied. As we are using the logger extensively this small copy is visible in the flamegraph. A call to isXLevelEnabled check does not avoid this path.

--- hivemq-edge/src/main/java/com/hivemq/bootstrap/LoggingBootstrap.java
+++ hivemq-edge/src/main/java/com/hivemq/bootstrap/LoggingBootstrap.java
@@ -25,8 +25,9 @@ import ch.qos.logback.core.joran.spi.JoranException;
 import ch.qos.logback.core.read.ListAppender;
 import ch.qos.logback.core.util.StatusPrinter;
 import com.hivemq.extension.sdk.api.annotations.NotNull;
-import com.hivemq.logging.LogLevelModifier;
-import com.hivemq.logging.NettyLogLevelModifier;
+import com.hivemq.logging.modifier.MiscLogLevelModifier;
+import com.hivemq.logging.LogLevelModifierTurboFilter;
+import com.hivemq.logging.modifier.NettyLogLevelModifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.bridge.SLF4JBridgeHandler;
@@ -40,18 +41,15 @@ import java.util.List;
  * This class is responsible for all logging bootstrapping. This is only
  * needed at the very beginning of HiveMQs lifecycle and before bootstrapping other
  * resources
- *
- * @author Dominik Obermaier
  */
 public class LoggingBootstrap {
 
-    private static @NotNull ListAppender<ILoggingEvent> listAppender = new ListAppender<>();
-
     private static final Logger log = LoggerFactory.getLogger(LoggingBootstrap.class);
 
-    private static final NettyLogLevelModifier nettyLogLevelModifier = new NettyLogLevelModifier();
-    private static final LogLevelModifier LOG_LEVEL_MODIFIER = new LogLevelModifier();
+    private static @NotNull ListAppender<ILoggingEvent> listAppender = new ListAppender<>();
     private static final List<Appender<ILoggingEvent>> defaultAppenders = new LinkedList<>();
+    private static final @NotNull LogLevelModifierTurboFilter logLevelModifierTurboFilter =
+            new LogLevelModifierTurboFilter();
 
     /**
      * Prepares the logging. This method must be called before any logging occurs
@@ -84,9 +82,8 @@ public class LoggingBootstrap {
     public static void initLogging(final @NotNull File configFolder) {
 
         final LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
-        final ch.qos.logback.classic.Logger logger = getRootLogger();
 
-        context.addListener(new LogbackChangeListener(logger));
+        context.addListener(new LogbackChangeListener());
 
         final boolean overridden = overrideLogbackXml(configFolder);
 
@@ -98,8 +95,8 @@ public class LoggingBootstrap {
 
         reset();
 
-        context.addTurboFilter(nettyLogLevelModifier);
-        context.addTurboFilter(LOG_LEVEL_MODIFIER);
+        logLevelModifierTurboFilter.registerLogLevelModifier(new NettyLogLevelModifier());
+        logLevelModifierTurboFilter.registerLogLevelModifier(new MiscLogLevelModifier());
         log.trace("Added Netty log level modifier");
     }
 
@@ -191,14 +188,7 @@ public class LoggingBootstrap {
         listAppender.list.clear();
     }
 
-    private static class LogbackChangeListener implements LoggerContextListener {
-
-        private final @NotNull ch.qos.logback.classic.Logger logger;
-
-        private LogbackChangeListener(
-                final @NotNull ch.qos.logback.classic.Logger logger) {
-            this.logger = logger;
-        }
+    private static final class LogbackChangeListener implements LoggerContextListener {
 
         @Override
         public boolean isResetResistant() {
@@ -220,7 +210,7 @@ public class LoggingBootstrap {
         @Override
         public void onReset(final @NotNull LoggerContext context) {
             log.trace("logback.xml was changed");
-            addTurboFilters(context);
+            context.addTurboFilter(logLevelModifierTurboFilter);
         }
 
         @Override
@@ -231,11 +221,6 @@ public class LoggingBootstrap {
         @Override
         public void onLevelChange(final @NotNull ch.qos.logback.classic.Logger logger, final @NotNull Level level) {
             //noop
-        }
-
-        private void addTurboFilters(final @NotNull LoggerContext context) {
-            context.addTurboFilter(nettyLogLevelModifier);
-            context.addTurboFilter(LOG_LEVEL_MODIFIER);
         }
     }
 }
