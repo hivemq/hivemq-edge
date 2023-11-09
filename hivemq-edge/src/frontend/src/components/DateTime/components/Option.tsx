cfa5d289c6966f2cdba351c99bd0e54aadf2ede1
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-11-09 20:29:58 +0000

    refactor(17451): refactor the display of the duration options

--- hivemq-edge/src/frontend/src/components/DateTime/components/Option.tsx
+++ hivemq-edge/src/frontend/src/components/DateTime/components/Option.tsx
@@ -1,20 +1,48 @@
 import { ComponentType } from 'react'
 import { chakraComponents, GroupBase, OptionProps } from 'chakra-react-select'
-import { Icon } from '@chakra-ui/react'
-import { SiMqtt } from 'react-icons/si'
+import { Badge, Box, Button, HStack, Text } from '@chakra-ui/react'
 
 import { RangeOption } from '../types.ts'
 
 const Option: ComponentType<OptionProps<RangeOption, false, GroupBase<RangeOption>>> = ({ children, ...props }) => {
-  const { value } = props.data
+  const { value, label, isCommand } = props.data
 
   // Seems to be the best way of detecting the "Create option"
   const isOptionCreate = value === ''
 
+  if (isCommand) {
+    return (
+      <Box w={'100%'}>
+        <Button variant={'ghost'} size={'sm'} isDisabled>
+          {label}
+        </Button>
+      </Box>
+    )
+  }
+
+  const duration = props.data.duration?.toObject()
+  // TODO[NVL] This is not i18n-friendly
+  const [a, v] = Object.entries({ ...duration })[0]
+
   return (
     <chakraComponents.Option {...props}>
-      {!isOptionCreate && <Icon as={SiMqtt} color={props.data.color} mr={2} h={5} w={5} />}
-      {children}
+      <HStack flexWrap={'nowrap'}>
+        {!isOptionCreate && (
+          <Badge
+            as={'p'}
+            textAlign={'center'}
+            textTransform={'lowercase'}
+            size={'sm'}
+            data-testid={`dateRange-option-badge-${value}`}
+            variant="solid"
+            colorScheme={props.data.colorScheme}
+            mr={2}
+          >
+            {`${v}${a[0]}`}
+          </Badge>
+        )}
+        <Text>{children}</Text>
+      </HStack>
     </chakraComponents.Option>
   )
 }
