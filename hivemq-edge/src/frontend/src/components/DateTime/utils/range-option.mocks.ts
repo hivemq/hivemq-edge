48ff09a878bc9f90c3a5429b5a36174355044a0b
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-11-10 17:25:03 +0000

    test(17451): add tests for utilities

--- hivemq-edge/src/frontend/src/components/DateTime/utils/range-option.mocks.ts
+++ hivemq-edge/src/frontend/src/components/DateTime/utils/range-option.mocks.ts
@@ -1,5 +1,5 @@
 import { RangeOption } from '@/components/DateTime/types.ts'
-import { Duration } from 'luxon'
+import { DateTime, Duration } from 'luxon'
 
 export const MOCK_RANGE_OPTION: RangeOption = {
   value: 'range1',
@@ -7,3 +7,5 @@ export const MOCK_RANGE_OPTION: RangeOption = {
   colorScheme: 'yellow',
   duration: Duration.fromObject({ hour: 1 }),
 }
+
+export const MOCK_DATE_TIME_NOW = DateTime.fromObject({ year: 2023, month: 11, day: 10 })
