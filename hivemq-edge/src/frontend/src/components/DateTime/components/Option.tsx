ce4c61614164aed172ff7f47674a019ab88b606b
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-11-10 16:43:52 +0000

    refactor(17451): refactor option component

--- hivemq-edge/src/frontend/src/components/DateTime/components/Option.tsx
+++ hivemq-edge/src/frontend/src/components/DateTime/components/Option.tsx
@@ -1,47 +1,25 @@
 import { ComponentType } from 'react'
 import { chakraComponents, GroupBase, OptionProps } from 'chakra-react-select'
-import { Badge, Box, Button, HStack, Text } from '@chakra-ui/react'
-import { useTranslation } from 'react-i18next'
+import { HStack, Text } from '@chakra-ui/react'
 
 import { RangeOption } from '../types.ts'
-import { badgeFrom } from '../utils/range-option.utils.ts'
+import OptionBadge from './OptionBadge.tsx'
+import OptionCommand from './OptionCommand.tsx'
 
 const Option: ComponentType<OptionProps<RangeOption, false, GroupBase<RangeOption>>> = ({ children, ...props }) => {
-  const { t } = useTranslation('components')
-  const { value, label, isCommand } = props.data
+  const { value, isCommand } = props.data
 
   // Seems to be the best way of detecting the "Create option"
   const isOptionCreate = value === ''
 
   if (isCommand) {
-    return (
-      <Box w={'100%'}>
-        <Button variant={'ghost'} size={'sm'} isDisabled>
-          {label}
-        </Button>
-      </Box>
-    )
+    return <OptionCommand data={props.data} />
   }
-
-  const badge = badgeFrom(props.data, t)
 
   return (
     <chakraComponents.Option {...props}>
       <HStack flexWrap={'nowrap'}>
-        {!isOptionCreate && badge && (
-          <Badge
-            as={'p'}
-            textAlign={'center'}
-            textTransform={'lowercase'}
-            size={'sm'}
-            data-testid={`dateRange-option-badge-${value}`}
-            variant="solid"
-            colorScheme={props.data.colorScheme}
-            mr={2}
-          >
-            {badge}
-          </Badge>
-        )}
+        {!isOptionCreate && <OptionBadge data={props.data} />}
         <Text>{children}</Text>
       </HStack>
     </chakraComponents.Option>
---
+++ hivemq-edge/src/frontend/src/components/DateTime/components/OptionBadge.tsx
@@ -0,0 +1,33 @@
+import { FC } from 'react'
+import { Badge, type BadgeProps } from '@chakra-ui/react'
+
+import { RangeOption } from '../types.ts'
+import { useRangeTranslation } from '../hooks/useRangeTranslation.ts'
+
+interface OptionBadgeProps extends BadgeProps {
+  data: RangeOption
+}
+
+const OptionBadge: FC<OptionBadgeProps> = ({ data }) => {
+  const { translateBadgeFrom } = useRangeTranslation()
+  const badge = translateBadgeFrom(data)
+
+  if (!badge) return null
+
+  return (
+    <Badge
+      as={'p'}
+      textAlign={'center'}
+      textTransform={'lowercase'}
+      size={'sm'}
+      data-testid={`dateRange-option-badge-${data.value}`}
+      variant="solid"
+      colorScheme={data.colorScheme}
+      mr={2}
+    >
+      {badge}
+    </Badge>
+  )
+}
+
+export default OptionBadge
