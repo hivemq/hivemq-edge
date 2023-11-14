7d5d87d9e9a04f2cdee21810453f06a69e934900
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-11-14 11:20:28 +0000

    tests(17327): add tests

---
+++ hivemq-edge/src/frontend/src/components/DateTime/utils/duration.utils.spec.ts
@@ -0,0 +1,25 @@
+import { describe, expect } from 'vitest'
+import { DateTime } from 'luxon'
+
+import { toHuman } from './duration.utils.ts'
+import { MOCK_DATE_TIME_NOW } from './range-option.mocks.ts'
+
+interface ToHumanTestSuite {
+  date: DateTime
+  expected: string
+}
+
+const allTests: ToHumanTestSuite[] = [
+  { date: MOCK_DATE_TIME_NOW.minus({ month: 2 }), expected: '2 months ago' },
+  { date: MOCK_DATE_TIME_NOW.minus({ days: 9 }), expected: '9 days ago' },
+  { date: MOCK_DATE_TIME_NOW, expected: 'in 0 seconds' },
+  { date: MOCK_DATE_TIME_NOW.plus({ days: 2 }), expected: 'in 2 days' },
+  { date: MOCK_DATE_TIME_NOW.plus({ days: 3 }), expected: 'in 3 days' },
+  { date: MOCK_DATE_TIME_NOW.plus({ days: 3, hour: 19 }), expected: 'in 3 days' },
+]
+
+describe('toHuman', () => {
+  it.each<ToHumanTestSuite>(allTests)('should return $expected', ({ date, expected }) => {
+    expect(toHuman(date, MOCK_DATE_TIME_NOW)).toEqual(expected)
+  })
+})
