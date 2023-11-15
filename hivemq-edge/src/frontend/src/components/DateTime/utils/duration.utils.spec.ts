1b0ce08d375907ea6a430133523d06b7b6f2bf03
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-11-15 10:52:58 +0000

    test(17327): fix test

--- hivemq-edge/src/frontend/src/components/DateTime/utils/duration.utils.spec.ts
+++ hivemq-edge/src/frontend/src/components/DateTime/utils/duration.utils.spec.ts
@@ -13,7 +13,7 @@ const allTests: ToHumanTestSuite[] = [
   { date: MOCK_DATE_TIME_NOW.minus({ month: 2 }), expected: '2 months ago' },
   { date: MOCK_DATE_TIME_NOW.minus({ days: 9 }), expected: '9 days ago' },
   { date: MOCK_DATE_TIME_NOW, expected: 'in 0 seconds' },
-  { date: MOCK_DATE_TIME_NOW.plus({ days: 2 }), expected: 'in 2 days' },
+  { date: MOCK_DATE_TIME_NOW.plus({ days: 1 }), expected: 'in 1 day' },
   { date: MOCK_DATE_TIME_NOW.plus({ days: 3 }), expected: 'in 3 days' },
   { date: MOCK_DATE_TIME_NOW.plus({ days: 3, hour: 19 }), expected: 'in 3 days' },
 ]
