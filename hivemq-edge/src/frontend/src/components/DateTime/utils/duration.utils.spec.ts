5f9c28a4ec4d83af2044fd6724037a22b7c6ea1f
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-11-16 17:10:49 +0000

    test(17448): fix dangerous test

--- hivemq-edge/src/frontend/src/components/DateTime/utils/duration.utils.spec.ts
+++ hivemq-edge/src/frontend/src/components/DateTime/utils/duration.utils.spec.ts
@@ -12,7 +12,7 @@ interface ToHumanTestSuite {
 const allTests: ToHumanTestSuite[] = [
   { date: MOCK_DATE_TIME_NOW.minus({ month: 2 }), expected: '2 months ago' },
   { date: MOCK_DATE_TIME_NOW.minus({ days: 9 }), expected: '9 days ago' },
-  { date: MOCK_DATE_TIME_NOW, expected: 'in 0 seconds' },
+  { date: MOCK_DATE_TIME_NOW.minus({ second: 1 }), expected: '1 second ago' },
   { date: MOCK_DATE_TIME_NOW.plus({ days: 1 }), expected: 'in 1 day' },
   { date: MOCK_DATE_TIME_NOW.plus({ days: 3 }), expected: 'in 3 days' },
   { date: MOCK_DATE_TIME_NOW.plus({ days: 3, hour: 19 }), expected: 'in 3 days' },
