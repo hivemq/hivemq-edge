566d4ed40b8b751d888cef3d7d59fc20ae0ab171
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-11-17 15:33:00 +0000

    feat(17700): add metric name parser

---
+++ hivemq-edge/src/frontend/src/modules/Welcome/utils/metrics-name.utils.ts
@@ -0,0 +1,8 @@
+// TODO[NVL] This is a potentially dangerous approach as there is no guaranties that the pattern will be consistent with other type of adapters.
+export const extractMetricInfo = (metricName: string) => {
+  const pruneLeading = metricName.replace('com.hivemq.edge.', '')
+  const [device, type, id, ...metric] = pruneLeading.split('.')
+  const suffix = metric.join('.')
+
+  return { device, type, id, suffix }
+}
