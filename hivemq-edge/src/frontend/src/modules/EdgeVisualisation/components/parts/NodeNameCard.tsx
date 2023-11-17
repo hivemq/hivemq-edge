0d3d93d1987e4e6e3b6208a48081626acfdceea4
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-11-17 10:41:58 +0000

    refactor(17476): refactor component

--- hivemq-edge/src/frontend/src/modules/EdgeVisualisation/components/parts/NodeNameCard.tsx
+++ hivemq-edge/src/frontend/src/modules/EdgeVisualisation/components/parts/NodeNameCard.tsx
@@ -1,11 +1,13 @@
 import { FC, useMemo } from 'react'
 import { Node } from 'reactflow'
-import { Card, CardBody, HStack, Icon, StackDivider, Text } from '@chakra-ui/react'
+import { Card, CardBody, HStack, Icon, StackDivider, Tag, Text, VStack } from '@chakra-ui/react'
 import { PiBridgeThin, PiPlugsConnectedFill } from 'react-icons/pi'
+import { useTranslation } from 'react-i18next'
 
 import { Adapter, Bridge } from '@/api/__generated__'
-import { NodeTypes } from '@/modules/EdgeVisualisation/types.ts'
-import { useTranslation } from 'react-i18next'
+import { useGetAdapterTypes } from '@/api/hooks/useProtocolAdapters/useGetAdapterTypes.tsx'
+
+import { NodeTypes } from '../../types.ts'
 
 interface NodeNameCardProps {
   selectedNode: Node<Bridge | Adapter>
@@ -13,29 +15,47 @@ interface NodeNameCardProps {
 
 const NodeNameCard: FC<NodeNameCardProps> = ({ selectedNode }) => {
   const { t } = useTranslation()
+  const { data } = useGetAdapterTypes()
   const { type } = selectedNode
+
+  const adapterType = useMemo(() => {
+    if (!data) return undefined
+    if (type === NodeTypes.BRIDGE_NODE) return undefined
+
+    const adapterType = (selectedNode as Node<Adapter>).data.type
+    return data?.items?.find((e) => e.id === adapterType)
+  }, [data, selectedNode, type])
 
   const EntityIcon = useMemo(() => {
     if (type === NodeTypes.BRIDGE_NODE)
       return (
         <Icon
           data-testid={'node-type-icon'}
-          data-nodeIcon={NodeTypes.BRIDGE_NODE}
+          data-nodeicon={NodeTypes.BRIDGE_NODE}
           as={PiBridgeThin}
-          fontSize={'20px'}
+          fontSize={'40px'}
         />
       )
-    return <Icon data-testid={'node-type-icon'} data-nodeIcon={NodeTypes.BRIDGE_NODE} as={PiPlugsConnectedFill} />
+    return <Icon data-testid={'node-type-icon'} data-nodeicon={NodeTypes.ADAPTER_NODE} as={PiPlugsConnectedFill} />
   }, [type])
 
   return (
     <Card size={'sm'} direction={'row'}>
       <CardBody>
         <HStack divider={<StackDivider />}>
-          {EntityIcon}
-          <Text>{t('workspace.device.type', { context: type })}</Text>
-          {type === NodeTypes.ADAPTER_NODE && <Text>{(selectedNode as Node<Adapter>).data.type}</Text>}
-          <Text noOfLines={1}>{selectedNode.data.id}</Text>
+          <VStack>
+            {EntityIcon}
+            <Tag data-testid={'node-type-text'} textTransform={'uppercase'}>
+              {t('workspace.device.type', { context: type })}{' '}
+            </Tag>
+          </VStack>
+
+          <VStack alignItems={'flex-start'}>
+            {adapterType && <Text data-testid={'node-adapter-type'}>{adapterType.name}</Text>}
+            <Text data-testid={'node-name'} noOfLines={1}>
+              {selectedNode.data.id}
+            </Text>
+          </VStack>
         </HStack>
       </CardBody>
     </Card>
