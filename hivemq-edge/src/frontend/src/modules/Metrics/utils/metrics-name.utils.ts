444a2bcf3efe3ca96f12c8db1e5dad4c03053944
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-11-18 17:10:52 +0000

    fix(17700): add type

--- hivemq-edge/src/frontend/src/modules/Metrics/utils/metrics-name.utils.ts
+++ hivemq-edge/src/frontend/src/modules/Metrics/utils/metrics-name.utils.ts
@@ -1,5 +1,12 @@
+export interface MetricInfo {
+  device: string
+  type: string
+  id: string
+  suffix: string
+}
+
 // TODO[NVL] This is a potentially dangerous approach as there is no guaranties that the pattern will be consistent with other type of adapters.
-export const extractMetricInfo = (metricName: string) => {
+export const extractMetricInfo = (metricName: string): MetricInfo => {
   const pruneLeading = metricName.replace('com.hivemq.edge.', '')
   const [device, type, id, ...metric] = pruneLeading.split('.')
   const suffix = metric.join('.')
