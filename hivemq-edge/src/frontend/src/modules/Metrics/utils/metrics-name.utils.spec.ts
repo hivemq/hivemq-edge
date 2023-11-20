79940701ede7716dc7ecda9072f2ea6479291470
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-11-20 19:20:56 +0000

    test(17700): add test for metric name parsing

--- hivemq-edge/src/frontend/src/modules/Metrics/utils/metrics-name.utils.spec.ts
+++ hivemq-edge/src/frontend/src/modules/Metrics/utils/metrics-name.utils.spec.ts
@@ -1,6 +1,13 @@
 import { describe, expect } from 'vitest'
 
 import { extractMetricInfo, MetricInfo } from './metrics-name.utils.ts'
+import {
+  MOCK_METRIC_ADAPTER,
+  MOCK_METRIC_BRIDGE,
+  MOCK_METRIC_MESSAGE,
+  MOCK_METRICS,
+} from '@/api/hooks/useGetMetrics/__handlers__'
+import { mockBridgeId } from '@/api/hooks/useGetBridges/__handlers__'
 
 interface MetricInfoSuite {
   metricName: string
@@ -10,12 +17,34 @@ interface MetricInfoSuite {
 describe('extractMetricInfo', () => {
   test.each<MetricInfoSuite>([
     {
-      metricName: 'com.hivemq.edge.protocol-adapters.modbus.fff.connection.success.count',
+      metricName: MOCK_METRIC_ADAPTER,
       expected: {
         device: 'protocol-adapters',
-        id: 'fff',
-        suffix: 'connection.success.count',
-        type: 'modbus',
+        id: 'my-adapter',
+        suffix: 'connection.failed.count',
+        type: 'opc-ua-client',
+      },
+    },
+    {
+      metricName: MOCK_METRIC_BRIDGE,
+      expected: {
+        device: 'bridge',
+        id: mockBridgeId,
+        suffix: 'forward.publish.count',
+      },
+    },
+    {
+      metricName: MOCK_METRIC_MESSAGE,
+      expected: {
+        device: 'messages',
+        suffix: 'dropped.count',
+      },
+    },
+    {
+      metricName: MOCK_METRICS[20].name as string,
+      expected: {
+        device: 'networking',
+        suffix: 'bytes.read.total',
       },
     },
   ])('should returns $expected.suffix with metricName', ({ metricName, expected }) => {
