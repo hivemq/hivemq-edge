f54968b3f04f7ef5b33e565ae2cf214a7141045b
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-11-21 13:58:06 +0000

    fix(18025): fix rebase

--- hivemq-edge/src/frontend/src/modules/App/routes.tsx
+++ hivemq-edge/src/frontend/src/modules/App/routes.tsx
@@ -19,95 +19,93 @@ import NodePanelController from '@/modules/EdgeVisualisation/components/controls
 
 // import EvenLogPage from '@/modules/EventLog/EvenLogPage.tsx'
 const EvenLogPage = React.lazy(() => import('@/modules/EventLog/EvenLogPage.tsx'))
-const NodePropertyDrawer = React.lazy(
-        () => import('@/modules/EdgeVisualisation/components/drawers/NodePropertyDrawer.tsx')
-)
+
 const EdgeFlowPage = React.lazy(() => import('@/modules/EdgeVisualisation/EdgeFlowPage.tsx'))
 const UnifiedNamespaceEditor = React.lazy(
-        () => import(/* webpackChunkName: "modulename"*/ '@/modules/UnifiedNamespace/components/UnifiedNamespaceEditor.tsx')
+  () => import(/* webpackChunkName: "modulename"*/ '@/modules/UnifiedNamespace/components/UnifiedNamespaceEditor.tsx')
 )
 const UnifiedNamespacePage = React.lazy(
-        () => import(/* webpackChunkName: "modulename"*/ '@/modules/UnifiedNamespace/UnifiedNamespacePage.tsx')
+  () => import(/* webpackChunkName: "modulename"*/ '@/modules/UnifiedNamespace/UnifiedNamespacePage.tsx')
 )
 const AdapterController = React.lazy(() => import('@/modules/ProtocolAdapters/components/AdapterController.tsx'))
 const BridgeEditor = React.lazy(() => import('@/modules/Bridges/components/panels/BridgeEditor.tsx'))
 const ProtocolAdapterPage = React.lazy(() => import('@/modules/ProtocolAdapters/ProtocolAdapterPage.tsx'))
 
 export const routes = createBrowserRouter(
-        [
-          {
-            path: '/',
-            element: <Dashboard />,
-            errorElement: <ErrorPage />,
-            children: [
-              {
-                path: '',
-                index: true,
-                element: <WelcomePage />,
-              },
-              {
-                path: 'mqtt-bridges/',
-                element: <BridgePage />,
-                children: [
-                  {
-                    path: 'new',
-                    element: <BridgeEditor isNew />,
-                  },
-                  {
-                    path: ':bridgeId',
-                    element: <BridgeEditor />,
-                  },
-                ],
-              },
+  [
+    {
+      path: '/',
+      element: <Dashboard />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: '',
+          index: true,
+          element: <WelcomePage />,
+        },
+        {
+          path: 'mqtt-bridges/',
+          element: <BridgePage />,
+          children: [
+            {
+              path: 'new',
+              element: <BridgeEditor isNew />,
+            },
+            {
+              path: ':bridgeId',
+              element: <BridgeEditor />,
+            },
+          ],
+        },
 
-              {
-                path: 'protocol-adapters/',
-                element: <ProtocolAdapterPage />,
-                children: [
-                  {
-                    path: 'new',
-                    element: <AdapterController isNew />,
-                  },
-                  {
-                    path: ':adapterId',
-                    element: <AdapterController />,
-                  },
-                ],
-              },
-              {
-                path: 'edge-flow/',
-                element: <EdgeFlowPage />,
-                children: [
-                  {
-                    path: ':nodeType/:nodeId',
-                    element: <NodePanelController />,
-                  },
-                ],
-              },
-              {
-                path: 'event-logs/',
-                element: <EvenLogPage />,
-              },
-              {
-                path: 'modules/',
-                element: <PageContainer />,
-              },
-              {
-                path: 'namespace/',
-                element: <UnifiedNamespacePage />,
-                children: [
-                  {
-                    path: 'edit',
-                    element: <UnifiedNamespaceEditor />,
-                  },
-                ],
-              },
-            ],
-          },
-          {
-            path: '/login',
-            element: <LoginPage />,
-          },
-        ],
-        { basename: '/app' }
+        {
+          path: 'protocol-adapters/',
+          element: <ProtocolAdapterPage />,
+          children: [
+            {
+              path: 'new',
+              element: <AdapterController isNew />,
+            },
+            {
+              path: ':adapterId',
+              element: <AdapterController />,
+            },
+          ],
+        },
+        {
+          path: 'edge-flow/',
+          element: <EdgeFlowPage />,
+          children: [
+            {
+              path: ':nodeType/:nodeId',
+              element: <NodePanelController />,
+            },
+          ],
+        },
+        {
+          path: 'event-logs/',
+          element: <EvenLogPage />,
+        },
+        {
+          path: 'modules/',
+          element: <PageContainer />,
+        },
+        {
+          path: 'namespace/',
+          element: <UnifiedNamespacePage />,
+          children: [
+            {
+              path: 'edit',
+              element: <UnifiedNamespaceEditor />,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      path: '/login',
+      element: <LoginPage />,
+    },
+  ],
+  { basename: '/app' }
 )
