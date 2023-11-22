1f0d2a652e469996408706f82735c1995b44a9ec
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-11-22 11:25:50 +0000

    feat(17781): add a reusable button badge component

---
+++ hivemq-edge/src/frontend/src/components/Chakra/ButtonBadge.tsx
@@ -0,0 +1,40 @@
+import { FC } from 'react'
+import { Avatar, AvatarBadge, AvatarProps, IconButton, Text } from '@chakra-ui/react'
+
+interface ButtonBadgeProps extends Omit<AvatarProps, 'aria-label'> {
+  badgeLabel?: string | number
+  isDisabled?: boolean
+  'aria-label': string
+}
+
+const ButtonBadge: FC<ButtonBadgeProps> = ({
+  ['aria-label']: ariaLabel,
+  badgeLabel,
+  icon,
+  isDisabled,
+  onClick,
+  ...props
+}) => {
+  // TODO[NVL] Not a good idea. Keep the button but disabled ?
+  const activeProps: Partial<Omit<AvatarProps, 'aria-label'>> = isDisabled
+    ? { backgroundColor: 'brand.200' }
+    : {
+        backgroundColor: 'brand.500',
+        as: IconButton,
+        _hover: { backgroundColor: 'brand.500' },
+        _active: { backgroundColor: 'brand.600' },
+        onClick: onClick,
+      }
+
+  return (
+    <Avatar aria-label={ariaLabel} isDisabled={isDisabled} icon={icon} bg="brand.400" {...activeProps} {...props}>
+      {badgeLabel && !isDisabled && (
+        <AvatarBadge borderColor="yellow.50" bg="yellow.500" boxSize="1.25em">
+          <Text fontSize={'.95rem'}>{badgeLabel}</Text>
+        </AvatarBadge>
+      )}
+    </Avatar>
+  )
+}
+
+export default ButtonBadge
