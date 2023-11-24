c648a72184ccbf8541dd27e3139dfcfadca9c5e7
Daniel Krueger <daniel.krueger@hivemq.com>
2023-11-24 17:21:04 +0100

    add new fields to Capability

--- hivemq-edge/src/main/java/com/hivemq/api/model/capabilities/Capability.java
+++ hivemq-edge/src/main/java/com/hivemq/api/model/capabilities/Capability.java
@@ -7,10 +7,23 @@ import io.swagger.v3.oas.annotations.media.Schema;
 public class Capability {
 
     @JsonProperty("id")
-    @Schema(description = "The clientIdentifier as known by the system")
+    @Schema(description = "The identifier of this capability")
     private final @NotNull String id;
 
-    public Capability(@JsonProperty("id") final @NotNull String id) {
+    @JsonProperty("displayName")
+    @Schema(description = "A human readable name, intended to be used to display at front end.")
+    private final @NotNull String displayName;
+
+    @JsonProperty("description")
+    @Schema(description = "A description for the capability")
+    private final @NotNull String description;
+
+    public Capability(@JsonProperty("id") final @NotNull String id,
+                      @JsonProperty("displayName") final @NotNull String displayName,
+                      @JsonProperty("description") final @NotNull String description) {
         this.id = id;
+        this.displayName = displayName;
+        this.description = description;
     }
 }
+
