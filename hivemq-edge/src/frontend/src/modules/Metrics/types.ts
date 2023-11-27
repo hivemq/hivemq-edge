e4b0387c25aed178f1b3b1d530ccb7f5890a9378
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-11-27 18:07:32 +0000

    feat(15311): add types

---
+++ hivemq-edge/src/frontend/src/modules/Metrics/types.ts
@@ -0,0 +1,28 @@
+export enum ChartType {
+  SAMPLE = 'SAMPLE',
+  LINE_CHART = 'LINE_CHART',
+  BAR_CHART = 'BAR_CHART',
+}
+
+export interface MetricNameOption {
+  label: string
+  value: string
+  isDisabled?: boolean
+}
+
+export interface ChartTypeOption {
+  label: string
+  value: ChartType
+}
+
+export interface MetricDefinition {
+  selectedTopic: MetricNameOption
+  selectedChart: ChartTypeOption
+}
+
+export interface MetricVisualisation {
+  metricNames: string[]
+  viewType: string
+  label: string
+  description?: string
+}
