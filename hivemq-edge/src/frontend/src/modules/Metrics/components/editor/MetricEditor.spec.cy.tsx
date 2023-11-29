e80803df73af15896c9610e2741afaa74d6a209c
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-11-29 10:53:03 +0000

    test(15311): fix tests for basic (stats) metrics

--- hivemq-edge/src/frontend/src/modules/Metrics/components/editor/MetricEditor.spec.cy.tsx
+++ hivemq-edge/src/frontend/src/modules/Metrics/components/editor/MetricEditor.spec.cy.tsx
@@ -5,6 +5,7 @@ import { MOCK_METRICS } from '@/api/hooks/useGetMetrics/__handlers__'
 import { mockBridgeId } from '@/api/hooks/useGetBridges/__handlers__'
 
 import MetricSelector from './MetricEditor.tsx'
+import { ChartType } from '@/modules/Metrics/types.ts'
 
 describe('MetricEditor', () => {
   beforeEach(() => {
@@ -12,49 +13,53 @@ describe('MetricEditor', () => {
     cy.intercept('/api/v1/metrics', { items: MOCK_METRICS } as MetricList).as('getMetrics')
   })
 
-  it('should render the selector', () => {
-    const onSubmit = cy.stub().as('onSubmit')
-    cy.mountWithProviders(
-      <MetricSelector
-        filter={mockBridgeId}
-        onSubmit={onSubmit}
-        selectedMetrics={[MOCK_METRICS[0].name as string, MOCK_METRICS[0].name as string]}
-      />
-    )
-    cy.get('div#react-select-2-placeholder').should('contain.text', 'Select...')
-    cy.get("button[type='submit']").should('be.disabled')
-    cy.get('input#metrics-select').click()
+  describe('Basic selector - default chart', () => {
+    it('should render the selector', () => {
+      const onSubmit = cy.stub().as('onSubmit')
+      cy.mountWithProviders(
+        <MetricSelector
+          filter={mockBridgeId}
+          onSubmit={onSubmit}
+          selectedChart={ChartType.SAMPLE}
+          selectedMetrics={[MOCK_METRICS[0].name as string, MOCK_METRICS[0].name as string]}
+        />
+      )
+      cy.get('div#react-select-2-placeholder').should('contain.text', 'Select...')
+      cy.get("button[type='submit']").should('be.disabled')
+      cy.get('input#metrics-select').click()
 
-    cy.get('div#react-select-2-listbox').find("[role='option']").should('have.length', 10)
-    cy.get('div#react-select-2-listbox')
-      .find("[role='option']")
-      .eq(3)
-      .should('have.text', '[Forward] Publish success (count)')
-      .should('have.attr', 'aria-disabled', 'true')
+      cy.get('div#react-select-2-listbox').find("[role='option']").should('have.length', 10)
+      cy.get('div#react-select-2-listbox')
+        .find("[role='option']")
+        .eq(3)
+        .should('have.text', '[Forward] Publish success (count)')
+        .should('have.attr', 'aria-disabled', 'true')
 
-    cy.get('div#metrics-container').should('contain.text', '[Local] Publish failed (count)')
-    cy.get('div#react-select-2-listbox').find("[role='option']").eq(5).click()
-    cy.get("button[type='submit']").should('not.be.disabled')
-    cy.get("button[type='submit']").click()
-    cy.get('@onSubmit').should('have.been.calledWithMatch', {
-      selectedTopic: {
-        label: '[Local] Publish failed (count)',
-        value: 'com.hivemq.edge.bridge.bridge-id-01.local.publish.failed.count',
-        isDisabled: false,
-      },
+      cy.get('div#metrics-container').should('contain.text', '[Local] Publish failed (count)')
+      cy.get('div#react-select-2-listbox').find("[role='option']").eq(5).click()
+      cy.get("button[type='submit']").should('not.be.disabled')
+      cy.get("button[type='submit']").click()
+      cy.get('@onSubmit').should('have.been.calledWithMatch', {
+        selectedTopic: {
+          label: '[Local] Publish failed (count)',
+          value: 'com.hivemq.edge.bridge.bridge-id-01.local.publish.failed.count',
+          isDisabled: false,
+        },
+      })
     })
-  })
 
-  it('should be accessible', () => {
-    cy.injectAxe()
-    cy.mountWithProviders(
-      <MetricSelector
-        filter={mockBridgeId}
-        onSubmit={cy.stub()}
-        selectedMetrics={[MOCK_METRICS[0].name as string, MOCK_METRICS[0].name as string]}
-      />
-    )
-    cy.checkAccessibility()
-    cy.percySnapshot('Component: MetricEditor')
+    it('should be accessible', () => {
+      cy.injectAxe()
+      cy.mountWithProviders(
+        <MetricSelector
+          filter={mockBridgeId}
+          onSubmit={cy.stub()}
+          selectedChart={ChartType.SAMPLE}
+          selectedMetrics={[MOCK_METRICS[0].name as string, MOCK_METRICS[0].name as string]}
+        />
+      )
+      cy.checkAccessibility()
+      cy.percySnapshot('Component: MetricEditor')
+    })
   })
 })
