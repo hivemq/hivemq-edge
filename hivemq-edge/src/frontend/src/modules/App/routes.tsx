dc58d76f8023bdd869c0a0eae96db481ce2d2448
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-12-01 13:27:14 +0000

    fix(18025): fix routes

--- hivemq-edge/src/frontend/src/modules/App/routes.tsx
+++ hivemq-edge/src/frontend/src/modules/App/routes.tsx
@@ -1,35 +1,24 @@
+/* eslint-disable react-refresh/only-export-components */
+import { lazy } from 'react'
 import { createBrowserRouter } from 'react-router-dom'
+
+import WelcomePage from '@/modules/Welcome/WelcomePage.tsx'
+import LoginPage from '@/modules/Login/LoginPage.tsx'
+
+const BridgePage = lazy(() => import('@/modules/Bridges/BridgePage.tsx'))
+const BridgeEditor = lazy(() => import('@/modules/Bridges/components/panels/BridgeEditor.tsx'))
+const ProtocolAdapterPage = lazy(() => import('@/modules/ProtocolAdapters/ProtocolAdapterPage.tsx'))
+const AdapterController = lazy(() => import('@/modules/ProtocolAdapters/components/AdapterController.tsx'))
+const UnifiedNamespaceEditor = lazy(() => import('@/modules/UnifiedNamespace/components/UnifiedNamespaceEditor.tsx'))
+const UnifiedNamespacePage = lazy(() => import('@/modules/UnifiedNamespace/UnifiedNamespacePage.tsx'))
+const EdgeFlowPage = lazy(() => import('@/modules/EdgeVisualisation/EdgeFlowPage.tsx'))
+const NodePanelController = lazy(
+  () => import('@/modules/EdgeVisualisation/components/controls/NodePanelController.tsx')
+)
+const EvenLogPage = lazy(() => import('@/modules/EventLog/EvenLogPage.tsx'))
 
 import Dashboard from '../Dashboard/Dashboard.tsx'
 import ErrorPage from './components/ErrorPage.tsx'
-import PageContainer from '../../components/PageContainer.tsx'
-
-import BridgePage from '@/modules/Bridges/BridgePage.tsx'
-// import ProtocolAdapterPage from '@/modules/ProtocolAdapters/ProtocolAdapterPage.tsx'
-// import BridgeEditor from '@/modules/Bridges/components/panels/BridgeEditor.tsx'
-// import AdapterController from '@/modules/ProtocolAdapters/components/AdapterController.tsx'
-// import UnifiedNamespacePage from '@/modules/UnifiedNamespace/UnifiedNamespacePage.tsx'
-import WelcomePage from '@/modules/Welcome/WelcomePage.tsx'
-import LoginPage from '@/modules/Login/LoginPage.tsx'
-// import UnifiedNamespaceEditor from '@/modules/UnifiedNamespace/components/UnifiedNamespaceEditor.tsx'
-// import EdgeFlowPage from '@/modules/EdgeVisualisation/EdgeFlowPage.tsx'
-// import NodePropertyDrawer from '@/modules/EdgeVisualisation/components/drawers/NodePropertyDrawer.tsx'
-import React from 'react'
-import NodePanelController from '@/modules/EdgeVisualisation/components/controls/NodePanelController.tsx'
-
-// import EvenLogPage from '@/modules/EventLog/EvenLogPage.tsx'
-const EvenLogPage = React.lazy(() => import('@/modules/EventLog/EvenLogPage.tsx'))
-
-const EdgeFlowPage = React.lazy(() => import('@/modules/EdgeVisualisation/EdgeFlowPage.tsx'))
-const UnifiedNamespaceEditor = React.lazy(
-  () => import(/* webpackChunkName: "modulename"*/ '@/modules/UnifiedNamespace/components/UnifiedNamespaceEditor.tsx')
-)
-const UnifiedNamespacePage = React.lazy(
-  () => import(/* webpackChunkName: "modulename"*/ '@/modules/UnifiedNamespace/UnifiedNamespacePage.tsx')
-)
-const AdapterController = React.lazy(() => import('@/modules/ProtocolAdapters/components/AdapterController.tsx'))
-const BridgeEditor = React.lazy(() => import('@/modules/Bridges/components/panels/BridgeEditor.tsx'))
-const ProtocolAdapterPage = React.lazy(() => import('@/modules/ProtocolAdapters/ProtocolAdapterPage.tsx'))
 
 export const routes = createBrowserRouter(
   [
@@ -85,10 +74,6 @@ export const routes = createBrowserRouter(
         {
           path: 'event-logs/',
           element: <EvenLogPage />,
-        },
-        {
-          path: 'modules/',
-          element: <PageContainer />,
         },
         {
           path: 'namespace/',
