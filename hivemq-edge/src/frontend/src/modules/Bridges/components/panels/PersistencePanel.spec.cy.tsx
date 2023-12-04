47ed235c5ae467b7bdea146215d7c24ea6e32297
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-12-04 14:11:53 +0000

    test(17821): update tests

--- hivemq-edge/src/frontend/src/modules/Bridges/components/panels/PersistencePanel.spec.cy.tsx
+++ hivemq-edge/src/frontend/src/modules/Bridges/components/panels/PersistencePanel.spec.cy.tsx
@@ -1,7 +1,37 @@
 /// <reference types="cypress" />
 
+import { FC } from 'react'
+import { useForm } from 'react-hook-form'
+
+import { Bridge } from '@/api/__generated__'
+import { mockBridge } from '@/api/hooks/useGetBridges/__handlers__'
 import { MOCK_CAPABILITY_PERSISTENCE } from '@/api/hooks/useFrontendServices/__handlers__'
+import ButtonCTA from '@/components/Chakra/ButtonCTA.tsx'
+
 import PersistencePanel from './PersistencePanel.tsx'
+
+interface TestingComponentProps {
+  onSubmit: (data: Bridge) => void
+  defaultValues: Bridge
+}
+
+const TestingComponent: FC<TestingComponentProps> = ({ onSubmit, defaultValues }) => {
+  const form = useForm<Bridge>({
+    mode: 'all',
+    criteriaMode: 'all',
+    defaultValues: defaultValues,
+  })
+  return (
+    <div>
+      <form id="bridge-form" onSubmit={form.handleSubmit(onSubmit)}>
+        <PersistencePanel form={form} hasPersistence={MOCK_CAPABILITY_PERSISTENCE} />
+      </form>
+      <ButtonCTA type={'submit'} form="bridge-form" data-testid={'form-submit'} mt={8}>
+        Submit
+      </ButtonCTA>
+    </div>
+  )
+}
 
 describe('PersistencePanel', () => {
   beforeEach(() => {
@@ -10,12 +40,15 @@ describe('PersistencePanel', () => {
 
   it('should be accessible', () => {
     cy.injectAxe()
-    cy.mountWithProviders(<PersistencePanel hasPersistence={MOCK_CAPABILITY_PERSISTENCE} />)
+    cy.mountWithProviders(<TestingComponent onSubmit={cy.stub} defaultValues={mockBridge} />)
 
-    cy.get('fieldset').should(
-      'contain.text',
-      'Mqtt Traffic with QoS greater than 0 is stored persistently on disc and loaded on restart of Edge.'
-    )
+    cy.get('#field-\\:r1\\:-helptext')
+      .should('be.visible')
+      .should(
+        'contain.text',
+        'Mqtt Traffic with QoS greater than 0 will be stored persistently on disc and loaded on restart of Edge'
+      )
+    cy.get('label').should('contain.text', 'Persist Data for MQTT traffic')
     cy.checkAccessibility()
     cy.percySnapshot('Component: PersistencePanel')
   })
