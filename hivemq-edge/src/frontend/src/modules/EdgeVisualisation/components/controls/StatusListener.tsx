9d5af8a68bea3200ff31d463a1a2b349aba25bb3
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-12-05 10:49:56 +0000

    fix(18092): fix update of status in tghe workspace - fix bug in determining status of nodes - add update for the edges status - refactor styling

--- hivemq-edge/src/frontend/src/modules/EdgeVisualisation/components/controls/StatusListener.tsx
+++ hivemq-edge/src/frontend/src/modules/EdgeVisualisation/components/controls/StatusListener.tsx
@@ -1,22 +1,35 @@
 import { useEffect } from 'react'
 import { useReactFlow } from 'reactflow'
+import { useTheme } from '@chakra-ui/react'
 
 import { useGetAdaptersStatus } from '@/api/hooks/useConnection/useGetAdaptersStatus.tsx'
 import { useGetBridgesStatus } from '@/api/hooks/useConnection/useGetBridgesStatus.tsx'
+import { useGetAdapterTypes } from '@/api/hooks/useProtocolAdapters/useGetAdapterTypes.tsx'
 
-import { updateNodeStatus } from '../../utils/status-utils.ts'
+import { updateEdgesStatus, updateNodeStatus } from '../../utils/status-utils.ts'
 
 const StatusListener = () => {
   const { data: adapterConnections } = useGetAdaptersStatus()
   const { data: bridgeConnections } = useGetBridgesStatus()
-  const { setNodes } = useReactFlow()
+  const { data: adapterTypes } = useGetAdapterTypes()
+  const { setNodes, getNode, setEdges } = useReactFlow()
+  const theme = useTheme()
 
   useEffect(() => {
-    if (adapterConnections?.items && bridgeConnections?.items) {
-      const updates = [...adapterConnections.items, ...bridgeConnections.items]
-      setNodes((currentNodes) => updateNodeStatus(currentNodes, updates))
+    const { items } = adapterTypes || {}
+    if (adapterConnections?.items || bridgeConnections?.items) {
+      const updates = [...(adapterConnections?.items || []), ...(bridgeConnections?.items || [])]
+
+      setNodes((currentNodes) => {
+        return updateNodeStatus(currentNodes, updates)
+      })
+
+      if (items)
+        setEdges((currentEdges) => {
+          return updateEdgesStatus(items, currentEdges, updates, getNode, theme)
+        })
     }
-  }, [adapterConnections, bridgeConnections, setNodes])
+  }, [adapterConnections, adapterTypes, bridgeConnections, getNode, setEdges, setNodes, theme])
 
   return null
 }
--- hivemq-edge/src/frontend/src/modules/EdgeVisualisation/utils/status-utils.ts
+++ hivemq-edge/src/frontend/src/modules/EdgeVisualisation/utils/status-utils.ts
@@ -1,9 +1,12 @@
-import { Node } from 'reactflow'
+import { Edge, Instance, MarkerType, Node } from 'reactflow'
+import { GenericObjectType } from '@rjsf/utils'
 import { WithCSSVar } from '@chakra-ui/react'
 import { Dict } from '@chakra-ui/utils'
 
-import { Adapter, Bridge, Status } from '@/api/__generated__'
-import { NodeTypes } from '@/modules/EdgeVisualisation/types.ts'
+import { Adapter, Bridge, ProtocolAdapter, Status } from '@/api/__generated__'
+
+import { NodeTypes } from '../types.ts'
+import { discoverAdapterTopics, getBridgeTopics } from './topics-utils.ts'
 
 /**
  * @param theme
@@ -44,16 +47,64 @@ export const updateNodeStatus = (currentNodes: Node[], updates: Status[]) => {
       const newData = { ...n.data } as Adapter
       const newStatus = updates.find((s) => s.id === newData.id)
       if (!newStatus) return n
-      if (newStatus.connection === newData.status?.connection) return n
+      // if (newStatus.connection === newData.status?.connection) return n
 
       n.data = {
         ...newData,
-        status: {
-          connection: newStatus.connection,
-        },
+        status: { ...newStatus },
       }
       return n
     }
     return n
+  })
+}
+
+export type EdgeStyle = Pick<Edge, 'style' | 'animated' | 'markerEnd'>
+
+export const getEdgeStatus = (isConnected: boolean, hasTopics: boolean, themeForStatus: string): EdgeStyle => {
+  const edge: EdgeStyle = {}
+  edge.style = {
+    strokeWidth: isConnected ? 1.5 : 0.5,
+    stroke: themeForStatus,
+  }
+  edge.animated = isConnected && hasTopics
+  edge.markerEnd = {
+    type: MarkerType.ArrowClosed,
+    width: 20,
+    height: 20,
+    color: themeForStatus,
+  }
+  return edge
+}
+
+export const updateEdgesStatus = (
+  adapterTypes: ProtocolAdapter[],
+  currentEdges: Edge[],
+  updates: Status[],
+  getNode: Instance.GetNode<Partial<Bridge | Adapter>>,
+  theme: Partial<WithCSSVar<Dict>>
+): Edge[] => {
+  return currentEdges.map((edge) => {
+    const [a, b] = edge.source.split('@')
+    const status = updates.find((e) => e.id === b && e.type === a)
+    if (!status) return edge
+
+    const source = getNode(edge.source)
+    const isConnected =
+      status?.connection === Status.connection.CONNECTED ||
+      (status?.runtime === Status.runtime.STARTED && status?.connection === Status.connection.STATELESS)
+
+    if (source && source.type === NodeTypes.ADAPTER_NODE) {
+      const type = adapterTypes?.find((e) => e.id === (source.data as Adapter).type)
+      const topics = type ? discoverAdapterTopics(type, (source.data as Adapter).config as GenericObjectType) : []
+
+      return { ...edge, ...getEdgeStatus(isConnected, !!topics.length, getThemeForStatus(theme, status)) }
+    }
+
+    if (source && source.type === NodeTypes.BRIDGE_NODE) {
+      const { remote } = getBridgeTopics(source.data as Bridge)
+      return { ...edge, ...getEdgeStatus(isConnected, !!remote.length, getThemeForStatus(theme, status)) }
+    }
+    return edge
   })
 }
