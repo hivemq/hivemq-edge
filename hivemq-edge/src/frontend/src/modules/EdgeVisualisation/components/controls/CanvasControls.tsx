6420466f29b72751527c393caa0f1eb6f1c277fd
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-12-05 19:09:23 +0000

    fix(18150): fix translations

--- hivemq-edge/src/frontend/src/modules/EdgeVisualisation/components/controls/CanvasControls.tsx
+++ hivemq-edge/src/frontend/src/modules/EdgeVisualisation/components/controls/CanvasControls.tsx
@@ -1,14 +1,17 @@
 import { FC } from 'react'
+import { useTranslation } from 'react-i18next'
 import { ControlButton, Controls } from 'reactflow'
-import { useEdgeFlowContext } from '@/modules/EdgeVisualisation/hooks/useEdgeFlowContext.tsx'
 import { IoMdOptions } from 'react-icons/io'
 
+import { useEdgeFlowContext } from '../../hooks/useEdgeFlowContext.tsx'
+
 const CanvasControls: FC = () => {
+  const { t } = useTranslation()
   const { optionDrawer } = useEdgeFlowContext()
 
   return (
     <Controls>
-      <ControlButton onClick={() => optionDrawer.onOpen()} title="configuration">
+      <ControlButton onClick={() => optionDrawer.onOpen()} title={t('workspace.configuration.header') as string}>
         <IoMdOptions />
       </ControlButton>
     </Controls>
