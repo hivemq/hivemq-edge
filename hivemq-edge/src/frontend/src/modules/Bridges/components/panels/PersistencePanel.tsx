c3a32052f290f3b0fa1772516a015bc033ef577d
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-12-05 18:04:33 +0000

    fix(17821): add restart warning upon change

--- hivemq-edge/src/frontend/src/modules/Bridges/components/panels/PersistencePanel.tsx
+++ hivemq-edge/src/frontend/src/modules/Bridges/components/panels/PersistencePanel.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react'
 import { useTranslation } from 'react-i18next'
-import { Checkbox, FormControl, FormErrorMessage, FormHelperText } from '@chakra-ui/react'
+import { Alert, AlertIcon, Checkbox, FormControl, FormErrorMessage, FormHelperText } from '@chakra-ui/react'
 
 import { Capability } from '@/api/__generated__'
 import { BridgePanelType } from '../../types.ts'
@@ -13,17 +13,25 @@ const PersistencePanel: FC<PersistencePanelType> = ({ form }) => {
   const { t } = useTranslation()
   const {
     register,
-    formState: { errors },
+    formState: { errors, dirtyFields },
   } = form
 
   return (
-    <FormControl variant={'hivemq'} flexGrow={1} display={'flex'} flexDirection={'column'} gap={4} as={'fieldset'}>
-      <FormControl isInvalid={!!errors.persist}>
-        <Checkbox {...register('persist')}>{t('bridge.persistence.persist.label')}</Checkbox>
-        <FormHelperText>{t('bridge.persistence.persist.helper')}</FormHelperText>
-        <FormErrorMessage>{errors.persist && errors.persist.message}</FormErrorMessage>
+    <>
+      {dirtyFields.persist && (
+        <Alert status="info" mb={4}>
+          <AlertIcon />
+          {t('bridge.persistence.restartWarning')}
+        </Alert>
+      )}
+      <FormControl variant={'hivemq'} flexGrow={1} display={'flex'} flexDirection={'column'} gap={4} as={'fieldset'}>
+        <FormControl isInvalid={!!errors.persist}>
+          <Checkbox {...register('persist')}>{t('bridge.persistence.persist.label')}</Checkbox>
+          <FormHelperText>{t('bridge.persistence.persist.helper')}</FormHelperText>
+          <FormErrorMessage>{errors.persist && errors.persist.message}</FormErrorMessage>
+        </FormControl>
       </FormControl>
-    </FormControl>
+    </>
   )
 }
 
