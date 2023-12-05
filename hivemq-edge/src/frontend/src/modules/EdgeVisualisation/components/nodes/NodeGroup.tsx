afb37672e4cb46e02db596e094eb1f41bc97c0b9
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-12-05 19:25:22 +0000

    refactor(18150): refactor the group node - add open/close control - update styling and layout

--- hivemq-edge/src/frontend/src/modules/EdgeVisualisation/components/nodes/NodeGroup.tsx
+++ hivemq-edge/src/frontend/src/modules/EdgeVisualisation/components/nodes/NodeGroup.tsx
@@ -1,28 +1,51 @@
 import { FC } from 'react'
-import { Handle, NodeProps, Position } from 'reactflow'
-import { Box, useTheme, VisuallyHidden } from '@chakra-ui/react'
+import { Handle, NodeProps, NodeResizer, NodeToolbar, Position } from 'reactflow'
 import { useNavigate } from 'react-router-dom'
+import { Box, Button, Text, useTheme, VStack } from '@chakra-ui/react'
 
-const NodeGroup: FC<NodeProps<string>> = ({ id, data }) => {
+import { Group } from '../../types.ts'
+import useWorkspaceStore from '../../utils/store.ts'
+import { useTranslation } from 'react-i18next'
+
+const NodeGroup: FC<NodeProps<Group>> = ({ id, data, selected }) => {
+  const { t } = useTranslation()
   const navigate = useNavigate()
   const { colors } = useTheme()
+  const { onToggleGroup } = useWorkspaceStore()
 
   return (
     <>
+      <NodeToolbar isVisible={selected} position={Position.Top}>
+        <VStack alignItems={'flex-start'}>
+          <Button
+            size={'xs'}
+            onClick={() => {
+              data.isOpen = !data.isOpen
+              onToggleGroup({ id, data }, data.isOpen)
+            }}
+          >
+            {data.isOpen ? t('workspace.grouping.command.close') : t('workspace.grouping.command.open')}
+          </Button>
+        </VStack>
+      </NodeToolbar>
+      <NodeResizer isVisible={true} minWidth={180} minHeight={100} />
+
       <Box
         w={'100%'}
         h={'100%'}
         style={{
-          backgroundColor: colors.red[500],
-          borderRadius: '100%',
-          opacity: 0.05,
-          borderColor: 'red',
+          backgroundColor: data.isOpen ? undefined : colors.red[50],
+          // borderRadius: '100%',
+          // opacity: 0.05,
+          // borderColor: 'red',
           borderWidth: 3,
           borderStyle: 'solid',
         }}
         onDoubleClick={() => navigate(`/edge-flow/group/${id}`)}
       >
-        <VisuallyHidden>{data}</VisuallyHidden>
+        <Text m={2} color={'blackAlpha.900'}>
+          {data.title}
+        </Text>
       </Box>
       <Handle type="source" position={Position.Bottom} id="a" />
     </>
