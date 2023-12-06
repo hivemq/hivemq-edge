3662b85d1fc378cd688923f61a509d7186c1ee98
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-12-06 14:36:05 +0000

    test(18150): add tests for the workspace store

---
+++ hivemq-edge/src/frontend/src/modules/EdgeVisualisation/hooks/useWorkspaceStore.spec.ts
@@ -0,0 +1,20 @@
+import { expect } from 'vitest'
+import { renderHook } from '@testing-library/react'
+
+import { server } from '@/__test-utils__/msw/mockServer.ts'
+import { handlers } from '@/api/hooks/useEvents/__handlers__'
+
+import { RFState } from '../types.ts'
+import useWorkspaceStore from './useWorkspaceStore.ts'
+
+describe('useWorkspaceStore', () => {
+  it('should start with an empty store', async () => {
+    server.use(...handlers)
+
+    const { result } = renderHook<RFState, any>(useWorkspaceStore)
+    const { nodes, edges } = result.current
+
+    expect(nodes).toHaveLength(0)
+    expect(edges).toHaveLength(0)
+  })
+})
