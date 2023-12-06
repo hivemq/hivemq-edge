0d55dde44a884d4b703ba45915f4a7f7677f7d11
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-12-06 09:51:40 +0000

    Merge pull request #205
    feat(17781): Notification framework and new release notice
    
    * feat(17781): add a reusable button badge component
    
    * test(17781): add tests for the button badge
    
    * feat(17781): add react query hook for notifications
    
    * feat(17781): add react query hook for github releases
    
    * feat(17781): add a notification badge component
    
    * feat(17781): add a hook for managing the notification
    
    * feat(17781): update translations
    
    * feat(17781): add the notification badge to the navigation panel
    
    * fix(17781): fix dependencies
    
    * refactor(17781): export the skip notification component
    
    * refactor(17781): change badge props
    
    * refactor(17781): add "notification viewed" flags
    
    * refactor(17781): add mocks
    
    * refactor(17781): add success and read flags to the hook's return
    
    * test(17781): add tests for the hook
    
    * fix(17781): fix translations
    
    * test(17781): add tests
    
    * chore(17781): fix cypress tconfig
    
    * refactor(17781): refactor the notification to handle skip and localst…
    
    * test(17781): add tests
    
    * fix(17781): change icon for the notification badge

---
+++ hivemq-edge/src/frontend/src/components/Chakra/ButtonBadge.tsx
@@ -0,0 +1,40 @@
+import { FC } from 'react'
+import { Avatar, AvatarBadge, AvatarProps, IconButton, Text } from '@chakra-ui/react'
+
+interface ButtonBadgeProps extends Omit<AvatarProps, 'aria-label'> {
+  badgeCount?: number
+  isDisabled?: boolean
+  'aria-label': string
+}
+
+const ButtonBadge: FC<ButtonBadgeProps> = ({
+  ['aria-label']: ariaLabel,
+  badgeCount,
+  icon,
+  isDisabled,
+  onClick,
+  ...props
+}) => {
+  // TODO[NVL] Not a good idea. Keep the button but disabled ?
+  const activeProps: Partial<Omit<AvatarProps, 'aria-label'>> = isDisabled
+    ? { backgroundColor: 'brand.200' }
+    : {
+        backgroundColor: 'brand.500',
+        as: IconButton,
+        _hover: { backgroundColor: 'brand.500' },
+        _active: { backgroundColor: 'brand.600' },
+        onClick: onClick,
+      }
+
+  return (
+    <Avatar aria-label={ariaLabel} isDisabled={isDisabled} icon={icon} bg="brand.400" {...activeProps} {...props}>
+      {((badgeCount && !isDisabled) || !isDisabled) && (
+        <AvatarBadge borderColor="yellow.50" bg="yellow.500" boxSize="1.25em" data-testid={'buttonBadge-counter'}>
+          <Text fontSize={'.95rem'}>{badgeCount}</Text>
+        </AvatarBadge>
+      )}
+    </Avatar>
+  )
+}
+
+export default ButtonBadge
---
+++ hivemq-edge/src/frontend/src/modules/Notifications/NotificationBadge.tsx
@@ -0,0 +1,44 @@
+import { FC } from 'react'
+import { useTranslation } from 'react-i18next'
+import { IoNotifications } from 'react-icons/io5'
+import { Icon, useToast, UseToastOptions } from '@chakra-ui/react'
+
+import ButtonBadge from '@/components/Chakra/ButtonBadge.tsx'
+
+import { useGetManagedNotifications } from './hooks/useGetManagedNotifications.tsx'
+import { SkipNotification } from './components/SkipNotification.tsx'
+
+const NotificationBadge: FC = () => {
+  const { t } = useTranslation()
+  const toast = useToast()
+  const { notifications } = useGetManagedNotifications()
+
+  const handleClick = () => {
+    notifications.forEach((notification) => {
+      if (notification.id && !toast.isActive(notification.id)) {
+        const notificationWithSkip: UseToastOptions = {
+          ...notification,
+          description: (
+            <>
+              {notification.description}
+              <SkipNotification id={notification.id.toString()} />
+            </>
+          ),
+        }
+        toast(notificationWithSkip)
+      }
+    })
+  }
+
+  return (
+    <ButtonBadge
+      aria-label={t('notifications.badge.ariaLabel')}
+      badgeCount={notifications.length}
+      isDisabled={!notifications.length}
+      icon={<Icon as={IoNotifications} fontSize={'2xl'} />}
+      onClick={handleClick}
+    />
+  )
+}
+
+export default NotificationBadge
