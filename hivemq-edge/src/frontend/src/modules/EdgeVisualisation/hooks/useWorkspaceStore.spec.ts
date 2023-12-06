9695814112bfd29b9d66df0a7d84758b176c8a3d
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-12-06 15:16:51 +0000

    test(18150): add tests for the workspace store

--- hivemq-edge/src/frontend/src/modules/EdgeVisualisation/hooks/useWorkspaceStore.spec.ts
+++ hivemq-edge/src/frontend/src/modules/EdgeVisualisation/hooks/useWorkspaceStore.spec.ts
@@ -1,20 +1,54 @@
 import { expect } from 'vitest'
-import { renderHook } from '@testing-library/react'
+import { act, renderHook } from '@testing-library/react'
+import { NodeAddChange, EdgeAddChange, Node, Edge } from 'reactflow'
 
-import { server } from '@/__test-utils__/msw/mockServer.ts'
-import { handlers } from '@/api/hooks/useEvents/__handlers__'
-
-import { RFState } from '../types.ts'
+import { WorkspaceAction, WorkspaceState } from '../types.ts'
 import useWorkspaceStore from './useWorkspaceStore.ts'
+import { MOCK_NODE_ADAPTER } from '@/__test-utils__/react-flow/nodes.ts'
 
 describe('useWorkspaceStore', () => {
-  it('should start with an empty store', async () => {
-    server.use(...handlers)
+  beforeEach(() => {
+    const { result } = renderHook<WorkspaceState & WorkspaceAction, unknown>(useWorkspaceStore)
+    act(() => {
+      result.current.reset()
+    })
+  })
 
-    const { result } = renderHook<RFState, unknown>(useWorkspaceStore)
+  it('should start with an empty store', async () => {
+    const { result } = renderHook<WorkspaceState & WorkspaceAction, unknown>(useWorkspaceStore)
     const { nodes, edges } = result.current
 
     expect(nodes).toHaveLength(0)
     expect(edges).toHaveLength(0)
+  })
+
+  it('should change nodes', async () => {
+    const { result } = renderHook<WorkspaceState & WorkspaceAction, unknown>(useWorkspaceStore)
+    expect(result.current.nodes).toHaveLength(0)
+    expect(result.current.edges).toHaveLength(0)
+
+    act(() => {
+      const { onNodesChange } = result.current
+      const item: Partial<Node> = { ...MOCK_NODE_ADAPTER, position: { x: 0, y: 0 } }
+      onNodesChange([{ item, type: 'add' } as NodeAddChange])
+    })
+
+    expect(result.current.nodes).toHaveLength(1)
+    expect(result.current.edges).toHaveLength(0)
+  })
+
+  it('should change edges', async () => {
+    const { result } = renderHook<WorkspaceState & WorkspaceAction, unknown>(useWorkspaceStore)
+    expect(result.current.nodes).toHaveLength(0)
+    expect(result.current.edges).toHaveLength(0)
+
+    act(() => {
+      const { onEdgesChange } = result.current
+      const item: Partial<Edge> = { id: '1-2', source: '1', target: '2' }
+      onEdgesChange([{ item, type: 'add' } as EdgeAddChange])
+    })
+
+    expect(result.current.nodes).toHaveLength(0)
+    expect(result.current.edges).toHaveLength(1)
   })
 })
