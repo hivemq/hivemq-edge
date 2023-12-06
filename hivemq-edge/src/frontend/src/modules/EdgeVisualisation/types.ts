bf9c58ea77621836366e2ea9943270e7e2a6003f
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-12-06 16:10:04 +0000

    refactor(18150): rename group action - remove unused action

--- hivemq-edge/src/frontend/src/modules/EdgeVisualisation/types.ts
+++ hivemq-edge/src/frontend/src/modules/EdgeVisualisation/types.ts
@@ -1,4 +1,4 @@
-import { Edge, Node, OnConnect, OnEdgesChange, OnNodesChange, NodeAddChange, EdgeAddChange } from 'reactflow'
+import { Edge, Node, OnEdgesChange, OnNodesChange, NodeAddChange, EdgeAddChange } from 'reactflow'
 
 export interface EdgeFlowOptions {
   showTopics: boolean
@@ -60,10 +60,9 @@ export interface WorkspaceAction {
   reset: () => void
   onNodesChange: OnNodesChange
   onEdgesChange: OnEdgesChange
-  onConnect: OnConnect
   onAddNodes: (changes: NodeAddChange[]) => void
   onAddEdges: (changes: EdgeAddChange[]) => void
-  onInsertNode: (node: Node<Group, NodeTypes.CLUSTER_NODE>, edge: Edge) => void
+  onInsertGroupNode: (node: Node<Group, NodeTypes.CLUSTER_NODE>, edge: Edge) => void
   onDeleteNode: (type: NodeTypes, adapterId: string) => void
   onToggleGroup: (node: Pick<Node<Group, NodeTypes.CLUSTER_NODE>, 'id' | 'data'>, show: boolean) => void
 }
