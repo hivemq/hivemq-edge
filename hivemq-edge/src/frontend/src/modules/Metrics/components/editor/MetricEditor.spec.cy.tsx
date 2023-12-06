4b24b2bf276cf84948ba581b237e7b7af4944732
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-12-06 14:35:39 +0000

    test(18150): add tests for multiple sets

--- hivemq-edge/src/frontend/src/modules/Metrics/components/editor/MetricEditor.spec.cy.tsx
+++ hivemq-edge/src/frontend/src/modules/Metrics/components/editor/MetricEditor.spec.cy.tsx
@@ -1,11 +1,12 @@
 /// <reference types="cypress" />
 
+import { MOCK_ADAPTER_ID } from '@/__test-utils__/mocks.ts'
 import { MetricList } from '@/api/__generated__'
 import { MOCK_METRICS } from '@/api/hooks/useGetMetrics/__handlers__'
 import { mockBridgeId } from '@/api/hooks/useGetBridges/__handlers__'
 
+import { ChartType } from '../../types.ts'
 import MetricSelector from './MetricEditor.tsx'
-import { ChartType } from '@/modules/Metrics/types.ts'
 
 describe('MetricEditor', () => {
   beforeEach(() => {
@@ -27,6 +28,8 @@ describe('MetricEditor', () => {
       cy.get('div#react-select-2-placeholder').should('contain.text', 'Select...')
       cy.get("button[type='submit']").should('be.disabled')
       cy.get('input#metrics-select').click()
+
+      cy.get('div#react-select-2-group-0-heading').should('not.exist')
 
       cy.get('div#react-select-2-listbox').find("[role='option']").should('have.length', 10)
       cy.get('div#react-select-2-listbox')
@@ -59,6 +62,34 @@ describe('MetricEditor', () => {
       )
       cy.checkAccessibility()
       cy.percySnapshot('Component: MetricEditor')
+    })
+  })
+
+  describe('Multi-metrics selector - default chart', () => {
+    it('should render the selector', () => {
+      const onSubmit = cy.stub().as('onSubmit')
+      cy.mountWithProviders(
+        <MetricSelector
+          filter={[mockBridgeId, MOCK_ADAPTER_ID]}
+          onSubmit={onSubmit}
+          selectedChart={ChartType.SAMPLE}
+          selectedMetrics={[MOCK_METRICS[0].name as string, MOCK_METRICS[1].name as string]}
+        />
+      )
+
+      cy.get('div#react-select-4-placeholder').should('contain.text', 'Select...')
+      cy.get("button[type='submit']").should('be.disabled')
+      cy.get('input#metrics-select').click()
+
+      cy.get('div#react-select-4-listbox').find("[role='option']").should('have.length', 19)
+      cy.get('div#react-select-4-listbox')
+        .find("[role='option']")
+        .eq(3)
+        .should('have.text', '[Forward] Publish success (count)')
+        .should('have.attr', 'aria-disabled', 'true')
+
+      cy.get('div#react-select-4-group-0-heading').should('contain.text', 'bridge-id-01')
+      cy.get('div#react-select-4-group-1-heading').should('contain.text', 'my-adapter')
     })
   })
 })
