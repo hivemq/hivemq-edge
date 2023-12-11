6fc6bf86e884a735758dc269444539a9a211b1bf
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-12-11 11:09:35 +0000

    style(18246): update style of wrapper node

--- hivemq-edge/src/frontend/src/modules/Workspace/components/parts/NodeWrapper.tsx
+++ hivemq-edge/src/frontend/src/modules/Workspace/components/parts/NodeWrapper.tsx
@@ -10,26 +10,17 @@ const NodeWrapper: FC<NodeWrapperProps> = ({ children, isSelected = false, ...re
 
   const selectedStyle: Partial<BoxProps> = {
     boxShadow: 'dark-lg',
-    rounded: 'md',
     bg: '#dddfe2',
   }
 
   return (
     <VStack
-      p={'24px'}
+      p={6}
+      rounded={'md'}
       borderColor={'#bec3c9'}
       backgroundColor={colors.white}
-      border={'1px'}
+      border={`1px`}
       _hover={{ bg: '#ebedf0' }}
-      _active={{
-        border: '2px',
-        bg: '#dddfe2',
-        transform: 'scale(0.98)',
-        borderColor: '#bec3c9',
-      }}
-      _focus={{
-        boxShadow: '0 0 10px 2px rgba(88, 144, 255, .75), 0 1px 1px rgba(0, 0, 0, .15)',
-      }}
       {...(isSelected ? { ...selectedStyle } : {})}
       {...rest}
     >
