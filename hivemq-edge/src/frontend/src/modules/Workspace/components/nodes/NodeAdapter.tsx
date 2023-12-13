bf462037acbedf438eab8d85336d329a53126c22
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-12-13 16:20:40 +0000

    Merge pull request #240
    fix(18293): Change mouse and keyboard interactions with nodes
    
    * fix(18293): add context menu hooks for the nodes
    
    * fix(18293): add navigation to panel on both double click and context …
    
    * fix(18293): change navigation to panel on click
    
    * refactor(18293): refactor the toolbar for the groups
    
    * test(18293): fix test
    
    * fix(18293): fix translations
    
    * test(18293): add tests
    
    * fix(18293): add keyboard access to the node's panel

--- hivemq-edge/src/frontend/src/modules/Workspace/components/nodes/NodeAdapter.tsx
+++ hivemq-edge/src/frontend/src/modules/Workspace/components/nodes/NodeAdapter.tsx
@@ -1,7 +1,6 @@
 import { FC, useMemo } from 'react'
 import { Handle, NodeProps, Position } from 'reactflow'
 import { Box, HStack, Image, Text, VStack } from '@chakra-ui/react'
-import { useNavigate } from 'react-router-dom'
 
 import { Adapter } from '@/api/__generated__'
 import { useGetAdapterTypes } from '@/api/hooks/useProtocolAdapters/useGetAdapterTypes.tsx'
@@ -12,24 +11,26 @@ import TopicsContainer from '../parts/TopicsContainer.tsx'
 import { discoverAdapterTopics } from '../../utils/topics-utils.ts'
 import { CONFIG_ADAPTER_WIDTH } from '../../utils/nodes-utils.ts'
 import { useEdgeFlowContext } from '../../hooks/useEdgeFlowContext.tsx'
+import { useContextMenu } from '../../hooks/useContextMenu.tsx'
 import { TopicFilter } from '../../types.ts'
 
 const NodeAdapter: FC<NodeProps<Adapter>> = ({ id, data: adapter, selected }) => {
   const { data: protocols } = useGetAdapterTypes()
   const adapterProtocol = protocols?.items?.find((e) => e.id === adapter.type)
   const { options } = useEdgeFlowContext()
-  const navigate = useNavigate()
   const topics = useMemo<TopicFilter[]>(() => {
     if (!adapterProtocol) return []
     if (!adapter.config) return []
     return discoverAdapterTopics(adapterProtocol, adapter.config).map((e) => ({ topic: e }))
   }, [adapter.config, adapterProtocol])
+  const { onContextMenu } = useContextMenu(id, selected, '/edge-flow/node')
 
   return (
     <>
       <NodeWrapper
         isSelected={selected}
-        onDoubleClick={() => navigate(`/edge-flow/node/${id}`)}
+        onDoubleClick={onContextMenu}
+        onContextMenu={onContextMenu}
         w={CONFIG_ADAPTER_WIDTH}
         p={2}
       >
