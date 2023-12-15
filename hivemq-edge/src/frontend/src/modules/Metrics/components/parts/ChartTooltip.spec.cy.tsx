aabee7204b477136dced6112772df89d35b3b811
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-12-15 10:07:54 +0000

    test(18077): fix test

--- hivemq-edge/src/frontend/src/modules/Metrics/components/parts/ChartTooltip.spec.cy.tsx
+++ hivemq-edge/src/frontend/src/modules/Metrics/components/parts/ChartTooltip.spec.cy.tsx
@@ -9,14 +9,23 @@ describe('ChartTooltip', () => {
   })
 
   it('should be accessible', () => {
+    const mockTime = DateTime.fromMillis(1000)
     cy.injectAxe()
-
     cy.mountWithProviders(
-      <ChartTooltip formattedValue={'the value'} color={'red.300'} date={DateTime.fromMillis(1000)} id={'the task'} />
+      <ChartTooltip formattedValue={'the value'} color={'red.300'} date={mockTime} id={'the task'} />
     )
 
+    const formatShortDate = new Intl.DateTimeFormat(navigator.language, {
+      month: 'short',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+    })
+    const shortDate = formatShortDate.format(mockTime.toJSDate())
+
     cy.getByTestId('chart-tooltip-id').should('contain.text', 'the task')
-    cy.getByTestId('date-time-full').should('contain.text', '1 Jan, 01:00:01')
+    cy.getByTestId('date-time-full').should('contain.text', shortDate)
     cy.getByTestId('chart-tooltip-value').should('contain.text', 'the value')
 
     cy.checkAccessibility()
