526f5d7be8ebe14534770e4fad093dfff5ca8d7f
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-12-15 09:10:21 +0000

    refactor(18077): refactor the tooltip

--- hivemq-edge/src/frontend/src/modules/Metrics/components/parts/ChartTooltip.tsx
+++ hivemq-edge/src/frontend/src/modules/Metrics/components/parts/ChartTooltip.tsx
@@ -1,14 +1,9 @@
 import { FC } from 'react'
-import { Badge, Card, Text } from '@chakra-ui/react'
+import { Card, HStack, Square, Text } from '@chakra-ui/react'
 import DateTimeRenderer from '@/components/DateTime/DateTimeRenderer.tsx'
 import { DateTime } from 'luxon'
-import { PointTooltipProps } from '@nivo/line'
-import { BarTooltipProps } from '@nivo/bar'
 
 interface TooltipProps {
-  d?: PointTooltipProps
-  f?: BarTooltipProps<unknown>
-
   color: string
   id: string | number
   date: DateTime
@@ -18,11 +13,15 @@ interface TooltipProps {
 const ChartTooltip: FC<TooltipProps> = ({ formattedValue, color, date, id }) => {
   return (
     <Card p={1} data-testid={'chart-tooltip'}>
-      <Badge backgroundColor={color} color={'white'}>
-        {id}
-      </Badge>
+      <HStack data-testid={'chart-tooltip-id'}>
+        <Square size={4} bg={color} />
+        <Text>{id}</Text>
+      </HStack>
+
       <DateTimeRenderer date={date} isShort />
-      <Text fontWeight={'bold'}>{formattedValue}</Text>
+      <Text fontWeight={'bold'} data-testid={'chart-tooltip-value'}>
+        {formattedValue}
+      </Text>
     </Card>
   )
 }
