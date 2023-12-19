d1dd8932b34b9fa291105288a4a773d6fff98669
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-12-19 11:05:44 +0000

    fix(18348): remove the "restart" warning -fix the test

--- hivemq-edge/src/frontend/src/modules/Bridges/components/panels/PersistencePanel.tsx
+++ hivemq-edge/src/frontend/src/modules/Bridges/components/panels/PersistencePanel.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react'
 import { useTranslation } from 'react-i18next'
-import { Alert, AlertIcon, Checkbox, FormControl, FormErrorMessage, FormHelperText } from '@chakra-ui/react'
+import { Checkbox, FormControl, FormErrorMessage, FormHelperText } from '@chakra-ui/react'
 
 import { Capability } from '@/api/__generated__'
 import { BridgePanelType } from '../../types.ts'
@@ -13,25 +13,17 @@ const PersistencePanel: FC<PersistencePanelType> = ({ form }) => {
   const { t } = useTranslation()
   const {
     register,
-    formState: { errors, dirtyFields },
+    formState: { errors },
   } = form
 
   return (
-    <>
-      {dirtyFields.persist && (
-        <Alert status="info" mb={4}>
-          <AlertIcon />
-          {t('bridge.persistence.restartWarning')}
-        </Alert>
-      )}
-      <FormControl variant={'hivemq'} flexGrow={1} display={'flex'} flexDirection={'column'} gap={4} as={'fieldset'}>
-        <FormControl isInvalid={!!errors.persist}>
-          <Checkbox {...register('persist')}>{t('bridge.persistence.persist.label')}</Checkbox>
-          <FormHelperText>{t('bridge.persistence.persist.helper')}</FormHelperText>
-          <FormErrorMessage>{errors.persist && errors.persist.message}</FormErrorMessage>
-        </FormControl>
+    <FormControl variant={'hivemq'} flexGrow={1} display={'flex'} flexDirection={'column'} gap={4} as={'fieldset'}>
+      <FormControl isInvalid={!!errors.persist}>
+        <Checkbox {...register('persist')}>{t('bridge.persistence.persist.label')}</Checkbox>
+        <FormHelperText>{t('bridge.persistence.persist.helper')}</FormHelperText>
+        <FormErrorMessage>{errors.persist && errors.persist.message}</FormErrorMessage>
       </FormControl>
-    </>
+    </FormControl>
   )
 }
 
