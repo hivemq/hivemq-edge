cc9ef925b4efd6871200582f3eaff521bb91e119
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2023-12-20 10:40:23 +0000

    refactor(18350): refactor the  spinner and its styling

--- hivemq-edge/src/frontend/src/modules/Theme/components/index.ts
+++ hivemq-edge/src/frontend/src/modules/Theme/components/index.ts
@@ -3,8 +3,10 @@ import { drawerTheme } from './Drawer.ts'
 import { formControlTheme } from './FormControl.ts'
 import { formErrorMessageTheme } from './FormErrorMessage.ts'
 import { buttonTheme } from './Button.ts'
+import { spinnerTheme } from './Spinner.ts'
 
 const components = {
+  Spinner: spinnerTheme,
   Button: buttonTheme,
   Stat: statTheme,
   Drawer: drawerTheme,
