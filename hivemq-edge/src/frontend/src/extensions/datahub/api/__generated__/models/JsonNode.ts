5455c6e4053ec48559efcf69955114b0347ba60b
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-01-15 13:42:18 +0000

    Revert "feat(18631): add generated stubs from the Datahub OpenAPI spec"
    This reverts commit 647cab216b0f0e6ba384b0ba915461521252a9e0.

--- hivemq-edge/src/frontend/src/extensions/datahub/api/__generated__/models/JsonNode.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do no edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-
-/**
- * The arguments of the fsm derived from the behavior policy.
- */
-export type JsonNode = Record<string, any>;
