383a9a943865327e9ba1c0d8655a54298540b528
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-01-17 11:41:13 +0000

    refactor(18689): create styles for node handle

--- hivemq-edge/src/frontend/src/extensions/datahub/components/nodes/BaseNode.tsx
+++ hivemq-edge/src/frontend/src/extensions/datahub/components/nodes/BaseNode.tsx
@@ -1,8 +1,9 @@
 import { FC } from 'react'
 import { Handle, NodeProps, Position } from 'reactflow'
-
-import { NodeWrapper } from './NodeWrapper.tsx'
 import { Text } from '@chakra-ui/react'
+
+import { styleSourceHandle } from '../../utils/node.utils.ts'
+import { NodeWrapper } from './NodeWrapper.tsx'
 
 export const BaseNode: FC<NodeProps> = (props) => {
   const { id, data, type } = props
@@ -13,7 +14,7 @@ export const BaseNode: FC<NodeProps> = (props) => {
         <Text>{data.label}</Text>
       </NodeWrapper>
       <Handle type="target" position={Position.Left} />
-      <Handle type="source" position={Position.Right} />
+      <Handle type="source" position={Position.Right} style={styleSourceHandle} />
     </>
   )
 }
