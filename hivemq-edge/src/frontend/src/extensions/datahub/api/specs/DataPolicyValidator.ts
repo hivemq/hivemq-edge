3ac771b0a5da31f8aabdc4a5bcdf49310fe78dbc
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-01-17 13:11:23 +0000

    feat(18689): add JSONSchema specs for the validator

---
+++ hivemq-edge/src/frontend/src/extensions/datahub/api/specs/DataPolicyValidator.ts
@@ -0,0 +1,40 @@
+import { RJSFSchema } from '@rjsf/utils'
+import { StrategyType, ValidatorType } from '@/extensions/datahub/types.ts'
+
+export const MOCK_VALIDATOR_SCHEMA: RJSFSchema = {
+  type: 'object',
+  definitions: {
+    SchemaReference: {
+      type: 'object',
+      required: ['schemaId', 'version'],
+      properties: {
+        schemaId: {
+          title: 'ID of the schema',
+          type: 'string',
+        },
+        version: {
+          title: 'version of the schema',
+          type: 'string',
+        },
+      },
+    },
+  },
+  required: ['type', 'strategy'],
+  properties: {
+    type: {
+      title: 'Validator Type',
+      enum: [ValidatorType.SCHEMA],
+      default: ValidatorType.SCHEMA,
+    },
+    strategy: {
+      title: 'Validation Strategy',
+      enum: [StrategyType.ANY_OF, StrategyType.ALL_OF],
+    },
+    schemas: {
+      type: 'array',
+      items: {
+        $ref: '#/definitions/SchemaReference',
+      },
+    },
+  },
+}
