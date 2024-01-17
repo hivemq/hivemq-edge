969906ff776d70bf482aeca301ef5f875561aa7a
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-01-17 11:41:13 +0000

    refactor(18689): create styles for node handle

--- hivemq-edge/src/frontend/src/extensions/datahub/utils/node.utils.ts
+++ hivemq-edge/src/frontend/src/extensions/datahub/utils/node.utils.ts
@@ -1,4 +1,12 @@
 import { Edge, Node } from 'reactflow'
+import { CSSProperties } from 'react'
+
+export const styleSourceHandle: CSSProperties = {
+  width: '12px',
+  right: '-6px',
+  borderRadius: 0,
+  height: '12px',
+}
 
 export const initialFlow = () => {
   const nodes: Node[] = []
