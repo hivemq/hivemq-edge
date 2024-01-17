3fd90bb9216fcf931ff8c7cd8265b44bf3f5e7cf
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-01-17 11:42:34 +0000

    feat(18689): add node for behavior policy

---
+++ hivemq-edge/src/frontend/src/extensions/datahub/components/nodes/BehaviorPolicyNode.tsx
@@ -0,0 +1,52 @@
+import { CSSProperties, FC } from 'react'
+import { Handle, NodeProps, Position } from 'reactflow'
+import { useTranslation } from 'react-i18next'
+import { HStack, Text, VStack } from '@chakra-ui/react'
+
+import { BehaviorPolicyData, DataHubNodeType } from '../../types.ts'
+import { styleSourceHandle } from '../../utils/node.utils.ts'
+import NodeIcon from '../helpers/NodeIcon.tsx'
+import { NodeWrapper } from './NodeWrapper.tsx'
+
+const styleDeserializationHandle: CSSProperties = { width: '12px', top: '-6px', height: '12px' }
+
+export const BehaviorPolicyNode: FC<NodeProps<BehaviorPolicyData>> = (props) => {
+  const { t } = useTranslation('datahub')
+  const { id, type, data } = props
+
+  return (
+    <>
+      <NodeWrapper route={`node/${DataHubNodeType.BEHAVIOR_POLICY}/${id}`} {...props}>
+        <HStack>
+          <NodeIcon type={DataHubNodeType.BEHAVIOR_POLICY} />
+          <Text w={'45%'}> {t('workspace.nodes.type', { context: type })}</Text>
+          <VStack>
+            <Text>{data.model || '< none >'}</Text>
+          </VStack>
+        </HStack>
+      </NodeWrapper>
+      <Handle type="target" position={Position.Left} id="clientFilter" />
+      <Handle
+        type="target"
+        position={Position.Top}
+        id="serial.will"
+        style={{
+          left: '33%',
+          background: 'green',
+          ...styleDeserializationHandle,
+        }}
+      />
+      <Handle
+        type="target"
+        position={Position.Top}
+        id="serial.publish"
+        style={{
+          left: '66%',
+          background: 'red',
+          ...styleDeserializationHandle,
+        }}
+      />
+      <Handle type="source" position={Position.Right} id="transitions" style={styleSourceHandle} />
+    </>
+  )
+}
