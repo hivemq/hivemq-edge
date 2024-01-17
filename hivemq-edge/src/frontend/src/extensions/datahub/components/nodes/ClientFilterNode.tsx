9362c82e0b6e33cdb91fd0adc1dbdff7ccd4ad9f
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-01-17 11:43:05 +0000

    feat(18689): add node for client filter

---
+++ hivemq-edge/src/frontend/src/extensions/datahub/components/nodes/ClientFilterNode.tsx
@@ -0,0 +1,46 @@
+import { FC } from 'react'
+import { Handle, NodeProps, Position } from 'reactflow'
+import { useTranslation } from 'react-i18next'
+import { HStack, Text, VStack } from '@chakra-ui/react'
+
+import Client from '@/components/MQTT/Client.tsx'
+
+import { ClientFilterData } from '../../types.ts'
+import { styleSourceHandle } from '../../utils/node.utils.ts'
+import { NodeWrapper } from './NodeWrapper.tsx'
+
+export const ClientFilterNode: FC<NodeProps<ClientFilterData>> = (props) => {
+  const { t } = useTranslation('datahub')
+  const { id, data, type } = props
+
+  return (
+    <>
+      <NodeWrapper route={`node/${type}/${id}`} {...props}>
+        <HStack>
+          <VStack>
+            <Text> {t('workspace.nodes.type', { context: type })}</Text>
+          </VStack>
+        </HStack>
+        <VStack ml={6}>
+          {data.clients?.map((t) => (
+            <Client client={t} key={t} />
+          ))}
+        </VStack>
+      </NodeWrapper>
+      {data.clients?.map((t, index) => (
+        <Handle
+          type="source"
+          position={Position.Right}
+          id={`${id}-${index}`}
+          key={`${id}-${index}`}
+          aria-label={t}
+          style={{
+            top: `calc(var(--chakra-space-3) + 12px + ${index * 24}px + ${0.5 * index}rem)`,
+            ...styleSourceHandle,
+          }}
+        />
+      ))}
+      <Handle type="source" position={Position.Bottom} style={styleSourceHandle} />
+    </>
+  )
+}
