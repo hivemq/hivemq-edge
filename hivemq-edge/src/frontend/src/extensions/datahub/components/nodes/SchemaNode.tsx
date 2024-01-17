7ddb10db2b1082dd26b51ed30ce69af0f2bca91f
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-01-17 11:44:59 +0000

    feat(18689): add node for schema

---
+++ hivemq-edge/src/frontend/src/extensions/datahub/components/nodes/SchemaNode.tsx
@@ -0,0 +1,28 @@
+import { HStack, Text, VStack } from '@chakra-ui/react'
+import { FC } from 'react'
+import { Handle, NodeProps, Position } from 'reactflow'
+
+import { DataHubNodeType, SchemaData } from '../../types.ts'
+import { styleSourceHandle } from '../../utils/node.utils.ts'
+import NodeIcon from '../helpers/NodeIcon.tsx'
+import { NodeWrapper } from './NodeWrapper.tsx'
+
+export const SchemaNode: FC<NodeProps<SchemaData>> = (props) => {
+  const { id, data } = props
+
+  return (
+    <>
+      <NodeWrapper route={`node/${DataHubNodeType.SCHEMA}/${id}`} {...props}>
+        <HStack>
+          <NodeIcon type={DataHubNodeType.SCHEMA} />
+          <Text>Schema</Text>
+          <VStack>
+            <Text>{data?.type}</Text>
+            <Text>{data?.schemaSource?.title || '< not defined>'}</Text>
+          </VStack>
+        </HStack>
+      </NodeWrapper>
+      <Handle type="source" position={Position.Bottom} id={'source'} style={styleSourceHandle} />
+    </>
+  )
+}
