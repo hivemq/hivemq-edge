dc45cab3d1201346c9968b134ed744a98c3650b9
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-01-18 11:29:05 +0000

    feat(18689): add editor for the schema nodes

---
+++ hivemq-edge/src/frontend/src/extensions/datahub/components/panels/index.ts
@@ -0,0 +1,6 @@
+import { ClientFilterPanel } from './ClientFilterPanel'
+import { TopicFilterPanel } from './TopicFilterPanel'
+import { ValidatorPanel } from './ValidatorPanel'
+import { SchemaPanel } from './SchemaPanel'
+
+export { ClientFilterPanel, TopicFilterPanel, ValidatorPanel, SchemaPanel }
