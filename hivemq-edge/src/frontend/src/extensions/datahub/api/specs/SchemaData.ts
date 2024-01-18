91b8c9582df9b8500ca48b9b36083c8568a93a58
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-01-18 11:29:05 +0000

    feat(18689): add editor for the schema nodes

---
+++ hivemq-edge/src/frontend/src/extensions/datahub/api/specs/SchemaData.ts
@@ -0,0 +1,28 @@
+import { PanelSpecs, SchemaType } from '@/extensions/datahub/types.ts'
+
+export const MOCK_SCHEMA_SCHEMA: PanelSpecs = {
+  schema: {
+    type: 'object',
+    description: `Data validation relies on the definition of schemas to interact with policies. The HiveMQ Data Hub supports schema definitions with JSON Schema or Protobuf formats:`,
+    required: ['type', 'version'],
+    properties: {
+      type: {
+        title: 'Schema',
+        enum: [SchemaType.JSON],
+        default: SchemaType.JSON,
+      },
+      version: {
+        type: 'string',
+      },
+      schemaSource: {
+        type: 'string',
+        format: 'application/schema+json',
+      },
+    },
+  },
+  uiSchema: {
+    schemaSource: {
+      'ui:widget': 'application/schema+json',
+    },
+  },
+}
