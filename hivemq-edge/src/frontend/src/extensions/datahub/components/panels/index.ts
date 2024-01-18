0bde9e6871d9f5daa2a7bf1e97d89bbf71f2f48d
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-01-18 17:55:11 +0000

    feat(18689): add editor for the operation nodes - add schemas for the internal functions - refactor types

--- hivemq-edge/src/frontend/src/extensions/datahub/components/panels/index.ts
+++ hivemq-edge/src/frontend/src/extensions/datahub/components/panels/index.ts
@@ -1,6 +1,17 @@
-import { ClientFilterPanel } from './ClientFilterPanel'
-import { TopicFilterPanel } from './TopicFilterPanel'
-import { ValidatorPanel } from './ValidatorPanel'
-import { SchemaPanel } from './SchemaPanel'
+import { BehaviorPolicyPanel } from './BehaviorPolicyPanel.tsx'
+import { ClientFilterPanel } from './ClientFilterPanel.tsx'
+import { OperationPanel } from './OperationPanel.tsx'
+import { SchemaPanel } from './SchemaPanel.tsx'
+import { TopicFilterPanel } from './TopicFilterPanel.tsx'
+import { TransitionPanel } from './TransitionPanel.tsx'
+import { ValidatorPanel } from './ValidatorPanel.tsx'
 
-export { ClientFilterPanel, TopicFilterPanel, ValidatorPanel, SchemaPanel }
+export {
+  BehaviorPolicyPanel,
+  ClientFilterPanel,
+  OperationPanel,
+  SchemaPanel,
+  TopicFilterPanel,
+  TransitionPanel,
+  ValidatorPanel,
+}
