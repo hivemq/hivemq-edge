747a941650f3961cfc2f85a3990417781e407551
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-01-18 10:50:01 +0000

    feat(18689): add editor to client filter nodes

---
+++ hivemq-edge/src/frontend/src/extensions/datahub/components/panels/ClientFilterPanel.tsx
@@ -0,0 +1,39 @@
+import { FC, useCallback, useMemo } from 'react'
+import { Node } from 'reactflow'
+import { IChangeEvent } from '@rjsf/core'
+
+import { MOCK_CLIENT_FILTER_SCHEMA } from '../../api/specs/ClientFilterData.ts'
+import { ClientFilterData, PanelProps } from '../../types.ts'
+import useDataHubDraftStore from '../../hooks/useDataHubDraftStore.ts'
+import { ReactFlowSchemaForm } from '../helpers/ReactFlowSchemaForm.tsx'
+
+export const ClientFilterPanel: FC<PanelProps> = ({ selectedNode, onClose }) => {
+  const { nodes, onUpdateNodes } = useDataHubDraftStore()
+
+  const clients = useMemo(() => {
+    const adapterNode = nodes.find((e) => e.id === selectedNode) as Node<ClientFilterData> | undefined
+    return adapterNode ? adapterNode.data.clients : null
+  }, [selectedNode, nodes])
+
+  const onFormSubmit = useCallback(
+    (data: IChangeEvent) => {
+      const { formData } = data
+      onUpdateNodes(selectedNode, formData)
+      onClose?.()
+    },
+    [selectedNode, onUpdateNodes, onClose]
+  )
+
+  return (
+    <>
+      <ReactFlowSchemaForm
+        schema={MOCK_CLIENT_FILTER_SCHEMA.schema}
+        uiSchema={MOCK_CLIENT_FILTER_SCHEMA.uiSchema}
+        formData={{ clients: clients }}
+        onChange={() => console.log('changed')}
+        onSubmit={onFormSubmit}
+        onError={() => console.log('errors')}
+      />
+    </>
+  )
+}
