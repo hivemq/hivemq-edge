2703f2744c4727f910db9dc84ae555df292fc3ba
Daniel Krueger <daniel.krueger@hivemq.com>
2024-01-19 08:30:37 +0100

    fix unit tests

--- hivemq-edge/src/main/java/com/hivemq/bootstrap/factories/HandlerProvider.java
+++ hivemq-edge/src/main/java/com/hivemq/bootstrap/factories/HandlerProvider.java
@@ -7,12 +7,10 @@ import com.hivemq.extensions.core.HandlerService;
 import com.hivemq.mqtt.handler.connack.MqttConnacker;
 import com.hivemq.mqtt.handler.disconnect.MqttServerDisconnector;
 import com.hivemq.mqtt.message.dropping.IncomingPublishDropper;
-import com.hivemq.mqtt.message.dropping.MessageDroppedService;
 
 import javax.inject.Inject;
 
 public class HandlerProvider {
-
 
     private final @NotNull HandlerService handlerService;
     private final @NotNull MqttConnacker mqttConnacker;
@@ -21,13 +19,12 @@ public class HandlerProvider {
     private final @NotNull ConfigurationService configurationService;
 
     @Inject
-    public HandlerProvider(final @NotNull HandlerService handlerService,
-                           final @NotNull MqttConnacker mqttConnacker,
-                           final @NotNull MqttServerDisconnector mqttServerDisconnector,
-                           final @NotNull IncomingPublishDropper incomingPublishDropper,
-                           final @NotNull ConfigurationService configurationService){
-
-
+    public HandlerProvider(
+            final @NotNull HandlerService handlerService,
+            final @NotNull MqttConnacker mqttConnacker,
+            final @NotNull MqttServerDisconnector mqttServerDisconnector,
+            final @NotNull IncomingPublishDropper incomingPublishDropper,
+            final @NotNull ConfigurationService configurationService) {
         this.handlerService = handlerService;
         this.mqttConnacker = mqttConnacker;
         this.mqttServerDisconnector = mqttServerDisconnector;
@@ -36,12 +33,15 @@ public class HandlerProvider {
     }
 
 
-    public final @Nullable HandlerPackage get(){
+    public final @Nullable HandlerPackage get() {
         final HandlerFactory handlerFactory = handlerService.getHandlerFactory();
-        if(handlerFactory== null){
+        if (handlerFactory == null) {
             return null;
         }
-        return handlerFactory.build(mqttConnacker,mqttServerDisconnector,  incomingPublishDropper, configurationService);
+        return handlerFactory.build(mqttConnacker,
+                mqttServerDisconnector,
+                incomingPublishDropper,
+                configurationService);
     }
 
 }
