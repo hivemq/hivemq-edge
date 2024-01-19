c8940ae23a66dc19c67fca33ec0f2db38aad59f1
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-01-19 10:29:51 +0000

    fix(18689): fix mocks

---
+++ hivemq-edge/src/frontend/src/extensions/datahub/__test-utils__/schema-mocks.ts
@@ -0,0 +1,53 @@
+export const MOCK_PROTOBUF_SCHEMA = `
+syntax = "proto3";
+
+message GpsCoordinates {
+  int32 longitude = 1;
+  int32 latitude = 2;
+}
+`
+
+const MOCK_JSONSCHEMA_SCHEMA = `
+{
+   "$id":"https://example.com/geographical-location.schema.json",
+   "$schema":"https://json-schema.org/draft/2020-12/schema",
+   "title":"Longitude and Latitude Values",
+   "description":"A geographical coordinate.",
+   "required":[
+      "latitude",
+      "longitude"
+   ],
+   "type":"object",
+   "properties":{
+      "latitude":{
+         "type":"number",
+         "minimum":-90,
+         "maximum":90
+      },
+      "longitude":{
+         "type":"number",
+         "minimum":-180,
+         "maximum":180
+      }
+   }
+}
+`
+
+export const MOCK_JAVASCRIPT_SCHEMA = `
+/**
+ *
+ * @param {Object} publish
+ * @param {string} publish.topic    The MQTT topic that is currently specified for this PUBLISH packet.
+ * @param {Object} publish.payload  A list of the name and value of all user properties of the MQTT 5 PUBLISH packet. This setting has no effect on MQTT 3 clients.
+ * @param {Record<string, string>[]} publish.userProperties The JSON object representation of the deserialized MQTT payload.
+ * @param {Object} context
+ * @param {Record<string, string>[]} context.arguments  The arguments provided to the script. Currently, arguments can only be provided via a data policy.
+ * @param {string} context.policyId The policy id of the policy from which the transformation function is called.
+ * @param {string} context.clientId The client Id of the client from which the MQTT publish was sent.
+ * @returns {Object} The publish-object is passed as a parameter into the transform function. The same object or a new object is returned as the transformed object.
+ */
+function transform(publish, context) {
+  return publish
+}
+
+`
