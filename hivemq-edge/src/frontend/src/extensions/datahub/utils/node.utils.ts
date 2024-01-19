dd7338adb9451ac4d2b362b03945a7b84c0ec6b6
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-01-19 14:06:28 +0000

    feat(18689): add utilities for initial node's id and payload

--- hivemq-edge/src/frontend/src/extensions/datahub/utils/node.utils.ts
+++ hivemq-edge/src/frontend/src/extensions/datahub/utils/node.utils.ts
@@ -1,5 +1,19 @@
 import { Edge, Node } from 'reactflow'
 import { CSSProperties } from 'react'
+import { MOCK_INITIAL_POLICY } from '../__test-utils__/react-flow.mocks.tsx'
+import { MOCK_JSONSCHEMA_SCHEMA } from '../__test-utils__/schema-mocks.ts'
+
+import {
+  ClientFilterData,
+  DataHubNodeType,
+  OperationData,
+  SchemaData,
+  SchemaType,
+  StrategyType,
+  TopicFilterData,
+  ValidatorData,
+  ValidatorType,
+} from '../types.ts'
 
 export const styleSourceHandle: CSSProperties = {
   width: '12px',
@@ -13,4 +27,44 @@ export const initialFlow = () => {
   const edges: Edge[] = []
 
   return { nodes, edges }
+}
+
+export const getNodeId = () => `node_${self.crypto.randomUUID()}`
+
+export const getNodePayload = (type: string) => {
+  if (type === DataHubNodeType.TOPIC_FILTER) {
+    const payload: TopicFilterData = {
+      topics: ['root/test1', 'root/test2'],
+    }
+    return payload
+  }
+  if (type === DataHubNodeType.CLIENT_FILTER) {
+    const payload: ClientFilterData = {
+      clients: ['client10', 'client20', 'client30'],
+    }
+    return payload
+  }
+
+  if (type === DataHubNodeType.VALIDATOR) {
+    const payload: ValidatorData = {
+      type: ValidatorType.SCHEMA,
+      strategy: StrategyType.ALL_OF,
+    }
+    return payload
+  }
+  if (type === DataHubNodeType.OPERATION) {
+    const payload: OperationData = {
+      action: undefined,
+    }
+    return payload
+  }
+  if (type === DataHubNodeType.SCHEMA) {
+    const payload: SchemaData = {
+      type: SchemaType.JSON,
+      version: '1',
+      schemaSource: MOCK_JSONSCHEMA_SCHEMA,
+    }
+    return payload
+  }
+  return { label: `${type} node` }
 }
