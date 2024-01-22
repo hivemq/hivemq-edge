cabd352d3d2e878f505c67efa8d292160e056d0e
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-01-22 09:59:32 +0000

    test(18689): refactor capability override to make it dev only

--- hivemq-edge/src/frontend/src/api/hooks/useFrontendServices/useGetCapability.tsx
+++ hivemq-edge/src/frontend/src/api/hooks/useFrontendServices/useGetCapability.tsx
@@ -2,18 +2,10 @@ import { useMemo } from 'react'
 import { Capability } from '@/api/__generated__'
 
 import { useGetCapabilities } from './useGetCapabilities.tsx'
-import { MOCK_CAPABILITY_DUMMY } from '@/api/hooks/useFrontendServices/__handlers__'
 
 export enum CAPABILITY {
   PERSISTENCE = 'mqtt-persistence',
   DATAHUB = 'data-hub',
-}
-
-const getConfig = (id: string) => {
-  const config: string | undefined = import.meta.env.VITE_FLAG_CAPABILITIES
-  if (!config) return false
-
-  return config.split(',').includes(id)
 }
 
 export const useGetCapability = (id: string) => {
@@ -21,8 +13,6 @@ export const useGetCapability = (id: string) => {
 
   return useMemo<Capability | undefined>(() => {
     if (!data || !isSuccess || id.trim() === '') return undefined
-
-    if (getConfig(id)) return MOCK_CAPABILITY_DUMMY
 
     const index = data.items?.findIndex((e) => e.id === id)
     if (index === -1 || index === undefined) return undefined
