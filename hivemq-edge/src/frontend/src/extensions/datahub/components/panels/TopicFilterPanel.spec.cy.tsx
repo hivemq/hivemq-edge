6630e87365de0829e36f11607a7dd8071fb5977d
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-01-22 10:09:40 +0000

    test(18689): add test for topic filter panel

---
+++ hivemq-edge/src/frontend/src/extensions/datahub/components/panels/TopicFilterPanel.spec.cy.tsx
@@ -0,0 +1,51 @@
+/// <reference types="cypress" />
+
+import { MockStoreWrapper } from '../../__test-utils__/react-flow.mocks.tsx'
+import { DataHubNodeType } from '../../types.ts'
+import { getNodePayload } from '../../utils/node.utils.ts'
+import { TopicFilterPanel } from '../panels/TopicFilterPanel.tsx'
+
+const wrapper: React.JSXElementConstructor<{ children: React.ReactNode }> = ({ children }) => (
+  <MockStoreWrapper
+    config={{
+      initialState: {
+        nodes: [
+          {
+            id: '3',
+            type: DataHubNodeType.TOPIC_FILTER,
+            position: { x: 0, y: 0 },
+            data: getNodePayload(DataHubNodeType.TOPIC_FILTER),
+          },
+        ],
+      },
+    }}
+  >
+    {children}
+  </MockStoreWrapper>
+)
+
+describe('TopicFilterPanel', () => {
+  beforeEach(() => {
+    cy.viewport(800, 800)
+  })
+
+  it('should render the fields for a Validator', () => {
+    cy.mountWithProviders(<TopicFilterPanel selectedNode={'3'} />, { wrapper })
+
+    cy.get('h5').eq(0).should('contain.text', 'Topic Filters')
+    // first item
+    cy.get('label#root_topics_0-label').should('contain.text', 'topics-0')
+    cy.get('label#root_topics_0-label + input').should('have.value', 'root/test1')
+    // first item
+    cy.get('label#root_topics_1-label').should('contain.text', 'topics-1')
+    cy.get('label#root_topics_1-label + input').should('have.value', 'root/test2')
+  })
+
+  it('should be accessible', () => {
+    cy.injectAxe()
+    cy.mountWithProviders(<TopicFilterPanel selectedNode={'3'} />, { wrapper })
+
+    cy.checkAccessibility()
+    cy.percySnapshot('Component: TopicFilterPanel')
+  })
+})
