13c578daf608d33f70b539efe18670b1fb473b96
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-01-22 09:59:32 +0000

    test(18689): refactor capability override to make it dev only

--- hivemq-edge/src/frontend/src/api/hooks/useFrontendServices/useGetCapabilities.tsx
+++ hivemq-edge/src/frontend/src/api/hooks/useFrontendServices/useGetCapabilities.tsx
@@ -1,13 +1,26 @@
 import { useQuery } from '@tanstack/react-query'
+import { ApiError, CapabilityList } from '@/api/__generated__'
 import { useHttpClient } from '@/api/hooks/useHttpClient/useHttpClient.ts'
 import { QUERY_KEYS } from '@/api/utils.ts'
-import { ApiError, CapabilityList } from '@/api/__generated__'
+
+import { MOCK_CAPABILITIES } from './__handlers__'
+
+const getLocalConfig = (): CapabilityList | undefined => {
+  const config: string | undefined =
+    import.meta.env.MODE === 'development' ? import.meta.env.VITE_FLAG_CAPABILITIES : undefined
+  if (!config) return undefined
+
+  console.log('%c[HiveMQ Edge] Capability override: %s', 'color:#ffc000;font-weight:bold;', config)
+
+  const capabilities = config.split(',')
+  return { items: MOCK_CAPABILITIES.items?.filter((e) => capabilities.includes(e.id as string)) }
+}
 
 export const useGetCapabilities = () => {
   const appClient = useHttpClient()
 
   return useQuery<CapabilityList, ApiError>([QUERY_KEYS.FRONTEND_CAPABILITIES], async () => {
     const item = await appClient.frontend.getCapabilities()
-    return item
+    return getLocalConfig() || item
   })
 }
