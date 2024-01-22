9dd21f7738a738680fa87b895b847a7eba71eb05
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-01-22 10:17:54 +0000

    test(18689): add tests for the node utilities

---
+++ hivemq-edge/src/frontend/src/extensions/datahub/utils/node.utils.spec.ts
@@ -0,0 +1,67 @@
+import { describe, expect } from 'vitest'
+import { DataHubNodeType, SchemaType, StrategyType, ValidatorType } from '../types.ts'
+import { getNodeId, getNodePayload, initialFlow } from './node.utils.ts'
+import { MOCK_JSONSCHEMA_SCHEMA } from '@/extensions/datahub/__test-utils__/schema-mocks.ts'
+
+describe('initialFlow', () => {
+  it('should return the initial state of the store', async () => {
+    expect(initialFlow()).toStrictEqual({ nodes: [], edges: [] })
+  })
+})
+
+describe('getNodeId', () => {
+  it('should return the initial state of the store', async () => {
+    expect(getNodeId()).toContain('node_')
+  })
+})
+
+interface NodePayloadSuite {
+  type: string
+  expected: unknown
+}
+
+describe('getNodePayload', () => {
+  test.each<NodePayloadSuite>([
+    {
+      type: 'undefined',
+      expected: {
+        label: `undefined node`,
+      },
+    },
+    {
+      type: DataHubNodeType.TOPIC_FILTER,
+      expected: {
+        topics: ['root/test1', 'root/test2'],
+      },
+    },
+    {
+      type: DataHubNodeType.CLIENT_FILTER,
+      expected: {
+        clients: ['client10', 'client20', 'client30'],
+      },
+    },
+    {
+      type: DataHubNodeType.VALIDATOR,
+      expected: {
+        type: ValidatorType.SCHEMA,
+        strategy: StrategyType.ALL_OF,
+      },
+    },
+    {
+      type: DataHubNodeType.OPERATION,
+      expected: {
+        action: undefined,
+      },
+    },
+    {
+      type: DataHubNodeType.SCHEMA,
+      expected: {
+        type: SchemaType.JSON,
+        version: '1',
+        schemaSource: MOCK_JSONSCHEMA_SCHEMA,
+      },
+    },
+  ])('should returns the payload for $type ', ({ type, expected }) => {
+    expect(getNodePayload(type)).toStrictEqual(expected)
+  })
+})
