8215b980da48b7bcd99195424b097d03036b3657
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-01-22 10:23:15 +0000

    test(18689): add test wrapper for the store-based suites

---
+++ hivemq-edge/src/frontend/src/extensions/datahub/__test-utils__/MockStoreWrapper.tsx
@@ -0,0 +1,38 @@
+import { FC, useEffect } from 'react'
+
+import useDataHubDraftStore from '../hooks/useDataHubDraftStore.ts'
+import { WorkspaceState } from '../types.ts'
+
+type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>
+interface MockStoreWrapperConfig {
+  initialState?: Optional<WorkspaceState, 'nodes' | 'edges'>
+}
+
+interface MockStoreWrapperProps {
+  children: React.ReactNode
+  config: MockStoreWrapperConfig
+}
+
+export const MockStoreWrapper: FC<MockStoreWrapperProps> = ({ config, children }) => {
+  const { onAddNodes, onAddEdges } = useDataHubDraftStore()
+
+  useEffect(() => {
+    const { initialState } = config
+    if (initialState?.nodes)
+      onAddNodes(
+        initialState.nodes.map((n) => ({
+          item: n,
+          type: 'add',
+        }))
+      )
+    if (initialState?.edges)
+      onAddEdges(
+        initialState.edges.map((e) => ({
+          item: e,
+          type: 'add',
+        }))
+      )
+  }, [config, onAddEdges, onAddNodes])
+
+  return <>{children}</>
+}
