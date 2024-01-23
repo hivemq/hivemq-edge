c9b1b5f8f797b9c33ad472fcac3cde1339437779
Daniel Krueger <daniel.krueger@hivemq.com>
2024-01-23 11:53:40 +0100

    added data policies for adapters

--- hivemq-edge/src/main/java/com/hivemq/extensions/core/HandlerService.java
+++ hivemq-edge/src/main/java/com/hivemq/extensions/core/HandlerService.java
@@ -1,5 +1,6 @@
 package com.hivemq.extensions.core;
 
+import com.hivemq.bootstrap.factories.AdapterHandlingFactory;
 import com.hivemq.bootstrap.factories.ClientSessionLocalPersistenceFactory;
 import com.hivemq.bootstrap.factories.HandlerFactory;
 import com.hivemq.extension.sdk.api.annotations.NotNull;
@@ -8,13 +9,23 @@ import com.hivemq.extension.sdk.api.annotations.Nullable;
 public class HandlerService {
 
     private @Nullable HandlerFactory handlerFactory;
+    private @Nullable AdapterHandlingFactory adapterHandlingFactory;
+
 
     public void supplyHandlerFactory(final @NotNull HandlerFactory handlerFactory) {
         this.handlerFactory = handlerFactory;
     }
 
+    public void supplyAdapterHandlingFactory(final @NotNull AdapterHandlingFactory adapterHandlingFactory) {
+        this.adapterHandlingFactory = adapterHandlingFactory;
+    }
+
 
     public @Nullable HandlerFactory getHandlerFactory() {
         return handlerFactory;
+    }
+
+    public @Nullable AdapterHandlingFactory getAdapterHandlerFactory() {
+        return adapterHandlingFactory;
     }
 }
