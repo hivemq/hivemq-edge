2316a969a547edb04285ca46c542a70524a307fc
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-01-25 11:51:36 +0000

    feat(18842): Implement connectivity controls between the different types of node (#271)
    * feat(18842): define connectivity rules
    
    * feat(18842): add connectivity rules to the ReactFlow wrapper
    
    * fix(18842): fix JSONSchema for rge validator
    
    * chore(18842): add dependencies
    
    * style(18842): adding SCSS module for the datahub ReactFlow overrides
    
    * chore(18842): cleaning
    
    * feat(18842): add a custom handle renderer
    
    * feat(18842): add custom handles to the nodes
    
    * refactor(18842): clean deprecated styles
    
    * fix(18842): change invalid styling amd add support for not connectable
    
    * fix(18842): fix cardinality for data policy connections
    
    * fix(18842): fix translations
    
    * fix(18842): update node attributes for testing
    
    * test(18842): add tests for nodes
    
    * chore(18842): linting
    
    * fix(18842): fix Operation type and payload handling
    
    * Update hivemq-edge/src/frontend/src/extensions/datahub/components/nodes/CustomHandle.tsx
    
    Co-authored-by: Andrej Schujkow <19648055+h2xd@users.noreply.github.com>
    
    * Update hivemq-edge/src/frontend/src/extensions/datahub/components/nodes/CustomHandle.tsx
    
    Co-authored-by: Andrej Schujkow <19648055+h2xd@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Andrej Schujkow <19648055+h2xd@users.noreply.github.com>

--- hivemq-edge/src/frontend/src/extensions/datahub/components/nodes/ClientFilterNode.tsx
+++ hivemq-edge/src/frontend/src/extensions/datahub/components/nodes/ClientFilterNode.tsx
@@ -1,13 +1,13 @@
 import { FC } from 'react'
-import { Handle, NodeProps, Position } from 'reactflow'
+import { NodeProps, Position } from 'reactflow'
 import { useTranslation } from 'react-i18next'
 import { HStack, Text, VStack } from '@chakra-ui/react'
 
 import Client from '@/components/MQTT/Client.tsx'
 
 import { ClientFilterData } from '../../types.ts'
-import { styleSourceHandle } from '../../utils/node.utils.ts'
 import { NodeWrapper } from './NodeWrapper.tsx'
+import { CustomHandle } from './CustomHandle.tsx'
 
 export const ClientFilterNode: FC<NodeProps<ClientFilterData>> = (props) => {
   const { t } = useTranslation('datahub')
@@ -18,25 +18,23 @@ export const ClientFilterNode: FC<NodeProps<ClientFilterData>> = (props) => {
       <NodeWrapper route={`node/${type}/${id}`} {...props}>
         <HStack>
           <VStack>
-            <Text> {t('workspace.nodes.type', { context: type })}</Text>
+            <Text data-testid={'node-title'}> {t('workspace.nodes.type', { context: type })}</Text>
           </VStack>
         </HStack>
-        <VStack ml={6}>
+        <VStack ml={6} data-testid={'node-model'}>
           {data.clients?.map((t) => (
             <Client client={t} key={t} />
           ))}
         </VStack>
       </NodeWrapper>
       {data.clients?.map((t, index) => (
-        <Handle
+        <CustomHandle
           type="source"
           position={Position.Right}
           id={`${id}-${index}`}
           key={`${id}-${t}-${index}`}
-          // aria-label={t}
           style={{
             top: `calc(var(--chakra-space-3) + 12px + ${index * 24}px + ${0.5 * index}rem)`,
-            ...styleSourceHandle,
           }}
         />
       ))}
