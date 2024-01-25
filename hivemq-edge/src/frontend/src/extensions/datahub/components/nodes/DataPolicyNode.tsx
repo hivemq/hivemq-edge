1915a7c62c04defb4dc637dc11ba76e79d42451d
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-01-25 11:51:36 +0000

    feat(18842): Implement connectivity controls between the different types of node (#271)
    * feat(18842): define connectivity rules
    
    * feat(18842): add connectivity rules to the ReactFlow wrapper
    
    * fix(18842): fix JSONSchema for rge validator
    
    * chore(18842): add dependencies
    
    * style(18842): adding SCSS module for the datahub ReactFlow overrides
    
    * chore(18842): cleaning
    
    * feat(18842): add a custom handle renderer
    
    * feat(18842): add custom handles to the nodes
    
    * refactor(18842): clean deprecated styles
    
    * fix(18842): change invalid styling amd add support for not connectable
    
    * fix(18842): fix cardinality for data policy connections
    
    * fix(18842): fix translations
    
    * fix(18842): update node attributes for testing
    
    * test(18842): add tests for nodes
    
    * chore(18842): linting
    
    * fix(18842): fix Operation type and payload handling
    
    * Update hivemq-edge/src/frontend/src/extensions/datahub/components/nodes/CustomHandle.tsx
    
    Co-authored-by: Andrej Schujkow <19648055+h2xd@users.noreply.github.com>
    
    * Update hivemq-edge/src/frontend/src/extensions/datahub/components/nodes/CustomHandle.tsx
    
    Co-authored-by: Andrej Schujkow <19648055+h2xd@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Andrej Schujkow <19648055+h2xd@users.noreply.github.com>

--- hivemq-edge/src/frontend/src/extensions/datahub/components/nodes/DataPolicyNode.tsx
+++ hivemq-edge/src/frontend/src/extensions/datahub/components/nodes/DataPolicyNode.tsx
@@ -1,12 +1,12 @@
 import { FC } from 'react'
-import { Handle, NodeProps, Position } from 'reactflow'
+import { NodeProps, Position } from 'reactflow'
 import { useTranslation } from 'react-i18next'
 import { HStack, Text, VStack } from '@chakra-ui/react'
 
 import { DataHubNodeType, DataPolicyData } from '../../types.ts'
-import { styleSourceHandle } from '../../utils/node.utils.ts'
 import NodeIcon from '../helpers/NodeIcon.tsx'
 import { NodeWrapper } from './NodeWrapper.tsx'
+import { CustomHandle } from './CustomHandle.tsx'
 
 export const DataPolicyNode: FC<NodeProps<DataPolicyData>> = (props) => {
   const { t } = useTranslation('datahub')
@@ -17,38 +17,38 @@ export const DataPolicyNode: FC<NodeProps<DataPolicyData>> = (props) => {
       <NodeWrapper route={`node/${DataHubNodeType.DATA_POLICY}/${id}`} {...props}>
         <HStack>
           <NodeIcon type={DataHubNodeType.DATA_POLICY} />
-          <Text> {t('workspace.nodes.type', { context: type })}</Text>
+          <Text data-testid={'node-title'}> {t('workspace.nodes.type', { context: type })}</Text>
         </HStack>
-        <VStack ml={6} alignItems={'flex-end'}>
+        <VStack ml={6} alignItems={'flex-end'} data-testid={'node-model'}>
           <Text fontSize={'xs'}>
             {t('workspace.handles.validation', { context: DataPolicyData.Handle.ON_SUCCESS })}
           </Text>
           <Text fontSize={'xs'}>{t('workspace.handles.validation', { context: DataPolicyData.Handle.ON_ERROR })}</Text>
         </VStack>
       </NodeWrapper>
-      <Handle type="target" position={Position.Left} id={DataPolicyData.Handle.TOPIC_FILTER} />
-      <Handle type="target" position={Position.Top} id={DataPolicyData.Handle.VALIDATION} />
-      <Handle
+      <CustomHandle type="target" position={Position.Left} id={DataPolicyData.Handle.TOPIC_FILTER} />
+      <CustomHandle type="target" position={Position.Top} id={DataPolicyData.Handle.VALIDATION} />
+      <CustomHandle
         type="source"
         position={Position.Right}
         id={DataPolicyData.Handle.ON_SUCCESS}
+        className={DataPolicyData.Handle.ON_SUCCESS}
         style={{
           top: `calc(var(--chakra-space-3) + 10px)`,
-          background: 'green',
-          ...styleSourceHandle,
+          // background: 'green',
         }}
+        isConnectable={1}
       />
-      <Handle
+      <CustomHandle
         type="source"
         position={Position.Right}
         id={DataPolicyData.Handle.ON_ERROR}
+        className={DataPolicyData.Handle.ON_ERROR}
         style={{
           top: `calc(var(--chakra-space-3) + 10px + 16px + 0.5rem)`,
-          background: 'red',
-          ...styleSourceHandle,
         }}
+        isConnectable={1}
       />
-      {/*<Handle type="source" position={Position.Right} id="finally" />*/}
     </>
   )
 }
