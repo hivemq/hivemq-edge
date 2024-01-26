af8cd96d1c034071a52f9892380e6e63bb074d56
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-01-26 13:00:43 +0000

    refactor(18846): refactor function metadata

--- hivemq-edge/src/frontend/src/extensions/datahub/components/nodes/OperationNode.tsx
+++ hivemq-edge/src/frontend/src/extensions/datahub/components/nodes/OperationNode.tsx
@@ -1,27 +1,17 @@
-import { FC, useMemo } from 'react'
+import { FC } from 'react'
 import { useTranslation } from 'react-i18next'
 import { NodeProps, Position } from 'reactflow'
 import { HStack, Text, VStack } from '@chakra-ui/react'
 
-import { DataHubNodeType, FunctionDefinition, OperationData } from '../../types.ts'
+import { DataHubNodeType, OperationData } from '../../types.ts'
 import NodeIcon from '../helpers/NodeIcon.tsx'
 import { NodeWrapper } from './NodeWrapper.tsx'
-import { CustomHandle } from '@/extensions/datahub/components/nodes/CustomHandle.tsx'
+import { CustomHandle } from './CustomHandle.tsx'
 
 export const OperationNode: FC<NodeProps<OperationData>> = (props) => {
   const { t } = useTranslation('datahub')
   const { data, id, type } = props
-  const { action } = data
-
-  const model = useMemo(() => {
-    if (!action) return undefined
-
-    return typeof action === 'string'
-      ? ({
-          functionId: action,
-        } as FunctionDefinition)
-      : action
-  }, [action])
+  const { functionId, metadata } = data
 
   return (
     <>
@@ -30,15 +20,17 @@ export const OperationNode: FC<NodeProps<OperationData>> = (props) => {
           <NodeIcon type={DataHubNodeType.OPERATION} />
           <Text data-testid="node-title"> {t('workspace.nodes.type', { context: type })}</Text>
           <VStack>
-            <Text data-testid="node-model">{model?.functionId || t('error.noSet.select')}</Text>
+            <Text data-testid="node-model">{functionId || t('error.noSet.select')}</Text>
           </VStack>
         </HStack>
       </NodeWrapper>
       <CustomHandle type="target" position={Position.Left} id={OperationData.Handle.INPUT} />
-      {!model?.isTerminal && (
+      {!metadata?.isTerminal && (
         <CustomHandle type="source" position={Position.Right} id={OperationData.Handle.OUTPUT} isConnectable={1} />
       )}
-      {model?.hasArguments && <CustomHandle type="target" position={Position.Top} id={OperationData.Handle.SCHEMA} />}
+      {metadata?.hasArguments && (
+        <CustomHandle type="target" position={Position.Top} id={OperationData.Handle.SCHEMA} />
+      )}
     </>
   )
 }
