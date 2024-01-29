75e338ed603cd020dc193ecbc95ea722dde8ba17
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-01-29 10:00:12 +0000

    refactor(18846): update widget list

--- hivemq-edge/src/frontend/src/extensions/datahub/components/helpers/EditorWidgetRegistry.tsx
+++ hivemq-edge/src/frontend/src/extensions/datahub/components/helpers/EditorWidgetRegistry.tsx
@@ -1,28 +1,10 @@
-import { RegistryWidgetsType, WidgetProps } from '@rjsf/utils'
-import { Editor } from '@monaco-editor/react'
+import { RegistryWidgetsType } from '@rjsf/utils'
 
+import { VersionManagerSelect } from './VersionManagerSelect.tsx'
 import FunctionCreatableSelect from './FunctionCreatableSelect.tsx'
-import { MetricCounterInput } from './MetricCounterInput.tsx'
-
-// eslint-disable-next-line react-refresh/only-export-components
-const MyCustomWidget = (lng: string, props: WidgetProps) => {
-  return (
-    <Editor
-      height="40vh"
-      // id={"schema-editor"}
-      defaultLanguage={lng}
-      defaultValue={props.value}
-      onChange={(event) => props.onChange(event)}
-    />
-  )
-}
-
-// eslint-disable-next-line react-refresh/only-export-components
-const JavascriptEditor = (props: WidgetProps) => MyCustomWidget('javascript', props)
-// eslint-disable-next-line react-refresh/only-export-components
-const JSONSchemaEditor = (props: WidgetProps) => MyCustomWidget('json', props)
-// eslint-disable-next-line react-refresh/only-export-components
-const ProtoSchemaEditor = (props: WidgetProps) => MyCustomWidget('proto', props)
+import { JsFunctionInput, MetricCounterInput } from './MetricCounterInput.tsx'
+import { JavascriptEditor, JSONSchemaEditor, ProtoSchemaEditor } from './CodeEditor.tsx'
+import { MessageInterpolationTextArea } from './MessageInterpolationTextArea.tsx'
 
 export const datahubRJSFWidgets: RegistryWidgetsType = {
   'application/schema+json': JSONSchemaEditor,
@@ -30,4 +12,7 @@ export const datahubRJSFWidgets: RegistryWidgetsType = {
   'application/octet-stream': ProtoSchemaEditor,
   'datahub:function-selector': FunctionCreatableSelect,
   'datahub:metric-counter': MetricCounterInput,
+  'datahub:function-name': JsFunctionInput,
+  'datahub:version': VersionManagerSelect,
+  'datahub:message-interpolation': MessageInterpolationTextArea,
 }
