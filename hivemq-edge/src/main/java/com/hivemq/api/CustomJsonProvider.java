a1556a2a473d21927ebbdaeee3247cbed6945079
Daniel Krueger <daniel.krueger@hivemq.com>
2024-01-30 10:09:11 +0100

    fix object mapper settings

--- hivemq-edge/src/main/java/com/hivemq/api/CustomJsonProvider.java
+++ hivemq-edge/src/main/java/com/hivemq/api/CustomJsonProvider.java
@@ -21,11 +21,8 @@ public class CustomJsonProvider extends JacksonJaxbJsonProvider {
     private final static @NotNull ObjectMapper mapper = new ObjectMapper();
 
     static {
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        mapper.setSerializationInclusion(JsonInclude.Include.ALWAYS);
-        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        mapper.enable(SerializationFeature.INDENT_OUTPUT);
-     //   mapper.enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS);
+        mapper.enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS);
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
     }
 
     public CustomJsonProvider() {
