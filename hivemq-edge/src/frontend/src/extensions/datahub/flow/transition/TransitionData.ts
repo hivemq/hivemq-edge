10e82aed7bddc0997c41b19ea0f874a6fe6e61e8
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-02-07 12:02:11 +0000

    refactor(18960): update transition schema

--- hivemq-edge/src/frontend/src/extensions/datahub/flow/transition/TransitionData.ts
+++ hivemq-edge/src/frontend/src/extensions/datahub/flow/transition/TransitionData.ts
@@ -1,8 +1,33 @@
 import { PanelSpecs } from '@/extensions/datahub/types.ts'
 import { RJSFSchema } from '@rjsf/utils'
 
-import schema from '@datahub/api/__generated__/schemas/TransitionData.json'
+const schema: RJSFSchema = {
+  type: 'object',
+  required: ['model', 'transition'],
+  properties: {
+    model: { type: 'string' },
+    transition: { type: 'string' },
+    from: { type: 'string' },
+    to: { type: 'string' },
+  },
+}
 
 export const MOCK_TRANSITION_SCHEMA: PanelSpecs = {
   schema: schema as RJSFSchema,
+  uiSchema: {
+    model: {
+      'ui:readonly': true,
+    },
+    from: {
+      'ui:readonly': true,
+      'ui:widget': 'hidden',
+    },
+    to: {
+      'ui:readonly': true,
+      'ui:widget': 'hidden',
+    },
+    transition: {
+      'ui:widget': 'datahub:transition-selector',
+    },
+  },
 }
