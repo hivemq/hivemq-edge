79faa29ed7abedb1cabe7c1f9074e55a8d041373
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-02-07 12:03:44 +0000

    feat(18960): add a single selector configuration for the transition - add option generation based on FSM selected in parent policy - add custom rendering and filtering - add FSM renderer

--- hivemq-edge/src/frontend/src/extensions/datahub/flow/transition/TransitionPanel.tsx
+++ hivemq-edge/src/frontend/src/extensions/datahub/flow/transition/TransitionPanel.tsx
@@ -1,29 +1,82 @@
 import { FC, useMemo } from 'react'
-import { Node } from 'reactflow'
-import { Card, CardBody } from '@chakra-ui/react'
+import { getIncomers, Node } from 'reactflow'
+import { Box, Card, CardBody } from '@chakra-ui/react'
 
-import { PanelProps, TransitionData } from '@datahub/types.ts'
+import {
+  BehaviorPolicyData,
+  DataHubNodeType,
+  FiniteStateMachineSchema,
+  PanelProps,
+  TransitionData,
+} from '@datahub/types.ts'
 import useDataHubDraftStore from '@datahub/hooks/useDataHubDraftStore.ts'
 import { ReactFlowSchemaForm } from '@datahub/components/forms/ReactFlowSchemaForm.tsx'
 import { MOCK_TRANSITION_SCHEMA } from '@datahub/flow/transition/TransitionData.ts'
+import { datahubRJSFWidgets } from '@datahub/flow/datahubRJSFWidgets.tsx'
+import { MOCK_BEHAVIOR_POLICY_SCHEMA } from '@datahub/flow/behavior_policy/BehaviorPolicySchema.ts'
+import { FiniteStateMachineFlow } from '@datahub/components/fsm/FiniteStateMachineFlow.tsx'
 
 export const TransitionPanel: FC<PanelProps> = ({ selectedNode, onFormSubmit }) => {
-  const { nodes } = useDataHubDraftStore()
+  const { nodes, edges } = useDataHubDraftStore()
+  // const store = useStoreApi()
+
+  const parentPolicy = useMemo(() => {
+    const adapterNode = nodes.find((e) => e.id === selectedNode) as Node<TransitionData> | undefined
+    if (!adapterNode) return undefined
+    const incomers = getIncomers(adapterNode, nodes, edges)
+    if (!incomers) return undefined
+    if (incomers.length !== 1) return undefined
+    const [behavior] = incomers
+    if (behavior.type !== DataHubNodeType.BEHAVIOR_POLICY) return undefined
+
+    // store.getState().onError?.('datahub-123', 'You need to define a parent first')
+    return behavior as Node<BehaviorPolicyData>
+  }, [edges, nodes, selectedNode])
 
   const data = useMemo(() => {
     const adapterNode = nodes.find((e) => e.id === selectedNode) as Node<TransitionData> | undefined
-    return adapterNode ? adapterNode.data : null
-  }, [selectedNode, nodes])
+    if (!adapterNode) return null
+    return { ...adapterNode.data, model: parentPolicy ? parentPolicy.data.model : undefined }
+  }, [nodes, parentPolicy, selectedNode])
+
+  const options = useMemo(() => {
+    if (!parentPolicy) return null
+    const { model } = parentPolicy.data
+
+    const definition = MOCK_BEHAVIOR_POLICY_SCHEMA.schema.definitions?.[model]
+    if (!definition) return null
+
+    const { metadata } = definition as FiniteStateMachineSchema
+    const { states, transitions } = metadata
+    if (!states || !transitions) return null
+
+    return metadata
+  }, [parentPolicy])
 
   return (
     <Card>
       <CardBody>
         <ReactFlowSchemaForm
           schema={MOCK_TRANSITION_SCHEMA.schema}
-          // uiSchema={MOCK_TOPIC_FILTER_SCHEMA.uiSchema}
+          uiSchema={{
+            ...MOCK_TRANSITION_SCHEMA.uiSchema,
+            transition: {
+              ...MOCK_TRANSITION_SCHEMA.uiSchema?.transition,
+              'ui:options': {
+                metadata: options,
+              },
+            },
+          }}
           formData={data}
+          widgets={datahubRJSFWidgets}
           onSubmit={onFormSubmit}
         />
+        {options && (
+          <Box w="100%" height="400px">
+            {/*<Mermaid text={template} />*/}
+            <FiniteStateMachineFlow transitions={options.transitions} states={options.states} />
+          </Box>
+        )}
       </CardBody>
     </Card>
   )
