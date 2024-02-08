268b95f95a7e6b54fb004ba7df93cdd327fbfc79
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-02-08 16:05:20 +0000

    refactor(18960): change conditional

--- hivemq-edge/src/frontend/src/extensions/datahub/components/fsm/FiniteStateMachineFlow.tsx
+++ hivemq-edge/src/frontend/src/extensions/datahub/components/fsm/FiniteStateMachineFlow.tsx
@@ -7,10 +7,11 @@ import { MermaidRenderer } from '@datahub/components/fsm/MermaidRenderer.tsx'
 import { ReactFlowRenderer } from '@datahub/components/fsm/ReactFlowRenderer.tsx'
 
 export const FiniteStateMachineFlow: FC<FiniteStateMachine> = (props) => {
-  if (import.meta.env.VITE_FLAG_DATAHUB_STATIC_FSM === 'true') return <MermaidRenderer {...props} />
-  return (
-    <Box w="100%" height="400px">
-      <ReactFlowRenderer {...props} />
-    </Box>
-  )
+  if (import.meta.env.VITE_FLAG_DATAHUB_STATIC_FSM === 'false')
+    return (
+      <Box w="100%" height="400px">
+        <ReactFlowRenderer {...props} />
+      </Box>
+    )
+  return <MermaidRenderer {...props} />
 }
