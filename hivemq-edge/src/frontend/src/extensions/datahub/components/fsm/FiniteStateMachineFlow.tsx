c7ac9881e0b780afaee16b57f955a3912a35eeb6
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-02-09 13:58:31 +0000

    fix(18960): change flag and render conditional

--- hivemq-edge/src/frontend/src/extensions/datahub/components/fsm/FiniteStateMachineFlow.tsx
+++ hivemq-edge/src/frontend/src/extensions/datahub/components/fsm/FiniteStateMachineFlow.tsx
@@ -8,7 +8,7 @@ import { ReactFlowRenderer } from '@datahub/components/fsm/ReactFlowRenderer.tsx
 import { ReactFlowProvider } from 'reactflow'
 
 export const FiniteStateMachineFlow: FC<FiniteStateMachine> = (props) => {
-  if (import.meta.env.VITE_FLAG_DATAHUB_STATIC_FSM === 'false')
+  if (import.meta.env.VITE_FLAG_DATAHUB_FSM_REACTFLOW === 'true')
     return (
       <Box w="100%" height="400px">
         <ReactFlowProvider>
