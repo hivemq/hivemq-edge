ac18dba118e81038e4ae1b3349da32abe5dee781
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-02-12 16:27:07 +0000

    feat(19240): add styles for check status

--- hivemq-edge/src/frontend/src/extensions/datahub/components/nodes/NodeWrapper.tsx
+++ hivemq-edge/src/frontend/src/extensions/datahub/components/nodes/NodeWrapper.tsx
@@ -1,15 +1,17 @@
 import { FC, ReactNode, useEffect, useState } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
 import { NodeProps } from 'reactflow'
-import { BoxProps, Card, CardBody, CardBodyProps, HStack } from '@chakra-ui/react'
 
-interface NodeWrapperProps extends NodeProps {
+import { BoxProps, Card, CardBody, CardBodyProps, HStack } from '@chakra-ui/react'
+import { DataHubNodeData, PolicyDryRunStatus } from '@datahub/types.ts'
+
+interface NodeWrapperProps extends NodeProps<DataHubNodeData> {
   children: ReactNode
   route: string
   wrapperProps?: CardBodyProps
 }
 
-export const NodeWrapper: FC<NodeWrapperProps> = ({ selected, children, route, wrapperProps }) => {
+export const NodeWrapper: FC<NodeWrapperProps> = ({ selected, children, route, wrapperProps, ...rest }) => {
   const [internalSelection, setInternalSelection] = useState(false)
   const navigate = useNavigate()
   const { pathname } = useLocation()
@@ -22,9 +24,19 @@ export const NodeWrapper: FC<NodeWrapperProps> = ({ selected, children, route, w
     boxShadow: 'var(--chakra-shadows-outline)',
   }
 
+  const errorStyle: Partial<BoxProps> = {
+    boxShadow: '0 0 10px 2px rgba(226, 85, 85, 0.75), 0 1px 1px rgb(0 0 0 / 15%)',
+  }
+
+  const successStyle: Partial<BoxProps> = {
+    boxShadow: '0 0 10px 2px rgba(0,121,36, 0.75), 0 1px 1px rgb(0 0 0 / 15%)',
+  }
+
   return (
     <Card
       variant="elevated"
+      {...(rest.data.dryRunStatus === PolicyDryRunStatus.FAILURE ? { ...errorStyle } : {})}
+      {...(rest.data.dryRunStatus === PolicyDryRunStatus.SUCCESS ? { ...successStyle } : {})}
       {...(selected ? { ...selectedStyle } : {})}
       size="sm"
       onClick={(event) => {
