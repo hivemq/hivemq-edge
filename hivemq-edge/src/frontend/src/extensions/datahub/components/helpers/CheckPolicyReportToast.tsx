c5f8c3e289431ff676d60f3ed01d819a91f2dcde
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-02-12 16:23:05 +0000

    feat(19240): add component for reporting  policy checks

---
+++ hivemq-edge/src/frontend/src/extensions/datahub/components/helpers/CheckPolicyReportToast.tsx
@@ -0,0 +1,69 @@
+import { FC } from 'react'
+import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Box,
+  Button,
+  ButtonGroup,
+  Text,
+  VStack,
+} from '@chakra-ui/react'
+import { ProblemDetailsExtended } from '@/api/types/http-problem-details.ts'
+import { useTranslation } from 'react-i18next'
+
+interface CheckPolicyReportToastProps {
+  errors: ProblemDetailsExtended[]
+  onFitView?: (id: string) => void
+  openConfig?: (id: string) => void
+}
+
+const CheckPolicyReportToast: FC<CheckPolicyReportToastProps> = ({ errors, onFitView, openConfig }) => {
+  const { t } = useTranslation('datahub')
+
+  return (
+    <>
+      <Box as="p" mb={2}>
+        <Text as="span" fontWeight="bold">
+          {t('workspace.dryRun.report.title')}
+        </Text>{' '}
+        <Text as="span">{t('workspace.dryRun.report.subtitle')}</Text>
+      </Box>
+
+      <Accordion allowToggle overflow="auto" maxHeight="180px">
+        {errors.map((problem) => {
+          const { id, title, detail } = problem
+          return (
+            <AccordionItem key={id as string} borderColor="blackAlpha.500">
+              <h2>
+                <AccordionButton>
+                  <Box as="span" flex="1" textAlign="left">
+                    {t('workspace.nodes.type', { context: title })}
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+              </h2>
+              <AccordionPanel pb={4}>
+                <VStack alignItems="flex-start">
+                  <Text>{detail}</Text>
+                  <ButtonGroup alignItems="flex-start" size="sm">
+                    <Button onClick={() => onFitView?.(id as string)}>
+                      {t('workspace.dryRun.report.cta.highlight')}
+                    </Button>
+                    <Button onClick={() => openConfig?.(id as string)}>
+                      {t('workspace.dryRun.report.cta.config')}
+                    </Button>
+                  </ButtonGroup>
+                </VStack>
+              </AccordionPanel>
+            </AccordionItem>
+          )
+        })}
+      </Accordion>
+    </>
+  )
+}
+
+export default CheckPolicyReportToast
