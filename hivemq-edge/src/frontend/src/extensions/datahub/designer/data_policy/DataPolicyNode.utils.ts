f791157ede0d319654a1018edaf07d5d639904a6
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-02-13 14:53:11 +0000

    feat(19240): add validation for pipeline

--- hivemq-edge/src/frontend/src/extensions/datahub/designer/data_policy/DataPolicyNode.utils.ts
+++ hivemq-edge/src/frontend/src/extensions/datahub/designer/data_policy/DataPolicyNode.utils.ts
@@ -1,5 +1,7 @@
 import { getIncomers, Node } from 'reactflow'
 import { DataHubNodeType, DataPolicyData, DryRunResults, TopicFilterData, WorkspaceState } from '@datahub/types.ts'
+import { PolicyOperation } from '@/api/__generated__'
+import { checkValidityTransformFunction } from '@datahub/designer/operation/OperationNode.utils.ts'
 
 export function checkValidityFilter(
   dataPolicyNode: Node<DataPolicyData>,
@@ -43,4 +45,63 @@ export function checkValidityFilter(
     // TODO[XXXX] Multiple topics?
     data: incomers[0].data.topics[0],
   }
+}
+
+export function checkValidityPipeline(
+  dataPolicyNode: Node<DataPolicyData>,
+  handle: DataPolicyData.Handle,
+  store: WorkspaceState
+): DryRunResults<PolicyOperation>[] {
+  const { nodes, edges } = store
+
+  const getNextNode = (node: Node | undefined): Node | undefined => {
+    if (node) {
+      const outEdge = edges.find((edge) => edge.source === node.id)
+      if (outEdge) {
+        const nextNode = nodes.find((node) => node.id === outEdge.target)
+        if (nextNode) return nextNode
+      }
+    }
+    return undefined
+  }
+
+  const [outboundEdge] = edges.filter((edge) => edge.source === dataPolicyNode.id && edge.sourceHandle === handle)
+  if (!outboundEdge) {
+    return []
+  }
+
+  const pipeline: Node[] = []
+  let nextNode = nodes.find((node) => node.id === outboundEdge.target)
+  while (nextNode) {
+    pipeline.push(nextNode)
+    nextNode = getNextNode(nextNode)
+  }
+
+  return pipeline.map((node) => {
+    if (!node.data.functionId) {
+      return {
+        node: node,
+        error: {
+          status: 404,
+          title: node.type as string,
+          detail: 'The event has not been defined',
+          type: 'datahub.notDefined',
+          id: node.id,
+        },
+      }
+    }
+
+    if (node.data.functionId === 'DataHub.transform') {
+      return checkValidityTransformFunction(node, store)
+    }
+
+    // TODO[NVL] Serialisers need to be dealt with
+
+    const operation: PolicyOperation = {
+      functionId: node.data.functionId,
+      arguments: node.data.formData,
+      id: node.id,
+    }
+    return { node: node, data: operation }
+  })
 }
