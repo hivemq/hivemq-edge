e85d03c3e6f08c2b52be4322ed92a5e0c4bed831
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-02-15 09:22:40 +0000

    refactor(19240): refactor the validity check hook - add check for behavior policy

--- hivemq-edge/src/frontend/src/extensions/datahub/hooks/usePolicyDryRun.ts
+++ hivemq-edge/src/frontend/src/extensions/datahub/hooks/usePolicyDryRun.ts
@@ -1,14 +1,18 @@
 import { getIncomers, Node } from 'reactflow'
 
-import { BehaviorPolicy, Schema, Script } from '@/api/__generated__'
+import { PolicyOperation, Schema, Script } from '@/api/__generated__'
 
 import useDataHubDraftStore from '@datahub/hooks/useDataHubDraftStore.ts'
-import { DataHubNodeData, DataHubNodeType, DataPolicyData, DryRunResults, PolicyDryRunStatus } from '@datahub/types.ts'
 import {
-  checkValidityFilter,
-  checkValidityPipeline,
-  getSubFlow,
-} from '@datahub/designer/data_policy/DataPolicyNode.utils.ts'
+  BehaviorPolicyData,
+  DataHubNodeData,
+  DataHubNodeType,
+  DataPolicyData,
+  DryRunResults,
+  PolicyDryRunStatus,
+  TransitionData,
+} from '@datahub/types.ts'
+import { checkValidityFilter, getSubFlow } from '@datahub/designer/data_policy/DataPolicyNode.utils.ts'
 import { checkValidityPolicyValidators } from '@datahub/designer/validator/ValidatorNode.utils.ts'
 import { checkValidityClients } from '@datahub/designer/client_filter/ClientFilterNode.utils.ts'
 import { checkValidityModel } from '@datahub/designer/behavior_policy/BehaviorPolicyNode.utils.ts'
@@ -16,6 +20,12 @@ import { checkValidityTransitions } from '@datahub/designer/transition/Transitio
 import { checkValidityPipeline } from '@datahub/designer/operation/OperationNode.utils.ts'
 
 const mockDelay = (ms = 100) => new Promise((resolve) => setTimeout(resolve, ms))
+const resourceReducer = (acc: DryRunResults<unknown, never>[], oper: DryRunResults<unknown, never>) => {
+  if (oper.resources) {
+    acc.push(...oper.resources)
+  }
+  return acc
+}
 
 export const usePolicyDryRun = () => {
   const store = useDataHubDraftStore()
@@ -26,7 +36,25 @@ export const usePolicyDryRun = () => {
       ...results.node.data,
       dryRunStatus: results.error ? PolicyDryRunStatus.FAILURE : PolicyDryRunStatus.SUCCESS,
     })
-    await mockDelay()
+    await mockDelay(500)
+  }
+
+  const runPolicyChecks = async (
+    allNodes: Node<DataHubNodeData>[],
+    processedNodes: DryRunResults<unknown, never>[]
+  ) => {
+    for (const node of allNodes) {
+      onUpdateNodes<DataHubNodeData>(node.id, {
+        ...node.data,
+        dryRunStatus: PolicyDryRunStatus.RUNNING,
+      })
+      await mockDelay(100)
+    }
+
+    for (const result of processedNodes) {
+      await updateNodeStatus(result)
+    }
+    return processedNodes
   }
 
   const checkDataPolicyAsync = (dataPolicyNode: Node<DataPolicyData>) => {
@@ -37,54 +65,19 @@ export const usePolicyDryRun = () => {
     const reducedStore = { ...store, nodes: allNodes }
 
     const filter = checkValidityFilter(dataPolicyNode, reducedStore)
-
     const validators = checkValidityPolicyValidators(dataPolicyNode, reducedStore)
-
     const onSuccessPipeline = checkValidityPipeline(dataPolicyNode, DataPolicyData.Handle.ON_SUCCESS, reducedStore)
     const onErrorPipeline = checkValidityPipeline(dataPolicyNode, DataPolicyData.Handle.ON_ERROR, reducedStore)
 
-    const successResources = onSuccessPipeline.reduce((acc, oper) => {
-      if (oper.resources) {
-        acc.push(...oper.resources)
-      }
-      return acc
-    }, [] as DryRunResults<Script>[])
-    const errorResources = onErrorPipeline.reduce((acc, oper) => {
-      if (oper.resources) {
-        acc.push(...oper.resources)
-      }
-      return acc
-    }, [] as DryRunResults<Script>[])
-    const valid = validators.reduce((acc, vv) => {
-      if (vv.resources) {
-        acc.push(...vv.resources)
-      }
-      return acc
-    }, [] as DryRunResults<Schema>[])
-
+    const successResources = onSuccessPipeline.reduce(resourceReducer, [] as DryRunResults<Script>[])
+    const errorResources = onErrorPipeline.reduce(resourceReducer, [] as DryRunResults<Script>[])
+    const valid = validators.reduce(resourceReducer, [] as DryRunResults<Schema>[])
     const allResources = [...successResources, ...errorResources, ...valid]
 
-    const runPolicyChecks = async () => {
-      for (const node of allNodes) {
-        onUpdateNodes<DataHubNodeData>(node.id, {
-          ...node.data,
-          dryRunStatus: PolicyDryRunStatus.RUNNING,
-        })
-        await mockDelay()
-      }
-      await mockDelay(1000)
-
-      const processedNodes = [filter, ...validators, ...onSuccessPipeline, ...onErrorPipeline, ...allResources]
-      for (const result of processedNodes) {
-        await updateNodeStatus(result)
-      }
-      return processedNodes
-    }
-
-    return runPolicyChecks()
+    return runPolicyChecks(allNodes, [filter, ...validators, ...onSuccessPipeline, ...onErrorPipeline, ...allResources])
   }
 
-  const checkBehaviorPolicyAsync = (behaviourPolicyNode: Node<BehaviorPolicy>) => {
+  const checkBehaviorPolicyAsync = (behaviourPolicyNode: Node<BehaviorPolicyData>) => {
     const incomers = getIncomers(behaviourPolicyNode, nodes, edges).filter(
       (node) => node.type === DataHubNodeType.CLIENT_FILTER
     )
@@ -96,32 +89,27 @@ export const usePolicyDryRun = () => {
     const reducedStore = { ...store, nodes: allNodes }
 
     const clients = checkValidityClients(behaviourPolicyNode, reducedStore)
+    const model = checkValidityModel(behaviourPolicyNode)
+    const transitionResults = checkValidityTransitions(behaviourPolicyNode, reducedStore)
 
-    const runPolicyChecks = async () => {
-      for (const node of allNodes) {
-        onUpdateNodes<DataHubNodeData>(node.id, {
-          ...node.data,
-          dryRunStatus: PolicyDryRunStatus.RUNNING,
-        })
-        await mockDelay()
-      }
-      await mockDelay(1000)
-
-      const processedNodes = [clients]
-      for (const result of processedNodes) {
-        await updateNodeStatus(result)
-      }
-      return processedNodes
+    const pipelines: DryRunResults<PolicyOperation>[] = []
+    for (const transitionResult of transitionResults) {
+      const onErrorPipeline = checkValidityPipeline(
+        transitionResult.node,
+        TransitionData.Handle.OPERATION,
+        reducedStore
+      )
+      pipelines.push(...onErrorPipeline)
     }
 
-    return runPolicyChecks()
+    return runPolicyChecks(allNodes, [clients, model, ...transitionResults, ...pipelines])
   }
 
   return {
-    checkPolicyAsync: (policy: Node<BehaviorPolicy> | Node<DataPolicyData>) => {
+    checkPolicyAsync: (policy: Node<BehaviorPolicyData> | Node<DataPolicyData>) => {
       if (policy.type === DataHubNodeType.DATA_POLICY) return checkDataPolicyAsync(policy as Node<DataPolicyData>)
       if (policy.type === DataHubNodeType.BEHAVIOR_POLICY)
-        return checkBehaviorPolicyAsync(policy as Node<BehaviorPolicy>)
+        return checkBehaviorPolicyAsync(policy as Node<BehaviorPolicyData>)
       return Promise.reject(new Error(`Policy Type not supported : ${policy.type}`))
     },
   }
