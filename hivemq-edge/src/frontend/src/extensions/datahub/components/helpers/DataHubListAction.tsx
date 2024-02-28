7ede83cb07cce8b2e5b6811fcf762739f7b35fef
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-02-28 10:48:54 +0000

    feat(19709): add component for the table's actions

---
+++ hivemq-edge/src/frontend/src/extensions/datahub/components/helpers/DataHubListAction.tsx
@@ -0,0 +1,29 @@
+import { FC, MouseEventHandler } from 'react'
+import { ButtonGroup } from '@chakra-ui/react'
+import IconButton from '@/components/Chakra/IconButton.tsx'
+import { FaEdit } from 'react-icons/fa'
+import { useTranslation } from 'react-i18next'
+import { FaTrashCan } from 'react-icons/fa6'
+
+interface DataHubListActionProps {
+  onEdit?: MouseEventHandler<HTMLButtonElement>
+  onDelete?: MouseEventHandler<HTMLButtonElement>
+  isEditDisabled?: boolean
+}
+
+const DataHubListAction: FC<DataHubListActionProps> = ({ onEdit, onDelete, isEditDisabled = false }) => {
+  const { t } = useTranslation('datahub')
+  return (
+    <ButtonGroup size="sm" isAttached>
+      <IconButton
+        onClick={onEdit}
+        aria-label={t('Listings.action.edit')}
+        icon={<FaEdit />}
+        isDisabled={isEditDisabled}
+      />
+      <IconButton onClick={onDelete} aria-label={t('Listings.action.delete')} icon={<FaTrashCan />} />
+    </ButtonGroup>
+  )
+}
+
+export default DataHubListAction
