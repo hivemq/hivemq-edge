7e2055848d62c65ef697b4774e16a1dc531a2a21
Daniel Krueger <daniel.krueger@hivemq.com>
2024-02-28 13:40:32 +0100

    fix error handling > improve and streamline error handling

---
+++ hivemq-edge/src/main/java/com/hivemq/http/HttpStatus.java
@@ -0,0 +1,19 @@
+package com.hivemq.http;
+
+public class HttpStatus {
+
+    public static final int OK_200 = 200;
+    public static final int CREATED_201 = 201;
+
+    public static final int BAD_REQUEST_400 = 400;
+    public static final int FORBIDDEN_403 = 403;
+    public static final int NOT_FOUND_404 = 404;
+    public static final int GONE_410 = 410;
+    public static final int PRECONDITION_FAILED_412 = 412;
+
+
+    public static final int INTERNAL_SERVER_ERROR_500 = 500;
+    public static final int SERVICE_UNAVAILABLE_503 = 503;
+    public static final int INSUFFICIENT_STORAGE_507 = 507;
+
+}
