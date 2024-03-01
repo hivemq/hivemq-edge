a24378c8eaea6bab693c28d196e2bdf0ae7482ec
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-03-01 12:25:21 +0000

    feat(19819): add a confirmation dialogue for the deletion of an item

--- hivemq-edge/src/frontend/src/extensions/datahub/components/pages/DataHubListings.tsx
+++ hivemq-edge/src/frontend/src/extensions/datahub/components/pages/DataHubListings.tsx
@@ -1,12 +1,62 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { Tab, TabList, TabPanel, TabPanels, Tabs, Text } from '@chakra-ui/react'
+import { Tab, TabList, TabPanel, TabPanels, Tabs, Text, useDisclosure, useToast } from '@chakra-ui/react'
+import { UseMutateAsyncFunction } from '@tanstack/react-query'
+
+import ConfirmationDialog from '@/components/Modal/ConfirmationDialog.tsx'
+
 import PolicyTable from '@datahub/components/pages/PolicyTable.tsx'
 import SchemaTable from '@datahub/components/pages/SchemaTable.tsx'
 import ScriptTable from '@datahub/components/pages/ScriptTable.tsx'
+import { dataHubToastOption } from '@datahub/utils/toast.utils.ts'
+
+interface DeleteMutationRequest {
+  mutation: UseMutateAsyncFunction<void, unknown, string, unknown>
+  type: string
+  id: string
+}
 
 const DataHubListings: FC = () => {
   const { t } = useTranslation('datahub')
+  const { isOpen: isConfirmDeleteOpen, onOpen: onConfirmDeleteOpen, onClose: onConfirmDeleteClose } = useDisclosure()
+  const [deleteItem, setDeleteItem] = useState<DeleteMutationRequest | undefined>(undefined)
+  const toast = useToast()
+
+  const handleConfirmOnClose = () => {
+    onConfirmDeleteClose()
+    setDeleteItem(undefined)
+  }
+
+  const handleConfirmOnSubmit = () => {
+    console.log(deleteItem)
+    if (!deleteItem?.mutation) return
+    deleteItem
+      ?.mutation(deleteItem?.id)
+      .then(() =>
+        toast({
+          ...dataHubToastOption,
+          title: t('error.delete.title', { source: deleteItem?.type }),
+          status: 'success',
+        })
+      )
+      .catch((e) =>
+        toast({
+          ...dataHubToastOption,
+          title: t('error.delete.error', { source: deleteItem?.type }),
+          description: e.toString(),
+          status: 'error',
+        })
+      )
+  }
+
+  const handleOnDelete = (
+    mutation: UseMutateAsyncFunction<void, unknown, string, unknown>,
+    type: string,
+    id: string
+  ) => {
+    onConfirmDeleteOpen()
+    setDeleteItem({ mutation, type, id })
+  }
 
   return (
     <Tabs isLazy colorScheme="brand" data-testid="list-tabs">
@@ -25,7 +75,7 @@ const DataHubListings: FC = () => {
       <TabPanels>
         <TabPanel>
           <Text mb={3}>{t('Listings.tabs.policy.description')}</Text>
-          <PolicyTable />
+          <PolicyTable onDeleteItem={handleOnDelete} />
         </TabPanel>
         <TabPanel>
           <Text mb={3}>{t('Listings.tabs.schema.description')}</Text>
@@ -36,6 +86,13 @@ const DataHubListings: FC = () => {
           <ScriptTable />
         </TabPanel>
       </TabPanels>
+      <ConfirmationDialog
+        isOpen={isConfirmDeleteOpen}
+        onClose={handleConfirmOnClose}
+        onSubmit={handleConfirmOnSubmit}
+        message={t('modals.generics.confirmation')}
+        header={t('modals.deleteBridgeDialog.header')}
+      />
     </Tabs>
   )
 }
