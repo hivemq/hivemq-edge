f72c6dff91d8c65aba8d0dff8bf241b2f04b3fe3
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-03-01 10:18:12 +0000

    fix(19825): fix type error in policy pipeline

--- hivemq-edge/src/frontend/src/extensions/datahub/designer/transition/TransitionNode.utils.ts
+++ hivemq-edge/src/frontend/src/extensions/datahub/designer/transition/TransitionNode.utils.ts
@@ -2,7 +2,7 @@ import { getOutgoers, Node } from 'reactflow'
 
 import { BehaviorPolicyData, DataHubNodeType, DryRunResults, TransitionData, WorkspaceState } from '@datahub/types.ts'
 import { PolicyCheckErrors } from '@datahub/designer/validation.errors.ts'
-import { BehaviorPolicyOnTransition, PolicyOperation } from '@/api/__generated__'
+import { BehaviorPolicyOnEvent, BehaviorPolicyOnTransition, PolicyOperation } from '@/api/__generated__'
 import { checkValidityPipeline } from '@datahub/designer/operation/OperationNode.utils.ts'
 import { isTransitionNodeType } from '@datahub/utils/node.utils.ts'
 
@@ -43,13 +43,16 @@ export function checkValidityTransitions(
 
     // TODO[19240] Making an assumption: we can incorporate the "correct" parts of the pipeline
     const validPipeline = pipeline.filter((operation) => !!operation.data)
+    const policyEvents: BehaviorPolicyOnEvent = {
+      pipeline: validPipeline.map((operation) => operation.data as PolicyOperation),
+    }
 
     return {
       node: transition,
       data: {
         fromState: transition.data.from,
         toState: transition.data.to,
-        [transition.data.event]: { pipelines: validPipeline.map((operation) => operation.data) },
+        [transition.data.event]: policyEvents,
       },
     }
   })
