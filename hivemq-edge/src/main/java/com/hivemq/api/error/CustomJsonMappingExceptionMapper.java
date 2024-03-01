b3f0af81089cca5df6c04488607015baa8399825
Daniel Krueger <daniel.krueger@hivemq.com>
2024-03-01 11:59:21 +0100

    add custom mapper with priority

---
+++ hivemq-edge/src/main/java/com/hivemq/api/error/CustomJsonMappingExceptionMapper.java
@@ -0,0 +1,23 @@
+package com.hivemq.api.error;
+
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.hivemq.extension.sdk.api.annotations.NotNull;
+import com.hivemq.util.ErrorResponseUtil;
+
+import javax.annotation.Priority;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+@Priority(1)
+public class CustomJsonMappingExceptionMapper implements ExceptionMapper<JsonMappingException> {
+
+    @Override
+    public @NotNull Response toResponse(final @NotNull JsonMappingException exception) {
+        final String originalMessage = exception.getOriginalMessage();
+        if (originalMessage != null) {
+            return ErrorResponseUtil.invalidInput("Unable to parse JSON body: " + originalMessage);
+        } else {
+            return ErrorResponseUtil.invalidInput("Unable to parse JSON body: No more detailed information present.");
+        }
+    }
+}
