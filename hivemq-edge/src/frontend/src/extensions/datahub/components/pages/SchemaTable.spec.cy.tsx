6df11d7fe71c2306924a083bb75bc5735dec9aee
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-03-04 10:52:36 +0000

    test(19819): fix test for schemas

--- hivemq-edge/src/frontend/src/extensions/datahub/components/pages/SchemaTable.spec.cy.tsx
+++ hivemq-edge/src/frontend/src/extensions/datahub/components/pages/SchemaTable.spec.cy.tsx
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 
 import SchemaTable from '@datahub/components/pages/SchemaTable.tsx'
+import { mockSchemaTempHumidity } from '@datahub/api/hooks/DataHubSchemasService/__handlers__'
 
 describe('SchemaTable', () => {
   beforeEach(() => {
@@ -17,5 +18,20 @@ describe('SchemaTable', () => {
     cy.get('table').find('thead').find('th').eq(2).should('have.text', 'Version')
     cy.get('table').find('thead').find('th').eq(3).should('have.text', 'Created')
     cy.get('table').find('thead').find('th').eq(4).should('have.text', 'Actions')
+  })
+
+  it('should render the data', () => {
+    cy.intercept('/api/v1/data-hub/schemas', { items: [mockSchemaTempHumidity] }).as('getSchemas')
+
+    cy.mountWithProviders(<SchemaTable />)
+    cy.get('tbody tr').should('have.length', 1)
+    cy.get('tbody tr').first().as('firstItem')
+
+    cy.get('@firstItem').find('td').as('firstItemContent')
+    cy.get('@firstItemContent').should('have.length', 5)
+    cy.get('@firstItemContent').eq(0).should('have.text', 'my-schema-id')
+    cy.get('@firstItemContent').eq(1).should('have.text', 'JSON')
+    cy.get('@firstItemContent').eq(2).should('have.text', '1')
+    cy.get('@firstItemContent').eq(3).should('have.text', '5 months ago')
   })
 })
