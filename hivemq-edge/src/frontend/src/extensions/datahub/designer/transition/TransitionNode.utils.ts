513b2674e36329de09f6c1ab3a640ed79f642e26
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-03-06 15:01:51 +0000

    feat(19966): add loader for transition nodes

--- hivemq-edge/src/frontend/src/extensions/datahub/designer/transition/TransitionNode.utils.ts
+++ hivemq-edge/src/frontend/src/extensions/datahub/designer/transition/TransitionNode.utils.ts
@@ -1,10 +1,28 @@
-import { getOutgoers, Node } from 'reactflow'
+import { getOutgoers, Node, NodeAddChange, XYPosition } from 'reactflow'
 
-import { BehaviorPolicyData, DataHubNodeType, DryRunResults, TransitionData, WorkspaceState } from '@datahub/types.ts'
+import {
+  BehaviorPolicyData,
+  BehaviorPolicyType,
+  DataHubNodeType,
+  DryRunResults,
+  StateType,
+  TransitionData,
+  TransitionType,
+  WorkspaceAction,
+  WorkspaceState,
+} from '@datahub/types.ts'
 import { PolicyCheckErrors } from '@datahub/designer/validation.errors.ts'
-import { BehaviorPolicyOnEvent, BehaviorPolicyOnTransition, PolicyOperation } from '@/api/__generated__'
-import { checkValidityPipeline } from '@datahub/designer/operation/OperationNode.utils.ts'
-import { isTransitionNodeType } from '@datahub/utils/node.utils.ts'
+import {
+  BehaviorPolicy,
+  BehaviorPolicyOnEvent,
+  BehaviorPolicyOnTransition,
+  PolicyOperation,
+  Schema,
+  Script,
+} from '@/api/__generated__'
+import { checkValidityPipeline, loadBehaviorPolicyPipelines } from '@datahub/designer/operation/OperationNode.utils.ts'
+import { getNodeId, isTransitionNodeType } from '@datahub/utils/node.utils.ts'
+import { enumFromStringValue } from '@/utils/types.utils.ts'
 
 export function checkValidityTransitions(
   behaviorPolicyData: Node<BehaviorPolicyData>,
@@ -58,4 +76,65 @@ export function checkValidityTransitions(
   })
 
   return { behaviorPolicyTransitions, pipelines }
+}
+
+export const getActiveTransition = (transition: BehaviorPolicyOnTransition) => {
+  if (transition['Event.OnAny']) return 'Event.OnAny'
+  if (transition['Connection.OnDisconnect']) return 'Connection.OnDisconnect'
+  if (transition['Mqtt.OnInboundConnect']) return 'Mqtt.OnInboundConnect'
+  if (transition['Mqtt.OnInboundDisconnect']) return 'Mqtt.OnInboundDisconnect'
+  if (transition['Mqtt.OnInboundPublish']) return 'Mqtt.OnInboundPublish'
+  if (transition['Mqtt.OnInboundSubscribe']) return 'Mqtt.OnInboundSubscribe'
+  // TODO[DATAHUB] is an unknown or undefined transition allowed?
+  return undefined
+}
+
+const extractEventStates = (
+  behaviorPolicyTransition: BehaviorPolicyOnTransition
+): Pick<TransitionData, 'event' | 'from' | 'to'> => {
+  // return { event: TransitionType.ON_INBOUND_DISCONNECT, from: StateType.Publishing, to: StateType.Violated }
+  return {
+    event: enumFromStringValue(TransitionType, getActiveTransition(behaviorPolicyTransition) || ''),
+    from: enumFromStringValue(StateType, behaviorPolicyTransition.fromState),
+    to: enumFromStringValue(StateType, behaviorPolicyTransition.toState),
+  }
+}
+
+export const loadTransitions = (
+  behaviorPolicy: BehaviorPolicy,
+  schemas: Schema[],
+  scripts: Script[],
+  store: WorkspaceState & WorkspaceAction
+) => {
+  const { onNodesChange, onConnect } = store
+  const BehaviorPolicyNode = store.nodes.find((n) => n.id === behaviorPolicy.id)
+  if (!BehaviorPolicyNode) throw new Error('cannot find the behavior policy node')
+  const model = enumFromStringValue(BehaviorPolicyType, behaviorPolicy.behavior.id)
+  if (!model) throw new Error('cannot find the behavior policy node')
+
+  const position: XYPosition = {
+    x: BehaviorPolicyNode.position.x + 350,
+    y: BehaviorPolicyNode.position.y - 100,
+  }
+
+  const shiftBottom = () => {
+    position.y += 100
+    return position
+  }
+
+  for (const behaviorPolicyTransition of behaviorPolicy.onTransitions || []) {
+    const transitionNode: Node<TransitionData> = {
+      id: getNodeId(),
+      type: DataHubNodeType.TRANSITION,
+      position: { ...shiftBottom() },
+      data: {
+        model: model,
+        ...extractEventStates(behaviorPolicyTransition),
+      },
+    }
+
+    onNodesChange([{ item: transitionNode, type: 'add' } as NodeAddChange])
+    onConnect({ source: behaviorPolicy.id, target: transitionNode.id, sourceHandle: null, targetHandle: null })
+    loadBehaviorPolicyPipelines(behaviorPolicy, transitionNode, schemas, scripts, store)
+  }
 }
