87604688c2db0c20dfb04afeba65645f7725148d
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-03-08 10:05:43 +0000

    refactor(20139): fix protobuf arguments

--- hivemq-edge/src/frontend/src/extensions/datahub/designer/schema/SchemaNode.utils.ts
+++ hivemq-edge/src/frontend/src/extensions/datahub/designer/schema/SchemaNode.utils.ts
@@ -1,6 +1,8 @@
 import { Node } from 'reactflow'
-import { DryRunResults, SchemaData } from '@datahub/types.ts'
+import { DryRunResults, SchemaData, SchemaProtobufArguments, SchemaType } from '@datahub/types.ts'
 import { Schema } from '@/api/__generated__'
+import { parse, Root } from 'protobufjs'
+
 import { PolicyCheckErrors } from '@datahub/designer/validation.errors.ts'
 
 export function checkValiditySchema(schemaNode: Node<SchemaData>): DryRunResults<Schema> {
@@ -11,14 +13,31 @@ export function checkValiditySchema(schemaNode: Node<SchemaData>): DryRunResults
     }
   }
 
+  // TODO[20139] No definition of arguments in OpenAPI!
+  let args: SchemaProtobufArguments | undefined = undefined
+  if (schemaNode.data.type === SchemaType.PROTOBUF) {
+    if (!schemaNode.data.messageType)
+      return {
+        node: schemaNode,
+        error: PolicyCheckErrors.notConfigured(schemaNode, 'messageType'),
+      }
+    args = { messageType: schemaNode.data.messageType }
+
+    try {
+      // @ts-ignore
+      const root: Root = parse(schemaNode.data.schemaSource).root
+      // TODO[20139] No compilation of PROTOBUF descriptor
+    } catch (e) {}
+  }
+
   const schema: Schema = {
     // @ts-ignore TODO[19466] Id should be user-facing; Need to fix before merging!
     id: schemaNode.id,
     type: schemaNode.data.type,
     version: schemaNode.data.version,
-    schemaDefinition: btoa(JSON.stringify(schemaNode.data.schemaSource)),
-    // TODO[19240] No definition of arguments!
-    arguments: {},
+    // TODO[20139] No compiled PROTOBUF descriptor. schemaDefinition is wrong
+    schemaDefinition: btoa(schemaNode.data.schemaSource),
+    arguments: { ...args },
   }
   return { data: schema, node: schemaNode }
 }
