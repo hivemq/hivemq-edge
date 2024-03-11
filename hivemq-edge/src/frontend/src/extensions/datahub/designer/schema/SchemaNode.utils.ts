f3899238b27b2d35c66869bf329098637c687879
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-03-11 09:27:04 +0000

    refactor(20139): refactor the json and protobuf checks - add encoding of protobuf code (descriptor) - add base64 encoding - add decoding checks

--- hivemq-edge/src/frontend/src/extensions/datahub/designer/schema/SchemaNode.utils.ts
+++ hivemq-edge/src/frontend/src/extensions/datahub/designer/schema/SchemaNode.utils.ts
@@ -1,8 +1,11 @@
 import { Node } from 'reactflow'
-import { DryRunResults, SchemaData, SchemaProtobufArguments, SchemaType } from '@datahub/types.ts'
-import { Schema } from '@/api/__generated__'
-import { parse } from 'protobufjs'
+import { parse, util as protobufUtils } from 'protobufjs'
+import descriptor from 'protobufjs/ext/descriptor'
 
+import i18n from '@/config/i18n.config.ts'
+
+import { Schema } from '@/api/__generated__'
+import { DryRunResults, SchemaData, SchemaType } from '@datahub/types.ts'
 import { PolicyCheckErrors } from '@datahub/designer/validation.errors.ts'
 
 export function checkValiditySchema(schemaNode: Node<SchemaData>): DryRunResults<Schema> {
@@ -13,32 +16,67 @@ export function checkValiditySchema(schemaNode: Node<SchemaData>): DryRunResults
     }
   }
 
-  // TODO[20139] No definition of arguments in OpenAPI!
-  let args: SchemaProtobufArguments | undefined = undefined
+  if (schemaNode.data.type === SchemaType.JSON) {
+    const jsonSchema: Schema = {
+      // TODO[19466] Id should be user-facing; Need to fix before merging!
+      id: schemaNode.id,
+      type: schemaNode.data.type,
+      version: schemaNode.data.version,
+      schemaDefinition: btoa(schemaNode.data.schemaSource),
+    }
+    return { data: jsonSchema, node: schemaNode }
+  }
+
   if (schemaNode.data.type === SchemaType.PROTOBUF) {
+    // TODO[DATAHUB] Compilation of descriptor a very experimental and outdated solution.
+    //    See https://github.com/protobufjs/protobuf.js/tree/master/ext/descriptor
     if (!schemaNode.data.messageType)
       return {
         node: schemaNode,
         error: PolicyCheckErrors.notConfigured(schemaNode, 'messageType'),
       }
-    args = { messageType: schemaNode.data.messageType }
 
     try {
-      parse(schemaNode.data.schemaSource)
-      // TODO[20139] No compilation of PROTOBUF descriptor
+      const root = parse(schemaNode.data.schemaSource).root
+      // @ts-ignore No typescript definition
+      const MyMessage = root.toDescriptor('proto3')
+      const buffer = descriptor.FileDescriptorSet.encode(MyMessage).finish()
+      const encoded = protobufUtils.base64.encode(buffer, 0, buffer.length)
+
+      // verifying the double encoding
+      const encodedGraphBytes = new Uint8Array(protobufUtils.base64.length(encoded))
+      protobufUtils.base64.decode(encoded, encodedGraphBytes, 0)
+      const decodedMessage = descriptor.FileDescriptorSet.decode(encodedGraphBytes)
+      if (JSON.stringify(MyMessage) !== JSON.stringify(decodedMessage))
+        return {
+          node: schemaNode,
+          error: PolicyCheckErrors.internal(
+            schemaNode,
+            new Error(i18n.t('datahub:error.validation.protobuf.encoding') as string)
+          ),
+        }
+
+      const schema: Schema = {
+        // @ts-ignore TODO[19466] Id should be user-facing; Need to fix before merging!
+        id: schemaNode.id,
+        type: schemaNode.data.type,
+        version: schemaNode.data.version,
+        schemaDefinition: encoded,
+        // TODO[20139] No definition of arguments in OpenAPI!
+        arguments: { messageType: schemaNode.data.messageType },
+      }
+      return { data: schema, node: schemaNode }
     } catch (e) {
       console.log(e)
+      return {
+        node: schemaNode,
+        error: PolicyCheckErrors.internal(schemaNode, e),
+      }
     }
   }
 
-  const schema: Schema = {
-    // @ts-ignore TODO[19466] Id should be user-facing; Need to fix before merging!
-    id: schemaNode.id,
-    type: schemaNode.data.type,
-    version: schemaNode.data.version,
-    // TODO[20139] No compiled PROTOBUF descriptor. schemaDefinition is wrong
-    schemaDefinition: btoa(schemaNode.data.schemaSource),
-    arguments: { ...args },
+  return {
+    node: schemaNode,
+    error: PolicyCheckErrors.notConfigured(schemaNode, 'schemaSource'),
   }
-  return { data: schema, node: schemaNode }
 }
