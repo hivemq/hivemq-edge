7e70413b4e716340130f0a63b6ec7292a9e0f9d2
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-03-12 20:56:37 +0000

    feat(20275): add clear button

--- hivemq-edge/src/frontend/src/extensions/datahub/components/helpers/DraftStatus.tsx
+++ hivemq-edge/src/frontend/src/extensions/datahub/components/helpers/DraftStatus.tsx
@@ -1,13 +1,22 @@
 import { FC } from 'react'
-import { HStack, Text } from '@chakra-ui/react'
+import { ButtonGroup, HStack, Text } from '@chakra-ui/react'
+import { useTranslation } from 'react-i18next'
+import { LuTrash2 } from 'react-icons/lu'
+
+import IconButton from '@/components/Chakra/IconButton.tsx'
+
 import { NodeIcon } from '@datahub/components/helpers/index.ts'
 import { DataHubNodeType } from '@datahub/types.ts'
-import { useTranslation } from 'react-i18next'
 import useDataHubDraftStore from '@datahub/hooks/useDataHubDraftStore.ts'
 
 const DraftStatus: FC = () => {
   const { t } = useTranslation('datahub')
-  const { status, name, type } = useDataHubDraftStore()
+  const { status, name, type, reset } = useDataHubDraftStore()
+
+  function onHandleClear() {
+    // TODO[NVL] Add confirmation modal
+    reset()
+  }
 
   return (
     <HStack
@@ -21,6 +30,14 @@ const DraftStatus: FC = () => {
     >
       <NodeIcon type={DataHubNodeType.DATA_POLICY} />
       <Text>{t('workspace.toolbox.draft.status', { context: status, name: name, type })}</Text>
+      <ButtonGroup size="xs" isAttached>
+        <IconButton
+          data-testid="designer-clear-draft"
+          onClick={onHandleClear}
+          aria-label={t('workspace.controls.clear')}
+          icon={<LuTrash2 />}
+        />
+      </ButtonGroup>
     </HStack>
   )
 }
