fb9cedd4bf6ef25b65957ce80dced6cfc57dd375
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-03-14 12:43:44 +0000

    refactor(20401): refactor json download util

--- hivemq-edge/src/frontend/src/extensions/datahub/utils/date.utils.ts
+++
@@ -1,4 +0,0 @@
-import { DateTime } from 'luxon'
-
-export const downloadTimeStamp = () =>
-  DateTime.now().startOf('second').toISO({ suppressMilliseconds: true, suppressSeconds: true, includeOffset: false })
