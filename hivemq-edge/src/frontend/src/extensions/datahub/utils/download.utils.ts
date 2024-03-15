0829d02d7a02e4c9e338325b39f918219f1125dd
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-03-15 10:13:38 +0000

    Merge pull request #321
    feat(20401): Support downloading payloads and read-only canvas
    
    * feat(20401): add support for downloading policy payload as a json file
    
    * feat(20401): add support for downloading policy payload as a json file
    
    * feat(20401): update translations
    
    * feat(20401): add timestamp formatter for exports
    
    * fix(20401): fix clear handler
    
    * refactor(20401): add read-only guards to the designer
    
    * refactor(20401): refactor json download util
    
    * feat(20401): add download of a policy payload
    
    * feat(20401): add download of a schema payload
    
    * feat(20401): add download of a script payload
    
    * test(20401): fix test

---
+++ hivemq-edge/src/frontend/src/extensions/datahub/utils/download.utils.ts
@@ -0,0 +1,13 @@
+import { DateTime } from 'luxon'
+
+const downloadTimeStamp = () =>
+  DateTime.now().startOf('second').toISO({ suppressMilliseconds: true, suppressSeconds: true, includeOffset: false })
+
+export function downloadJSON<T>(name: string, source: T) {
+  const jsonString = `data:text/json;charset=utf-8,${encodeURIComponent(JSON.stringify(source))}`
+  const link = document.createElement('a')
+  link.href = jsonString
+  link.download = `${name}-${downloadTimeStamp()}.json`
+
+  link.click()
+}
