48279e7b2a24c3eeae717eb06f584a83386b96c9
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-03-20 17:18:19 +0000

    refactor(20143): streamline content of default JSONSchema

--- hivemq-edge/src/frontend/src/extensions/datahub/__test-utils__/schema.mocks.ts
+++ hivemq-edge/src/frontend/src/extensions/datahub/__test-utils__/schema.mocks.ts
@@ -9,27 +9,12 @@ message GpsCoordinates {
 
 export const MOCK_JSONSCHEMA_SCHEMA = `
 {
-   "$id":"https://example.com/geographical-location.schema.json",
    "$schema":"https://json-schema.org/draft/2020-12/schema",
-   "title":"Longitude and Latitude Values",
-   "description":"A geographical coordinate.",
-   "required":[
-      "latitude",
-      "longitude"
-   ],
+   "title":"",
+   "description":"",
+   "required":[],
    "type":"object",
-   "properties":{
-      "latitude":{
-         "type":"number",
-         "minimum":-90,
-         "maximum":90
-      },
-      "longitude":{
-         "type":"number",
-         "minimum":-180,
-         "maximum":180
-      }
-   }
+   "properties":{}
 }
 `
 
