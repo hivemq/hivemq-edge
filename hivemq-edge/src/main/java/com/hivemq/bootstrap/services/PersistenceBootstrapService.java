04c484f069d788c872babb125ff6e463f1a20070
Daniel Krueger <daniel.krueger@hivemq.com>
2024-03-26 16:21:59 +0100

    concrete > refactor bootstrapping

---
+++ hivemq-edge/src/main/java/com/hivemq/bootstrap/services/PersistenceBootstrapService.java
@@ -0,0 +1,28 @@
+package com.hivemq.bootstrap.services;
+
+import com.hivemq.configuration.service.ConfigurationService;
+import com.hivemq.edge.HiveMQCapabilityService;
+import com.hivemq.extension.sdk.api.annotations.NotNull;
+import com.hivemq.extensions.core.PersistencesService;
+
+public interface PersistenceBootstrapService extends GeneralBootstrapService {
+
+    @NotNull ConfigurationService configurationService();
+
+    @NotNull PersistencesService persistenceService();
+
+    @NotNull HiveMQCapabilityService capabilityService();
+
+
+    static PersistenceBootstrapService decorate(
+            final @NotNull GeneralBootstrapService generalBootstrapService,
+            final @NotNull ConfigurationService configurationService,
+            final @NotNull PersistencesService persistencesService,
+            final @NotNull HiveMQCapabilityService capabilityService) {
+        return PersistenceBootstrapServiceImpl.decorate(generalBootstrapService,
+                configurationService,
+                persistencesService,
+                capabilityService);
+    }
+
+}
