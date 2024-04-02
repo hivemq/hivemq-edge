c7b4d7f91e867f551923fb49d5a0abb15d35a239
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-04-02 18:03:44 +0100

    Merge pull request #340
    chore(CI): reduce verbosity of cypress output
    
    * chore(CI): update cypress actions
    
    * chore(CI): fix intercept
    
    * chore(CI): fix intercept
    
    * chore(CI): fix intercept
    
    * chore(CI): revert morgan config
    
    * chore(CI): fix intercept
    
    * chore(CI): fix unused aliases

--- hivemq-edge/src/frontend/src/modules/Dashboard/components/SidePanel.spec.cy.tsx
+++ hivemq-edge/src/frontend/src/modules/Dashboard/components/SidePanel.spec.cy.tsx
@@ -7,7 +7,8 @@ import { mockGatewayConfiguration } from '@/api/hooks/useFrontendServices/__hand
 describe('SidePanel', () => {
   beforeEach(() => {
     cy.viewport(350, 800)
-    cy.intercept('/api/v1/frontend/configuration', mockGatewayConfiguration).as('getConfig')
+    cy.intercept('/api/v1/frontend/notifications', { statusCode: 404 })
+    cy.intercept('/api/v1/frontend/configuration', mockGatewayConfiguration)
   })
 
   it('should contain the version', () => {
