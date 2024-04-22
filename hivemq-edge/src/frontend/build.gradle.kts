70169e95ca554736b0fb3cfd2ea17b5ef31598e6
Daniel Krueger <daniel.krueger@hivemq.com>
2024-04-22 08:22:57 +0200

    gradle pnpm issue > define versions in build.gradle.kts

--- hivemq-edge/src/frontend/build.gradle.kts
+++ hivemq-edge/src/frontend/build.gradle.kts
@@ -1,37 +1,39 @@
 import com.github.gradle.node.pnpm.task.PnpmTask
 
 plugins {
-    id("com.github.node-gradle.node")
+  id("com.github.node-gradle.node")
 }
 
 group = "com.hivemq"
 description = "Frontend for HiveMQ Edge"
 
 val buildFrontend by tasks.registering(PnpmTask::class) {
-    pnpmCommand.set(listOf("build", "--base=/app"))
-    dependsOn(tasks.pnpmInstall)
-    inputs.dir(project.fileTree("src"))
-    inputs.dir(project.fileTree("public"))
-    inputs.dir("node_modules")
-    inputs.files("index.html", ".env", "vite.config.ts", ".browserslistrc", "tsconfig.json", "tsconfig.node.json")
-    outputs.dir("${project.projectDir}/dist")
+  pnpmCommand.set(listOf("build", "--base=/app"))
+  dependsOn(tasks.pnpmInstall)
+  inputs.dir(project.fileTree("src"))
+  inputs.dir(project.fileTree("public"))
+  inputs.dir("node_modules")
+  inputs.files("index.html", ".env", "vite.config.ts", ".browserslistrc", "tsconfig.json", "tsconfig.node.json")
+  outputs.dir("${project.projectDir}/dist")
 }
 
 /* ******************** artifacts ******************** */
 
 val releaseBinary: Configuration by configurations.creating {
-    isCanBeConsumed = true
-    isCanBeResolved = false
-    attributes {
-        attribute(Category.CATEGORY_ATTRIBUTE, objects.named("binary"))
-        attribute(Usage.USAGE_ATTRIBUTE, objects.named("release"))
-    }
+  isCanBeConsumed = true
+  isCanBeResolved = false
+  attributes {
+    attribute(Category.CATEGORY_ATTRIBUTE, objects.named("binary"))
+    attribute(Usage.USAGE_ATTRIBUTE, objects.named("release"))
+  }
 }
 
 artifacts {
-    add(releaseBinary.name, buildFrontend)
+  add(releaseBinary.name, buildFrontend)
 }
 
 node {
-    download.set(true)
+  download.set(true)
+  version = "16.14.2"
+  pnpmVersion = "8"
 }
