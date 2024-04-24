552f7ec631c0a133960de596896d0ae75b42c32a
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-04-24 20:03:06 +0100

    test(21481): fix tests

--- hivemq-edge/src/frontend/src/extensions/datahub/components/controls/CanvasControls.spec.cy.tsx
+++ hivemq-edge/src/frontend/src/extensions/datahub/components/controls/CanvasControls.spec.cy.tsx
@@ -14,10 +14,12 @@ describe('CanvasControls', () => {
         <CanvasControls />
       </ReactFlowProvider>
     )
-    cy.get("[role='group']").find('button').should('have.length', 4)
-    cy.get("[role='group']").find('button').eq(0).should('have.attr', 'aria-label', 'Zoom in')
-    cy.get("[role='group']").find('button').eq(1).should('have.attr', 'aria-label', 'Zoom out')
-    cy.get("[role='group']").find('button').eq(2).should('have.attr', 'aria-label', 'Fit to the canvas')
-    cy.get("[role='group']").find('button').eq(3).should('have.attr', 'aria-label', 'Lock the canvas')
+    cy.get("[role='group']").find('button').as('toolbox')
+    cy.get('@toolbox').should('have.length', 5)
+    cy.get('@toolbox').eq(0).should('have.attr', 'aria-label', 'Zoom in')
+    cy.get('@toolbox').eq(1).should('have.attr', 'aria-label', 'Zoom out')
+    cy.get('@toolbox').eq(2).should('have.attr', 'aria-label', 'Fit to the canvas')
+    cy.get('@toolbox').eq(3).should('have.attr', 'aria-label', 'Lock the canvas')
+    cy.get('@toolbox').eq(4).should('have.attr', 'aria-label', 'Help using the Designer')
   })
 })
