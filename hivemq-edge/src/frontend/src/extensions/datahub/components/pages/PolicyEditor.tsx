fd27e9efc772ea245fc8fd022dfcaf177910b969
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-04-25 16:21:50 +0100

    feat(21481): add support for creating nodes on drop

--- hivemq-edge/src/frontend/src/extensions/datahub/components/pages/PolicyEditor.tsx
+++ hivemq-edge/src/frontend/src/extensions/datahub/components/pages/PolicyEditor.tsx
@@ -1,5 +1,13 @@
 import React, { FC, useCallback, useMemo, useRef, useState } from 'react'
-import ReactFlow, { Connection, Node, ReactFlowInstance, ReactFlowProvider, XYPosition } from 'reactflow'
+import ReactFlow, {
+  Connection,
+  HandleType,
+  Node,
+  NodeAddChange,
+  ReactFlowInstance,
+  ReactFlowProvider,
+  XYPosition,
+} from 'reactflow'
 import { Outlet } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
 import { Box } from '@chakra-ui/react'
@@ -8,7 +16,7 @@ import styles from './PolicyEditor.module.scss'
 
 import { CustomNodeTypes } from '@datahub/designer/mappings.tsx'
 import useDataHubDraftStore from '@datahub/hooks/useDataHubDraftStore.ts'
-import { getNodeId, getNodePayload, isValidPolicyConnection } from '@datahub/utils/node.utils.ts'
+import { getConnectedNodeFrom, getNodeId, getNodePayload, isValidPolicyConnection } from '@datahub/utils/node.utils.ts'
 import CanvasControls from '@datahub/components/controls/CanvasControls.tsx'
 import Minimap from '@datahub/components/controls/Minimap.tsx'
 import DesignerToolbox from '@datahub/components/controls/DesignerToolbox.tsx'
@@ -16,11 +24,21 @@ import ToolboxSelectionListener from '@datahub/components/controls/ToolboxSelect
 import { CopyPasteListener } from '@datahub/components/controls/CopyPasteListener.tsx'
 import CopyPasteStatus from '@datahub/components/controls/CopyPasteStatus.tsx'
 
+export type OnConnectStartParams = {
+  nodeId: string | null
+  handleId: string | null
+  handleType: HandleType | null
+}
+interface OnConnectStartParamsNode extends OnConnectStartParams {
+  type: string | undefined
+}
+
 const PolicyEditor: FC = () => {
   const { t } = useTranslation('datahub')
   const reactFlowWrapper = useRef(null)
   const [reactFlowInstance, setReactFlowInstance] = useState<ReactFlowInstance | null>(null)
   const { nodes, edges, onNodesChange, onEdgesChange, onConnect, onAddNodes } = useDataHubDraftStore()
+  const edgeConnectStart = useRef<OnConnectStartParamsNode | undefined>(undefined)
 
   const nodeTypes = useMemo(() => CustomNodeTypes, [])
 
@@ -64,6 +82,60 @@ const PolicyEditor: FC = () => {
     [onAddNodes, reactFlowInstance]
   )
 
+  const onConnectStart = useCallback(
+    (_: unknown, params: OnConnectStartParams) => {
+      const nodeFound = nodes.find((e) => e.id === params.nodeId)
+      edgeConnectStart.current = undefined
+      if (nodeFound) {
+        edgeConnectStart.current = { ...params, type: nodeFound.type }
+      }
+    },
+    [nodes]
+  )
+
+  const onConnectEnd = useCallback(
+    (event: MouseEvent | TouchEvent) => {
+      const targetElement = event.target as Element
+      const isTargetCanvas = targetElement.classList.contains('react-flow__pane')
+
+      if (isTargetCanvas && edgeConnectStart.current && reactFlowInstance) {
+        const { type, handleId, nodeId } = edgeConnectStart.current
+
+        const droppedNode = getConnectedNodeFrom(type, handleId)
+        if (droppedNode) {
+          const id = getNodeId()
+          const newNode: Node = {
+            id,
+            position: reactFlowInstance?.screenToFlowPosition({
+              x: (event as MouseEvent).clientX || (event as TouchEvent).touches[0].clientX,
+              y: (event as MouseEvent).clientY || (event as TouchEvent).touches[0].clientY,
+            }),
+            type: droppedNode.type,
+            data: getNodePayload(droppedNode.type),
+          }
+
+          const edgeConnection: Connection = droppedNode.isSource
+            ? {
+                target: nodeId,
+                targetHandle: handleId,
+                source: id,
+                sourceHandle: droppedNode.handle,
+              }
+            : {
+                source: nodeId,
+                sourceHandle: handleId,
+                target: id,
+                targetHandle: droppedNode.handle,
+              }
+
+          onAddNodes([{ item: newNode, type: 'add' } as NodeAddChange])
+          onConnect(edgeConnection)
+        }
+      }
+    },
+    [onAddNodes, onConnect, reactFlowInstance]
+  )
+
   return (
     <>
       <ReactFlowProvider>
@@ -76,15 +148,14 @@ const PolicyEditor: FC = () => {
           onNodesChange={onNodesChange}
           onEdgesChange={onEdgesChange}
           // onEdgeUpdate={onEdgeUpdate}
-          // onConnectStart={onConnectStart}
-          // onConnectEnd={onConnectEnd}
+          onConnectStart={onConnectStart}
+          onConnectEnd={onConnectEnd}
           onConnect={onConnect}
           onInit={setReactFlowInstance}
           fitView
           snapToGrid
           snapGrid={[25, 25]}
           className={styles.dataHubFlow}
-          // nodesConnectable
           onDragOver={onDragOver}
           onDrop={onDrop}
           isValidConnection={checkValidity}
