5752ba6dc70a6b1609675040be5722ce062e47e0
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-04-29 09:56:34 +0100

    Merge pull request #392
    fix(21625): Prevent cycles in the Designer graphs
    
    * fix(21625): refactor the isValidConnection routine to detect self-con…
    
    * test(21625): add tests
    
    * refactor(21625): properly return a value
    
    * test(21625): fix test

--- hivemq-edge/src/frontend/src/components/Chakra/ShortcutRenderer.spec.cy.tsx
+++ hivemq-edge/src/frontend/src/components/Chakra/ShortcutRenderer.spec.cy.tsx
@@ -19,8 +19,10 @@ describe('ShortcutRenderer', () => {
 
   it('should render multiple shortcuts', () => {
     cy.mountWithProviders(<ShortcutRenderer hotkeys="CTRL+C,Meta+V,ESC" description="This is a description" />)
+    const cmd = Cypress.platform === 'darwin' ? 'Command' : 'Ctrl'
+    console.log('XXXXXX', cmd)
 
-    cy.get('[role="term"]').should('contain.text', 'CTRL + C , Command + V , ESC')
+    cy.get('[role="term"]').should('contain.text', `CTRL + C , ${cmd} + V , ESC`)
     cy.get('kbd').should('have.length', 5)
   })
 
