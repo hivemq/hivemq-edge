1deb1e642e4c47246eb6077142988719d5bffeac
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-04-30 16:37:38 +0100

    feat(21687): add privacy consent banner to the main page

---
+++ hivemq-edge/src/frontend/src/components/PrivacyConsentBanner.tsx
@@ -0,0 +1,80 @@
+import { FC, useEffect, useRef } from 'react'
+import { useTranslation } from 'react-i18next'
+import {
+  Button,
+  ButtonGroup,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  Link,
+  Text,
+  useDisclosure,
+} from '@chakra-ui/react'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
+import { useLocalStorage } from '@uidotdev/usehooks'
+
+interface PrivacySourceGranted {
+  heapAnalytics: boolean
+  sentry: boolean
+}
+
+const PrivacyConsentBanner: FC = () => {
+  const { t } = useTranslation('components')
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const cancelRef = useRef<HTMLButtonElement>(null)
+  const [privacy, setPrivacy] = useLocalStorage<PrivacySourceGranted | undefined>('edge.privacy', undefined)
+
+  useEffect(() => {
+    const headAppId = import.meta.env.VITE_MONITORING_HEAP
+    if (headAppId && !privacy) onOpen()
+  }, [onOpen, privacy])
+
+  const handleOptIn = () => {
+    setPrivacy({ heapAnalytics: true, sentry: true })
+    onClose()
+  }
+
+  const handleOptOut = () => {
+    setPrivacy({ heapAnalytics: false, sentry: false })
+    onClose()
+  }
+
+  const handleIgnore = () => {
+    setPrivacy(undefined)
+    onClose()
+  }
+
+  return (
+    <Drawer isOpen={isOpen} placement="bottom" onClose={() => undefined} initialFocusRef={cancelRef}>
+      <DrawerOverlay />
+      <DrawerContent>
+        <DrawerCloseButton onClick={handleIgnore} />
+        <DrawerHeader>{t('PrivacyConsentBanner.header')}</DrawerHeader>
+
+        <DrawerBody>
+          <Text>{t('PrivacyConsentBanner.body')}</Text>
+        </DrawerBody>
+
+        <DrawerFooter justifyContent="space-between">
+          <ButtonGroup>
+            <Link href="https://www.hivemq.com/legal/imprint/" isExternal>
+              {t('PrivacyConsentBanner.action.moreInfo')} <ExternalLinkIcon ml={2} />
+            </Link>
+          </ButtonGroup>
+          <ButtonGroup>
+            <Button onClick={handleOptOut}>{t('PrivacyConsentBanner.action.optOut')}</Button>
+            <Button variant="primary" onClick={handleOptIn} ref={cancelRef}>
+              {t('PrivacyConsentBanner.action.optIn')}
+            </Button>
+          </ButtonGroup>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  )
+}
+
+export default PrivacyConsentBanner
