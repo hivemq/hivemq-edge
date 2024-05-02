54c43170b5decc0425c8069d8bc54414ab1d02a7
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-05-02 13:41:48 +0100

    feat(19269): refactor the template for array items - add expand/collapse management for each item - change the focus order of the content & toolbar - reorganise the toolbar

--- hivemq-edge/src/frontend/src/modules/ProtocolAdapters/components/templates/ArrayFieldItemTemplate.tsx
+++ hivemq-edge/src/frontend/src/modules/ProtocolAdapters/components/templates/ArrayFieldItemTemplate.tsx
@@ -1,6 +1,10 @@
 import { FC, useMemo } from 'react'
-import { Box, ButtonGroup, HStack } from '@chakra-ui/react'
-import { ArrayFieldTemplateItemType } from '@rjsf/utils'
+import { useTranslation } from 'react-i18next'
+import { ArrayFieldTemplateItemType, getTemplate, getUiOptions } from '@rjsf/utils'
+import { Box, ButtonGroup, FormControl, HStack, useDisclosure, VStack } from '@chakra-ui/react'
+import { LuPanelTopClose, LuPanelTopOpen } from 'react-icons/lu'
+
+import IconButton from '@/components/Chakra/IconButton.tsx'
 
 // TODO[NVL] This is NOT a good approach to add a "role"; submit a PR!
 export const ArrayFieldItemTemplate: FC<ArrayFieldTemplateItemType> = (props) => {
@@ -20,67 +24,98 @@ export const ArrayFieldItemTemplate: FC<ArrayFieldTemplateItemType> = (props) =>
     uiSchema,
     registry,
   } = props
+  const { t } = useTranslation('components')
+  const { isOpen, onToggle, getButtonProps, getDisclosureProps } = useDisclosure({ defaultIsOpen: false })
+  const name = useMemo<string>(() => {
+    const childrenFormData = children.props.formData.destination
+    if (childrenFormData) return `${children.props.name} - ${childrenFormData}`
+    return children.props.name
+  }, [])
+
   const { CopyButton, MoveDownButton, MoveUpButton, RemoveButton } = registry.templates.ButtonTemplates
   const onCopyClick = useMemo(() => onCopyIndexClick(index), [index, onCopyIndexClick])
-
   const onRemoveClick = useMemo(() => onDropIndexClick(index), [index, onDropIndexClick])
-
   const onArrowUpClick = useMemo(() => onReorderClick(index, index - 1), [index, onReorderClick])
-
   const onArrowDownClick = useMemo(() => onReorderClick(index, index + 1), [index, onReorderClick])
+
+  const renderCollapsed = () => {
+    const uiOptions = getUiOptions(uiSchema)
+    const TitleFieldTemplate = getTemplate<'TitleFieldTemplate'>('TitleFieldTemplate', registry, uiOptions)
+    return (
+      <FormControl variant="hivemq">
+        <TitleFieldTemplate
+          title={name}
+          id={children.props.name}
+          registry={registry}
+          uiSchema={uiSchema}
+          schema={props.schema}
+        />
+      </FormControl>
+    )
+  }
+  const { hidden, ...rest } = getDisclosureProps()
 
   return (
     <HStack
-      alignItems="flex-end"
+      flexDirection="row-reverse"
+      alignItems="flex-start"
       py={1}
       // TODO[NVL] This is NOT a good approach to add a "role"; submit a PR!
       role="listitem"
     >
-      <Box w="100%">{children}</Box>
       {hasToolbar && (
-        <Box>
-          <ButtonGroup
-            isAttached
-            mb={1}
-            role="toolbar"
-            // TODO[NVL] This is NOT a good approach to add a "role"; submit a PR!
-            orientation="vertical"
-          >
-            {(hasMoveUp || hasMoveDown) && (
-              <MoveUpButton
-                disabled={disabled || readonly || !hasMoveUp}
-                onClick={onArrowUpClick}
-                uiSchema={uiSchema}
-                registry={registry}
-              />
-            )}
-            {(hasMoveUp || hasMoveDown) && (
-              <MoveDownButton
-                disabled={disabled || readonly || !hasMoveDown}
-                onClick={onArrowDownClick}
-                uiSchema={uiSchema}
-                registry={registry}
-              />
-            )}
-            {hasCopy && (
-              <CopyButton
-                disabled={disabled || readonly}
-                onClick={onCopyClick}
-                uiSchema={uiSchema}
-                registry={registry}
-              />
-            )}
-            {hasRemove && (
-              <RemoveButton
-                disabled={disabled || readonly}
-                onClick={onRemoveClick}
-                uiSchema={uiSchema}
-                registry={registry}
-              />
-            )}
+        // TODO[NVL] This is NOT a good approach to add a "role"; submit a PR!
+        <VStack gap={6} role="toolbar">
+          <ButtonGroup>
+            <IconButton
+              icon={isOpen ? <LuPanelTopClose /> : <LuPanelTopOpen />}
+              onClick={onToggle}
+              {...getButtonProps()}
+              aria-label={t('rjsf.ArrayFieldItem.Buttons.expanded', { context: isOpen ? 'true' : 'false' })}
+            />
           </ButtonGroup>
-        </Box>
+          {isOpen && (
+            <ButtonGroup isAttached mb={1} orientation="vertical">
+              {(hasMoveUp || hasMoveDown) && (
+                <MoveUpButton
+                  disabled={disabled || readonly || !hasMoveUp}
+                  onClick={onArrowUpClick}
+                  uiSchema={uiSchema}
+                  registry={registry}
+                />
+              )}
+              {(hasMoveUp || hasMoveDown) && (
+                <MoveDownButton
+                  disabled={disabled || readonly || !hasMoveDown}
+                  onClick={onArrowDownClick}
+                  uiSchema={uiSchema}
+                  registry={registry}
+                />
+              )}
+              {hasCopy && (
+                <CopyButton
+                  disabled={disabled || readonly}
+                  onClick={onCopyClick}
+                  uiSchema={uiSchema}
+                  registry={registry}
+                />
+              )}
+              {hasRemove && (
+                <RemoveButton
+                  disabled={disabled || readonly}
+                  onClick={onRemoveClick}
+                  uiSchema={uiSchema}
+                  registry={registry}
+                />
+              )}
+            </ButtonGroup>
+          )}
+        </VStack>
       )}
+
+      <Box w="100%" {...rest}>
+        {isOpen ? children : renderCollapsed()}
+      </Box>
     </HStack>
   )
 }
