f00be1c36abc1ada9256c6794deed13871814d1e
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-05-13 09:56:32 +0100

    refactor(21687): add types for heap.io

---
+++ hivemq-edge/src/frontend/src/modules/Trackers/heap.d.ts
@@ -0,0 +1,23 @@
+interface IHeap {
+  track: (event: string, properties?: object) => void
+  identify: (identity: string) => void
+  resetIdentity: () => void
+  addUserProperties: (properties: object) => void
+  addEventProperties: (properties: object) => void
+  removeEventProperty: (property: string) => void
+  clearEventProperties: () => void
+  appid: string
+  userId: string
+  identity: string | null
+  config: any
+  // added to cover the load function
+  load: (id: string) => void
+}
+
+declare global {
+  interface Window {
+    heap: IHeap
+  }
+}
+
+export {}
