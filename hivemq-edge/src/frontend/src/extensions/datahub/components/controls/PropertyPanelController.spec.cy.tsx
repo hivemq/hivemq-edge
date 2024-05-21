1a42997566d43c2e515c595ae2c3a0ae49576994
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-05-21 13:50:34 +0100

    test(22182): refactor mocks and add test for readonly

--- hivemq-edge/src/frontend/src/extensions/datahub/components/controls/PropertyPanelController.spec.cy.tsx
+++ hivemq-edge/src/frontend/src/extensions/datahub/components/controls/PropertyPanelController.spec.cy.tsx
@@ -1,24 +1,45 @@
 /// <reference types="cypress" />
 
-import { ReactFlowProvider } from 'reactflow'
 import { Route, Routes } from 'react-router-dom'
-import PropertyPanelController from './PropertyPanelController.tsx'
+
+import { MockStoreWrapper } from '@datahub/__test-utils__/MockStoreWrapper.tsx'
+import PropertyPanelController from '@datahub/components/controls/PropertyPanelController.tsx'
+import { DataHubNodeType, DesignerStatus } from '@datahub/types.ts'
+
+const getWrapperWith = (status?: DesignerStatus) => {
+  const Wrapper: React.JSXElementConstructor<{ children: React.ReactNode }> = ({ children }) => {
+    return (
+      <MockStoreWrapper
+        config={{
+          initialState: {
+            status: status,
+          },
+        }}
+      >
+        <Routes>
+          <Route path="/node/:type/:nodeId" element={children}></Route>
+        </Routes>
+      </MockStoreWrapper>
+    )
+  }
+
+  return Wrapper
+}
 
 describe('PropertyPanelController', () => {
   beforeEach(() => {
-    cy.viewport(800, 600)
+    cy.viewport(800, 800)
     cy.intercept('/api/v1/management/protocol-adapters/adapters', { statusCode: 404 })
+    cy.intercept('/api/v1/data-hub/data-validation/policies', { statusCode: 404 })
   })
 
   it('should display an error panel without an action', () => {
-    cy.mountWithProviders(
-      <ReactFlowProvider>
-        <Routes>
-          <Route path="/node/:type/:nodeId" element={<PropertyPanelController />}></Route>
-        </Routes>
-      </ReactFlowProvider>,
-      { routerProps: { initialEntries: ['/node/Unknown/1'] } }
-    )
+    cy.mountWithProviders(<PropertyPanelController />, {
+      routerProps: {
+        initialEntries: [`/node/UNKNOWN/123`],
+      },
+      wrapper: getWrapperWith(DesignerStatus.DRAFT),
+    })
 
     cy.getByTestId('node-editor-content').should('be.visible')
     cy.getByTestId('node-editor-name').should('contain.text', 'Unknown type')
@@ -30,17 +51,25 @@ describe('PropertyPanelController', () => {
   })
 
   it('should render a panel with a submit button', () => {
-    cy.mountWithProviders(
-      <ReactFlowProvider>
-        <Routes>
-          <Route path="/node/:type/:nodeId" element={<PropertyPanelController />}></Route>
-        </Routes>
-      </ReactFlowProvider>,
-      { routerProps: { initialEntries: ['/node/INTERNAL/1'] } }
-    )
+    cy.mountWithProviders(<PropertyPanelController />, {
+      routerProps: {
+        initialEntries: [`/node/${DataHubNodeType.INTERNAL}/123`],
+      },
+      wrapper: getWrapperWith(DesignerStatus.DRAFT),
+    })
 
     cy.getByTestId('node-editor-under-construction').should('not.exist')
     cy.getByTestId('loading-spinner').should('be.visible')
     cy.get('button[type="submit"]').should('be.visible')
+  })
+
+  it('should disable the configuration panel if readonly', () => {
+    cy.mountWithProviders(<PropertyPanelController />, {
+      routerProps: {
+        initialEntries: ['/node/TOPIC_FILTER/123'],
+      },
+      wrapper: getWrapperWith(DesignerStatus.LOADED),
+    })
+    cy.get('button[type="submit"]').should('be.visible').should('be.disabled')
   })
 })
