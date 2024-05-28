d03710343fefe45e86c34f8db3dc718287f0004d
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-05-28 14:11:41 +0100

    feat(22552): add modal with stepper to the CTA trigger

--- hivemq-edge/src/frontend/src/components/rjsf/BatchSubscription/BatchUploadButton.tsx
+++ hivemq-edge/src/frontend/src/components/rjsf/BatchSubscription/BatchUploadButton.tsx
@@ -1,23 +1,66 @@
 import { FC } from 'react'
-import { LuHardDriveUpload } from 'react-icons/lu'
-import { RJSFSchema } from '@rjsf/utils/src/types.ts'
-import { Button, useDisclosure } from '@chakra-ui/react'
 import { useTranslation } from 'react-i18next'
+import { RJSFSchema } from '@rjsf/utils/src/types.ts'
+import {
+  Button,
+  ButtonGroup,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from '@chakra-ui/react'
+import { LuHardDriveUpload } from 'react-icons/lu'
+import { UploadStepper } from '@/components/rjsf/BatchSubscription/components/UploadStepper.tsx'
+import { useBatchModeSteps } from '@/components/rjsf/BatchSubscription/hooks/useBatchModeSteps.tsx'
 
 export interface BatchSubscriptionProps {
   schema: RJSFSchema
 }
 
-const BatchUploadButton: FC<BatchSubscriptionProps> = ({ schema }) => {
-  const { onOpen } = useDisclosure()
+const BatchUploadButton: FC<BatchSubscriptionProps> = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
   const { t } = useTranslation('components')
-
-  console.log('XXXXXX schema', schema)
+  const { activeStep, steps, goToNext, goToPrevious } = useBatchModeSteps()
 
   return (
-    <Button colorScheme="red" onClick={onOpen} leftIcon={<LuHardDriveUpload />}>
-      {t('rjsf.BatchUpload.Button')}
-    </Button>
+    <>
+      <Button colorScheme="red" onClick={onOpen} leftIcon={<LuHardDriveUpload />}>
+        {t('rjsf.batchUpload.button')}
+      </Button>
+      <Modal
+        size="3xl"
+        closeOnOverlayClick={false}
+        // initialFocusRef={cancelRef}
+        // finalFocusRef={cancelRef}
+        isOpen={isOpen}
+        onClose={onClose}
+        scrollBehavior="inside"
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{t('rjsf.batchUpload.modal.header')}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <UploadStepper steps={steps} activeStep={activeStep} />
+          </ModalBody>
+          <ModalFooter>
+            <ButtonGroup>
+              <Button onClick={goToPrevious} isDisabled={activeStep === 0}>
+                {t('rjsf.batchUpload.modal.action.previous')}
+              </Button>
+              <Button onClick={goToNext} isDisabled={activeStep === steps.length}>
+                {t('rjsf.batchUpload.modal.action.next')}
+              </Button>
+              <Button onClick={onClose}>{t('rjsf.batchUpload.modal.action.cancel')}</Button>
+            </ButtonGroup>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   )
 }
 
