dd1ae5b1b340c51a01ead9260c6b843b7d44cec7
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-05-29 11:21:43 +0100

    feat(22554): add utils for the dropzone

---
+++ hivemq-edge/src/frontend/src/components/rjsf/BatchSubscription/utils/dropzone.utils.ts
@@ -0,0 +1,22 @@
+import { Accept } from 'react-dropzone'
+
+// TODO[NVl] This should be configurable in the uiSchema of each adapter, customising the supported formats
+export const acceptMimeTypes: Accept = {
+  'application/vnd.ms-excel': ['.xls'],
+  'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet': ['.xlsx'],
+  'text/csv': ['.csv'],
+}
+
+export const readFileAsync = (file: File) => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader()
+
+    reader.onload = () => {
+      resolve(reader.result)
+    }
+
+    reader.onerror = reject
+
+    reader.readAsArrayBuffer(file)
+  })
+}
