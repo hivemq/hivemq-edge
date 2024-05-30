6351f450bc35c0a79eb57d0200269d9029b26e17
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-05-30 11:21:28 +0100

    Merge pull request #439
    eat(22552): Create infrastructure and UX for batch creation of subscriptions
    
    * feat(22552): add batch mode CTA to designated array-based properties
    
    * feat(22552): add hook for steps management
    
    * feat(22552): add stepper
    
    * feat(22552): add placeholder for data source upload
    
    * feat(22552): add placeholder for column matching
    
    * feat(22552): add placeholder for subscription validation
    
    * feat(22552): add modal with stepper to the CTA trigger
    
    * feat(22552): update translations
    
    * feat(22552): add support for step completion
    
    * test(22552): add tests
    
    * refactor(22552): refactor renderer for steps
    
    * feat(22552): refactor stepper to support final confirmation step
    
    * fix(22552): fix prev/next conditions
    
    * test(22552): add tests
    
    * refactor(22552): fix translations
    
    * test(22552): add tests
    
    * Merge pull request #440

---
+++ hivemq-edge/src/frontend/src/components/rjsf/BatchSubscription/BatchUploadButton.spec.cy.tsx
@@ -0,0 +1,25 @@
+import BatchUploadButton from '@/components/rjsf/BatchSubscription/BatchUploadButton.tsx'
+
+const MOCK_SCHEMA = {}
+
+describe('BatchUploadButton', () => {
+  beforeEach(() => {
+    cy.viewport(800, 800)
+  })
+  it('should renders the CTA', () => {
+    cy.mountWithProviders(<BatchUploadButton schema={MOCK_SCHEMA} />)
+
+    cy.getByTestId('array-field-batch-cta').should('contain.text', 'Upload').click()
+    cy.get('section[role="dialog"]').should('exist')
+    cy.get('header').should('contain.text', 'Bulk Subscription Creation')
+    cy.get('header + button').as('closeModal').should('have.attr', 'aria-label', 'Close')
+
+    cy.get('footer > [role="group"] > button').as('stepperButton').should('have.length', 3)
+    cy.get('@stepperButton').eq(0).should('have.text', 'Previous').should('be.disabled')
+    cy.get('@stepperButton').eq(1).should('have.text', 'Next')
+    cy.get('@stepperButton').eq(2).should('have.text', 'Cancel')
+
+    cy.get('@closeModal').click()
+    cy.get('section[role="dialog"]').should('not.exist')
+  })
+})
---
+++ hivemq-edge/src/frontend/src/components/rjsf/BatchSubscription/components/ConfirmStep.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react'
+
+const ConfirmStep: FC = () => {
+  return <div></div>
+}
+
+export default ConfirmStep
