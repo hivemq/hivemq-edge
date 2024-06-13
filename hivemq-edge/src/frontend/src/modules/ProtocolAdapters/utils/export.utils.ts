75c725fc292d7f21e31c314c39794b7b22e2345e
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-06-13 11:33:10 +0100

    feat(23029): refactor definition of configuration export

--- hivemq-edge/src/frontend/src/modules/ProtocolAdapters/utils/export.utils.ts
+++ hivemq-edge/src/frontend/src/modules/ProtocolAdapters/utils/export.utils.ts
@@ -1,8 +1,11 @@
 import { ExportFormat } from '@/modules/ProtocolAdapters/types.ts'
+import { downloadJSON, downloadTimeStamp } from '@datahub/utils/download.utils.ts'
 
 export const adapterExportFormats: ExportFormat[] = [
   {
     value: ExportFormat.Type.CONFIGURATION,
+    formats: ['.json'],
+    downloader: (name, _, source) => downloadJSON<JsonNode>(name, source),
   },
   {
     value: ExportFormat.Type.SUBSCRIPTIONS,
