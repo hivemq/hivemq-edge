ea7427f10553f37d02d1e810fa96ab2ed89cd708
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-06-13 11:36:10 +0100

    feat(23029): refactor definition of subscription export

--- hivemq-edge/src/frontend/src/modules/ProtocolAdapters/utils/export.utils.ts
+++ hivemq-edge/src/frontend/src/modules/ProtocolAdapters/utils/export.utils.ts
@@ -1,4 +1,8 @@
 import { ExportFormat } from '@/modules/ProtocolAdapters/types.ts'
+import { Adapter, JsonNode, ProtocolAdapter } from '@/api/__generated__'
+import { getTopicPaths, TOPIC_PATH_ITEMS_TOKEN } from '@/modules/Workspace/utils/topics-utils.ts'
+import { AdapterExportError, ExportFormat } from '@/modules/ProtocolAdapters/types.ts'
+import { acceptMimeTypes } from '@/components/rjsf/BatchSubscription/utils/config.utils.ts'
 import { downloadJSON, downloadTimeStamp } from '@datahub/utils/download.utils.ts'
 
 export const adapterExportFormats: ExportFormat[] = [
@@ -9,6 +13,14 @@ export const adapterExportFormats: ExportFormat[] = [
   },
   {
     value: ExportFormat.Type.SUBSCRIPTIONS,
-    formats: ['xlsx, csv'],
+    formats: Object.values(acceptMimeTypes).flat(),
+    isDisabled: (protocol?: ProtocolAdapter) => {
+      if (!protocol) return true
+      const paths = getTopicPaths(protocol.configSchema || {})
+      return !paths.some((path) => path.includes(`.${TOPIC_PATH_ITEMS_TOKEN}.`))
+    },
+    downloader: (name, ext, source, protocol) => {
+      downloadTableData(`${name}-${downloadTimeStamp()}${ext}`, source, protocol)
+    },
   },
 ]
