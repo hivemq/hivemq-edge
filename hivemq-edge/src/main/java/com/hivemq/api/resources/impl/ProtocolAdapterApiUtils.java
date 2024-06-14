6183ddaacef2a2734d847112435d9d50b8693e76
Daniel Krüger <45030029+DC2-DanielKrueger@users.noreply.github.com>
2024-06-14 08:22:42 +0200

    fix default ui-schema (#459)
    * fix default ui-schema
    
    * fix default ui-schema

--- hivemq-edge/src/main/java/com/hivemq/api/resources/impl/ProtocolAdapterApiUtils.java
+++ hivemq-edge/src/main/java/com/hivemq/api/resources/impl/ProtocolAdapterApiUtils.java
@@ -53,113 +53,11 @@ public class ProtocolAdapterApiUtils {
 
     private static final Logger LOG = LoggerFactory.getLogger(ProtocolAdapterApiUtils.class);
     private static final String DEFAULT_SCHEMA = "{\n" +
-            "  \"ui:tabs\": [\n" +
-            "    {\n" +
-            "      \"id\": \"coreFields\",\n" +
-            "      \"title\": \"Core Fields\",\n" +
-            "      \"properties\": [\"id\", \"port\", \"host\", \"uri\", \"url\", \"timeout\"]\n" +
-            "    },\n" +
-            "    {\n" +
-            "      \"id\": \"subFields\",\n" +
-            "      \"title\": \"Subscription\",\n" +
-            "      \"properties\": [\"subscriptions\"]\n" +
-            "    },\n" +
-            "    {\n" +
-            "      \"id\": \"security\",\n" +
-            "      \"title\": \"protocolAdapter.uiSchema.groups.security\",\n" +
-            "      \"properties\": [\"security\", \"tls\"]\n" +
-            "    },\n" +
-            "    {\n" +
-            "      \"id\": \"publishing\",\n" +
-            "      \"title\": \"protocolAdapter.uiSchema.groups.publishing\",\n" +
-            "      \"properties\": [\n" +
-            "        \"maxPollingErrorsBeforeRemoval\",\n" +
-            "        \"publishChangedDataOnly\",\n" +
-            "        \"publishingInterval\",\n" +
-            "        \"pollingIntervalMillis\",\n" +
-            "        \"destination\",\n" +
-            "        \"qos\",\n" +
-            "        \"minValue\",\n" +
-            "        \"maxValue\"\n" +
-            "      ]\n" +
-            "    },\n" +
-            "    {\n" +
-            "      \"id\": \"authentication\",\n" +
-            "      \"title\": \"protocolAdapter.uiSchema.groups.authentication\",\n" +
-            "      \"properties\": [\"auth\"]\n" +
-            "    },\n" +
-            "    {\n" +
-            "      \"id\": \"http\",\n" +
-            "      \"title\": \"protocolAdapter.uiSchema.groups.http\",\n" +
-            "      \"properties\": [\n" +
-            "        \"httpRequestMethod\",\n" +
-            "        \"httpRequestBodyContentType\",\n" +
-            "        \"httpRequestBody\",\n" +
-            "        \"httpHeaders\",\n" +
-            "        \"httpConnectTimeout\",\n" +
-            "        \"httpRequestBodyContentType\",\n" +
-            "        \"assertResponseIsJson\",\n" +
-            "        \"httpPublishSuccessStatusCodeOnly\",\n" +
-            "        \"allowUntrustedCertificates\"\n" +
-            "      ]\n" +
-            "    },\n" +
-            "    {\n" +
-            "      \"id\": \"ads\",\n" +
-            "      \"title\": \"protocolAdapter.uiSchema.groups.ads\",\n" +
-            "      \"properties\": [\"sourceAmsPort\", \"targetAmsPort\", \"sourceAmsNetId\", \"targetAmsNetId\"]\n" +
-            "    },\n" +
-            "    {\n" +
-            "      \"id\": \"eip\",\n" +
-            "      \"title\": \"protocolAdapter.uiSchema.groups.eip\",\n" +
-            "      \"properties\": [\"slot\", \"backplane\"]\n" +
-            "    },\n" +
-            "    {\n" +
-            "      \"id\": \"s7advanced\",\n" +
-            "      \"title\": \"protocolAdapter.uiSchema.groups.s7advanced\",\n" +
-            "      \"properties\": [\n" +
-            "        \"controllerType\",\n" +
-            "        \"remoteRack\",\n" +
-            "        \"remoteSlot\",\n" +
-            "        \"ping\",\n" +
-            "        \"pingTime\",\n" +
-            "        \"maxAmqCaller\",\n" +
-            "        \"maxAmqCallee\",\n" +
-            "        \"remoteTsap\",\n" +
-            "        \"remoteRack2\",\n" +
-            "        \"remoteSlot2\",\n" +
-            "        \"pduSize\",\n" +
-            "        \"retryTime\",\n" +
-            "        \"retryTimeout\",\n" +
-            "        \"readTimeout\"\n" +
-            "      ]\n" +
-            "    }\n" +
-            "  ],\n" +
-            "  \"ui:submitButtonOptions\": {\n" +
-            "    \"norender\": true\n" +
-            "  },\n" +
+            "  \"ui:tabs\": [],\n" +
             "  \"id\": {\n" +
             "    \"ui:disabled\": true\n" +
             "  },\n" +
-            "  \"port\": {\n" +
-            "    \"ui:widget\": \"updown\"\n" +
-            "  },\n" +
-            "  \"httpRequestBody\": {\n" +
-            "    \"ui:widget\": \"textarea\"\n" +
-            "  },\n" +
-            "  \"subscriptions\": {\n" +
-            "    \"ui:batchMode\": true,\n" +
-            "    \"items\": {\n" +
-            "      \"ui:order\": [\"node\", \"holding-registers\", \"mqtt-topic\", \"destination\", \"qos\", \"*\"],\n" +
-            "      \"ui:collapsable\": {\n" +
-            "        \"titleKey\": \"destination\"\n" +
-            "      }\n" +
-            "    }\n" +
-            "  },\n" +
-            "  \"auth\": {\n" +
-            "    \"basic\": {\n" +
-            "      \"ui:order\": [\"username\", \"password\", \"*\"]\n" +
-            "    }\n" +
-            "  }\n" +
+            "  \"ui:order\": [\"id\",  \"*\"]\n" +
             "}";
 
     /**
