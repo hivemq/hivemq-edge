eac7c8c04d36da02cc6a68a3068f41dd4a92dbc4
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-06-18 13:10:29 +0100

    chore(22908): cleaning proxy log

--- hivemq-edge/src/frontend/vite.config.ts
+++ hivemq-edge/src/frontend/vite.config.ts
@@ -19,17 +19,17 @@ export default defineConfig({
         target: 'http://127.0.0.1:8080/',
         changeOrigin: true,
         secure: false,
-        configure: (proxy, _options) => {
-          proxy.on('error', (err, _req, _res) => {
-            console.log('proxy error', err, _req)
-          })
-          proxy.on('proxyReq', (proxyReq, req, _res) => {
-            console.log('Sending Request to the Target:', req.method, req.url, proxyReq.path)
-          })
-          proxy.on('proxyRes', (proxyRes, req, _res) => {
-            console.log('Received Response from the Target:', proxyRes.statusCode, req.url)
-          })
-        },
+        // configure: (proxy, _options) => {
+        //   proxy.on('error', (err, _req, _res) => {
+        //     console.log('proxy error', err, _req)
+        //   })
+        //   proxy.on('proxyReq', (proxyReq, req, _res) => {
+        //     console.log('Sending Request to the Target:', req.method, req.url, proxyReq.path)
+        //   })
+        //   proxy.on('proxyRes', (proxyRes, req, _res) => {
+        //     console.log('Received Response from the Target:', proxyRes.statusCode, req.url)
+        //   })
+        // },
       },
     },
   },
