d2d3b22144b51844553c0e177c14a67cadcce34f
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-06-19 17:01:16 +0100

    refactor(22908): remove dependency from external lib and add breadcrumb to metadata

--- hivemq-edge/src/frontend/src/components/rjsf/Widgets/types.ts
+++ hivemq-edge/src/frontend/src/components/rjsf/Widgets/types.ts
@@ -1,3 +1,20 @@
 import type { ObjectNode } from '@/api/__generated__'
 
-export type FlatObjectNode = Omit<ObjectNode, 'children'>
+export interface FlatObjectNode extends Omit<ObjectNode, 'children'> {
+  breadcrumb?: string[]
+}
+
+export interface INode<M> {
+  /** A non-negative integer that uniquely identifies the node */
+  id: string
+  /** Used to match on key press */
+  name: string
+  /** An array with the ids of the children nodes */
+  children: string[]
+  /** The parent of the node; null for the root node */
+  parent: string | null
+  /** Used to indicated whether a node is branch to be able load async data onExpand*/
+  isBranch?: boolean
+  /** User-defined metadata */
+  metadata?: M
+}
