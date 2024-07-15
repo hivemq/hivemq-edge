9560c89b18d6bba10b13124c073b8fb50d695e25
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-07-15 18:09:24 +0100

    test(cypress): enable videos

--- hivemq-edge/src/frontend/cypress.config.ts
+++ hivemq-edge/src/frontend/cypress.config.ts
@@ -4,6 +4,7 @@ import installLogsPrinter from 'cypress-terminal-report/src/installLogsPrinter'
 export default defineConfig({
   retries: { runMode: 2, openMode: 0 },
   e2e: {
+    video: true,
     baseUrl: 'http://localhost:3000',
     setupNodeEvents(on) {
       installLogsPrinter(on, {
@@ -14,6 +15,8 @@ export default defineConfig({
   },
 
   component: {
+    video: true,
+
     setupNodeEvents(on) {
       installLogsPrinter(on, {
         printLogsToConsole: 'onFail',
