cc3753295c9b3fe52fd7b180f4feb2a2ee4ff711
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-07-15 10:00:21 +0100

    refactor(24143): add mappings for node schemas

--- hivemq-edge/src/frontend/src/extensions/datahub/designer/mappings.tsx
+++ hivemq-edge/src/frontend/src/extensions/datahub/designer/mappings.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react'
 import { NodeProps } from 'reactflow'
+import { RJSFSchema } from '@rjsf/utils'
 
 import { DataHubNodeType, PanelProps } from '@datahub/types.ts'
 import { TopicFilterPanel } from '@datahub/designer/topic_filter/TopicFilterPanel.tsx'
@@ -22,6 +23,15 @@ import { TransitionNode } from '@datahub/designer/transition/TransitionNode.tsx'
 
 import LoaderSpinner from '@/components/Chakra/LoaderSpinner.tsx'
 import { DataPolicyPanel } from '@datahub/designer/data_policy/DataPolicyPanel.tsx'
+import { MOCK_TOPIC_FILTER_SCHEMA } from '@datahub/designer/topic_filter/TopicFilterData.ts'
+import { MOCK_BEHAVIOR_POLICY_SCHEMA } from '@datahub/designer/behavior_policy/BehaviorPolicySchema.ts'
+import { MOCK_CLIENT_FILTER_SCHEMA } from '@datahub/designer/client_filter/ClientFilterSchema.ts'
+import { MOCK_DATA_POLICY_SCHEMA } from '@datahub/designer/data_policy/DataPolicySchema.ts'
+import { MOCK_OPERATION_SCHEMA } from '@datahub/designer/operation/OperationData.ts'
+import { MOCK_SCHEMA_SCHEMA } from '@datahub/designer/schema/SchemaData.ts'
+import { MOCK_FUNCTION_SCHEMA } from '@datahub/designer/script/FunctionData.ts'
+import { MOCK_TRANSITION_SCHEMA } from '@datahub/designer/transition/TransitionData.ts'
+import { MOCK_VALIDATOR_SCHEMA } from '@datahub/designer/validator/DataPolicyValidator.ts'
 
 /**
  * Used in the side panel editor to render the content of the selected node
@@ -52,4 +62,16 @@ export const CustomNodeTypes: Record<string, FC<NodeProps>> = {
   [DataHubNodeType.FUNCTION]: FunctionNode,
   [DataHubNodeType.BEHAVIOR_POLICY]: BehaviorPolicyNode,
   [DataHubNodeType.TRANSITION]: TransitionNode,
+}
+
+export const CustomNodeJSONSchema: Record<string, RJSFSchema> = {
+  [DataHubNodeType.TOPIC_FILTER]: MOCK_TOPIC_FILTER_SCHEMA.schema,
+  [DataHubNodeType.CLIENT_FILTER]: MOCK_CLIENT_FILTER_SCHEMA.schema,
+  [DataHubNodeType.DATA_POLICY]: MOCK_DATA_POLICY_SCHEMA.schema,
+  [DataHubNodeType.VALIDATOR]: MOCK_VALIDATOR_SCHEMA,
+  [DataHubNodeType.SCHEMA]: MOCK_SCHEMA_SCHEMA.schema,
+  [DataHubNodeType.OPERATION]: MOCK_OPERATION_SCHEMA.schema,
+  [DataHubNodeType.FUNCTION]: MOCK_FUNCTION_SCHEMA.schema,
+  [DataHubNodeType.BEHAVIOR_POLICY]: MOCK_BEHAVIOR_POLICY_SCHEMA.schema,
+  [DataHubNodeType.TRANSITION]: MOCK_TRANSITION_SCHEMA.schema,
 }
