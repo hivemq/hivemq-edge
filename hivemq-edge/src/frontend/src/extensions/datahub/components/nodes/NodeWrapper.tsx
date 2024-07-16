bda92088a868ade0f2a80dd32a4dbdecb3ac2e2a
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-07-16 13:06:58 +0100

    feat(24140): add the toolbar to every datahub nodes

--- hivemq-edge/src/frontend/src/extensions/datahub/components/nodes/NodeWrapper.tsx
+++ hivemq-edge/src/frontend/src/extensions/datahub/components/nodes/NodeWrapper.tsx
@@ -1,11 +1,13 @@
 import { FC, ReactNode, useEffect, useMemo, useState } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
 import { NodeProps } from 'reactflow'
-
 import { Avatar, BoxProps, Card, CardBody, CardBodyProps, HStack } from '@chakra-ui/react'
-import { DataHubNodeData, PolicyDryRunStatus } from '@datahub/types.ts'
 
+import NodeDatahubToolbar from '@datahub/components/nodes/NodeDatahubToolbar.tsx'
+import { DataHubNodeData, PolicyDryRunStatus } from '@datahub/types.ts'
 import { getDryRunStatusIcon } from '@datahub/utils/node.utils.ts'
+import { parseHotkey } from '@datahub/utils/hotkeys.utils.ts'
+import { DATAHUB_HOTKEY } from '@datahub/utils/datahub.utils.ts'
 
 interface NodeWrapperProps extends NodeProps<DataHubNodeData> {
   children: ReactNode
@@ -24,6 +26,23 @@ export const NodeWrapper: FC<NodeWrapperProps> = ({ selected, children, route, w
     if (!selected) setInternalSelection(false)
   }, [selected])
 
+  const onHandleCopy = () => {
+    document.dispatchEvent(new KeyboardEvent('keydown', parseHotkey(DATAHUB_HOTKEY.COPY)))
+  }
+
+  const onHandleDelete = () => {
+    document.dispatchEvent(new KeyboardEvent('keydown', parseHotkey(DATAHUB_HOTKEY.DELETE)))
+  }
+
+  const onHandleEdit = (event: React.BaseSyntheticEvent) => {
+    if (internalSelection) {
+      navigate(route, { state: { origin: pathname } })
+      event.preventDefault()
+      event.stopPropagation()
+    }
+    setInternalSelection(true)
+  }
+
   const selectedStyle: Partial<BoxProps> = {
     boxShadow: 'var(--chakra-shadows-outline)',
   }
@@ -37,37 +56,41 @@ export const NodeWrapper: FC<NodeWrapperProps> = ({ selected, children, route, w
   }
 
   const isDryRun = data.dryRunStatus !== undefined && data.dryRunStatus !== PolicyDryRunStatus.IDLE
+  const isToolbarEnabled = import.meta.env.VITE_FLAG_DATAHUB_SHOW_TOOLBAR === 'true'
 
   return (
-    <Card
-      variant="elevated"
-      {...(data.dryRunStatus === PolicyDryRunStatus.FAILURE ? { ...errorStyle } : {})}
-      {...(data.dryRunStatus === PolicyDryRunStatus.SUCCESS ? { ...successStyle } : {})}
-      {...(selected ? { ...selectedStyle } : {})}
-      size="sm"
-      onClick={(event) => {
-        if (internalSelection) {
-          navigate(route, { state: { origin: pathname } })
-          event.preventDefault()
-          event.stopPropagation()
-        }
-        setInternalSelection(true)
-      }}
-    >
-      {isDryRun && (
-        <Avatar
-          position="absolute"
-          left="-1rem"
-          top="-1rem"
-          size="sm"
-          icon={<CheckIcon fontSize="1.2rem" />}
-          {...(data.dryRunStatus === PolicyDryRunStatus.FAILURE ? { ...errorStyle, background: 'red.500' } : {})}
-          {...(data.dryRunStatus === PolicyDryRunStatus.SUCCESS ? { ...successStyle, background: 'green.500' } : {})}
+    <>
+      {isToolbarEnabled && (
+        <NodeDatahubToolbar
+          isVisible={selected}
+          onCopy={onHandleCopy}
+          onDelete={onHandleDelete}
+          onEdit={onHandleEdit}
         />
       )}
-      <CardBody as={HStack} {...wrapperProps}>
-        {children}
-      </CardBody>
-    </Card>
+      <Card
+        variant="elevated"
+        {...(data.dryRunStatus === PolicyDryRunStatus.FAILURE ? { ...errorStyle } : {})}
+        {...(data.dryRunStatus === PolicyDryRunStatus.SUCCESS ? { ...successStyle } : {})}
+        {...(selected ? { ...selectedStyle } : {})}
+        size="sm"
+        onClick={onHandleEdit}
+      >
+        {isDryRun && (
+          <Avatar
+            position="absolute"
+            left="-1rem"
+            top="-1rem"
+            size="sm"
+            icon={<CheckIcon fontSize="1.2rem" />}
+            {...(data.dryRunStatus === PolicyDryRunStatus.FAILURE ? { ...errorStyle, background: 'red.500' } : {})}
+            {...(data.dryRunStatus === PolicyDryRunStatus.SUCCESS ? { ...successStyle, background: 'green.500' } : {})}
+          />
+        )}
+        <CardBody as={HStack} {...wrapperProps}>
+          {children}
+        </CardBody>
+      </Card>
+    </>
   )
 }
