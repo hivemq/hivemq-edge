3041cd3d64b5d63f9c9174f05f58b0811f8e1e91
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-07-16 14:07:53 +0100

    refactor(24140): add dynamic size for buttons

--- hivemq-edge/src/frontend/src/extensions/datahub/components/nodes/NodeDatahubToolbar.tsx
+++ hivemq-edge/src/frontend/src/extensions/datahub/components/nodes/NodeDatahubToolbar.tsx
@@ -1,9 +1,10 @@
-import { FC } from 'react'
+import { FC, useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
 import { ButtonGroup, ButtonGroupProps } from '@chakra-ui/react'
 import { LuCopy, LuDelete, LuFileEdit } from 'react-icons/lu'
 
 import IconButton from '@/components/Chakra/IconButton.tsx'
+import { useStore } from 'reactflow'
 
 interface NodeToolbarProps extends ButtonGroupProps {
   onCopy?: (event: React.BaseSyntheticEvent) => void
@@ -13,9 +14,17 @@ interface NodeToolbarProps extends ButtonGroupProps {
 
 const NodeDatahubToolbar: FC<NodeToolbarProps> = (props) => {
   const { t } = useTranslation('datahub')
+  const zoomFactor = useStore((s) => s.transform[2])
+
+  const getToolbarSize = useMemo<string>(() => {
+    if (zoomFactor >= 1.5) return 'lg'
+    if (zoomFactor >= 1) return 'md'
+    if (zoomFactor >= 0.75) return 'sm'
+    return 'xs'
+  }, [zoomFactor])
 
   return (
-    <ButtonGroup size="xs" variant="solid" colorScheme="gray" isAttached {...props}>
+    <ButtonGroup size={getToolbarSize} variant="solid" colorScheme="gray" isAttached {...props}>
       <IconButton
         icon={<LuFileEdit />}
         data-testid="node-toolbar-edit"
