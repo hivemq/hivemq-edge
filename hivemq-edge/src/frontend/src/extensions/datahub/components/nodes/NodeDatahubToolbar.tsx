b2e72e25328d51d9091721e52fd36c90d2edcab9
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-07-16 13:41:20 +0100

    refactor(24140): fix translations

--- hivemq-edge/src/frontend/src/extensions/datahub/components/nodes/NodeDatahubToolbar.tsx
+++ hivemq-edge/src/frontend/src/extensions/datahub/components/nodes/NodeDatahubToolbar.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react'
+import { useTranslation } from 'react-i18next'
 import { ButtonGroup, ButtonGroupProps } from '@chakra-ui/react'
 import { LuCopy, LuDelete, LuFileEdit } from 'react-icons/lu'
 
@@ -11,11 +12,29 @@ interface NodeToolbarProps extends ButtonGroupProps {
 }
 
 const NodeDatahubToolbar: FC<NodeToolbarProps> = (props) => {
+  const { t } = useTranslation('datahub')
+
   return (
     <ButtonGroup size="xs" variant="solid" colorScheme="gray" isAttached {...props}>
-      <IconButton icon={<LuFileEdit />} aria-label="Edit" onClick={props.onEdit} />
-      <IconButton icon={<LuCopy />} aria-label="Copy" onClick={props.onCopy} />
-      <IconButton icon={<LuDelete />} aria-label="Delete" colorScheme="red" onClick={props.onDelete} />
+      <IconButton
+        icon={<LuFileEdit />}
+        data-testid="node-toolbar-edit"
+        aria-label={t('Listings.action.edit')}
+        onClick={props.onEdit}
+      />
+      <IconButton
+        icon={<LuCopy />}
+        data-testid="node-toolbar-copy"
+        aria-label={t('Listings.action.copy')}
+        onClick={props.onCopy}
+      />
+      <IconButton
+        icon={<LuDelete />}
+        data-testid="node-toolbar-delete"
+        aria-label={t('Listings.action.delete')}
+        colorScheme="red"
+        onClick={props.onDelete}
+      />
     </ButtonGroup>
   )
 }
