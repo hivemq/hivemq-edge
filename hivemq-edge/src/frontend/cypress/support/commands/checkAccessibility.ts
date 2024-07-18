1158afaf95c92cb5b1846146bdf9229633355391
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-07-18 20:12:55 +0100

    Merge pull request #499
    fix(24363): Fix flaky accessibility tests
    
    * chore(24363): update dependencies
    
    * chore(24363): remove deprecated exclusions
    
    * chore(24363): refactor violation callback
    
    * Revert "chore(24363): remove deprecated exclusions"
    
    * chore(24363): update dependencies
    
    * test(24363): false positive

--- hivemq-edge/src/frontend/cypress/support/commands/checkAccessibility.ts
+++ hivemq-edge/src/frontend/cypress/support/commands/checkAccessibility.ts
@@ -2,10 +2,17 @@ import { ignoreGlobalRules } from '../a11y.ts'
 import * as axe from 'axe-core'
 import { Options } from 'cypress-axe'
 
+const violationCallback = (violations: axe.Result[]) => {
+  for (const violation of violations) {
+    for (const node of violation.nodes) {
+      cy.log(violation.id, node.target, node.html)
+    }
+  }
+}
+
 export const checkAccessibility = (
   context?: string | Node | axe.ContextObject | undefined,
   options?: Options | undefined,
-  violationCallback?: ((violations: axe.Result[]) => void) | undefined,
   skipFailures?: boolean
 ): void => {
   const { rules, ...rest } = options || {}
