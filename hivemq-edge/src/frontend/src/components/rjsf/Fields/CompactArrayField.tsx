e703d3d4614bbbe1bdd094afa31d02817bbf13b9
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-07-26 16:32:09 +0200

    refactor(24318): refactor compact grid field

--- hivemq-edge/src/frontend/src/components/rjsf/Fields/CompactArrayField.tsx
+++ hivemq-edge/src/frontend/src/components/rjsf/Fields/CompactArrayField.tsx
@@ -1,106 +1,27 @@
-import { FC, useCallback, useEffect, useMemo, useState } from 'react'
-import { FieldProps, getTemplate, getUiOptions } from '@rjsf/utils'
+import { FC } from 'react'
+import { FieldProps } from '@rjsf/utils'
 import { RJSFSchema } from '@rjsf/utils/src/types.ts'
-import { JSONSchema7 } from 'json-schema'
 
-import { DataGridProps, CustomPropertyForm, FormDataItem, CustomPropertyValue } from '@/components/rjsf/Fields/types.ts'
-import DataGridWidget from '@/components/rjsf/Fields/DataGridWidget.tsx'
-import DataTableWidget from '@/components/rjsf/Fields/DataTableWidget.tsx'
 import { AdapterContext } from '@/modules/ProtocolAdapters/types.ts'
+import { CompactArrayFieldTemplate } from '@/components/rjsf/Templates/CompactArrayFieldTemplate.tsx'
+import { CompactFieldTemplate } from '@/components/rjsf/Templates/CompactFieldTemplate.tsx'
+import { CompactBaseInputTemplate } from '@/components/rjsf/Templates/CompactBaseInputTemplate.tsx'
+import { CompactObjectFieldTemplate } from '@/components/rjsf/Templates/CompactObjectFieldTemplate.tsx'
+import { CompactArrayFieldItemTemplate } from '@/components/rjsf/Templates/CompactArrayFieldItemTemplate.tsx'
 
 const CompactArrayField: FC<FieldProps<unknown, RJSFSchema, AdapterContext>> = (props) => {
-  const { idSchema, registry, formData, schema, disabled, readonly } = props
-  const uiOptions = getUiOptions(props.uiSchema)
-  const [rawData, setRawData] = useState<CustomPropertyForm>([])
+  const { registry } = props
 
-  useEffect(() => {
-    const newData = formData as CustomPropertyForm | undefined
-    if (!newData || !newData.length) return
-
-    setRawData(formData as CustomPropertyForm)
-  }, [formData])
-
-  useEffect(() => {
-    console.log('XXXXXX raw', rawData)
-    // props.onChange(rawData)
-  }, [rawData])
-
-  const { items } = schema
-  const { properties, maxItems, required } = items as JSONSchema7
-
-  const columnTypes = useMemo(() => {
-    if (!properties) return []
-    return Object.entries(properties) as [string, JSONSchema7][]
-  }, [properties])
-
-  const makeNewItem = useCallback(() => {
-    return Object.fromEntries(columnTypes.map(([columnId]) => [columnId, ''])) as FormDataItem
-  }, [columnTypes])
-
-  const onHandleAdd = () => {
-    const newData = makeNewItem()
-    setRawData((old) => [...old, newData])
+  registry.templates = {
+    ...registry.templates,
+    ArrayFieldTemplate: CompactArrayFieldTemplate,
+    ArrayFieldItemTemplate: CompactArrayFieldItemTemplate,
+    FieldTemplate: CompactFieldTemplate,
+    BaseInputTemplate: CompactBaseInputTemplate,
+    ObjectFieldTemplate: CompactObjectFieldTemplate,
   }
 
-  const onHandleDelete = (index: number) => {
-    setRawData((old) => {
-      const newData = [...old]
-      newData.splice(index, 1)
-      return newData
-    })
-  }
-
-  const onUpdateData = (rowIndex: number, columnId: string, value: CustomPropertyValue) => {
-    setRawData((old) => {
-      if (old.length === 0) return old
-
-      const newData = [...old]
-      newData[rowIndex] = { ...newData[rowIndex], [columnId]: value }
-      return newData
-    })
-  }
-
-  const ArrayFieldTitleTemplate = getTemplate<'ArrayFieldTitleTemplate'>('ArrayFieldTitleTemplate', registry, uiOptions)
-  const ArrayFieldDescriptionTemplate = getTemplate<'ArrayFieldDescriptionTemplate'>(
-    'ArrayFieldDescriptionTemplate',
-    props.registry,
-    uiOptions
-  )
-
-  // TODO Check for other conditions on the Schema and UISchema
-  if (schema.type !== 'array') return <props.registry.fields.ArrayField {...props} />
-
-  const commonProps: DataGridProps = {
-    onHandleAddItem: onHandleAdd,
-    onHandleDeleteItem: onHandleDelete,
-    onUpdateData: onUpdateData,
-    data: rawData,
-    columnTypes: columnTypes,
-    maxItems: maxItems,
-    required: required,
-  }
-
-  return (
-    <>
-      <ArrayFieldTitleTemplate
-        idSchema={idSchema}
-        title={uiOptions.title || props.schema.title}
-        schema={props.schema}
-        uiSchema={props.uiSchema}
-        required={props.required}
-        registry={registry}
-      />
-      <ArrayFieldDescriptionTemplate
-        idSchema={idSchema}
-        description={uiOptions.description || props.schema.description}
-        schema={props.schema}
-        uiSchema={props.uiSchema}
-        registry={registry}
-      />
-      <DataTableWidget {...commonProps} isDisabled={disabled || readonly} />
-      <DataGridWidget {...commonProps} onSetData={setRawData} />
-    </>
-  )
+  return <props.registry.fields.ArrayField {...props} />
 }
 
 export default CompactArrayField
