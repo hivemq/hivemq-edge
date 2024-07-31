afe28d31efa5fa4662cc95aaa0aad633f06caf7b
yannick <yannick.weber@hivemq.com>
2024-07-31 09:55:58 +0200

    refactor ModbusAdapterConfig to use JsonCreator add tests

--- modules/hivemq-edge-module-modbus/src/test/java/com/hivemq/edge/adapters/modbus/ModbusAdapterConfigTest.java
+++ modules/hivemq-edge-module-modbus/src/test/java/com/hivemq/edge/adapters/modbus/ModbusAdapterConfigTest.java
@@ -1,16 +1,20 @@
 package com.hivemq.edge.adapters.modbus;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hivemq.adapter.sdk.api.config.UserProperty;
 import com.hivemq.configuration.entity.HiveMQConfigEntity;
 import com.hivemq.configuration.reader.ConfigFileReaderWriter;
 import com.hivemq.configuration.reader.ConfigurationFile;
+import com.hivemq.edge.adapters.modbus.config.AddressRange;
 import com.hivemq.edge.adapters.modbus.config.ModbusAdapterConfig;
+import com.hivemq.edge.adapters.modbus.config.PollingContextImpl;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.net.URL;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.Map;
 
 import static com.hivemq.adapter.sdk.api.config.MessageHandlingOptions.MQTTMessagePerSubscription;
@@ -182,6 +186,99 @@ public class ModbusAdapterConfigTest {
         final ModbusProtocolAdapterFactory modbusProtocolAdapterFactory = new ModbusProtocolAdapterFactory();
         assertThatThrownBy(() -> modbusProtocolAdapterFactory.convertConfigObject(mapper,
                 (Map) adapters.get("modbus"))).hasMessageContaining("Missing required creator property 'endIdx'");
+    }
+
+    @Test
+    public void unconvertConfigObject_full_valid() {
+        final PollingContextImpl pollingContext = new PollingContextImpl("my/destination",
+                1,
+                MQTTMessagePerSubscription,
+                false,
+                true,
+                List.of(new UserProperty("my-name", "my-value")),
+                new AddressRange(1, 2));
+
+        final ModbusAdapterConfig modbusAdapterConfig = new ModbusAdapterConfig("my-modbus-adapter",
+                12,
+                13,
+                14,
+                "my.host.com",
+                15,
+                true,
+                List.of(pollingContext));
+
+        final ModbusProtocolAdapterFactory modbusProtocolAdapterFactory = new ModbusProtocolAdapterFactory();
+        final Map<String, Object> config =
+                modbusProtocolAdapterFactory.unconvertConfigObject(mapper, modbusAdapterConfig);
+
+        assertThat(config.get("id")).isEqualTo("my-modbus-adapter");
+        assertThat(config.get("pollingIntervalMillis")).isEqualTo(12);
+        assertThat(config.get("maxPollingErrorsBeforeRemoval")).isEqualTo(13);
+        assertThat(config.get("port")).isEqualTo(14);
+        assertThat(config.get("host")).isEqualTo("my.host.com");
+        assertThat(config.get("timeout")).isEqualTo(15);
+        assertThat(config.get("publishChangedDataOnly")).isEqualTo(true);
+
+        assertThat((List<Map<String, Object>>) config.get("subscriptions")).satisfiesExactly((subscription) -> {
+            assertThat(subscription.get("destination")).isEqualTo("my/destination");
+            assertThat(subscription.get("qos")).isEqualTo(1);
+            assertThat(subscription.get("messageHandlingOptions")).isEqualTo("MQTTMessagePerSubscription");
+            assertThat(subscription.get("includeTimestamp")).isEqualTo(false);
+            assertThat(subscription.get("includeTagNames")).isEqualTo(true);
+            assertThat((List<Map<String, Object>>) subscription.get("userProperties")).satisfiesExactly((userProperty) -> {
+                assertThat(userProperty.get("name")).isEqualTo("my-name");
+                assertThat(userProperty.get("value")).isEqualTo("my-value");
+            });
+            assertThat((Map<String, Object>) subscription.get("addressRange")).satisfies((addressRange) -> {
+                assertThat(addressRange.get("startIdx")).isEqualTo(1);
+                assertThat(addressRange.get("endIdx")).isEqualTo(2);
+            });
+        });
+    }
+
+    @Test
+    public void unconvertConfigObject_defaults() {
+        final PollingContextImpl pollingContext = new PollingContextImpl("my/destination",
+                null,
+                null,
+                null,
+                null,
+                null,
+                new AddressRange(1, 2));
+
+        final ModbusAdapterConfig modbusAdapterConfig = new ModbusAdapterConfig("my-modbus-adapter",
+                null,
+                null,
+                13,
+                "my.host.com",
+                null,
+                null,
+                List.of(pollingContext));
+
+        final ModbusProtocolAdapterFactory modbusProtocolAdapterFactory = new ModbusProtocolAdapterFactory();
+        final Map<String, Object> config =
+                modbusProtocolAdapterFactory.unconvertConfigObject(mapper, modbusAdapterConfig);
+
+        assertThat(config.get("id")).isEqualTo("my-modbus-adapter");
+        assertThat(config.get("pollingIntervalMillis")).isEqualTo(1000);
+        assertThat(config.get("maxPollingErrorsBeforeRemoval")).isEqualTo(10);
+        assertThat(config.get("port")).isEqualTo(13);
+        assertThat(config.get("host")).isEqualTo("my.host.com");
+        assertThat(config.get("timeout")).isEqualTo(5000);
+        assertThat(config.get("publishChangedDataOnly")).isEqualTo(true);
+
+        assertThat((List<Map<String, Object>>) config.get("subscriptions")).satisfiesExactly((subscription) -> {
+            assertThat(subscription.get("destination")).isEqualTo("my/destination");
+            assertThat(subscription.get("qos")).isEqualTo(0);
+            assertThat(subscription.get("messageHandlingOptions")).isEqualTo("MQTTMessagePerSubscription");
+            assertThat(subscription.get("includeTimestamp")).isEqualTo(true);
+            assertThat(subscription.get("includeTagNames")).isEqualTo(false);
+            assertThat((List<Map<String, Object>>) subscription.get("userProperties")).isEmpty();
+            assertThat((Map<String, Object>) subscription.get("addressRange")).satisfies((addressRange) -> {
+                assertThat(addressRange.get("startIdx")).isEqualTo(1);
+                assertThat(addressRange.get("endIdx")).isEqualTo(2);
+            });
+        });
     }
 
     private @NotNull HiveMQConfigEntity loadConfig(final @NotNull File configFile) {
