badca607753c0f19e2bf2f0c2c86007a05583d07
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-08-09 10:58:11 +0100

    refactor(25055): refactor edge node to handle user interactions

--- hivemq-edge/src/frontend/src/modules/Workspace/components/parts/NodeWrapper.tsx
+++ hivemq-edge/src/frontend/src/modules/Workspace/components/parts/NodeWrapper.tsx
@@ -1,33 +1,18 @@
 import { FC } from 'react'
-import { type BoxProps, Card } from '@chakra-ui/react'
+import { type CardProps, Card } from '@chakra-ui/react'
 
-interface NodeWrapperProps extends BoxProps {
+interface NodeWrapperProps extends CardProps {
   isSelected?: boolean
 }
 
 const NodeWrapper: FC<NodeWrapperProps> = ({ children, isSelected = false, ...rest }) => {
-  // const {  ...props } = useTheme()
-
-  const selectedStyle: Partial<BoxProps> = {
-    // boxShadow: 'dark-lg',
-    // bg: '#dddfe2',
+  const selectedStyle: Partial<CardProps> = {
     boxShadow: 'var(--chakra-shadows-outline)',
   }
 
   return (
-    <Card
-      variant="elevated"
-      // colorScheme={'green'}
-      p={6}
-      rounded="md"
-      border="1px"
-      // borderColor={'#bec3c9'}
-      // backgroundColor={colors.white}
-      // _hover={{ bg: '#ebedf0' }}
-      {...(isSelected ? { ...selectedStyle } : {})}
-      {...rest}
-    >
-      <>{children}</>
+    <Card variant="elevated" {...(isSelected ? { ...selectedStyle } : {})} p={6} rounded="md" borderWidth={1} {...rest}>
+      {children}
     </Card>
   )
 }
