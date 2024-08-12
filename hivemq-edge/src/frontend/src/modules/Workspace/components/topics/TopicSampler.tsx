4dbe80112ccb5c15ee2587ee6c5d5488fa00d7ee
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-08-12 17:21:27 +0100

    fix(25055): fix translation

--- hivemq-edge/src/frontend/src/modules/Workspace/components/topics/TopicSampler.tsx
+++ hivemq-edge/src/frontend/src/modules/Workspace/components/topics/TopicSampler.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react'
+import { useTranslation } from 'react-i18next'
 import { usePostTopicSamples } from '@/api/hooks/useTopicOntology/useGetTopicSamples.tsx'
 import { Button, Card, CardBody, CardFooter, CardHeader, Heading, HStack, Text } from '@chakra-ui/react'
 
@@ -7,29 +8,25 @@ interface TopicSamplerProps {
 }
 
 const TopicSampler: FC<TopicSamplerProps> = ({ topic }) => {
+  const { t } = useTranslation()
   const topicSampler = usePostTopicSamples()
+
   return (
     <Card size="sm">
       <CardHeader as={HStack} justifyContent="space-between">
         <Heading size="md">{topic}</Heading>
       </CardHeader>
       <CardBody>
-        <Text>
-          The device is using a wildcard to subscribe to topics, they are therefore not known at configuration.
-        </Text>
-        <Text>
-          You can trigger a short monitoring of the relevant MQTT traffic in order to extract a snapshot of the MQTT
-          topics involved.
-        </Text>
-        <Text>They will be temporarily added to the Topic Wheel</Text>
+        <Text>{t('workspace.topicWheel.topicSampler.title')} </Text>
+        <Text>{t('workspace.topicWheel.topicSampler.description')} </Text>
       </CardBody>
       <CardFooter>
         <Button
           onClick={() => topicSampler.mutateAsync({ adapter: 'sss', topic: 'ss' })}
           isLoading={topicSampler.isPending}
-          loadingText="Monitoring MQTT traffic"
+          loadingText={t('workspace.topicWheel.topicSampler.sampler.loader')}
         >
-          Get Topic samples
+          {t('workspace.topicWheel.topicSampler.sampler.cta')}
         </Button>
       </CardFooter>
     </Card>
