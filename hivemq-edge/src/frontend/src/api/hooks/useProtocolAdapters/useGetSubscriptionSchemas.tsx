2cb2fa401d5fe7ed400dc22b96e0774986fd3f56
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-08-12 12:18:58 +0100

    refactor(25055): add dummy react query hook for topic metadata

---
+++ hivemq-edge/src/frontend/src/api/hooks/useProtocolAdapters/useGetSubscriptionSchemas.tsx
@@ -0,0 +1,18 @@
+import { useQuery } from '@tanstack/react-query'
+import { QUERY_KEYS } from '@/api/utils.ts'
+import { RJSFSchema } from '@rjsf/utils'
+
+import { ApiError } from '@/api/__generated__'
+
+import { MOCK_MQTT_SCHEMA_REFS } from '@/__test-utils__/adapters/mqtt-subscription.mocks.ts'
+
+export const useGetSubscriptionSchemas = (topic: string, adapter?: string) => {
+  return useQuery<RJSFSchema, ApiError>({
+    queryKey: [QUERY_KEYS.DISCOVERY_SCHEMAS, topic],
+    queryFn: async () => {
+      await new Promise((resolve) => setTimeout(resolve, 1000))
+      return MOCK_MQTT_SCHEMA_REFS
+    },
+    enabled: Boolean(adapter),
+  })
+}
