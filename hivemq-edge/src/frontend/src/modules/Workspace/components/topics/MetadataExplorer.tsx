cd6d4e155610a5e6c4d9c8eea0a7df4b60c7cd07
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-08-14 11:11:04 +0100

    fix(25055): fix disabled state

--- hivemq-edge/src/frontend/src/modules/Workspace/components/topics/MetadataExplorer.tsx
+++ hivemq-edge/src/frontend/src/modules/Workspace/components/topics/MetadataExplorer.tsx
@@ -3,8 +3,8 @@ import { useTranslation } from 'react-i18next'
 import { Box, Card, CardBody, CardHeader, Heading, HStack } from '@chakra-ui/react'
 import { LuLoader } from 'react-icons/lu'
 
-import IconButton from '@/components/Chakra/IconButton.tsx'
 import { useGetSubscriptionSchemas } from '@/api/hooks/useTopicOntology/useGetSubscriptionSchemas.tsx'
+import IconButton from '@/components/Chakra/IconButton.tsx'
 import LoaderSpinner from '@/components/Chakra/LoaderSpinner.tsx'
 import JsonSchemaBrowser from '@/components/rjsf/MqttTransformation/JsonSchemaBrowser.tsx'
 
@@ -20,7 +20,7 @@ const MetadataExplorer: FC<MetadataExplorerProps> = ({ topic }) => {
     <Card size="sm">
       <CardHeader as={HStack} justifyContent="space-between">
         <Heading size="md">{topic}</Heading>
-        <IconButton size="sm" icon={<LuLoader />} aria-label={t('workspace.topicWheel.metadata.sample')} />
+        <IconButton size="sm" icon={<LuLoader />} aria-label={t('workspace.topicWheel.metadata.sample')} isDisabled />
       </CardHeader>
       <CardBody>
         {isLoading && <LoaderSpinner />}
