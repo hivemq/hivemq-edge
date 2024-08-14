eb0e1b2786d1d6306d07655f27f2d35d52ebe3d4
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-08-14 11:29:29 +0100

    test(25055): add tests

--- hivemq-edge/src/frontend/src/__test-utils__/adapters/mqtt-subscription.mocks.ts
+++ hivemq-edge/src/frontend/src/__test-utils__/adapters/mqtt-subscription.mocks.ts
@@ -49,6 +49,63 @@ export const MOCK_MQTT_SCHEMA_REFS: RJSFSchema = {
   },
 }
 
+export const MOCK_MQTT_SCHEMA_PLAIN: RJSFSchema = {
+  title: 'A registration form',
+  description: 'A simple form example.',
+  type: 'object',
+  required: ['firstName', 'lastName'],
+  properties: {
+    firstName: {
+      type: 'string',
+      title: 'First name',
+      default: 'Chuck',
+    },
+    lastName: {
+      type: 'string',
+      title: 'Last name',
+    },
+    age: {
+      type: 'integer',
+      title: 'Age',
+    },
+    bio: {
+      type: 'string',
+      title: 'Bio',
+    },
+    password: {
+      type: 'string',
+      title: 'Password',
+      minLength: 3,
+    },
+    telephone: {
+      type: 'string',
+      title: 'Telephone',
+      minLength: 10,
+    },
+    listOfStrings: {
+      type: 'array',
+      title: 'A list of strings',
+      items: {
+        type: 'string',
+        default: 'bazinga',
+      },
+    },
+    minItemsList: {
+      type: 'array',
+      title: 'A list with a minimal number of items',
+      minItems: 3,
+      items: {
+        type: 'object',
+        properties: {
+          name: {
+            type: 'string',
+            default: 'Default name',
+          },
+        },
+      },
+    },
+  },
+}
 export const MOCK_MQTT_TOPIC_SAMPLES = [
   'broker1/topic1/segment1',
   'broker1/topic1/segment2',
