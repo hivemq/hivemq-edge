e0d50321b5cdabe545ad9e076298571a75666d4c
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-08-14 14:09:57 +0100

    feat(25160): add mock for bi-directional - add icons

--- hivemq-edge/src/frontend/src/modules/Workspace/utils/adapter.utils.ts
+++ hivemq-edge/src/frontend/src/modules/Workspace/utils/adapter.utils.ts
@@ -1,4 +1,10 @@
 import { ProtocolAdapter } from '@/api/__generated__'
+import { IconType } from 'react-icons'
+import { TbSettingsAutomation } from 'react-icons/tb'
+import { FaIndustry } from 'react-icons/fa6'
+import { GrConnectivity } from 'react-icons/gr'
+import { AiFillExperiment } from 'react-icons/ai'
+import { MdInput, MdOutlineFindInPage, MdOutput } from 'react-icons/md'
 
 /**
  * @deprecated This is a mock, replacing the missing WRITE capability from the adapters
@@ -6,4 +12,32 @@ import { ProtocolAdapter } from '@/api/__generated__'
  */
 export const isBidirectional = (adapter: ProtocolAdapter | undefined) => {
   return Boolean(adapter?.id?.includes('opc-ua-client'))
+}
+
+/**
+ * @deprecated This is a mock, mapping should be based on ProtocolAdapterCategory and image property
+ * @see ProtocolAdapterCategory
+ */
+export const deviceCategoryIcon: Record<string, IconType> = {
+  BUILDING_AUTOMATION: TbSettingsAutomation,
+  INDUSTRIAL: FaIndustry,
+  CONNECTIVITY: GrConnectivity,
+  SIMULATION: AiFillExperiment,
+}
+
+// https://stackoverflow.com/questions/41253310/typescript-retrieve-element-type-information-from-array-type
+type ArrayElement<ArrayType extends readonly unknown[]> = ArrayType extends readonly (infer ElementType)[]
+  ? ElementType
+  : never
+
+type CapabilitiesArray = NonNullable<ProtocolAdapter['capabilities']>
+type CapabilityType = ArrayElement<CapabilitiesArray> | 'WRITE'
+
+/**
+ * @deprecated This is a mock, replacing the missing WRITE capability from the adapters
+ */
+export const deviceCapabilityIcon: Record<CapabilityType, IconType> = {
+  ['READ']: MdOutput,
+  ['DISCOVER']: MdOutlineFindInPage,
+  ['WRITE']: MdInput,
 }
