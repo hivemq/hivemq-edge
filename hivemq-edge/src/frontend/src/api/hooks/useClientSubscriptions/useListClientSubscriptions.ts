d82cad11a3820905bde833ef9faaaee3721e12e1
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-08-19 11:35:05 +0100

    feat(25282): add react-query hook for listing clients

---
+++ hivemq-edge/src/frontend/src/api/hooks/useClientSubscriptions/useListClientSubscriptions.ts
@@ -0,0 +1,19 @@
+import { useQuery } from '@tanstack/react-query'
+import { QUERY_KEYS } from '@/api/utils.ts'
+
+import { ApiError } from '@/api/__generated__'
+import { BrokerClient } from '@/api/types/api-broker-client.ts'
+import { mockClientSubscriptionsList } from '@/api/hooks/useClientSubscriptions/__handlers__'
+
+/**
+ * @deprecated This is a mock, missing persistence from backend (https://hivemq.kanbanize.com/ctrl_board/57/cards/25322/details/)
+ */
+export const useListClientSubscriptions = () => {
+  return useQuery<BrokerClient[], ApiError>({
+    queryKey: [QUERY_KEYS.CLIENTS],
+    queryFn: async () => {
+      await new Promise((resolve) => setTimeout(resolve, 500))
+      return mockClientSubscriptionsList
+    },
+  })
+}
