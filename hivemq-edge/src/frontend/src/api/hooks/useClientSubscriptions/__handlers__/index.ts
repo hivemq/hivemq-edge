e2d55a9c2b4c12af454c582c7a1837d29bcad1c6
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-08-19 12:47:39 +0100

    fix(25282): fix mocks

--- hivemq-edge/src/frontend/src/api/hooks/useClientSubscriptions/__handlers__/index.ts
+++ hivemq-edge/src/frontend/src/api/hooks/useClientSubscriptions/__handlers__/index.ts
@@ -1,11 +1,22 @@
-import { BrokerClient } from '@/api/types/api-broker-client.ts'
+import { BrokerClient, BrokerClientSubscription } from '@/api/types/api-broker-client.ts'
+import maxQoS = BrokerClientSubscription.maxQoS
 
 export const mockClientSubscription: BrokerClient = {
   config: {
     id: 'my-first-client',
+    subscription: [{ destination: 'test/topic/1', maxQoS: maxQoS._0 }],
   },
   id: 'my-first-client',
   type: 'broker-client',
 }
 
-export const mockClientSubscriptionsList: BrokerClient[] = [mockClientSubscription]
+export const mockClientSubscriptionsList: BrokerClient[] = [
+  mockClientSubscription,
+  {
+    config: {
+      id: 'my-first-client2',
+    },
+    id: 'my-first-client2',
+    type: 'broker-client',
+  },
+]
