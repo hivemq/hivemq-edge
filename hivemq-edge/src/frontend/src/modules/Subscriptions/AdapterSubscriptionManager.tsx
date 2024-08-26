d01f9bf88bddf43346c4bf2846c4f2540de68051
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-08-26 11:39:00 +0100

    feat(25263): add form

--- hivemq-edge/src/frontend/src/modules/Subscriptions/AdapterSubscriptionManager.tsx
+++ hivemq-edge/src/frontend/src/modules/Subscriptions/AdapterSubscriptionManager.tsx
@@ -14,6 +14,8 @@ import {
 } from '@chakra-ui/react'
 import { LuExpand, LuShrink } from 'react-icons/lu'
 
+import SubscriptionForm from '@/modules/Subscriptions/components/SubscriptionForm.tsx'
+
 interface AdapterSubscriptionManagerProps {
   type: 'inward' | 'outward'
 }
@@ -21,18 +23,20 @@ interface AdapterSubscriptionManagerProps {
 const AdapterSubscriptionManager: FC<AdapterSubscriptionManagerProps> = ({ type }) => {
   const { isOpen, onOpen, onClose } = useDisclosure()
   const navigate = useNavigate()
-  const { nodeType, nodeId } = useParams()
+  const { nodeType, device, nodeId } = useParams()
   const [isExpanded, setExpanded] = useBoolean(false)
 
+  const isValid = nodeType === 'node' && device === 'adapter'
+
   const handleClose = () => {
+    if (!isValid) return
     onClose()
     navigate('/workspace')
   }
 
   useEffect(() => {
-    console.log('XXXXXX node', nodeType, nodeId)
-    onOpen()
-  }, [nodeId, nodeType, onOpen])
+    if (isValid) onOpen()
+  }, [isValid, onOpen])
 
   return (
     <Drawer isOpen={isOpen} placement="right" size={isExpanded ? 'full' : 'sm'} onClose={handleClose} variant="hivemq">
@@ -60,7 +64,9 @@ const AdapterSubscriptionManager: FC<AdapterSubscriptionManagerProps> = ({ type 
         <DrawerHeader>
           <Text>Manage {type} subscriptions</Text>
         </DrawerHeader>
-        <DrawerBody display="flex" flexDirection="column" gap={6}></DrawerBody>
+        <DrawerBody display="flex" flexDirection="column" gap={6}>
+          {nodeId && <SubscriptionForm id={nodeId} type={type} />}
+        </DrawerBody>
       </DrawerContent>
     </Drawer>
   )
