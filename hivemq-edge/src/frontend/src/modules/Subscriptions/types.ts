ef0e22dbfa8a41deb5b41bdb9317494464d53d2f
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-08-26 13:28:37 +0100

    feat(25263): add type for the outward subscriptions

--- hivemq-edge/src/frontend/src/modules/Subscriptions/types.ts
+++ hivemq-edge/src/frontend/src/modules/Subscriptions/types.ts
@@ -4,9 +4,31 @@ export interface SubscriptionManagerType {
   schema: RJSFSchema
   formData: { subscriptions: Record<string, unknown> }
   uiSchema: UiSchema
+  onSubmit?: (data: any) => void
 }
 
 /**
  * @deprecated This is a mock, will need to be replaced by OpenAPI specs when available
  */
-export type OutwardSubscription = {}
+export interface OutwardSubscription {
+  node: string
+  mqttTopic: string[]
+  mapping: Mapping[]
+}
+
+/**
+ * @deprecated This is a mock, will need to be replaced by OpenAPI specs when available
+ */
+export interface Mapping {
+  source: string[]
+  destination: string
+  transformation: Transformation
+}
+
+/**
+ * @deprecated This is a mock, will need to be replaced by OpenAPI specs when available
+ */
+export interface Transformation {
+  function: 'toString' | 'toInt' | 'join'
+  params: string
+}
