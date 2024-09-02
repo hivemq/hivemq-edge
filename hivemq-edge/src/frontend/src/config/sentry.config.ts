f1d1220588f59d0b64a20c049b926907bca48646
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-09-02 13:55:50 +0100

    feat(25188): add support for react router

--- hivemq-edge/src/frontend/src/config/sentry.config.ts
+++ hivemq-edge/src/frontend/src/config/sentry.config.ts
@@ -1,4 +1,6 @@
 import * as Sentry from '@sentry/react'
+import { useEffect } from 'react'
+import { createRoutesFromChildren, matchRoutes, useLocation, useNavigationType } from 'react-router-dom'
 
 if (import.meta.env.MODE !== 'development')
   Sentry.init({
@@ -6,7 +8,18 @@ if (import.meta.env.MODE !== 'development')
     release: `hivemq-edge@${import.meta.env.VITE_APP_VERSION}`,
     environment: import.meta.env.MODE,
 
-    integrations: [Sentry.browserProfilingIntegration(), Sentry.replayIntegration()],
+    integrations: [
+      Sentry.browserProfilingIntegration(),
+      Sentry.replayIntegration(),
+      // https://docs.sentry.io/platforms/javascript/guides/react/configuration/integrations/react-router/
+      Sentry.reactRouterV6BrowserTracingIntegration({
+        useEffect,
+        useLocation,
+        useNavigationType,
+        createRoutesFromChildren,
+        matchRoutes,
+      }),
+    ],
     // Set `tracePropagationTargets` to control for which URLs trace propagation should be enabled
     tracePropagationTargets: [],
     // Set tracesSampleRate to 1.0 to capture 100%
