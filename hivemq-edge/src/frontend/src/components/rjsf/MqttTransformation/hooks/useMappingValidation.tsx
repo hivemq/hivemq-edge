02ef457642157450c2a38005939609b54ffe09ed
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-09-05 17:23:01 +0100

    chore(25265): eslint

--- hivemq-edge/src/frontend/src/components/rjsf/MqttTransformation/hooks/useMappingValidation.tsx
+++ hivemq-edge/src/frontend/src/components/rjsf/MqttTransformation/hooks/useMappingValidation.tsx
@@ -1,6 +1,7 @@
 import { MappingValidation, OutwardSubscription } from '@/modules/Subscriptions/types.ts'
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const useMappingValidation = (_item: OutwardSubscription) => {
-  const status: MappingValidation = { status: 'error', errors: [] }
+  const status: MappingValidation = { status: 'success', errors: [] }
   return status
 }
