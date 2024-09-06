163043519de1f31dc7e04ab1ec21aa5dbd24ae32
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-09-06 14:26:06 +0100

    test(25265): add tests

---
+++ hivemq-edge/src/frontend/src/components/rjsf/MqttTransformation/components/SubscriptionContainer.spec.cy.tsx
@@ -0,0 +1,35 @@
+import { OutwardSubscription } from '@/modules/Subscriptions/types.ts'
+import SubscriptionContainer from './SubscriptionContainer.tsx'
+import { mockAdapter, mockProtocolAdapter } from '@/api/hooks/useProtocolAdapters/__handlers__'
+import { mockBridge } from '@/api/hooks/useGetBridges/__handlers__'
+
+const MOCK_SUBS: OutwardSubscription = {
+  node: 'my-node',
+  'mqtt-topic': ['my-topic'],
+  mapping: [{ source: ['dropped-property'], destination: 'Second String' }],
+}
+
+const wrapper: React.JSXElementConstructor<{ children: React.ReactNode }> = ({ children }) => {
+  return <h2>{children}</h2>
+}
+
+describe('SubscriptionContainer', () => {
+  beforeEach(() => {
+    cy.viewport(1200, 900)
+    cy.intercept('/api/v1/management/protocol-adapters/types', { items: [mockProtocolAdapter] }).as('getProtocols')
+    cy.intercept('api/v1/management/protocol-adapters/adapters', { items: [mockAdapter] }).as('getAdapters')
+    cy.intercept('/api/v1/management/bridges', { items: [mockBridge] }).as('getConfig3')
+  })
+
+  it('should be accessible ', () => {
+    cy.injectAxe()
+
+    cy.mountWithProviders(
+      <SubscriptionContainer onClose={cy.stub()} onSubmit={cy.stub()} onChange={cy.stub()} item={MOCK_SUBS} />,
+      { wrapper }
+    )
+
+    cy.checkAccessibility()
+    cy.percySnapshot('Component: SubscriptionContainer')
+  })
+})
