774279f5524abffe0c0b748b4edf1e8af5d62dea
yannick <yannick.weber@hivemq.com>
2024-09-11 14:20:54 +0200

    file protocol adapter > legacy config handling

--- modules/hivemq-edge-module-file/src/main/java/com/hivemq/edge/adapters/file/FileProtocolAdapterFactory.java
+++ modules/hivemq-edge-module-file/src/main/java/com/hivemq/edge/adapters/file/FileProtocolAdapterFactory.java
@@ -15,12 +15,20 @@
  */
 package com.hivemq.edge.adapters.file;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hivemq.adapter.sdk.api.ProtocolAdapter;
 import com.hivemq.adapter.sdk.api.ProtocolAdapterInformation;
 import com.hivemq.adapter.sdk.api.factories.ProtocolAdapterFactory;
 import com.hivemq.adapter.sdk.api.model.ProtocolAdapterInput;
 import com.hivemq.edge.adapters.file.config.FileAdapterConfig;
+import com.hivemq.edge.adapters.file.config.FileToMqttConfig;
+import com.hivemq.edge.adapters.file.config.FileToMqttMapping;
+import com.hivemq.edge.adapters.file.config.legacy.LegacyFileAdapterConfig;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class FileProtocolAdapterFactory implements ProtocolAdapterFactory<FileAdapterConfig> {
 
@@ -30,14 +38,51 @@ public class FileProtocolAdapterFactory implements ProtocolAdapterFactory<FileAd
     }
 
     @Override
-    public @NotNull ProtocolAdapter createAdapter(final @NotNull ProtocolAdapterInformation adapterInformation, @NotNull final ProtocolAdapterInput<FileAdapterConfig> input) {
+    public @NotNull ProtocolAdapter createAdapter(
+            final @NotNull ProtocolAdapterInformation adapterInformation,
+            @NotNull final ProtocolAdapterInput<FileAdapterConfig> input) {
         return new FilePollingProtocolAdapter(adapterInformation, input);
     }
 
+    @Override
+    public @NotNull FileAdapterConfig convertConfigObject(
+            final @NotNull ObjectMapper objectMapper, final @NotNull Map<String, Object> config) {
+        if (config.get("FileToMqtt") != null || config.get("mqttToFile") != null) {
+            return ProtocolAdapterFactory.super.convertConfigObject(objectMapper, config);
+        } else {
+            return tryConvertLegacyConfig(objectMapper, config);
+        }
+    }
 
     @Override
     public @NotNull Class<FileAdapterConfig> getConfigClass() {
         return FileAdapterConfig.class;
+    }
+
+    private static @NotNull FileAdapterConfig tryConvertLegacyConfig(
+            final @NotNull ObjectMapper objectMapper,
+            final @NotNull Map<String, Object> config) {
+        final LegacyFileAdapterConfig legacyFileAdapterConfig =
+                objectMapper.convertValue(config, LegacyFileAdapterConfig.class);
+
+        final List<FileToMqttMapping> FileToMqttMappings = legacyFileAdapterConfig.getPollingContexts()
+                .stream()
+                .map(context -> new FileToMqttMapping(context.getDestinationMqttTopic(),
+                        context.getQos(),
+                        context.getMessageHandlingOptions(),
+                        context.getIncludeTimestamp(),
+                        context.getIncludeTagNames(),
+                        context.getUserProperties(),
+                        context.getFilePath(),
+                        context.getContentType()))
+                .collect(Collectors.toList());
+
+        final FileToMqttConfig FileToMqttConfig =
+                new FileToMqttConfig(legacyFileAdapterConfig.getPollingIntervalMillis(),
+                        legacyFileAdapterConfig.getMaxPollingErrorsBeforeRemoval(),
+                        FileToMqttMappings);
+
+        return new FileAdapterConfig(legacyFileAdapterConfig.getId(), FileToMqttConfig);
     }
 
 }
---
+++ modules/hivemq-edge-module-file/src/main/java/com/hivemq/edge/adapters/file/config/legacy/LegacyFilePollingContext.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2023-present HiveMQ GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hivemq.edge.adapters.file.config.legacy;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.hivemq.adapter.sdk.api.config.MessageHandlingOptions;
+import com.hivemq.adapter.sdk.api.config.MqttUserProperty;
+import com.hivemq.adapter.sdk.api.data.JsonPayloadCreator;
+import com.hivemq.edge.adapters.file.config.ContentType;
+import com.hivemq.edge.adapters.file.payload.FileJsonPayloadCreator;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LegacyFilePollingContext {
+    @JsonProperty(value = "destination", required = true)
+    protected @Nullable String destination;
+
+    @JsonProperty(value = "qos", required = true)
+    protected int qos = 0;
+
+    @JsonProperty(value = "messageHandlingOptions")
+    protected @NotNull MessageHandlingOptions messageHandlingOptions = MessageHandlingOptions.MQTTMessagePerTag;
+
+    @JsonProperty(value = "includeTimestamp")
+    protected @NotNull Boolean includeTimestamp = Boolean.TRUE;
+
+    @JsonProperty(value = "includeTagNames")
+    protected @NotNull Boolean includeTagNames = Boolean.FALSE;
+
+    @JsonProperty(value = "userProperties")
+    private @NotNull List<MqttUserProperty> userProperties = new ArrayList<>();
+
+    @JsonProperty(value = "filePath", required = true)
+    protected @NotNull String filePath;
+
+    @JsonProperty(value = "contentType", required = true)
+    protected @NotNull ContentType contentType;
+
+    @JsonCreator
+    public LegacyFilePollingContext(
+            @JsonProperty("destination") @Nullable final String destination,
+            @JsonProperty("qos") final int qos,
+            @JsonProperty("userProperties") @Nullable List<MqttUserProperty> userProperties,
+            @JsonProperty("filePath") @NotNull String filePath,
+            @JsonProperty("contentType") @NotNull ContentType contentType) {
+        this.destination = destination;
+        this.qos = qos;
+        this.contentType = contentType;
+        if (userProperties != null) {
+            this.userProperties = userProperties;
+        }
+        this.filePath = filePath;
+    }
+
+
+    public @NotNull String getFilePath() {
+        return filePath;
+    }
+
+    public @Nullable String getDestinationMqttTopic() {
+        return destination;
+    }
+
+    public int getQos() {
+        return qos;
+    }
+
+    public @NotNull MessageHandlingOptions getMessageHandlingOptions() {
+        return messageHandlingOptions;
+    }
+
+    public @NotNull Boolean getIncludeTimestamp() {
+        return includeTimestamp;
+    }
+
+    public @NotNull Boolean getIncludeTagNames() {
+        return includeTagNames;
+    }
+
+    public @NotNull List<MqttUserProperty> getUserProperties() {
+        return userProperties;
+    }
+
+    public @Nullable JsonPayloadCreator getJsonPayloadCreator() {
+        return FileJsonPayloadCreator.INSTANCE;
+    }
+
+    public @NotNull ContentType getContentType() {
+        return contentType;
+    }
+}
