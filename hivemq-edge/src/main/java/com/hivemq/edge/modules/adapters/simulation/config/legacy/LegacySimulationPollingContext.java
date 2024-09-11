972b9cdd21c28e8444603119313b9164a4c6c20b
Florian-Limpoeck <florian.limpoeck@hivemq.com>
2024-09-11 15:15:29 +0200

    simulation > added legacy protocol adapter config migration

---
+++ hivemq-edge/src/main/java/com/hivemq/edge/modules/adapters/simulation/config/legacy/LegacySimulationPollingContext.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2019-present HiveMQ GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hivemq.edge.modules.adapters.simulation.config.legacy;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.hivemq.adapter.sdk.api.config.MessageHandlingOptions;
+import com.hivemq.adapter.sdk.api.config.MqttUserProperty;
+import com.hivemq.extension.sdk.api.annotations.NotNull;
+import com.hivemq.extension.sdk.api.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LegacySimulationPollingContext {
+
+    @JsonProperty(value = "destination", required = true)
+    protected @Nullable String destination;
+
+    @JsonProperty(value = "qos", required = true)
+    protected int qos = 0;
+
+    @JsonProperty(value = "messageHandlingOptions")
+    protected @NotNull MessageHandlingOptions messageHandlingOptions = MessageHandlingOptions.MQTTMessagePerTag;
+
+    @JsonProperty(value = "includeTimestamp")
+    protected @NotNull Boolean includeTimestamp = Boolean.TRUE;
+
+    @JsonProperty(value = "includeTagNames")
+    protected @NotNull Boolean includeTagNames = Boolean.FALSE;
+
+    @JsonProperty(value = "userProperties")
+    private @NotNull List<MqttUserProperty> userProperties = new ArrayList<>();
+
+    public @Nullable String getMqttTopic() {
+        return destination;
+    }
+
+    public int getMqttQos() {
+        return qos;
+    }
+
+    public @NotNull MessageHandlingOptions getMessageHandlingOptions() {
+        return messageHandlingOptions;
+    }
+
+    public @NotNull Boolean getIncludeTimestamp() {
+        return includeTimestamp;
+    }
+
+    public @NotNull Boolean getIncludeTagNames() {
+        return includeTagNames;
+    }
+
+    public @NotNull List<MqttUserProperty> getUserProperties() {
+        return userProperties;
+    }
+}
---
+++ hivemq-edge/src/test/java/com/hivemq/edge/modules/adapters/simulation/config/legacy/LegacySimulationAdapterConfigTest.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright 2019-present HiveMQ GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hivemq.edge.modules.adapters.simulation.config.legacy;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hivemq.adapter.sdk.api.config.MqttUserProperty;
+import com.hivemq.configuration.entity.HiveMQConfigEntity;
+import com.hivemq.configuration.reader.ConfigFileReaderWriter;
+import com.hivemq.configuration.reader.ConfigurationFile;
+import com.hivemq.edge.modules.adapters.simulation.SimulationProtocolAdapterFactory;
+import com.hivemq.edge.modules.adapters.simulation.config.SimulationAdapterConfig;
+import com.hivemq.edge.modules.adapters.simulation.config.SimulationToMqttConfig;
+import com.hivemq.edge.modules.adapters.simulation.config.SimulationToMqttMapping;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.net.URL;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+
+import static com.hivemq.adapter.sdk.api.config.MessageHandlingOptions.MQTTMessagePerSubscription;
+import static com.hivemq.adapter.sdk.api.config.MessageHandlingOptions.MQTTMessagePerTag;
+import static com.hivemq.protocols.ProtocolAdapterUtils.createProtocolAdapterMapper;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.mock;
+
+@SuppressWarnings("unchecked")
+class LegacySimulationAdapterConfigTest {
+
+    private final @NotNull ObjectMapper mapper = createProtocolAdapterMapper(new ObjectMapper());
+
+    @Test
+    public void convertConfigObject_fullConfig_valid() throws Exception {
+        final URL resource = getClass().getResource("/configs/simulation/legacy-simulation-adapter-full-config.xml");
+        final File path = Path.of(resource.toURI()).toFile();
+
+        final HiveMQConfigEntity configEntity = loadConfig(path);
+        final Map<String, Object> adapters = configEntity.getProtocolAdapterConfig();
+
+        final SimulationProtocolAdapterFactory simulationProtocolAdapterFactory =
+                new SimulationProtocolAdapterFactory();
+        final SimulationAdapterConfig config =
+                simulationProtocolAdapterFactory.convertConfigObject(mapper, (Map) adapters.get("simulation"));
+
+        assertThat(config.getId()).isEqualTo("my-simulation-protocol-adapter");
+        assertThat(config.getMinValue()).isEqualTo(0);
+        assertThat(config.getMaxValue()).isEqualTo(1000);
+        assertThat(config.getMinDelay()).isEqualTo(0);
+        assertThat(config.getMaxDelay()).isEqualTo(1000);
+        assertThat(config.getSimulationToMqttConfig().getMaxPollingErrorsBeforeRemoval()).isEqualTo(9);
+        assertThat(config.getSimulationToMqttConfig().getSimulationToMqttMappings()).satisfiesExactly(subscription -> {
+            assertThat(subscription.getMqttTopic()).isEqualTo("my/topic");
+            assertThat(subscription.getMqttQos()).isEqualTo(1);
+            assertThat(subscription.getMessageHandlingOptions()).isEqualTo(MQTTMessagePerSubscription);
+            assertThat(subscription.getIncludeTimestamp()).isFalse();
+            assertThat(subscription.getIncludeTagNames()).isTrue();
+
+            assertThat(subscription.getUserProperties()).satisfiesExactly(userProperty -> {
+                assertThat(userProperty.getName()).isEqualTo("my-name");
+                assertThat(userProperty.getValue()).isEqualTo("my-value");
+            });
+        }, subscription -> {
+            assertThat(subscription.getMqttTopic()).isEqualTo("my/topic/2");
+            assertThat(subscription.getMqttQos()).isEqualTo(1);
+            assertThat(subscription.getMessageHandlingOptions()).isEqualTo(MQTTMessagePerSubscription);
+            assertThat(subscription.getIncludeTimestamp()).isFalse();
+            assertThat(subscription.getIncludeTagNames()).isTrue();
+
+            assertThat(subscription.getUserProperties()).satisfiesExactly(userProperty -> {
+                assertThat(userProperty.getName()).isEqualTo("my-name");
+                assertThat(userProperty.getValue()).isEqualTo("my-value");
+            });
+        });
+    }
+
+    @Test
+    public void convertConfigObject_defaults_valid() throws Exception {
+        final URL resource = getClass().getResource("/configs/simulation/legacy-simulation-adapter-minimal-config.xml");
+        final File path = Path.of(resource.toURI()).toFile();
+
+        final HiveMQConfigEntity configEntity = loadConfig(path);
+        final Map<String, Object> adapters = configEntity.getProtocolAdapterConfig();
+
+        final SimulationProtocolAdapterFactory simulationProtocolAdapterFactory =
+                new SimulationProtocolAdapterFactory();
+        final SimulationAdapterConfig config =
+                simulationProtocolAdapterFactory.convertConfigObject(mapper, (Map) adapters.get("simulation"));
+
+        assertThat(config.getId()).isEqualTo("my-simulation-protocol-adapter");
+        assertThat(config.getSimulationToMqttConfig().getMaxPollingErrorsBeforeRemoval()).isEqualTo(10);
+        assertThat(config.getSimulationToMqttConfig().getSimulationToMqttMappings()).satisfiesExactly(subscription -> {
+            assertThat(subscription.getMqttTopic()).isEqualTo("my/topic");
+            assertThat(subscription.getMqttQos()).isEqualTo(0);
+            assertThat(subscription.getMessageHandlingOptions()).isEqualTo(MQTTMessagePerTag);
+            assertThat(subscription.getIncludeTimestamp()).isTrue();
+            assertThat(subscription.getIncludeTagNames()).isFalse();
+
+            assertThat(subscription.getUserProperties()).isEmpty();
+        });
+    }
+
+    private @NotNull HiveMQConfigEntity loadConfig(final @NotNull File configFile) {
+        final ConfigFileReaderWriter readerWriter = new ConfigFileReaderWriter(new ConfigurationFile(configFile),
+                mock(),
+                mock(),
+                mock(),
+                mock(),
+                mock(),
+                mock(),
+                mock(),
+                mock(),
+                mock(),
+                mock(),
+                mock(),
+                mock(),
+                mock(),
+                mock());
+        return readerWriter.applyConfig();
+    }
+}
