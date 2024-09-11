4e458f7e24d5763435ee0b8e2db65850d823081d
Florian-Limpoeck <florian.limpoeck@hivemq.com>
2024-09-11 14:03:16 +0200

    plc4x > added ads and s7 config migration

--- modules/hivemq-edge-module-plc4x/src/main/java/com/hivemq/edge/adapters/plc4x/types/ads/ADSProtocolAdapterFactory.java
+++ modules/hivemq-edge-module-plc4x/src/main/java/com/hivemq/edge/adapters/plc4x/types/ads/ADSProtocolAdapterFactory.java
@@ -15,12 +15,21 @@
  */
 package com.hivemq.edge.adapters.plc4x.types.ads;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hivemq.adapter.sdk.api.ProtocolAdapter;
 import com.hivemq.adapter.sdk.api.ProtocolAdapterInformation;
+import com.hivemq.adapter.sdk.api.config.MqttUserProperty;
 import com.hivemq.adapter.sdk.api.factories.ProtocolAdapterFactory;
 import com.hivemq.adapter.sdk.api.model.ProtocolAdapterInput;
+import com.hivemq.edge.adapters.plc4x.config.Plc4xToMqttMapping;
 import com.hivemq.edge.adapters.plc4x.types.ads.config.ADSAdapterConfig;
+import com.hivemq.edge.adapters.plc4x.types.ads.config.ADSToMqttConfig;
+import com.hivemq.edge.adapters.plc4x.types.ads.config.legacy.LegacyADSAdapterConfig;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @author HiveMQ Adapter Generator
@@ -42,4 +51,47 @@ public class ADSProtocolAdapterFactory implements ProtocolAdapterFactory<ADSAdap
         return ADSAdapterConfig.class;
     }
 
+    @Override
+    public @NotNull ADSAdapterConfig convertConfigObject(
+            final @NotNull ObjectMapper objectMapper, final @NotNull Map<String, Object> config) {
+        if(config.get("adsToMqtt") != null || config.get("mqttToAds") != null) {
+            return ProtocolAdapterFactory.super.convertConfigObject(objectMapper, config);
+        } else  {
+            return tryConvertLegacyConfig(objectMapper, config);
+        }
+    }
+
+    private static @NotNull ADSAdapterConfig tryConvertLegacyConfig(final @NotNull ObjectMapper objectMapper, final @NotNull Map<String, Object> config) {
+        final LegacyADSAdapterConfig legacyAdsAdapterConfig =
+                objectMapper.convertValue(config, LegacyADSAdapterConfig.class);
+
+        final List<Plc4xToMqttMapping> plc4xToMqttMappings = legacyAdsAdapterConfig.getSubscriptions()
+                .stream()
+                .map(subscription -> new Plc4xToMqttMapping(subscription.getMqttTopic(),
+                        subscription.getMqttQos(),
+                        subscription.getMessageHandlingOptions(),
+                        subscription.getIncludeTimestamp(),
+                        subscription.getIncludeTagNames(),
+                        subscription.getTagName(),
+                        subscription.getTagAddress(),
+                        subscription.getDataType(),
+                        subscription.getUserProperties()))
+                .collect(Collectors.toList());
+
+        final ADSToMqttConfig modbusToMqttConfig =
+                new ADSToMqttConfig(legacyAdsAdapterConfig.getPollingIntervalMillis(),
+                        legacyAdsAdapterConfig.getMaxPollingErrorsBeforeRemoval(),
+                        legacyAdsAdapterConfig.getPublishChangedDataOnly(),
+                        plc4xToMqttMappings);
+
+
+        return new ADSAdapterConfig(legacyAdsAdapterConfig.getId(),
+                legacyAdsAdapterConfig.getPort(),
+                legacyAdsAdapterConfig.getHost(),
+                legacyAdsAdapterConfig.getTargetAmsPort(),
+                legacyAdsAdapterConfig.getSourceAmsPort(),
+                legacyAdsAdapterConfig.getTargetAmsNetId(),
+                legacyAdsAdapterConfig.getSourceAmsNetId(),
+                modbusToMqttConfig);
+    }
 }
