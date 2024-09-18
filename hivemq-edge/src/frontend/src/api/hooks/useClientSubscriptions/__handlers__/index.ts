664e515384ec09d55df0591168365e94bca24b78
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-09-18 10:24:32 +0100

    refactor(26237): update mocks

--- hivemq-edge/src/frontend/src/api/hooks/useClientSubscriptions/__handlers__/index.ts
+++ hivemq-edge/src/frontend/src/api/hooks/useClientSubscriptions/__handlers__/index.ts
@@ -1,22 +1,31 @@
-import { BrokerClient, BrokerClientSubscription } from '@/api/types/api-broker-client.ts'
-import maxQoS = BrokerClientSubscription.maxQoS
+import { http, HttpResponse } from 'msw'
+import { ClientFilter, ClientFilterList, type ClientTopicList } from '@/api/__generated__'
 
-export const mockClientSubscription: BrokerClient = {
-  config: {
-    id: 'my-first-client',
-    subscriptions: [{ destination: 'test/topic/1', maxQoS: maxQoS._0 }],
-  },
+export const mockClientSubscription: ClientFilter = {
+  topicFilters: [{ destination: 'test/topic/1' }],
   id: 'my-first-client',
-  type: 'broker-client',
 }
 
-export const mockClientSubscriptionsList: BrokerClient[] = [
+export const mockClientSubscriptionsList: ClientFilter[] = [
   mockClientSubscription,
   {
-    config: {
-      id: 'my-first-client2',
-    },
+    topicFilters: [],
     id: 'my-first-client2',
-    type: 'broker-client',
   },
+]
+
+export const mockClientTopicList: string[] = ['client1/topic/1', 'client1/topic/2', 'client1/topic/3']
+
+export const handlers = [
+  http.get('**/management/client/filters', () => {
+    return HttpResponse.json<ClientFilterList>([mockClientSubscription], { status: 200 })
+  }),
+
+  http.post('**/management/client/filters', () => {
+    return HttpResponse.json({}, { status: 200 })
+  }),
+
+  http.get('**/management/client/topic-samples', () => {
+    return HttpResponse.json<ClientTopicList>({ items: mockClientTopicList }, { status: 200 })
+  }),
 ]
