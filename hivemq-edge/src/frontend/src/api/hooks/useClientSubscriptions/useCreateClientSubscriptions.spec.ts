010f3f4d06e5a253c9758af34e6f1b4f90921bd0
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-09-18 10:24:42 +0100

    test(26237): update tests

--- hivemq-edge/src/frontend/src/api/hooks/useClientSubscriptions/useCreateClientSubscriptions.spec.ts
+++ hivemq-edge/src/frontend/src/api/hooks/useClientSubscriptions/useCreateClientSubscriptions.spec.ts
@@ -1,29 +1,29 @@
 import { expect } from 'vitest'
 import { act, renderHook, waitFor } from '@testing-library/react'
 
+import { server } from '@/__test-utils__/msw/mockServer.ts'
 import { SimpleWrapper as wrapper } from '@/__test-utils__/hooks/SimpleWrapper.tsx'
 import { useCreateClientSubscriptions } from '@/api/hooks/useClientSubscriptions/useCreateClientSubscriptions.ts'
 import { mockClientSubscription } from '@/api/hooks/useClientSubscriptions/__handlers__'
+import { handlers } from './__handlers__'
 
 describe('useCreateClientSubscriptions', () => {
+  afterEach(() => {
+    server.resetHandlers()
+  })
+
   it('should load the data', async () => {
+    server.use(...handlers)
+
     const { result } = renderHook(() => useCreateClientSubscriptions(), { wrapper })
-    await waitFor(() => {
-      expect(result.current.isSuccess).toBeFalsy()
-    })
 
+    expect(result.current.isSuccess).toBeFalsy()
     act(() => {
-      result.current.mutateAsync({ id: 'fake-id', config: mockClientSubscription.config })
+      result.current.mutateAsync(mockClientSubscription)
     })
-
     await waitFor(() => {
       expect(result.current.isSuccess).toBeTruthy()
-      expect(result.current.data).toStrictEqual(
-        expect.objectContaining({
-          id: 'fake-id',
-          config: mockClientSubscription.config,
-        })
-      )
     })
+    expect(result.current.data).toStrictEqual({})
   })
 })
