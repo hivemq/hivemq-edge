ff83278bf2d612a411eff141469edf176d04ebab
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-10-04 15:01:54 +0100

    feat(25881): update the device side panel

--- hivemq-edge/src/frontend/src/modules/Workspace/components/drawers/DevicePropertyDrawer.tsx
+++ hivemq-edge/src/frontend/src/modules/Workspace/components/drawers/DevicePropertyDrawer.tsx
@@ -1,17 +1,23 @@
 import { FC } from 'react'
 import { useTranslation } from 'react-i18next'
-import { Node } from 'reactflow'
+import { getIncomers, Node } from 'reactflow'
 import {
   Drawer,
   DrawerBody,
   DrawerCloseButton,
   DrawerContent,
-  DrawerFooter,
   DrawerHeader,
   DrawerOverlay,
   Text,
 } from '@chakra-ui/react'
 
+import { Adapter } from '@/api/__generated__'
+import { useGetAdapterTypes } from '@/api/hooks/useProtocolAdapters/useGetAdapterTypes.ts'
+import LoaderSpinner from '@/components/Chakra/LoaderSpinner.tsx'
+import ErrorMessage from '@/components/ErrorMessage.tsx'
+import DeviceMetadataViewer from '@/modules/Device/components/DeviceMetadataViewer.tsx'
+import DeviceTagList from '@/modules/Device/components/DeviceTagList.tsx'
+import useWorkspaceStore from '@/modules/Workspace/hooks/useWorkspaceStore.ts'
 import { DeviceMetadata } from '@/modules/Workspace/types.ts'
 
 interface DevicePropertyDrawerProps {
@@ -24,6 +30,15 @@ interface DevicePropertyDrawerProps {
 
 const DevicePropertyDrawer: FC<DevicePropertyDrawerProps> = ({ isOpen, selectedNode, onClose }) => {
   const { t } = useTranslation()
+  const { nodes, edges } = useWorkspaceStore()
+  const { data, isError, isLoading } = useGetAdapterTypes()
+
+  const [adapterNode] = getIncomers<Adapter, any>(selectedNode, nodes, edges)
+
+  const protocol = data?.items?.find((e) => e.id === adapterNode.data.type)
+
+  if (isLoading) return <LoaderSpinner />
+  if (isError) return <ErrorMessage message="XXXX Cannot load the protocols" />
 
   return (
     <Drawer isOpen={isOpen} placement="right" size="md" onClose={onClose} variant="hivemq">
@@ -33,8 +48,12 @@ const DevicePropertyDrawer: FC<DevicePropertyDrawerProps> = ({ isOpen, selectedN
         <DrawerHeader>
           <Text> {t('workspace.property.header', { context: selectedNode.type })}</Text>
         </DrawerHeader>
-        <DrawerBody display="flex" flexDirection="column" gap={6}></DrawerBody>
-        <DrawerFooter borderTopWidth="1px"></DrawerFooter>
+        <DrawerBody display="flex" flexDirection="column" gap={6}>
+          <DrawerBody display="flex" flexDirection="column" gap={6}>
+            <DeviceMetadataViewer protocolAdapter={protocol} />
+            <DeviceTagList adapter={adapterNode.data} />
+          </DrawerBody>
+        </DrawerBody>
       </DrawerContent>
     </Drawer>
   )
