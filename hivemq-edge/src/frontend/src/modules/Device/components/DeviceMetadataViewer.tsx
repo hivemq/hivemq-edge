19aaaec12fd2f128ecb8c01d97a89a4458209c50
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-10-04 14:59:59 +0100

    feat(25881): add a placeholder for the device metadata

---
+++ hivemq-edge/src/frontend/src/modules/Device/components/DeviceMetadataViewer.tsx
@@ -0,0 +1,53 @@
+import { FC } from 'react'
+import { useTranslation } from 'react-i18next'
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  Avatar,
+  Box,
+  Card,
+  CardBody,
+  CardHeader,
+  Flex,
+  Heading,
+  Text,
+} from '@chakra-ui/react'
+import { LuUpload } from 'react-icons/lu'
+
+import IconButton from '@/components/Chakra/IconButton.tsx'
+import { DeviceMetadata } from '@/modules/Workspace/types.ts'
+
+interface DeviceMetadataProps {
+  protocolAdapter?: DeviceMetadata
+}
+
+const DeviceMetadataViewer: FC<DeviceMetadataProps> = ({ protocolAdapter }) => {
+  const { t } = useTranslation()
+
+  return (
+    <Card size="sm">
+      <CardHeader>
+        <Flex gap="4">
+          <Flex flex="1" gap="4" alignItems="center" flexWrap="wrap">
+            <Avatar src={protocolAdapter?.logoUrl} />
+
+            <Box>
+              <Heading size="sm">{protocolAdapter?.id}</Heading>
+              <Text>{protocolAdapter?.category?.displayName}</Text>
+            </Box>
+          </Flex>
+          <IconButton aria-label="Load Metadata" icon={<LuUpload />} isDisabled />
+        </Flex>
+      </CardHeader>
+      <CardBody>
+        <Alert status="error">
+          <AlertIcon />
+          <AlertDescription>{t('XXXX no metadata loaded for the device')}</AlertDescription>
+        </Alert>
+      </CardBody>
+    </Card>
+  )
+}
+
+export default DeviceMetadataViewer
