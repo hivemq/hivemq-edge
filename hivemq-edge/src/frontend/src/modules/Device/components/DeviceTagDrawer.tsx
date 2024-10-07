d1a93d74c140ee4ed612fb0dd86bcbc8b97ed5a4
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-10-07 16:35:53 +0100

    feat(25881): add drawer for the tag editor

---
+++ hivemq-edge/src/frontend/src/modules/Device/components/DeviceTagDrawer.tsx
@@ -0,0 +1,60 @@
+import { FC } from 'react'
+import {
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  Text,
+  useDisclosure,
+} from '@chakra-ui/react'
+import IconButton from '@/components/Chakra/IconButton.tsx'
+import { LuFileCog } from 'react-icons/lu'
+import { useTranslation } from 'react-i18next'
+import DeviceTagForm from '@/modules/Device/components/DeviceTagForm.tsx'
+import { Adapter } from '@/api/__generated__'
+
+interface DeviceTagDrawerProps {
+  adapter?: Adapter
+}
+
+const DeviceTagDrawer: FC<DeviceTagDrawerProps> = ({ adapter }) => {
+  const { t } = useTranslation()
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  return (
+    <>
+      <IconButton
+        variant="primary"
+        aria-label={t('device.drawer.tagList.cta.edit')}
+        icon={<LuFileCog />}
+        isDisabled={false}
+        onClick={onOpen}
+      />
+      <Drawer isOpen={isOpen} placement="right" size="md" onClose={onClose} closeOnOverlayClick={false}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>
+            <Text> {t('device.drawer.tagEditor.title')}</Text>
+          </DrawerHeader>
+
+          <DrawerBody>
+            <DeviceTagForm adapterId={adapter?.id as string} adapterType={adapter?.type} />
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Button variant="primary" type="submit" form="namespace-form">
+              {t('unifiedNamespace.submit.label')}
+            </Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  )
+}
+
+export default DeviceTagDrawer
