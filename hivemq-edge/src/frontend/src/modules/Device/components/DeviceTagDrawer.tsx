cedfb617d0592d78f55120ab177e9a7cdf0311e4
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-10-09 14:58:46 +0100

    refactor(25881): refactor the onSubmit handling

--- hivemq-edge/src/frontend/src/modules/Device/components/DeviceTagDrawer.tsx
+++ hivemq-edge/src/frontend/src/modules/Device/components/DeviceTagDrawer.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react'
+import { useTranslation } from 'react-i18next'
 import {
   Button,
   Drawer,
@@ -11,20 +12,27 @@ import {
   Text,
   useDisclosure,
 } from '@chakra-ui/react'
-import IconButton from '@/components/Chakra/IconButton.tsx'
 import { LuFileCog } from 'react-icons/lu'
-import { useTranslation } from 'react-i18next'
+
+import { DomainTagList } from '@/api/__generated__'
+import IconButton from '@/components/Chakra/IconButton.tsx'
 import DeviceTagForm from '@/modules/Device/components/DeviceTagForm.tsx'
-import { Adapter } from '@/api/__generated__'
+import { ManagerContextType } from '@/modules/Mappings/types.ts'
 
 interface DeviceTagDrawerProps {
-  adapter?: Adapter
   isDisabled?: boolean
+  context: ManagerContextType
+  onSubmit?: (data: DomainTagList | undefined) => void
 }
 
-const DeviceTagDrawer: FC<DeviceTagDrawerProps> = ({ adapter, isDisabled = false }) => {
+const DeviceTagDrawer: FC<DeviceTagDrawerProps> = ({ context, onSubmit, isDisabled = false }) => {
   const { t } = useTranslation()
   const { isOpen, onOpen, onClose } = useDisclosure()
+
+  const onHandleSubmit = (data: DomainTagList | undefined) => {
+    onSubmit?.(data)
+    onClose()
+  }
 
   return (
     <>
@@ -44,11 +52,11 @@ const DeviceTagDrawer: FC<DeviceTagDrawerProps> = ({ adapter, isDisabled = false
           </DrawerHeader>
 
           <DrawerBody>
-            <DeviceTagForm adapterId={adapter?.id as string} adapterType={adapter?.type} />
+            <DeviceTagForm context={context} onSubmit={onHandleSubmit} />
           </DrawerBody>
 
           <DrawerFooter>
-            <Button variant="primary" type="submit" form="adapter-instance-form">
+            <Button variant="primary" type="submit" form="domainTags-instance-form">
               {t('unifiedNamespace.submit.label')}
             </Button>
           </DrawerFooter>
--- hivemq-edge/src/frontend/src/modules/Device/components/DeviceTagForm.tsx
+++ hivemq-edge/src/frontend/src/modules/Device/components/DeviceTagForm.tsx
@@ -4,8 +4,7 @@ import type { IChangeEvent } from '@rjsf/core'
 import Form from '@rjsf/chakra-ui'
 import debug from 'debug'
 
-import { useGetDomainTags } from '@/api/hooks/useProtocolAdapters/useGetDomainTags.tsx'
-import LoaderSpinner from '@/components/Chakra/LoaderSpinner.tsx'
+import { DomainTagList } from '@/api/__generated__'
 import ErrorMessage from '@/components/ErrorMessage.tsx'
 import { ObjectFieldTemplate } from '@/components/rjsf/ObjectFieldTemplate.tsx'
 import { FieldTemplate } from '@/components/rjsf/FieldTemplate.tsx'
@@ -14,42 +13,36 @@ import { ArrayFieldTemplate } from '@/components/rjsf/ArrayFieldTemplate.tsx'
 import { ArrayFieldItemTemplate } from '@/components/rjsf/ArrayFieldItemTemplate.tsx'
 import { customFormatsValidator } from '@/modules/ProtocolAdapters/utils/validation-utils.ts'
 import { adapterJSFFields, adapterJSFWidgets } from '@/modules/ProtocolAdapters/utils/uiSchema.utils.ts'
-import { useMappingManager } from '@/modules/Mappings/hooks/useMappingManager.tsx'
-import { DomainTagList } from '@/api/__generated__'
+import { ManagerContextType } from '@/modules/Mappings/types.ts'
 
 interface DeviceTagFormProps {
-  adapterId: string
-  adapterType?: string
+  context: ManagerContextType
+  onSubmit?: (data: DomainTagList | undefined) => void
 }
 
 const rjsfLog = debug('RJSF:DeviceTagForm')
 
-const DeviceTagForm: FC<DeviceTagFormProps> = ({ adapterId, adapterType }) => {
+const DeviceTagForm: FC<DeviceTagFormProps> = ({ context, onSubmit }) => {
   const { t } = useTranslation()
-  const { tagsManager, isLoading } = useMappingManager(adapterId)
-  const { data: tags, isError, isLoading: isLoadingTags } = useGetDomainTags(adapterId, adapterType)
 
   const onFormSubmit = useCallback(
-    (data: IChangeEvent) => {
-      const subscriptions = data.formData?.subscriptions
-      tagsManager?.onSubmit?.(subscriptions.tags)
+    (data: IChangeEvent<DomainTagList>) => {
+      onSubmit?.(data.formData)
     },
-    [tagsManager]
+    [onSubmit]
   )
 
-  if (isLoadingTags || isLoading) return <LoaderSpinner />
-  if (!tagsManager || isError || tagsManager.errors)
-    return <ErrorMessage message={t('protocolAdapter.error.loading')} />
+  if (!context.schema) return <ErrorMessage message={t('device.errors.noFormSchema')} />
 
   return (
     <Form
-      id="adapter-instance-form"
-      schema={tagsManager.schema}
-      uiSchema={tagsManager.uiSchema}
-      formData={{ tags: tags?.items }}
+      id="domainTags-instance-form"
+      schema={context.schema}
+      uiSchema={context.uiSchema}
+      formData={context.formData}
       liveValidate
       noHtml5Validate
-      // focusOnFirstError
+      focusOnFirstError
       onSubmit={onFormSubmit}
       validator={customFormatsValidator}
       showErrorList="bottom"
