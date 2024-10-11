4870073a580f3b44f4c8451c1e633527a2684d86
Daniel Krüger <45030029+DC2-DanielKrueger@users.noreply.github.com>
2024-10-11 14:55:40 +0200

    Feature/26414 tag rest api (#584)

---
+++ hivemq-edge/src/main/java/com/hivemq/persistence/domain/DomainTagDeleteResult.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2019-present HiveMQ GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hivemq.persistence.domain;
+
+import com.hivemq.extension.sdk.api.annotations.NotNull;
+import com.hivemq.extension.sdk.api.annotations.Nullable;
+
+public class DomainTagDeleteResult {
+
+    private final @NotNull DomainTagDeleteStatus dataPolicyUpdateStatus;
+    private final @Nullable String errorMessage;
+
+    public DomainTagDeleteResult(
+            final @NotNull DomainTagDeleteStatus dataPolicyUpdateStatus, final @Nullable String errorMessage) {
+        this.dataPolicyUpdateStatus = dataPolicyUpdateStatus;
+        this.errorMessage = errorMessage;
+    }
+
+    public static @NotNull DomainTagDeleteResult success() {
+        return new DomainTagDeleteResult(DomainTagDeleteStatus.SUCCESS, null);
+    }
+
+    public static @NotNull DomainTagDeleteResult failed(final @NotNull DomainTagDeleteStatus putStatus) {
+        return new DomainTagDeleteResult(putStatus,  null);
+    }
+
+    public static @NotNull DomainTagDeleteResult failed(
+            final @NotNull DomainTagDeleteStatus deleteResult, final @Nullable String errorMessage) {
+        return new DomainTagDeleteResult(deleteResult, errorMessage);
+    }
+
+    public @NotNull DomainTagDeleteStatus getDomainTagDeleteStatus() {
+        return dataPolicyUpdateStatus;
+    }
+
+
+    public @Nullable String getErrorMessage() {
+        return errorMessage;
+    }
+
+
+    public enum DomainTagDeleteStatus {
+        SUCCESS(),
+        NOT_FOUND()
+    }
+}
