40b0c3becbb06aa18cba7254ccb8aeefb14c070e
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-10-17 11:36:02 +0100

    refactor(25908): refactor onward mapping mock for

--- hivemq-edge/src/frontend/src/__test-utils__/adapters/mapping.utils.ts
+++ hivemq-edge/src/frontend/src/__test-utils__/adapters/mapping.utils.ts
@@ -1,5 +1,6 @@
 import { type RJSFSchema, UiSchema } from '@rjsf/utils'
 import { OutwardMapping } from '@/modules/Mappings/types.ts'
+import mqttToXMappings from './mapping.utils.json'
 
 interface MockMapping {
   schema?: RJSFSchema
@@ -10,83 +11,18 @@ interface MockMapping {
  * @deprecated This is a mock, will need to be replaced by OpenAPI specs when available
  */
 export const MOCK_OUTWARD_MAPPING_OPCUA: MockMapping = {
-  schema: {
-    $schema: 'https://json-schema.org/draft/2020-12/schema',
-    type: 'object',
-    properties: {
-      subscriptions: {
-        type: 'array',
-        items: {
-          type: 'object',
-          required: ['node', 'mqtt-topic'],
-          properties: {
-            node: {
-              type: 'string',
-              title: 'Destination Node ID',
-              description: 'identifier of the node on the OPC-UA server. Example: "ns=3;s=85/0:Temperature"',
-            },
-            'mqtt-topic': {
-              type: 'array',
-              title: 'Source Topics',
-              description: 'The MQTT topics used to identify the source of the mapping',
-              items: {
-                type: 'string',
-                uniqueItems: true,
-                format: 'mqtt-topic',
-              },
-            },
-            mapping: {
-              type: 'array',
-              title: 'Mapping instructions',
-              description:
-                'The list of data model transformations required to produce a valid destination node from the selected MQTT topics',
-              maxItems: 20,
-              items: {
-                type: 'object',
-                properties: {
-                  source: {
-                    type: 'array',
-                    description: 'The path of the property to use from the source data model',
-                    items: {
-                      type: 'string',
-                    },
-                  },
-                  destination: {
-                    type: 'string',
-                    description: 'The path of the property to populate in the destination data model',
-                  },
-                  transformation: {
-                    type: 'object',
-                    description: 'The transformation to apply to the source data points',
-                    properties: {
-                      function: {
-                        enum: ['toString', 'toInt', 'join'],
-                      },
-                      params: {
-                        type: 'string',
-                      },
-                    },
-                  },
-                },
-              },
-            },
-          },
-        },
-      },
-    },
-    required: ['subscriptions'],
-  },
+  schema: mqttToXMappings as RJSFSchema,
   uiSchema: {
     'ui:submitButtonOptions': {
-      norender: true,
+      // norender: true,
     },
-    subscriptions: {
-      'ui:field': 'mqtt:transform',
+    mqttToOpcuaMappings: {
+      // 'ui:field': 'mqtt:transform',
       items: {
-        'mqtt-topic': {
-          items: {
-            'ui:options': { create: false, multiple: false },
-          },
+        'ui:order': ['*', 'mqttMaxQos'],
+        fieldMapping: {
+          // This is not working
+          'ui:minItems': 1,
         },
       },
     },
