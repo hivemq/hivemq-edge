c08049df065240dbb7af52c714bdc99318d29abc
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-10-23 13:47:08 +0100

    fix(25908): fix icon for number

--- hivemq-edge/src/frontend/src/components/rjsf/MqttTransformation/utils/data-type.utils.tsx
+++ hivemq-edge/src/frontend/src/components/rjsf/MqttTransformation/utils/data-type.utils.tsx
@@ -1,20 +1,14 @@
 import type { JSONSchema7TypeName } from 'json-schema'
 import type { IconType } from 'react-icons'
-import {
-  MdDataObject,
-  MdNumbers,
-  MdOutlineDataArray,
-  MdOutlineNumbers,
-  MdOutlineTextFields,
-  MdQuestionMark,
-} from 'react-icons/md'
+import { MdDataObject, MdNumbers, MdOutlineDataArray, MdOutlineTextFields, MdQuestionMark } from 'react-icons/md'
 import { RxComponentBoolean } from 'react-icons/rx'
+import { TbDecimal } from 'react-icons/tb'
 
 export const DataTypeIcon: Record<JSONSchema7TypeName, IconType> = {
   ['object']: MdDataObject,
   ['string']: MdOutlineTextFields,
   ['array']: MdOutlineDataArray,
-  ['number']: MdOutlineNumbers,
+  ['number']: TbDecimal,
   ['boolean']: RxComponentBoolean,
   ['integer']: MdNumbers,
   ['null']: MdQuestionMark,
