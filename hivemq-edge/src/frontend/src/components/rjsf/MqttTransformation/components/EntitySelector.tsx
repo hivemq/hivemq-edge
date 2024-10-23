98eecedb32cfe54b58f8aaafb81a7b3efaba35d9
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-10-23 14:11:04 +0100

    fix(25908): fix placeholders for source and destination entities

--- hivemq-edge/src/frontend/src/components/rjsf/MqttTransformation/components/EntitySelector.tsx
+++ hivemq-edge/src/frontend/src/components/rjsf/MqttTransformation/components/EntitySelector.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react'
+import { useTranslation } from 'react-i18next'
 
 import { useGetDomainTags } from '@/api/hooks/useProtocolAdapters/useGetDomainTags.tsx'
 import { SingleTopicCreatableSelect } from '@/components/MQTT/TopicCreatableSelect.tsx'
@@ -14,9 +15,12 @@ interface SourceSelectorProps {
 
 export const SelectSourceTopics: FC<SourceSelectorProps> = ({ value, onChange }) => {
   const { data, isLoading } = useGetEdgeTopics({ publishOnly: false })
+  const { t } = useTranslation('components')
 
   return (
     <SingleTopicCreatableSelect
+      placeholder={t('rjsf.MqttTransformationField.sources.selector.placeholder')}
+      noOptionsMessage={() => t('rjsf.MqttTransformationField.sources.selector.noOptionsMessage')}
       isLoading={isLoading}
       options={data}
       id="mapping-select-source"
@@ -29,10 +33,13 @@ export const SelectSourceTopics: FC<SourceSelectorProps> = ({ value, onChange })
 }
 
 export const SelectDestinationTag: FC<SourceSelectorProps> = ({ adapterId, value, onChange }) => {
+  const { t } = useTranslation('components')
   const { isLoading, data } = useGetDomainTags(adapterId)
 
   return (
     <SingleTopicCreatableSelect
+      placeholder={t('rjsf.MqttTransformationField.destination.selector.placeholder')}
+      noOptionsMessage={() => t('rjsf.MqttTransformationField.destination.selector.noOptionsMessage')}
       isLoading={isLoading}
       options={data?.items?.map((deviceTag) => deviceTag.tag) || []}
       id="mapping-select-destination"
