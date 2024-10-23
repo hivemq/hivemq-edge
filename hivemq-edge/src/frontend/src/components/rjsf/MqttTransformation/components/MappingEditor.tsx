f754c2323b49fc49586abbd396d41dfac6e46393
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-10-23 14:00:13 +0100

    refactor(25908): refactor editor - add error handling - refactor property item out of its list

--- hivemq-edge/src/frontend/src/components/rjsf/MqttTransformation/components/MappingEditor.tsx
+++ hivemq-edge/src/frontend/src/components/rjsf/MqttTransformation/components/MappingEditor.tsx
@@ -1,16 +1,17 @@
 import { FC } from 'react'
 import { useTranslation } from 'react-i18next'
-import { Button, Card, CardBody, CardHeader, CardProps, Heading, HStack, VStack } from '@chakra-ui/react'
+import { Button, Card, CardBody, CardHeader, CardProps, Heading, HStack, List, ListItem } from '@chakra-ui/react'
 import { LuWand } from 'react-icons/lu'
 
 import { useGetTagSchemas } from '@/api/hooks/useDomainModel/useGetTagSchemas.ts'
+import ErrorMessage from '@/components/ErrorMessage.tsx'
 import LoaderSpinner from '@/components/Chakra/LoaderSpinner.tsx'
 import MappingInstruction from '@/components/rjsf/MqttTransformation/components/mapping/MappingInstruction.tsx'
 import { getPropertyListFrom } from '@/components/rjsf/MqttTransformation/utils/json-schema.utils.ts'
 import { FieldMapping } from '@/modules/Mappings/types.ts'
 
 interface MappingEditorProps extends Omit<CardProps, 'onChange'> {
-  topic: string
+  topic: string | undefined
   showTransformation?: boolean
   mapping?: FieldMapping[]
   onChange?: (v: FieldMapping[] | undefined) => void
@@ -18,7 +19,7 @@ interface MappingEditorProps extends Omit<CardProps, 'onChange'> {
 
 const MappingEditor: FC<MappingEditorProps> = ({ topic, showTransformation = false, mapping, onChange, ...props }) => {
   const { t } = useTranslation('components')
-  const { data, isLoading } = useGetTagSchemas([topic])
+  const { data, isLoading, isError, error, isSuccess } = useGetTagSchemas(topic ? [topic] : [])
 
   const properties = data ? getPropertyListFrom(data) : []
 
@@ -32,28 +33,36 @@ const MappingEditor: FC<MappingEditorProps> = ({ topic, showTransformation = fal
           {t('rjsf.MqttTransformationField.mapping.auto.aria-label')}
         </Button>
       </CardHeader>
-      <CardBody as={VStack} maxH="58vh" overflowY="scroll">
+      <CardBody maxH="58vh" overflowY="scroll">
         {isLoading && <LoaderSpinner />}
-
-        {properties.map((property) => {
-          const instruction = mapping ? mapping.findIndex((e) => e.destination === property.title) : -1
-          return (
-            <MappingInstruction
-              showTransformation={showTransformation}
-              property={property}
-              key={property.title}
-              mapping={instruction !== -1 ? mapping?.[instruction] : undefined}
-              onChange={(source, destination) => {
-                const newMappings = [...(mapping || [])]
-                const newItem = { source: [source], destination }
-                if (instruction !== -1) {
-                  newMappings[instruction] = newItem
-                } else newMappings.push(newItem)
-                onChange?.(newMappings)
-              }}
-            />
-          )
-        })}
+        {isError && error && <ErrorMessage message={error.message} />}
+        {isSuccess && (
+          <List>
+            {properties.map((property) => {
+              const instruction = mapping ? mapping.findIndex((e) => e.destination.propertyPath === property.title) : -1
+              return (
+                <ListItem key={property.title}>
+                  <MappingInstruction
+                    showTransformation={showTransformation}
+                    property={property}
+                    mapping={instruction !== -1 ? mapping?.[instruction] : undefined}
+                    onChange={(source, destination) => {
+                      const newMappings = [...(mapping || [])]
+                      const newItem: FieldMapping = {
+                        source: { propertyPath: source },
+                        destination: { propertyPath: destination },
+                      }
+                      if (instruction !== -1) {
+                        newMappings[instruction] = newItem
+                      } else newMappings.push(newItem)
+                      onChange?.(newMappings)
+                    }}
+                  />
+                </ListItem>
+              )
+            })}
+          </List>
+        )}
       </CardBody>
     </Card>
   )
