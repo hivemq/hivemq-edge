c16f7bb94465f4f96e459a1231e49a4eb3352bc7
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-10-24 15:02:10 +0100

    fix(25908): add utilities for types and properties

--- hivemq-edge/src/frontend/src/components/rjsf/MqttTransformation/utils/data-type.utils.ts
+++ hivemq-edge/src/frontend/src/components/rjsf/MqttTransformation/utils/data-type.utils.ts
@@ -4,6 +4,8 @@ import { MdDataObject, MdNumbers, MdOutlineDataArray, MdOutlineTextFields, MdQue
 import { RxComponentBoolean } from 'react-icons/rx'
 import { TbDecimal } from 'react-icons/tb'
 
+import { FlatJSONSchema7 } from '@/components/rjsf/MqttTransformation/utils/json-schema.utils.ts'
+
 export const DataTypeIcon: Record<JSONSchema7TypeName, IconType> = {
   ['object']: MdDataObject,
   ['string']: MdOutlineTextFields,
@@ -13,3 +15,11 @@ export const DataTypeIcon: Record<JSONSchema7TypeName, IconType> = {
   ['integer']: MdNumbers,
   ['null']: MdQuestionMark,
 }
+
+export const isMappingSupported = (property: FlatJSONSchema7) => {
+  return property.type !== 'object'
+}
+
+export const filterSupportedProperties = (property: FlatJSONSchema7) => Boolean(property.path.length === 0)
+
+export const formatPath = (path: string) => path.replaceAll('.', '.')
