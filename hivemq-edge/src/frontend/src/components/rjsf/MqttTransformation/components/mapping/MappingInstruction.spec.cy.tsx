5aae6832e4951c218e1b0c088715279c13e9b776
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-10-25 14:27:36 +0100

    test(25908): fix tests

--- hivemq-edge/src/frontend/src/components/rjsf/MqttTransformation/components/mapping/MappingInstruction.spec.cy.tsx
+++ hivemq-edge/src/frontend/src/components/rjsf/MqttTransformation/components/mapping/MappingInstruction.spec.cy.tsx
@@ -5,25 +5,56 @@ import MappingInstruction from './MappingInstruction.tsx'
 const MOCK_SUBS: OutwardMapping = {
   tag: 'my-node',
   mqttTopicFilter: 'my-topic',
-  fieldMapping: [],
+  fieldMapping: [
+    {
+      source: { propertyPath: 'this is a mapping' },
+      destination: { propertyPath: 'my-node' },
+    },
+  ],
 }
 
-const MOCK_PROPERTY: FlatJSONSchema7 = {
+const MOCK_PROPERTY_OBJECT: FlatJSONSchema7 = {
   description: undefined,
   path: [],
   key: 'billing-address',
   title: 'Billing address',
   type: 'object',
 }
+
+const MOCK_PROPERTY_STRING: FlatJSONSchema7 = {
+  description: undefined,
+  path: [],
+  key: 'billing-address',
+  title: 'Billing address',
+  type: 'string',
+}
+
 describe('MappingInstruction', () => {
   beforeEach(() => {
     cy.viewport(800, 900)
   })
 
-  it('should render properly', () => {
+  it('should render number properly', () => {
     cy.mountWithProviders(
       <MappingInstruction
-        property={MOCK_PROPERTY}
+        property={MOCK_PROPERTY_STRING}
+        showTransformation={false}
+        onChange={cy.stub()}
+        mapping={MOCK_SUBS.fieldMapping[1]}
+      />
+    )
+
+    cy.getByAriaLabel('Clear mapping').should('be.disabled')
+    cy.getByTestId('property-name').should('have.text', 'Billing address')
+    cy.getByAriaLabel('Property').should('have.attr', 'data-type', 'string').should('not.have.attr', 'draggable')
+    cy.getByTestId('mapping-instruction-dropzone').should('have.text', 'Drag a source property here')
+    cy.get('[role="alert"]').should('contain.text', 'Required').should('have.attr', 'data-status', 'error')
+  })
+
+  it('should render mapping properly', () => {
+    cy.mountWithProviders(
+      <MappingInstruction
+        property={MOCK_PROPERTY_STRING}
         showTransformation={false}
         onChange={cy.stub()}
         mapping={MOCK_SUBS.fieldMapping[0]}
@@ -31,14 +62,27 @@ describe('MappingInstruction', () => {
     )
 
     cy.getByAriaLabel('Clear mapping').should('not.be.disabled')
-    cy.get('ul li')
-      .eq(0)
-      .should('have.text', 'Billing address')
-      .should('have.attr', 'data-type', 'object')
-      .should('not.have.attr', 'draggable')
-    cy.getByTestId('mapping-instruction-dropzone').should('have.text', 'Drag a source property here')
+    cy.getByTestId('property-name').should('have.text', 'Billing address')
+    cy.getByAriaLabel('Property').should('have.attr', 'data-type', 'string').should('not.have.attr', 'draggable')
+    cy.getByTestId('mapping-instruction-dropzone').should('have.text', 'this is a mapping')
+    cy.get('[role="alert"]').should('contain.text', 'Matching').should('have.attr', 'data-status', 'success')
+  })
 
-    // TODO[NVL] Test drag and drop
+  it('should render object properly', () => {
+    cy.mountWithProviders(
+      <MappingInstruction
+        property={MOCK_PROPERTY_OBJECT}
+        showTransformation={false}
+        onChange={cy.stub()}
+        mapping={MOCK_SUBS.fieldMapping[1]}
+      />
+    )
+
+    cy.getByTestId('property-name').should('have.text', 'Billing address')
+    cy.getByAriaLabel('Property').should('have.attr', 'data-type', 'object').should('not.have.attr', 'draggable')
+    cy.getByTestId('mapping-instruction-dropzone').should('not.exist')
+    cy.getByAriaLabel('Clear mapping').should('not.exist')
+    cy.get('[role="alert"]').should('contain.text', 'Not supported').should('have.attr', 'data-status', 'warning')
   })
 
   it('should be accessible ', () => {
@@ -46,7 +90,7 @@ describe('MappingInstruction', () => {
 
     cy.mountWithProviders(
       <MappingInstruction
-        property={MOCK_PROPERTY}
+        property={MOCK_PROPERTY_STRING}
         showTransformation={false}
         onChange={cy.stub()}
         mapping={MOCK_SUBS.fieldMapping[0]}
@@ -54,5 +98,6 @@ describe('MappingInstruction', () => {
     )
 
     cy.checkAccessibility()
+    // TODO[NVL] Test drag and drop
   })
 })
