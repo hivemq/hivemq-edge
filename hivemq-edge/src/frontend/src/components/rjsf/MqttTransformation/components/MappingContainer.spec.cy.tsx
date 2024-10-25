b74e1dac0a1abceb10fe1607ba30d42623fd52fc
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-10-25 15:24:24 +0100

    test(25908): fix tests

--- hivemq-edge/src/frontend/src/components/rjsf/MqttTransformation/components/MappingContainer.spec.cy.tsx
+++ hivemq-edge/src/frontend/src/components/rjsf/MqttTransformation/components/MappingContainer.spec.cy.tsx
@@ -2,6 +2,7 @@ import { OutwardMapping } from '@/modules/Mappings/types.ts'
 import MappingContainer from './MappingContainer.tsx'
 import { mockAdapter, mockProtocolAdapter } from '@/api/hooks/useProtocolAdapters/__handlers__'
 import { mockBridge } from '@/api/hooks/useGetBridges/__handlers__'
+import { GENERATE_DATA_MODELS } from '@/api/hooks/useDomainModel/__handlers__'
 
 const MOCK_SUBS: OutwardMapping = {
   tag: 'my-tag',
@@ -13,12 +14,15 @@ const wrapper: React.JSXElementConstructor<{ children: React.ReactNode }> = ({ c
   return <h2>{children}</h2>
 }
 
-describe('SubscriptionContainer', () => {
+describe('MappingContainer', () => {
   beforeEach(() => {
     cy.viewport(1200, 900)
     cy.intercept('/api/v1/management/protocol-adapters/types', { items: [mockProtocolAdapter] }).as('getProtocols')
     cy.intercept('api/v1/management/protocol-adapters/adapters', { items: [mockAdapter] }).as('getAdapters')
     cy.intercept('/api/v1/management/bridges', { items: [mockBridge] }).as('getConfig3')
+    cy.intercept('/api/v1/management/client/filters', { statusCode: 404 })
+    cy.intercept('/api/v1/management/domain/tags/schema?*', GENERATE_DATA_MODELS(false, 'my-topic'))
+    cy.intercept('/api/v1/management/domain/topics/schema?*', GENERATE_DATA_MODELS(true, 'test'))
   })
 
   it('should be accessible ', () => {
@@ -30,6 +34,6 @@ describe('SubscriptionContainer', () => {
     )
 
     cy.checkAccessibility()
-    cy.percySnapshot('Component: SubscriptionContainer')
+    cy.percySnapshot('Component: MappingContainer')
   })
 })
