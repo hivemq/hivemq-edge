3118099790df1f5f69a545f55208fcf619814419
Daniel Krueger <daniel.krueger@hivemq.com>
2024-10-29 14:44:21 +0100

    add conditional import for hivemq-extension-sdk

--- hivemq-edge/settings.gradle.kts
+++ hivemq-edge/settings.gradle.kts
@@ -25,6 +25,23 @@ includeBuild("./src/frontend") {
     name = "hivemq-edge-frontend"
 }
 
+if (file("../hivemq-extension-sdk").exists()) {
+    includeBuild("../hivemq-extension-sdk")
+} else {
+    logger.warn(
+        """
+        ######################################################################################################
+        You can not use the latest changes of or modify the hivemq-extension-sdk.
+        Please checkout the hivemq-extension-sdk repository next to the hivemq-community-edition repository.
+        Execute the following command from your project directory:
+        git clone https://github.com/hivemq/hivemq-extension-sdk.git ../hivemq-extension-sdk
+        You can also clone your fork:
+        git clone https://github.com/<replace-with-your-fork>/hivemq-extension-sdk.git ../hivemq-extension-sdk
+        ######################################################################################################
+        """.trimIndent()
+    )
+}
+
 if (file("../../hivemq-edge-extension-sdk").exists()) {
     includeBuild("../../hivemq-edge-extension-sdk")
 } else {
