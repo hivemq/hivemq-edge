997a0a6c8ed50a15d6cbc997b4ccbbe1d25c468f
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-10-31 09:18:53 +0000

    feat(27489): add routing to topic filter management

--- hivemq-edge/src/frontend/src/modules/Workspace/components/nodes/NodeEdge.tsx
+++ hivemq-edge/src/frontend/src/modules/Workspace/components/nodes/NodeEdge.tsx
@@ -1,20 +1,33 @@
 import { FC } from 'react'
 import { Handle, Position, NodeProps } from 'reactflow'
-import { Image } from '@chakra-ui/react'
 import { useTranslation } from 'react-i18next'
+import { Icon, Image } from '@chakra-ui/react'
 
 import logo from '@/assets/edge/05-icon-industrial-hivemq-edge.svg'
+import ToolbarButtonGroup from '@/components/react-flow/ToolbarButtonGroup.tsx'
+import IconButton from '@/components/Chakra/IconButton.tsx'
+import { TopicIcon } from '@/components/Icons/TopicIcon.tsx'
 import NodeWrapper from '@/modules/Workspace/components/parts/NodeWrapper.tsx'
 import { useContextMenu } from '@/modules/Workspace/hooks/useContextMenu.ts'
 import ContextualToolbar from '@/modules/Workspace/components/nodes/ContextualToolbar.tsx'
+import { useNavigate } from 'react-router-dom'
 
 const NodeEdge: FC<NodeProps> = (props) => {
   const { t } = useTranslation()
   const { onContextMenu } = useContextMenu(props.id, props.selected, '/workspace/edge')
+  const navigate = useNavigate()
 
   return (
     <>
-      <ContextualToolbar id={props.id} dragging={props.dragging} onOpenPanel={onContextMenu} />
+      <ContextualToolbar id={props.id} dragging={props.dragging} onOpenPanel={onContextMenu}>
+        <ToolbarButtonGroup>
+          <IconButton
+            icon={<Icon as={TopicIcon} />}
+            aria-label={t('workspace.toolbar.command.edge.topicFilter')}
+            onClick={() => navigate(`topic-filters/`)}
+          />
+        </ToolbarButtonGroup>
+      </ContextualToolbar>
       <NodeWrapper
         isSelected={props.selected}
         onDoubleClick={onContextMenu}
