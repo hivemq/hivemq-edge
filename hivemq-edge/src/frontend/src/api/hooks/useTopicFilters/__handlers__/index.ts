1f3710fc9323abd6e9edae69b2876357dd106ff4
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-10-31 09:16:19 +0000

    test(27489): add tests

---
+++ hivemq-edge/src/frontend/src/api/hooks/useTopicFilters/__handlers__/index.ts
@@ -0,0 +1,29 @@
+import { http, HttpResponse } from 'msw'
+import { type TopicFilter, type TopicFilterList } from '@/api/__generated__'
+
+export const MOCK_TOPIC_FILTER: TopicFilter = {
+  topicFilter: 'a/topic/+/filter',
+  description: 'This is a topic filter',
+}
+
+export const handlers = [
+  http.post('**/management/topic-filters', () => {
+    return HttpResponse.json({}, { status: 200 })
+  }),
+
+  http.delete('**/management/topic-filters/:name', ({ params }) => {
+    const { name } = params
+
+    return HttpResponse.json({ name: name }, { status: 200 })
+  }),
+
+  http.put('**/management/topic-filters/:name', ({ params }) => {
+    const { name } = params
+
+    return HttpResponse.json({ name: name }, { status: 200 })
+  }),
+
+  http.get('**/management/topic-filters', () => {
+    return HttpResponse.json<TopicFilterList>({ items: [MOCK_TOPIC_FILTER] }, { status: 200 })
+  }),
+]
