bf6ea9cc945f446d1fb4ae8b78a3075fbc09a89f
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-11-07 10:14:25 +0000

    test(27489): fix test

--- hivemq-edge/src/frontend/src/components/rjsf/SplitArrayEditor/components/ArrayItemDrawer.spec.cy.tsx
+++ hivemq-edge/src/frontend/src/components/rjsf/SplitArrayEditor/components/ArrayItemDrawer.spec.cy.tsx
@@ -1,33 +1,35 @@
 /// <reference types="cypress" />
 
 import ArrayItemDrawer from '@/components/rjsf/SplitArrayEditor/components/ArrayItemDrawer.tsx'
+import { Button } from '@chakra-ui/react'
 
-describe('DeviceTagDrawer', () => {
+describe('ArrayItemDrawer', () => {
   beforeEach(() => {
     cy.viewport(800, 800)
   })
 
-  it('should render in disabled state', () => {
-    cy.mountWithProviders(<ArrayItemDrawer context={{}} isDisabled />, {
-      routerProps: { initialEntries: [`/node/wrong-adapter`] },
-    })
-    cy.getByAriaLabel('Edit tags').should('be.visible').should('be.disabled')
-    cy.get('[role="dialog"]').should('not.exist')
-  })
-
   it('should properly', () => {
     const onSubmit = cy.stub().as('onSubmit')
-    cy.mountWithProviders(<ArrayItemDrawer context={{}} onSubmit={onSubmit} />, {
-      routerProps: { initialEntries: [`/node/wrong-adapter`] },
-    })
+    cy.mountWithProviders(
+      <ArrayItemDrawer
+        header="test"
+        context={{}}
+        trigger={({ onOpen }) => (
+          <Button data-testid="trigger" onClick={onOpen}>
+            Click me!
+          </Button>
+        )}
+        onSubmit={onSubmit}
+      />,
+      {
+        routerProps: { initialEntries: [`/node/wrong-adapter`] },
+      }
+    )
 
-    cy.getByAriaLabel('Edit tags').should('be.visible').should('not.be.disabled')
-    cy.getByAriaLabel('Edit tags').click()
-
+    cy.get('[role="dialog"]').should('not.exist')
+    cy.getByTestId('trigger').click()
     cy.get('[role="dialog"]').should('be.visible')
-    cy.get('header').should('contain.text', 'Edit the tags')
-
-    cy.get('button[type="submit"]').should('contain.text', 'Submit')
+    cy.get('header').should('contain.text', 'test')
     cy.injectAxe()
     cy.checkAccessibility()
   })
