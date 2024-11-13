84795c9151068a91896fffdee56ca93d96c82aba
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-11-13 11:06:05 +0000

    fix(27867): fix labels and form controls

--- hivemq-edge/src/frontend/src/components/rjsf/MqttTransformation/components/EntitySelector.tsx
+++ hivemq-edge/src/frontend/src/components/rjsf/MqttTransformation/components/EntitySelector.tsx
@@ -1,6 +1,8 @@
 import { FC } from 'react'
 
-import { SelectTag, SelectTopicFilter } from '@/components/MQTT/EntityCreatableSelect.tsx'
+import { SelectEntityType, SelectTag, SelectTopicFilter } from '@/components/MQTT/EntityCreatableSelect.tsx'
+import { FormControl, FormLabel } from '@chakra-ui/react'
+import { useTranslation } from 'react-i18next'
 
 interface SourceSelectorProps {
   adapterType?: string
@@ -10,16 +12,30 @@ interface SourceSelectorProps {
 }
 
 export const SelectSourceTopics: FC<SourceSelectorProps> = ({ value, onChange }) => {
-  return <SelectTopicFilter value={value || null} onChange={onChange} id="mapping-select-source" />
+  const { t } = useTranslation('components')
+  return (
+    <FormControl isRequired isInvalid={!value}>
+      <FormLabel htmlFor={`react-select-${SelectEntityType.TOPIC_FILTER}-input`}>
+        {t('rjsf.MqttTransformationField.sources.selector.formLabel')}
+      </FormLabel>
+      <SelectTopicFilter value={value || null} onChange={onChange} id="mapping-select-source" />{' '}
+    </FormControl>
+  )
 }
 
 export const SelectDestinationTag: FC<SourceSelectorProps> = ({ adapterId, value, onChange }) => {
+  const { t } = useTranslation('components')
   return (
-    <SelectTag
-      adapterId={adapterId as string}
-      id="mapping-select-destination"
-      onChange={onChange}
-      value={value || null}
-    />
+    <FormControl isRequired isInvalid={!value}>
+      <FormLabel htmlFor={`react-select-${SelectEntityType.TAG}-input`}>
+        {t('rjsf.MqttTransformationField.destination.selector.formLabel')}
+      </FormLabel>
+      <SelectTag
+        adapterId={adapterId as string}
+        id="mapping-select-destination"
+        onChange={onChange}
+        value={value || null}
+      />
+    </FormControl>
   )
 }
