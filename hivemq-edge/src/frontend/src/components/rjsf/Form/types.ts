f5c5f1b6244d5598e80104e9eab5cddd2d15b8a0
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-11-15 11:29:32 +0000

    refactor(27766): update type for support on error focus

--- hivemq-edge/src/frontend/src/components/rjsf/Form/types.ts
+++ hivemq-edge/src/frontend/src/components/rjsf/Form/types.ts
@@ -1,5 +1,6 @@
-import { FormContextType, IdSchema } from '@rjsf/utils'
+import { FormContextType, IdSchema, RJSFValidationError } from '@rjsf/utils'
 
 export interface ChakraRJSFormContext extends FormContextType {
   onBatchUpload?: (idSchema: IdSchema<unknown>, batch: Record<string, unknown>[]) => void
+  focusOnError?: (error: RJSFValidationError) => void
 }
