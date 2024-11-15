cafcbd8ac257cc434da673fdaba9908e493a1e79
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-11-15 11:30:32 +0000

    feat(27766): add utilities for focus on errors

---
+++ hivemq-edge/src/frontend/src/components/rjsf/Form/error-focus.utils.ts
@@ -0,0 +1,57 @@
+import { MutableRefObject } from 'react'
+import Form from '@rjsf/core'
+import { RJSFValidationError, UiSchema } from '@rjsf/utils'
+
+// From RJSF
+const focusOnError = (formElement: HTMLFormElement, error: RJSFValidationError) => {
+  // const { idPrefix = 'root', idSeparator = '_' } = props;
+  const idPrefix = 'root',
+    idSeparator = '_'
+
+  const { property } = error
+  if (!property) return
+
+  // WARNING: This is not a drop in replacement solution and
+  // it might not work for some edge cases. Test your code!
+  const _toPath = (path: string) => path?.match(/([^[.\]])+/g)
+
+  const path = _toPath(property)
+  if (!path) return
+  if (path[0] === '') {
+    // Most of the time the `.foo` property results in the first element being empty, so replace it with the idPrefix
+    path[0] = 'root'
+  } else {
+    // Otherwise insert the idPrefix into the first location using unshift
+    path.unshift(idPrefix)
+  }
+
+  //
+  const elementId = path.join(idSeparator)
+  // let field = formElement.elements[elementId]
+  let field = formElement.querySelector(`#${elementId}`) as HTMLElement
+
+  if (!field) {
+    // if not an exact match, try finding an input starting with the element id (like radio buttons or checkboxes)
+    // field = this.formElement.current.querySelector(`input[id^="${elementId}"`)
+    field = formElement.querySelector(`input[id^="${elementId}"`) as HTMLElement
+  }
+  if (field instanceof HTMLDivElement) {
+    // if the field is a select or other wrapper, find the nested input
+    field = formElement.querySelector<HTMLDivElement>(`#${elementId} input`) as HTMLElement
+    console.log('XXX', field)
+  }
+
+  if (field) {
+    field.focus()
+  }
+}
+
+export const customFocusError =
+  (wrapperRef: MutableRefObject<null>, uiSchema?: UiSchema) => (error: RJSFValidationError) => {
+    if (!wrapperRef.current) return
+
+    const rjsForm = wrapperRef.current as Form
+
+    console.log('I need to handle focusing on this error', rjsForm, uiSchema, error)
+    focusOnError(rjsForm.formElement.current, error)
+  }
