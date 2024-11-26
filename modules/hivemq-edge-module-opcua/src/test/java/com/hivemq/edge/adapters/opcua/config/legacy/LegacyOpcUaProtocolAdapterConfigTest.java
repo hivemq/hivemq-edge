9dadbb5d0d83b5a784324f84f51f34c81229b417
Jochen Mader <jochen.mader@hivemq.com>
2024-11-26 18:56:14 +0100

    opc ua working

--- modules/hivemq-edge-module-opcua/src/test/java/com/hivemq/edge/adapters/opcua/config/legacy/LegacyOpcUaProtocolAdapterConfigTest.java
+++ modules/hivemq-edge-module-opcua/src/test/java/com/hivemq/edge/adapters/opcua/config/legacy/LegacyOpcUaProtocolAdapterConfigTest.java
@@ -15,25 +15,18 @@
  */
 package com.hivemq.edge.adapters.opcua.config.legacy;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hivemq.adapter.sdk.api.factories.ProtocolAdapterFactoryInput;
-import com.hivemq.configuration.entity.HiveMQConfigEntity;
-import com.hivemq.configuration.reader.ConfigFileReaderWriter;
+import com.hivemq.configuration.migration.ConfigurationMigrator;
 import com.hivemq.configuration.reader.ConfigurationFile;
 import com.hivemq.edge.adapters.opcua.OpcUaProtocolAdapterFactory;
-import com.hivemq.edge.adapters.opcua.config.OpcUaSpecificAdapterConfig;
-import com.hivemq.protocols.ProtocolAdapterConfig;
-import org.jetbrains.annotations.NotNull;
+import com.hivemq.edge.modules.ModuleLoader;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.net.URL;
-import java.nio.file.Path;
+import java.util.List;
 import java.util.Map;
 
-import static com.hivemq.edge.adapters.opcua.config.SecPolicy.BASIC128RSA15;
-import static com.hivemq.edge.adapters.opcua.config.SecPolicy.NONE;
-import static com.hivemq.protocols.ProtocolAdapterUtils.createProtocolAdapterMapper;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -41,148 +34,138 @@ import static org.mockito.Mockito.when;
 @SuppressWarnings("unchecked")
 class LegacyOpcUaProtocolAdapterConfigTest {
 
-    private final @NotNull ObjectMapper mapper = createProtocolAdapterMapper(new ObjectMapper());
-
-    // TODO
-    /*
     @Test
     public void convertConfigObject_fullConfig_valid() throws Exception {
         final URL resource = getClass().getResource("/legacy-opcua-adapter-full-config.xml");
-        final File path = Path.of(resource.toURI()).toFile();
 
-        final HiveMQConfigEntity configEntity = loadConfig(path);
-        final Map<String, Object> adapters = configEntity.getProtocolAdapterConfig();
-
-        ProtocolAdapterFactoryInput mockInput = mock(ProtocolAdapterFactoryInput.class);
+        final ConfigurationMigrator migrator = new ConfigurationMigrator(
+                new ConfigurationFile(new File(resource.toURI())),
+                mock(ModuleLoader.class));
+        final ProtocolAdapterFactoryInput mockInput = mock(ProtocolAdapterFactoryInput.class);
         when(mockInput.isWritingEnabled()).thenReturn(true);
-        final OpcUaProtocolAdapterFactory opcUaProtocolAdapterFactory =
-                new OpcUaProtocolAdapterFactory(mockInput);
 
-        final ProtocolAdapterConfig protocolAdapterConfig =
-                ProtocolAdapterConfig.fromAdapterConfigMap((Map<String, Object>) adapters.get("opcua"),
-                        true,
-                        mapper,
-                        opcUaProtocolAdapterFactory);
-        assertThat(protocolAdapterConfig.missingTags())
-                .isEmpty();
+        assertThat(migrator.migrateIfNeeded(Map.of("opcua", new OpcUaProtocolAdapterFactory(mockInput))))
+                .isNotEmpty()
+                .get()
+                .satisfies(cfg -> {
+                    assertThat(cfg.getProtocolAdapterConfig())
+                            .hasSize(1)
+                            .allSatisfy(entity -> {
+                                assertThat(entity.getProtocolId()).isEqualTo("opcua");
+                                assertThat(entity.getAdapterId()).isEqualTo("simulation-server-2");
+                                assertThat(entity.getConfig().get("uri")).isEqualTo("opc.tcp://CSM1.local:53530/OPCUA/SimulationServer");
+                                assertThat(entity.getConfig().get("overrideUri")).isEqualTo(true);
+                                assertThat(((Map<String,Object>)entity.getConfig().get("security")).get("policy")).isEqualTo("BASIC128RSA15");
 
-        final OpcUaSpecificAdapterConfig config = (OpcUaSpecificAdapterConfig) protocolAdapterConfig.getAdapterConfig();
+                                final Map<String, Object> auth =
+                                        (Map<String, Object>) entity.getConfig().get("auth");
+                                assertThat(((Map<String,Object>) auth.get("basic")).get("username")).isEqualTo("edge");
+                                assertThat(((Map<String,Object>) auth.get("basic")).get("password")).isEqualTo("password");
+                                assertThat(((Map<String,Object>) auth.get("x509")).get("enabled")).isEqualTo(true);
 
-        assertThat(config.getId()).isEqualTo("simulation-server-2");
-        assertThat(config.getUri()).isEqualTo("opc.tcp://CSM1.local:53530/OPCUA/SimulationServer");
-        assertThat(config.getOverrideUri()).isTrue();
-        assertThat(config.getSecurity()).satisfies(security -> {
-            assertThat(security.getPolicy()).isEqualTo(BASIC128RSA15);
-        });
+                                final Map<String, Object> tls =
+                                        (Map<String, Object>) entity.getConfig().get("tls");
+                                assertThat(tls.get("enabled")).isEqualTo(true);
 
-        assertThat(config.getAuth()).satisfies(auth -> {
-            assertThat(auth.getBasicAuth()).isNotNull();
-            assertThat(auth.getBasicAuth().getUsername()).isEqualTo("edge");
-            assertThat(auth.getBasicAuth().getPassword()).isEqualTo("password");
+                                final Map<String, Object> keystore =
+                                        (Map<String, Object>) tls.get("keystore");
+                                assertThat(keystore.get("path")).isEqualTo("path/to/keystore");
+                                assertThat(keystore.get("password")).isEqualTo("keystore-password");
+                                assertThat(keystore.get("privateKeyPassword")).isEqualTo("private-key-password");
 
-            assertThat(auth.getX509Auth()).isNotNull();
-            assertThat(auth.getX509Auth().isEnabled()).isTrue();
-        });
+                                assertThat(((Map<String,Object>) tls.get("truststore")).get("path")).isEqualTo("path/to/truststore");
+                                assertThat(((Map<String,Object>) tls.get("truststore")).get("password")).isEqualTo("truststore-password");
 
-        assertThat(config.getTls()).satisfies(tls -> {
-            assertThat(tls.isEnabled()).isTrue();
+                                List<Map<String,Object>> mappings = (List<Map<String,Object>>)((Map<String,Object>) entity.getConfig().get("opcuaToMqtt")).get("opcuaToMqttMappings");
+                                assertThat(mappings.get(0).get("tagName").toString()).startsWith("simulation-server-2-");
+                                assertThat(mappings.get(0).get("mqttTopic").toString()).isEqualTo("test/blubb/#");
+                                assertThat(mappings.get(0).get("publishingInterval")).isEqualTo(12);
+                                assertThat(mappings.get(0).get("serverQueueSize")).isEqualTo(13);
+                                assertThat(mappings.get(0).get("mqttQos")).isEqualTo(1);
+                                assertThat(mappings.get(0).get("messageExpiryInterval")).isEqualTo(15L);
 
-            assertThat(tls.getKeystore()).isNotNull();
-            assertThat(tls.getKeystore().getPath()).isEqualTo("path/to/keystore");
-            assertThat(tls.getKeystore().getPassword()).isEqualTo("keystore-password");
-            assertThat(tls.getKeystore().getPrivateKeyPassword()).isEqualTo("private-key-password");
+                                assertThat(mappings.get(1).get("tagName").toString()).startsWith("simulation-server-2-");
+                                assertThat(mappings.get(1).get("mqttTopic").toString()).isEqualTo("test/blubbb/#");
+                                assertThat(mappings.get(1).get("publishingInterval")).isEqualTo(13);
+                                assertThat(mappings.get(1).get("serverQueueSize")).isEqualTo(14);
+                                assertThat(mappings.get(1).get("mqttQos")).isEqualTo(2);
+                                assertThat(mappings.get(1).get("messageExpiryInterval")).isEqualTo(16L);
 
-            assertThat(tls.getTruststore()).isNotNull();
-            assertThat(tls.getTruststore().getPath()).isEqualTo("path/to/truststore");
-            assertThat(tls.getTruststore().getPassword()).isEqualTo("truststore-password");
-        });
-
-        assertThat(config.getOpcuaToMqttConfig()).isNotNull();
-        assertThat(config.getOpcuaToMqttConfig().getOpcuaToMqttMappings()).satisfiesExactly(mapping -> {
-            assertThat(mapping.getTagName()).startsWith("simulation-server");
-            assertThat(mapping.getMqttTopic()).isEqualTo("test/blubb/#");
-            assertThat(mapping.getQos()).isEqualTo(1);
-            assertThat(mapping.getPublishingInterval()).isEqualTo(12);
-            assertThat(mapping.getServerQueueSize()).isEqualTo(13);
-            assertThat(mapping.getMessageExpiryInterval()).isEqualTo(15);
-        }, mapping -> {
-            assertThat(mapping.getTagName()).startsWith("simulation-server");
-            assertThat(mapping.getMqttTopic()).isEqualTo("test/blubbb/#");
-            assertThat(mapping.getQos()).isEqualTo(2);
-            assertThat(mapping.getPublishingInterval()).isEqualTo(13);
-            assertThat(mapping.getServerQueueSize()).isEqualTo(14);
-            assertThat(mapping.getMessageExpiryInterval()).isEqualTo(16);
-        });
-
+                                assertThat(entity.getTags())
+                                        .hasSize(2)
+                                        .anySatisfy(tag -> {
+                                            assertThat(tag.getName()).startsWith("simulation-server-2-");
+                                            assertThat(tag.getDefinition())
+                                                    .extracting("node")
+                                                    .isEqualTo("ns=1;i=1004");
+                                        })
+                                        .anySatisfy(tag -> {
+                                            assertThat(tag.getName()).startsWith("simulation-server-2-");
+                                            assertThat(tag.getDefinition())
+                                                    .extracting("node")
+                                                    .isEqualTo("ns=2;i=1004");
+                                        });
+                                assertThat(entity.getFromEdgeMappingEntities()).isEmpty(); // not yet available for opcua
+                                assertThat(entity.getFieldMappings()).isEmpty();
+                                assertThat(entity.getToEdgeMappingEntities()).isEmpty();
+                            });
+                });
     }
+
 
     @Test
     public void convertConfigObject_defaults_valid() throws Exception {
         final URL resource = getClass().getResource("/legacy-opcua-adapter-minimal-config.xml");
-        final File path = Path.of(resource.toURI()).toFile();
 
-        final HiveMQConfigEntity configEntity = loadConfig(path);
-        final Map<String, Object> adapters = configEntity.getProtocolAdapterConfig();
 
-        ProtocolAdapterFactoryInput mockInput = mock(ProtocolAdapterFactoryInput.class);
+        final ConfigurationMigrator migrator = new ConfigurationMigrator(
+                new ConfigurationFile(new File(resource.toURI())),
+                mock(ModuleLoader.class));
+        final ProtocolAdapterFactoryInput mockInput = mock(ProtocolAdapterFactoryInput.class);
         when(mockInput.isWritingEnabled()).thenReturn(true);
-        final OpcUaProtocolAdapterFactory opcUaProtocolAdapterFactory =
-                new OpcUaProtocolAdapterFactory(mockInput);
 
-        final ProtocolAdapterConfig protocolAdapterConfig =
-                ProtocolAdapterConfig.fromAdapterConfigMap((Map<String, Object>) adapters.get("opcua"),
-                        true,
-                        mapper,
-                        opcUaProtocolAdapterFactory);
-        assertThat(protocolAdapterConfig.missingTags())
-                .isEmpty();
+        assertThat(migrator.migrateIfNeeded(Map.of("opcua", new OpcUaProtocolAdapterFactory(mockInput))))
+                .isNotEmpty()
+                .get()
+                .satisfies(cfg -> {
+                    assertThat(cfg.getProtocolAdapterConfig())
+                            .hasSize(1)
+                            .allSatisfy(entity -> {
+                                assertThat(entity.getProtocolId()).isEqualTo("opcua");
+                                assertThat(entity.getAdapterId()).isEqualTo("simulation-server-2");
+                                assertThat(entity.getConfig().get("uri")).isEqualTo("opc.tcp://CSM1.local:53530/OPCUA/SimulationServer");
+                                assertThat(entity.getConfig().get("overrideUri")).isEqualTo(false);
+                                assertThat(((Map<String,Object>)entity.getConfig().get("security")).get("policy")).isEqualTo("NONE");
 
-        final OpcUaSpecificAdapterConfig config = (OpcUaSpecificAdapterConfig) protocolAdapterConfig.getAdapterConfig();
+                                final Map<String, Object> auth =
+                                        (Map<String, Object>) entity.getConfig().get("auth");
+                                assertThat(((Map<String,Object>) auth.get("basic"))).isNull();
+                                assertThat(((Map<String,Object>) auth.get("x509"))).isNull();
 
-        assertThat(config.getId()).isEqualTo("simulation-server-2");
-        assertThat(config.getUri()).isEqualTo("opc.tcp://CSM1.local:53530/OPCUA/SimulationServer");
-        assertThat(config.getOverrideUri()).isFalse();
-        assertThat(config.getSecurity().getPolicy()).isEqualTo(NONE);
+                                final Map<String, Object> tls =
+                                        (Map<String, Object>) entity.getConfig().get("tls");
+                                assertThat(tls.get("enabled")).isEqualTo(false);
 
-        assertThat(config.getAuth()).satisfies(auth -> {
-            assertThat(auth.getBasicAuth()).isNull();
-            assertThat(auth.getX509Auth()).isNull();
-        });
+                                List<Map<String,Object>> mappings = (List<Map<String,Object>>)((Map<String,Object>) entity.getConfig().get("opcuaToMqtt")).get("opcuaToMqttMappings");
+                                assertThat(mappings.get(0).get("tagName").toString()).startsWith("simulation-server-2-");
+                                assertThat(mappings.get(0).get("mqttTopic").toString()).isEqualTo("test/blubb/#");
+                                assertThat(mappings.get(0).get("publishingInterval")).isEqualTo(1000);
+                                assertThat(mappings.get(0).get("serverQueueSize")).isEqualTo(1);
+                                assertThat(mappings.get(0).get("mqttQos")).isEqualTo(0);
+                                assertThat(mappings.get(0).get("messageExpiryInterval")).isEqualTo(4294967295L);
 
-        assertThat(config.getTls()).satisfies(tls -> {
-            assertThat(tls.isEnabled()).isFalse();
-        });
-
-        assertThat(config.getOpcuaToMqttConfig()).isNotNull();
-        assertThat(config.getOpcuaToMqttConfig().getOpcuaToMqttMappings()).satisfiesExactly(mapping -> {
-            assertThat(mapping.getTagName()).startsWith("simulation-server");
-            assertThat(mapping.getMqttTopic()).isEqualTo("test/blubb/#");
-            assertThat(mapping.getQos()).isEqualTo(0);
-            assertThat(mapping.getPublishingInterval()).isEqualTo(1000);
-            assertThat(mapping.getServerQueueSize()).isEqualTo(1);
-            assertThat(mapping.getMessageExpiryInterval()).isEqualTo(4294967295L);
-        });
-
+                                assertThat(entity.getTags())
+                                        .hasSize(1)
+                                        .satisfiesExactly(tag -> {
+                                            assertThat(tag.getName()).startsWith("simulation-server-2-");
+                                            assertThat(tag.getDefinition())
+                                                    .extracting("node")
+                                                    .isEqualTo("ns=1;i=1004");
+                                        });
+                                assertThat(entity.getFromEdgeMappingEntities()).isEmpty(); // not yet available for opcua
+                                assertThat(entity.getFieldMappings()).isEmpty();
+                                assertThat(entity.getToEdgeMappingEntities()).isEmpty();
+                            });
+                });
     }
-
-    private @NotNull HiveMQConfigEntity loadConfig(final @NotNull File configFile) {
-        final ConfigFileReaderWriter readerWriter = new ConfigFileReaderWriter(new ConfigurationFile(configFile),
-                mock(),
-                mock(),
-                mock(),
-                mock(),
-                mock(),
-                mock(),
-                mock(),
-                mock(),
-                mock(),
-                mock(),
-                mock(),
-                mock(),
-                mock(),
-                mock());
-        return readerWriter.applyConfig();
-    }
-
-     */
 }
