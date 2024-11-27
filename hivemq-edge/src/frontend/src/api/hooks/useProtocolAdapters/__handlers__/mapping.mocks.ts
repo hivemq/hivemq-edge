33674800416afd0607cddaddaae70810a1d494d6
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-11-27 15:03:11 +0000

    test(28015): add tests

--- hivemq-edge/src/frontend/src/api/hooks/useProtocolAdapters/__handlers__/mapping.mocks.ts
+++ hivemq-edge/src/frontend/src/api/hooks/useProtocolAdapters/__handlers__/mapping.mocks.ts
@@ -1,5 +1,5 @@
 import { http, HttpResponse } from 'msw'
-import type { FieldMappingsListModel, FieldMappingsModel } from '@/api/__generated__'
+import type { FieldMappingsListModel, FieldMappingsModel, JsonNode } from '@/api/__generated__'
 import { GENERATE_DATA_MODELS } from '@/api/hooks/useDomainModel/__handlers__'
 
 export const MOCK_MAPPING: FieldMappingsModel = {
@@ -13,4 +13,12 @@ export const mappingHandlers = [
   http.get<{ adapterId: string }>('*/management/protocol-adapters/adapters/:adapterId/fieldmappings', () => {
     return HttpResponse.json<FieldMappingsListModel>({ items: [MOCK_MAPPING] }, { status: 200 })
   }),
+
+  http.get<{ adapterId: string; tagName: string }>(
+    '*/management/protocol-adapters/writing-schema/:adapterId/:tagName',
+    ({ params }) => {
+      const { tagName } = params
+      return HttpResponse.json<JsonNode>(GENERATE_DATA_MODELS(true, tagName), { status: 200 })
+    }
+  ),
 ]
---
+++ hivemq-edge/src/frontend/src/api/hooks/useProtocolAdapters/useGetWritingSchema.spec.ts
@@ -0,0 +1,37 @@
+import { beforeEach, expect } from 'vitest'
+import { renderHook, waitFor } from '@testing-library/react'
+
+import { server } from '@/__test-utils__/msw/mockServer.ts'
+import { SimpleWrapper as wrapper } from '@/__test-utils__/hooks/SimpleWrapper.tsx'
+import { useGetWritingSchema } from '@/api/hooks/useProtocolAdapters/useGetWritingSchema.ts'
+
+import { mappingHandlers } from './__handlers__'
+
+describe('useGetWritingSchema', () => {
+  beforeEach(() => {
+    server.use(...mappingHandlers)
+  })
+
+  afterEach(() => {
+    server.resetHandlers()
+  })
+
+  it('should load the data', async () => {
+    const { result } = renderHook(() => useGetWritingSchema('my-adapter', 'my-tag'), { wrapper })
+    await waitFor(() => {
+      expect(result.current.isLoading).toBeFalsy()
+      expect(result.current.isSuccess).toBeTruthy()
+    })
+    expect(result.current.data).toStrictEqual(
+      expect.objectContaining({
+        description: 'A simple form example.',
+        title: 'my-tag',
+        properties: expect.objectContaining({
+          age: expect.objectContaining({}),
+          array: expect.objectContaining({}),
+          firstName: expect.objectContaining({}),
+        }),
+      })
+    )
+  })
+})
