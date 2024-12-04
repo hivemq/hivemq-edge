1c226d1e76d93b4ab85ce5f9a6eb59c94039c402
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-12-04 09:39:32 +0000

    feat(28441): add radio button component

---
+++ hivemq-edge/src/frontend/src/components/Chakra/RadioButton.tsx
@@ -0,0 +1,35 @@
+import { FC } from 'react'
+import { UseRadioProps, Box, useRadio, Text } from '@chakra-ui/react'
+
+interface RadioButtonProps extends UseRadioProps {
+  children: React.ReactNode
+}
+
+export const RadioButton: FC<RadioButtonProps> = ({ children, ...rest }) => {
+  const { getInputProps, getRadioProps } = useRadio(rest)
+
+  return (
+    <Box as="label">
+      <input {...getInputProps()} />
+      <Box
+        {...getRadioProps()}
+        cursor="pointer"
+        borderWidth="1px"
+        borderRadius="md"
+        boxShadow="md"
+        _checked={{
+          bg: 'teal.600',
+          color: 'white',
+          borderColor: 'teal.600',
+        }}
+        _focus={{
+          boxShadow: 'outline',
+        }}
+        px={5}
+        py={1}
+      >
+        <Text>{children}</Text>
+      </Box>
+    </Box>
+  )
+}
