a547bca325a67dc62e249d07d2250b7653ee4c5d
Daniel Krüger <45030029+DC2-DanielKrueger@users.noreply.github.com>
2024-12-05 16:49:22 +0100

    Add translation to JS max safe value for message expiry and back (#674)
    * add translation to JS max safe value for message expiry and back
    
    * add license headers

---
+++ hivemq-edge/src/test/java/com/hivemq/api/model/mappings/northbound/NorthboundMappingModelTest.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2019-present HiveMQ GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hivemq.api.model.mappings.northbound;
+
+import com.hivemq.adapter.sdk.api.config.MessageHandlingOptions;
+import com.hivemq.api.model.JavaScriptConstants;
+import com.hivemq.persistence.mappings.NorthboundMapping;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class NorthboundMappingModelTest {
+
+
+    @Test
+    void test_constructor_whenInputExpiryIsMaxLong_thenStoredExpiryIsMaxSafeJSValue() {
+
+        final NorthboundMappingModel northboundMappingModel = new NorthboundMappingModel("topic",
+                "tag",
+                MessageHandlingOptions.MQTTMessagePerSubscription,
+                false,
+                false,
+                List.of(),
+                2,
+                Long.MAX_VALUE);
+
+        assertEquals(JavaScriptConstants.JS_MAX_SAFE_INTEGER, northboundMappingModel.getMessageExpiryInterval());
+    }
+
+    @Test
+    void test_to_whenMessageExpiryMaxSafeValue_thenParsedValueIsMaxLong() {
+
+        final NorthboundMappingModel northboundMappingModel = new NorthboundMappingModel("topic",
+                "tag",
+                MessageHandlingOptions.MQTTMessagePerSubscription,
+                false,
+                false,
+                List.of(),
+                2,
+                Long.MAX_VALUE);
+
+        assertEquals(JavaScriptConstants.JS_MAX_SAFE_INTEGER, northboundMappingModel.getMessageExpiryInterval());
+
+        final NorthboundMapping northboundMapping = northboundMappingModel.to();
+        assertEquals(Long.MAX_VALUE, northboundMapping.getMessageExpiryInterval());
+
+    }
+
+
+}
