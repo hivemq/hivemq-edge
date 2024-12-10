771c454bec586ef3ed60b6bcb131e95f84ac03d4
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-12-10 11:31:34 +0000

    refactor(28664): refactor uploader

--- hivemq-edge/src/frontend/src/modules/TopicFilters/components/SchemaUploader.tsx
+++ hivemq-edge/src/frontend/src/modules/TopicFilters/components/SchemaUploader.tsx
@@ -1,7 +1,7 @@
 import { FC, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useDropzone } from 'react-dropzone'
-import { AlertStatus, Button, Text, useToast, VStack } from '@chakra-ui/react'
+import { AlertStatus, Button, Card, CardBody, Text, useToast } from '@chakra-ui/react'
 
 import { DEFAULT_TOAST_OPTION } from '@/hooks/useEdgeToast/toast-utils.ts'
 import { getDropZoneBorder } from '@/modules/Theme/utils.ts'
@@ -48,25 +48,28 @@ const SchemaUploader: FC<SchemaUploaderProps> = ({ onUpload }) => {
   })
 
   return (
-    <VStack
-      {...getRootProps()}
-      {...getDropZoneBorder('blue.500')}
-      minHeight="calc(250px - 2rem)"
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      id="dropzone"
-    >
-      <input {...getInputProps()} data-testid="schema-dropzone" />
-      {isDragActive && <Text>{t('rjsf.batchUpload.dropZone.dropping', { ns: 'components' })}</Text>}
-      {loading && <Text>{t('rjsf.batchUpload.dropZone.loading', { ns: 'components' })}</Text>}
-      {!isDragActive && !loading && (
-        <>
-          <Text>{t('topicFilter.schema.actions.upload')}</Text>
-          <Button onClick={open}>{t('rjsf.batchUpload.dropZone.selectFile', { ns: 'components' })}</Button>
-        </>
-      )}
-    </VStack>
+    <Card variant="filled">
+      <CardBody
+        {...getRootProps()}
+        {...getDropZoneBorder('blue.500')}
+        minHeight="calc(250px - 2rem)"
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        id="dropzone"
+      >
+        <input {...getInputProps()} data-testid="schema-dropzone" />
+        {isDragActive && <Text>{t('rjsf.batchUpload.dropZone.dropping', { ns: 'components' })}</Text>}
+        {loading && <Text>{t('rjsf.batchUpload.dropZone.loading', { ns: 'components' })}</Text>}
+        {!isDragActive && !loading && (
+          <>
+            <Text>{t('topicFilter.schema.actions.upload')}</Text>
+            <Button onClick={open}>{t('rjsf.batchUpload.dropZone.selectFile', { ns: 'components' })}</Button>
+          </>
+        )}
+      </CardBody>
+    </Card>
   )
 }
 
