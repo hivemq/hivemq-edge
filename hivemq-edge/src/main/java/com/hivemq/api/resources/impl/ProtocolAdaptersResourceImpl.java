22d7e3c1d98517a70fc87eacc3066e2bc5620822
Daniel Krueger <daniel.krueger@hivemq.com>
2024-12-11 18:13:51 +0100

    fix after rebase

--- hivemq-edge/src/main/java/com/hivemq/api/resources/impl/ProtocolAdaptersResourceImpl.java
+++ hivemq-edge/src/main/java/com/hivemq/api/resources/impl/ProtocolAdaptersResourceImpl.java
@@ -27,6 +27,7 @@ import com.hivemq.adapter.sdk.api.tag.Tag;
 import com.hivemq.adapter.sdk.api.writing.WritingProtocolAdapter;
 import com.hivemq.api.AbstractApi;
 import com.hivemq.api.adapters.AdapterConfigModel;
+import com.hivemq.api.errors.InvalidInputError;
 import com.hivemq.api.format.DataUrl;
 import com.hivemq.api.errors.AlreadyExistsError;
 import com.hivemq.api.errors.BadRequestError;
@@ -654,8 +655,8 @@ public class ProtocolAdaptersResourceImpl extends AbstractApi implements Protoco
             } catch (final IllegalArgumentException illegalArgumentException) {
                 log.warn("Unable to parse tags for adapter '{}'", adapterName);
                 log.debug("Original Exception: ", illegalArgumentException);
-                return ErrorResponseUtil.invalidInput(
-                        "Exception during parsing of tags for the adapter. See log for further information.");
+                return ErrorResponseUtil.errorResponse(new InvalidInputError(
+                        "Exception during parsing of tags for the adapter. See log for further information."));
             }
 
             final List<NorthboundMapping> northboundMappings = adapter.getNorthboundMappingModels()
