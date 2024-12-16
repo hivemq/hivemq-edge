f48854e127e22e91da28bde394b32e3100475538
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2024-12-16 10:17:11 +0000

    refactor(28703): add a subheader to the drawer

--- hivemq-edge/src/frontend/src/components/ExpandableDrawer/ExpandableDrawer.tsx
+++ hivemq-edge/src/frontend/src/components/ExpandableDrawer/ExpandableDrawer.tsx
@@ -14,9 +14,10 @@ import DrawerExpandButton from '@/components/Chakra/DrawerExpandButton.tsx'
 
 interface ExpandableDrawerProps extends DrawerProps {
   header: string
+  subHeader?: JSX.Element
 }
 
-const ExpandableDrawer: FC<ExpandableDrawerProps> = ({ header, ...props }) => {
+const ExpandableDrawer: FC<ExpandableDrawerProps> = ({ header, subHeader, ...props }) => {
   // TODO[NVL] use  local storage
   const [isExpanded, setExpanded] = useBoolean(true)
 
@@ -28,6 +29,7 @@ const ExpandableDrawer: FC<ExpandableDrawerProps> = ({ header, ...props }) => {
         <DrawerExpandButton isExpanded={isExpanded} toggle={setExpanded.toggle} />
         <DrawerHeader>
           <Text>{header}</Text>
+          {subHeader}
         </DrawerHeader>
         <DrawerBody display="flex" flexDirection="column" gap={6}>
           {props.children}
