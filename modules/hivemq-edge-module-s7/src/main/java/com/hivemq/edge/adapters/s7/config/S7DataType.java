e461009adc4573019515fcdddec45abcd2036281
Jochen Mader <jochen.mader@hivemq.com>
2024-12-18 12:47:55 +0100

    Reqorking types!

--- modules/hivemq-edge-module-s7/src/main/java/com/hivemq/edge/adapters/s7/config/S7DataType.java
+++ modules/hivemq-edge-module-s7/src/main/java/com/hivemq/edge/adapters/s7/config/S7DataType.java
@@ -15,16 +15,61 @@
  */
 package com.hivemq.edge.adapters.s7.config;
 
+import java.math.BigInteger;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.List;
+
+import static com.hivemq.edge.adapters.s7.config.S7Versions.S7_1200;
+import static com.hivemq.edge.adapters.s7.config.S7Versions.S7_1500;
+import static com.hivemq.edge.adapters.s7.config.S7Versions.S7_200;
+import static com.hivemq.edge.adapters.s7.config.S7Versions.S7_300;
+import static com.hivemq.edge.adapters.s7.config.S7Versions.S7_400;
 
 /**
- * @author Simon L Johnson
+ * Documented here:
+ * https://support.industry.siemens.com/cs/mdm/109054417?c=69695636619&lc=en-GE
  */
 public enum S7DataType {
-    BOOL(Boolean.class),
-    BYTE(Byte.class),
+    BOOL(Boolean.class, 1, List.of(S7_300, S7_400, S7_1200, S7_1500), "Boolean", "https://support.industry.siemens.com/cs/mdm/109054417?c=46422035979&lc=en-GE"),
+    BYTE(Byte.class, 8, List.of(S7_300, S7_400, S7_1200, S7_1500), "Byte", "https://support.industry.siemens.com/cs/mdm/109054417?c=56595553163&lc=en-GE"),
+    WORD(Byte[].class, 16, List.of(S7_300, S7_400, S7_1200, S7_1500), "Word", "https://support.industry.siemens.com/cs/mdm/109054417?c=56595472523&lc=en-GE"),
+    DWORD(Byte[].class, 32, List.of(S7_300, S7_400, S7_1200, S7_1500), "Double Word", "https://support.industry.siemens.com/cs/mdm/109054417?c=56595322763&lc=en-GE"),
+    LWORD(Byte[].class, 64, List.of(S7_1500), "Long Word", "https://support.industry.siemens.com/cs/mdm/109054417?c=56595507211&lc=en-GE"),
+    USINT(Short.class, 8, List.of(S7_1200, S7_1500), "Unsigned Short Integer", "https://support.industry.siemens.com/cs/mdm/109054417?c=46521647883&lc=en-GE"),
+    UINT(Integer.class, 16, List.of(S7_1200, S7_1500), "Unsigned Integer", "https://support.industry.siemens.com/cs/mdm/109054417?c=46521834123&lc=en-GE"),
+    UDINT(Long.class, 32, List.of(S7_1200, S7_1500), "Unsigned Double Integer", "https://support.industry.siemens.com/cs/mdm/109054417?c=46521930763&lc=en-GE"),
+    ULINT(BigInteger.class, 64, List.of(S7_1500), "Unsigned Long Integer", "https://support.industry.siemens.com/cs/mdm/109054417?c=59653945739&lc=en-GE"),
+    SINT(Byte.class, 8, List.of(S7_1200, S7_1500), "Short Integer", "https://support.industry.siemens.com/cs/mdm/109054417?c=68894861835&lc=en-GE"),
+    INT(Short.class, 16, List.of(S7_300, S7_400, S7_1200, S7_1500),"Integer", "https://support.industry.siemens.com/cs/mdm/109054417?c=63679745547&lc=en-GE"),
+    DINT(Integer.class, 32, List.of(S7_300, S7_400, S7_1200, S7_1500), "Double Integer", "https://support.industry.siemens.com/cs/mdm/109054417?c=46521869963&lc=en-GE"),
+    LINT(Long.class, 64, List.of(S7_1500), "Long Integer", "https://support.industry.siemens.com/cs/mdm/109054417?c=66825552267&lc=en-GE"),
+    REAL(Float.class, 32, List.of(S7_300, S7_400, S7_1200, S7_1500), "Real", "https://support.industry.siemens.com/cs/mdm/109054417?c=68826794251&lc=en-GE"),
+    LREAL(Float.class, 64, List.of(S7_1200, S7_1500), "Long Real", "https://support.industry.siemens.com/cs/mdm/109054417?c=68826903691&lc=en-GE"),
+
+    CHAR(Character.class, 8, List.of(S7_300, S7_400, S7_1200, S7_1500), "Character", "https://support.industry.siemens.com/cs/mdm/109054417?c=57152595083&lc=en-GE"),
+    WCHAR(Short.class, 16, List.of(S7_1200, S7_1500), "Wide Character", "https://support.industry.siemens.com/cs/mdm/109054417?c=10488733835&lc=en-GE"),
+    STRING(String.class, -1, List.of(S7_300, S7_400, S7_1200, S7_1500), "String, 0 to 254 characters only ASCII", "https://support.industry.siemens.com/cs/mdm/109054417?c=63689840011&lc=en-GE"),
+    WSTRING(String.class, -1, List.of(S7_1200, S7_1500), "Wide String, 0 to 254 characters only Unicode", "https://support.industry.siemens.com/cs/mdm/109054417?c=61472021771&lc=en-GE"), //
+
+    TIME(Long.class, 32, List.of(S7_300, S7_400, S7_1200, S7_1500), "IEC Time (ms)", "https://support.industry.siemens.com/cs/mdm/109054417?c=61085966091&lc=en-GE"),
+    LTIME(BigInteger.class, 64, List.of(S7_1500), "IEC Time (ns)", "https://support.industry.siemens.com/cs/mdm/109054417?c=61410814475&lc=en-GE"),
+
+    DATE(Short.class, 8, List.of(S7_300, S7_400, S7_1200, S7_1500), "IEC Date, since 01-01-1990 (Year-Month-Day)", "https://support.industry.siemens.com/cs/mdm/109054417?c=46522046859&lc=en-GE"),
+    TOD(Long.class, 32, List.of(S7_300, S7_400, S7_1200, S7_1500), "Time Of Day (hours:minutes:seconds.milliseconds)", "https://support.industry.siemens.com/cs/mdm/109054417?c=64869849355&lc=en-GE"),
+    LTOD(BigInteger.class, 64, List.of(S7_1500), "Time-of-day (hours:minutes:seconds.nanoseconds)", "https://support.industry.siemens.com/cs/mdm/109054417?c=64869390987&lc=en-GE"),
+    DT(BigInteger.class, 64, List.of(S7_1500), "Date and time (year-month-day-hour:minute:second:millisecond)", "https://support.industry.siemens.com/cs/mdm/109054417?c=61473284875&lc=en-GE"),
+    LDT(BigInteger.class, 64, List.of(S7_1500), "Date and time (year-month-day-hour:minute:second:nanoseconds)", "https://support.industry.siemens.com/cs/mdm/109054417?c=71834521483&lc=en-GE"),
+    DTL(BigInteger.class, 64, List.of(S7_1500), "Date and time (year-month-day-hour:minute:second:nanoseconds)", "https://support.industry.siemens.com/cs/mdm/109054417?c=64682916235&lc=en-GE"),
+
+
+    /**
+     * UNSUPPORTED
+     * S5TIME https://support.industry.siemens.com/cs/mdm/109054417?c=63689295627&lc=en-GE
+     */
+
+    BYTE_ARRAY(Byte[].class),
     INT16(Short.class),
     UINT16(Short.class),
     INT32(Integer.class),
@@ -38,13 +83,36 @@ public enum S7DataType {
     DATE_AND_TIME(LocalDateTime.class),
     TIME(Long.class);
 
-    S7DataType(Class<?> javaType){
+    S7DataType(Class<?> javaType, int lengthInBits, List<S7Versions> availableOn, String description, String docs){
         this.javaType = javaType;
+        this.lengthInBits = lengthInBits;
+        this.availableOn = availableOn;
+        this.description = description;
+        this.docs = docs;
     }
     private Class<?> javaType;
+    private final int lengthInBits;
+    private final List<S7Versions> availableOn;
+    private final String description;
+    private final String docs;
 
     public Class<?> getJavaType() {
         return javaType;
     }
 
+    public int getLengthInBits() {
+        return lengthInBits;
+    }
+
+    public List<S7Versions> getAvailableOn() {
+        return availableOn;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getDocs() {
+        return docs;
+    }
 }
