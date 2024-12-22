5895c0feb61f83d6fe42f7b6d7e2be8ef12282de
Anthony Olazabal <anthony.olazabal@hotmail.fr>
2024-12-22 22:30:11 +0100

    Update to 2024.8 SDK

---
+++ modules/hivemq-edge-module-postgresql/src/main/java/com/hivemq/edge/adapters/postgresql/config/PostgreSQLAdapterTag.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2024-present HiveMQ GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hivemq.edge.adapters.postgresql.config;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.hivemq.adapter.sdk.api.annotations.ModuleConfigField;
+import com.hivemq.adapter.sdk.api.tag.Tag;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Objects;
+
+public class PostgreSQLAdapterTag implements Tag {
+
+    @JsonProperty(value = "name", required = true)
+    @ModuleConfigField(title = "name",
+            description = "Name of the tag to be used in mappings",
+            format = ModuleConfigField.FieldType.MQTT_TAG,
+            required = true)
+    private final @NotNull String name;
+
+    @JsonProperty(value = "description")
+    @ModuleConfigField(title = "description",
+            description = "Description of the tag")
+    private final @NotNull String description;
+
+    @JsonProperty(value = "definition", required = true)
+    @ModuleConfigField(title = "definition",
+            description = "The actual definition of the tag for PostgreSQL Query")
+    private final @NotNull PostgreSQLAdapterTagDefinition definition;
+
+
+    public PostgreSQLAdapterTag(
+            @JsonProperty(value = "name", required = true) final @NotNull String name,
+            @JsonProperty(value = "description") final @Nullable String description,
+            @JsonProperty(value = "definition", required = true) final @NotNull PostgreSQLAdapterTagDefinition definition) {
+        this.name = name;
+        this.description = Objects.requireNonNullElse(description, "no description present.");
+        this.definition = definition;
+    }
+
+    @Override
+    public @NotNull PostgreSQLAdapterTagDefinition getDefinition() {
+        return definition;
+    }
+
+    @Override
+    public @NotNull String getName() {
+        return name;
+    }
+
+    @Override
+    public @NotNull String getDescription() {
+        return description;
+    }
+
+    @Override
+    public boolean equals(final @Nullable Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final PostgreSQLAdapterTag postgreSQLAdapterTag = (PostgreSQLAdapterTag) o;
+        return Objects.equals(name, postgreSQLAdapterTag.name) &&
+                Objects.equals(description, postgreSQLAdapterTag.description) &&
+                Objects.equals(definition, postgreSQLAdapterTag.definition);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, description, definition);
+    }
+}
+
+
