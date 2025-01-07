d128f6a493eaecbf50b89c44a500e11c2f5c51a0
Daniel Krueger <daniel.krueger@hivemq.com>
2025-01-07 10:57:09 +0100

    include adapter in the edge platform

--- modules/hivemq-edge-module-postgresql/settings.gradle.kts
+++ modules/hivemq-edge-module-postgresql/settings.gradle.kts
@@ -1,10 +1,13 @@
 rootProject.name = "hivemq-postgresql-protocol-adapter"
 
 pluginManagement {
-    plugins {
-        id("com.github.johnrengelman.shadow") version "${extra["plugin.shadow.version"]}"
-        id("com.github.sgtsilvio.gradle.utf8") version "${extra["plugin.utf8.version"]}"
-        id("com.github.hierynomus.license") version "${extra["plugin.license.version"]}"
-        id("org.owasp.dependencycheck") version "${extra["plugin.dependencycheck.version"]}"
+    includeBuild("../../edge-plugins")
+}
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            from(files("../../gradle/libs.versions.toml"))
+        }
     }
 }
