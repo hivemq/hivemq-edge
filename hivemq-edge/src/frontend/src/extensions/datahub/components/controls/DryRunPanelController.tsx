2c597b266d6e74e42846f944901547b03b0d9168
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-01-09 11:52:09 +0000

    refactor(27085): add a side-panel report for dry runs

---
+++ hivemq-edge/src/frontend/src/extensions/datahub/components/controls/DryRunPanelController.tsx
@@ -0,0 +1,88 @@
+import { useCallback, useEffect } from 'react'
+import { useLocation, useNavigate, useParams } from 'react-router-dom'
+import { useReactFlow, Node } from 'reactflow'
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  useDisclosure,
+} from '@chakra-ui/react'
+
+import useDataHubDraftStore from '@datahub/hooks/useDataHubDraftStore.ts'
+import PolicySummaryReport from '@datahub/components/helpers/PolicySummaryReport.tsx'
+import PolicyErrorReport from '@datahub/components/helpers/PolicyErrorReport.tsx'
+import { usePolicyChecksStore } from '@datahub/hooks/usePolicyChecksStore.ts'
+import { PolicyDryRunStatus } from '@datahub/types.ts'
+
+import { ANIMATION } from '@datahub/utils/datahub.utils.ts'
+import { ToolbarPublish } from '@datahub/components/toolbar/ToolbarPublish.tsx'
+
+const DryRunPanelController = () => {
+  const { policyType } = useParams()
+  const { state } = useLocation()
+  const navigate = useNavigate()
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const { nodes } = useDataHubDraftStore()
+  const { status, getErrors } = usePolicyChecksStore()
+  const { fitView } = useReactFlow()
+
+  const errorNodeFrom = useCallback((id: string) => nodes.find((node) => node.id === id), [nodes])
+
+  useEffect(() => {
+    if (policyType) {
+      onOpen()
+    }
+  }, [onOpen, policyType])
+
+  const onDrawerClose = useCallback(() => {
+    onClose()
+    navigate(state?.origin || '/datahub')
+  }, [onClose, navigate, state?.origin])
+
+  const onShowNode = (node: Node) => {
+    fitView({ nodes: [node], padding: 3, duration: ANIMATION.FIT_VIEW_DURATION_MS })
+  }
+
+  const onShowEditor = (node: Node) => {
+    navigate(`../node/${node.type}/${node.id}`, { state: { origin: state?.origin } })
+  }
+
+  return (
+    <Drawer size="sm" isOpen={isOpen} placement="right" onClose={onDrawerClose}>
+      <DrawerContent data-testid="node-editor-content">
+        <DrawerCloseButton />
+        <DrawerHeader>Policy Dry Run</DrawerHeader>
+
+        <DrawerBody>
+          <Card size="sm">
+            <CardHeader>
+              <PolicySummaryReport status={status || PolicyDryRunStatus.FAILURE} />
+            </CardHeader>
+            <CardBody>
+              <PolicyErrorReport
+                errors={getErrors() || []}
+                onFitView={(id) => {
+                  const errorNode = errorNodeFrom(id)
+                  if (errorNode) onShowNode(errorNode)
+                }}
+                onOpenConfig={(id) => {
+                  const errorNode = errorNodeFrom(id)
+                  if (errorNode) onShowEditor(errorNode)
+                }}
+              />
+            </CardBody>
+          </Card>
+        </DrawerBody>
+        <DrawerFooter borderTopWidth="1px"> {status === PolicyDryRunStatus.SUCCESS && <ToolbarPublish />}</DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  )
+}
+
+export default DryRunPanelController
