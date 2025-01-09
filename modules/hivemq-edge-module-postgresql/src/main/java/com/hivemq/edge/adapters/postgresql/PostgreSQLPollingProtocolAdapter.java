87924cf549e319dfb7078dc6b42ca2ad8f433dec
Daniel Krueger <daniel.krueger@hivemq.com>
2025-01-09 17:16:52 +0100

    fix prepared statements add config option to set the connection timeout

--- modules/hivemq-edge-module-postgresql/src/main/java/com/hivemq/edge/adapters/postgresql/PostgreSQLPollingProtocolAdapter.java
+++ modules/hivemq-edge-module-postgresql/src/main/java/com/hivemq/edge/adapters/postgresql/PostgreSQLPollingProtocolAdapter.java
@@ -30,19 +30,18 @@ import com.hivemq.adapter.sdk.api.polling.PollingProtocolAdapter;
 import com.hivemq.adapter.sdk.api.state.ProtocolAdapterState;
 import com.hivemq.adapter.sdk.api.tag.Tag;
 import com.hivemq.edge.adapters.postgresql.config.PostgreSQLAdapterConfig;
+import com.hivemq.edge.adapters.postgresql.config.PostgreSQLAdapterTag;
 import com.hivemq.edge.adapters.postgresql.config.PostgreSQLAdapterTagDefinition;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Optional;
 
 
 public class PostgreSQLPollingProtocolAdapter implements PollingProtocolAdapter {
@@ -56,12 +55,7 @@ public class PostgreSQLPollingProtocolAdapter implements PollingProtocolAdapter 
     private final @NotNull ProtocolAdapterState protocolAdapterState;
     private final @NotNull String adapterId;
     private final @NotNull List<Tag> tags;
-    private final @NotNull String compiledUri;
-    private final @NotNull String username;
-    private final @NotNull String password;
-    private final @NotNull DatabaseConnection databaseConnection = new DatabaseConnection();
-    private final @NotNull List<PollingContext> pollingContexts;
-    private final @NotNull HashMap<String, PreparedStatement> tagNameToPreparedStatement = new HashMap<>();
+    private final @NotNull DatabaseConnection databaseConnection;
 
     public PostgreSQLPollingProtocolAdapter(
             final @NotNull ProtocolAdapterInformation adapterInformation,
@@ -71,13 +65,14 @@ public class PostgreSQLPollingProtocolAdapter implements PollingProtocolAdapter 
         this.adapterConfig = input.getConfig();
         this.protocolAdapterState = input.getProtocolAdapterState();
         this.tags = input.getTags();
-        this.compiledUri = String.format("jdbc:postgresql://%s:%s/%s",
+        final String compiledUri = String.format("jdbc:postgresql://%s:%s/%s",
                 adapterConfig.getServer(),
                 adapterConfig.getPort(),
                 adapterConfig.getDatabase());
-        this.username = adapterConfig.getUsername();
-        this.password = adapterConfig.getPassword();
-        this.pollingContexts = input.getPollingContexts();
+        this.databaseConnection = new DatabaseConnection(compiledUri,
+                adapterConfig.getUsername(),
+                adapterConfig.getPassword(),
+                adapterConfig.getConnectionTimeout());
     }
 
     @Override
@@ -94,14 +89,7 @@ public class PostgreSQLPollingProtocolAdapter implements PollingProtocolAdapter 
             output.failStart(e, null);
             return;
         }
-        databaseConnection.connect(compiledUri, username, password);
-
-        try {
-            preparePreparedStatements(output);
-        } catch (final SQLException e) {
-            output.failStart(e, null);
-            return;
-        }
+        databaseConnection.connect();
 
         try {
             log.debug("Starting connection to the database instance");
@@ -116,26 +104,6 @@ public class PostgreSQLPollingProtocolAdapter implements PollingProtocolAdapter 
         } catch (final Exception e) {
             output.failStart(e, null);
             protocolAdapterState.setConnectionStatus(ProtocolAdapterState.ConnectionStatus.DISCONNECTED);
-        }
-    }
-
-    private void preparePreparedStatements(final @NotNull ProtocolAdapterStartOutput output) throws SQLException {
-        for (final PollingContext pollingContext : pollingContexts) {
-            final Optional<Tag> optDomainTag =
-                    tags.stream().filter(tag -> tag.getName().equals(pollingContext.getTagName())).findFirst();
-            if (optDomainTag.isPresent()) {
-                final PostgreSQLAdapterTagDefinition definition =
-                        (PostgreSQLAdapterTagDefinition) optDomainTag.get().getDefinition();
-                final PreparedStatement preparedStatement =
-                        databaseConnection.getConnection().prepareStatement(definition.getQuery());
-                tagNameToPreparedStatement.put(pollingContext.getTagName(), preparedStatement);
-            } else {
-                output.failStart(new IllegalStateException(
-                                "Polling for PostgreSQL protocol adapter failed because the used tag '" +
-                                        pollingContext.getTagName() +
-                                        "' was not found. For the polling to work the tag must be created via REST API or the UI."),
-                        null);
-            }
         }
     }
 
@@ -164,49 +132,53 @@ public class PostgreSQLPollingProtocolAdapter implements PollingProtocolAdapter 
         tags.stream()
                 .filter(tag -> tag.getName().equals(pollingContext.getTagName()))
                 .findFirst()
-                .ifPresentOrElse(def -> loadDataFromDB(pollingOutput, def),
+                .ifPresentOrElse(tag -> loadDataFromDB(pollingOutput, (PostgreSQLAdapterTag) tag),
                         () -> pollingOutput.fail("Polling for PostgreSQL protocol adapter failed because the used tag '" +
                                 pollingInput.getPollingContext().getTagName() +
                                 "' was not found. For the polling to work the tag must be created via REST API or the UI."));
         pollingOutput.finish();
     }
 
-    private void loadDataFromDB(final @NotNull PollingOutput pollingOutput, final @NotNull Tag tag) {
+    private void loadDataFromDB(final @NotNull PollingOutput output, final @NotNull PostgreSQLAdapterTag tag) {
         try {
             log.debug("Getting tag definition");
             /* Get the tag definition (Query, RowLimit and Split Lines)*/
-            final PostgreSQLAdapterTagDefinition definition = (PostgreSQLAdapterTagDefinition) tag.getDefinition();
+            final PostgreSQLAdapterTagDefinition definition = tag.getDefinition();
 
             /* Execute query and handle result */
-            final PreparedStatement preparedStatement = tagNameToPreparedStatement.get(tag.getName());
-            final ResultSet result = preparedStatement.executeQuery();
-            assert result != null;
-            final ArrayList<ObjectNode> resultObject = new ArrayList<>();
-            final ResultSetMetaData resultSetMD = result.getMetaData();
-            while (result.next()) {
-                final int numColumns = resultSetMD.getColumnCount();
-                final ObjectNode node = OBJECT_MAPPER.createObjectNode();
-                for (int i = 1; i <= numColumns; i++) {
-                    final String column_name = resultSetMD.getColumnName(i);
-                    node.put(column_name, result.getString(column_name));
+            try (final Connection connection = databaseConnection.getConnection()) {
+                final PreparedStatement preparedStatement = connection.prepareStatement(tag.getDefinition().getQuery());
+                final ResultSet result = preparedStatement.executeQuery();
+                assert result != null;
+                final ArrayList<ObjectNode> resultObject = new ArrayList<>();
+                final ResultSetMetaData resultSetMD = result.getMetaData();
+                while (result.next()) {
+                    final int numColumns = resultSetMD.getColumnCount();
+                    final ObjectNode node = OBJECT_MAPPER.createObjectNode();
+                    for (int i = 1; i <= numColumns; i++) {
+                        final String column_name = resultSetMD.getColumnName(i);
+                        node.put(column_name, result.getString(column_name));
+                    }
+
+                    /* Publish datapoint with a single line if split is required */
+                    if (definition.getSpiltLinesInIndividualMessages()) {
+                        log.debug("Splitting lines in multiple messages");
+                        output.addDataPoint("queryResult", node);
+                    } else {
+                        resultObject.add(node);
+                    }
                 }
 
-                /* Publish datapoint with a single line if split is required */
-                if (definition.getSpiltLinesInIndividualMessages()) {
-                    log.debug("Splitting lines in multiple messages");
-                    pollingOutput.addDataPoint("queryResult", node);
-                } else {
-                    resultObject.add(node);
+                /* Publish datapoint with all lines if no split is required */
+                if (!definition.getSpiltLinesInIndividualMessages()) {
+                    log.debug("Publishing all lines in a single message");
+                    output.addDataPoint("queryResult", resultObject);
                 }
-            }
-
-            /* Publish datapoint with all lines if no split is required */
-            if (!definition.getSpiltLinesInIndividualMessages()) {
-                log.debug("Publishing all lines in a single message");
-                pollingOutput.addDataPoint("queryResult", resultObject);
+            } catch (final Exception e) {
+                output.fail(e, null);
             }
         } catch (final Exception e) {
-            pollingOutput.fail(e, null);
+            output.fail(e, null);
         }
     }
 
