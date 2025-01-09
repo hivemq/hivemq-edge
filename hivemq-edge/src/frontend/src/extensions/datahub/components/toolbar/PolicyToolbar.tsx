8e842cc6d391649fbaca39e6db74cb8fb752d9ca
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-01-09 18:58:53 +0000

    refactor(27085): restructure the publish toolbar

--- hivemq-edge/src/frontend/src/extensions/datahub/components/toolbar/PolicyToolbar.tsx
+++ hivemq-edge/src/frontend/src/extensions/datahub/components/toolbar/PolicyToolbar.tsx
@@ -1,38 +1,24 @@
 import { FC } from 'react'
-import { useLocation, useNavigate } from 'react-router-dom'
-import { Icon } from '@chakra-ui/react'
-import { RiPassportLine } from 'react-icons/ri'
-import { useTranslation } from 'react-i18next'
 
 import ToolbarButtonGroup from '@/components/react-flow/ToolbarButtonGroup.tsx'
-import IconButton from '@/components/Chakra/IconButton.tsx'
 
 import { ToolbarDryRun } from '@datahub/components/toolbar/ToolbarDryRun.tsx'
 import { ToolbarPublish } from '@datahub/components/toolbar/ToolbarPublish.tsx'
 import { usePolicyChecksStore } from '@datahub/hooks/usePolicyChecksStore.ts'
+import { ToolbarClear } from '@datahub/components/toolbar/ToolbarClear.tsx'
+import { ToolbarShowReport } from '@datahub/components/toolbar/ToolbarShowReport.tsx'
 
-interface PolicyToolbarProps {
-  id?: string
-}
-
-const PolicyToolbar: FC<PolicyToolbarProps> = () => {
-  const { t } = useTranslation('datahub')
+const PolicyToolbar: FC = () => {
   const { report } = usePolicyChecksStore()
-  const navigate = useNavigate()
-  const { pathname } = useLocation()
 
   return (
     <ToolbarButtonGroup orientation="horizontal" variant="outline" gap="0.5em">
       <ToolbarDryRun />
-      <ToolbarButtonGroup orientation="horizontal" variant="outline">
-        <IconButton
-          icon={<Icon as={RiPassportLine} boxSize="24px" />}
-          data-testid="node-toolbar-delete"
-          aria-label={t('Show Validity Report')}
-          isDisabled={!report}
-          onClick={() => navigate('validation/', { state: { origin: pathname } })}
-        />
+      <ToolbarButtonGroup isAttached variant="outline">
+        <ToolbarShowReport />
+        {report && <ToolbarClear />}
       </ToolbarButtonGroup>
+
       <ToolbarPublish />
     </ToolbarButtonGroup>
   )
--- hivemq-edge/src/frontend/src/extensions/datahub/components/toolbar/ToolbarDryRun.tsx
+++ hivemq-edge/src/frontend/src/extensions/datahub/components/toolbar/ToolbarDryRun.tsx
@@ -1,23 +1,19 @@
 import { FC, useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Button, Icon } from '@chakra-ui/react'
-import { CloseIcon } from '@chakra-ui/icons'
-
-import IconButton from '@/components/Chakra/IconButton.tsx'
-import ToolbarButtonGroup from '@/components/react-flow/ToolbarButtonGroup.tsx'
 
 import { usePolicyDryRun } from '@datahub/hooks/usePolicyDryRun.ts'
 import useDataHubDraftStore from '@datahub/hooks/useDataHubDraftStore.ts'
 import { usePolicyChecksStore } from '@datahub/hooks/usePolicyChecksStore.ts'
 import { getDryRunStatusIcon } from '@datahub/utils/node.utils.ts'
-import { DataHubNodeData, DesignerStatus, PolicyDryRunStatus } from '@datahub/types.ts'
+import { DesignerStatus, PolicyDryRunStatus } from '@datahub/types.ts'
 
 export const ToolbarDryRun: FC = () => {
   const { t } = useTranslation('datahub')
   const { checkPolicyAsync } = usePolicyDryRun()
 
-  const { nodes, onUpdateNodes, status: statusDraft } = useDataHubDraftStore()
-  const { status, node: selectedNode, report, initReport, setReport, setNode, reset } = usePolicyChecksStore()
+  const { status: statusDraft } = useDataHubDraftStore()
+  const { status, node: selectedNode, initReport, setReport } = usePolicyChecksStore()
 
   const CheckIcon = useMemo(() => getDryRunStatusIcon(status), [status])
   const isEditEnabled =
@@ -32,40 +28,17 @@ export const ToolbarDryRun: FC = () => {
     })
   }
 
-  const handleClearPolicy = () => {
-    reset()
-    setNode(selectedNode)
-    nodes.forEach((node) => {
-      onUpdateNodes<DataHubNodeData>(node.id, {
-        ...node.data,
-        dryRunStatus: PolicyDryRunStatus.IDLE,
-      })
-    })
-  }
-
-  console.log('XXXXXX status', status)
-
   return (
-    <ToolbarButtonGroup variant="outline">
-      <Button
-        data-testid="toolbox-policy-check"
-        leftIcon={<Icon as={CheckIcon} boxSize="24px" />}
-        isLoading={status === PolicyDryRunStatus.RUNNING}
-        loadingText={t('workspace.dryRun.toolbar.checking')}
-        onClick={handleCheckPolicy}
-        isDisabled={!selectedNode || !isEditEnabled}
-        data-status={status}
-      >
-        {t('workspace.toolbar.policy.check')}
-      </Button>
-      {report && (
-        <IconButton
-          icon={<Icon as={CloseIcon} boxSize="12px" />}
-          data-testid="toolbox-policy-clear"
-          aria-label={t('Clear Validity Report')}
-          onClick={handleClearPolicy}
-        />
-      )}
-    </ToolbarButtonGroup>
+    <Button
+      data-testid="toolbox-policy-check"
+      leftIcon={<Icon as={CheckIcon} boxSize="24px" />}
+      isLoading={status === PolicyDryRunStatus.RUNNING}
+      loadingText={t('workspace.dryRun.toolbar.checking')}
+      onClick={handleCheckPolicy}
+      isDisabled={!selectedNode || !isEditEnabled}
+      data-status={status}
+    >
+      {t('workspace.toolbar.policy.check')}
+    </Button>
   )
 }
