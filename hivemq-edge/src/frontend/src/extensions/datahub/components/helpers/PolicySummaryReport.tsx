f28a85dbd8d7899dba277a4fa393eb49cd158ad4
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-01-09 13:48:20 +0000

    fix(27085): fix the summary report

--- hivemq-edge/src/frontend/src/extensions/datahub/components/helpers/PolicySummaryReport.tsx
+++ hivemq-edge/src/frontend/src/extensions/datahub/components/helpers/PolicySummaryReport.tsx
@@ -1,18 +1,18 @@
 import { FC } from 'react'
-import { Alert, AlertDescription, AlertIcon, AlertStatus, AlertTitle, Box, Link, Text } from '@chakra-ui/react'
+import { Alert, AlertDescription, AlertIcon, AlertStatus, AlertTitle, Box, Text } from '@chakra-ui/react'
 import { useTranslation } from 'react-i18next'
-import { ConditionalWrapper } from '@/components/ConditonalWrapper.tsx'
 import { PolicyDryRunStatus } from '@datahub/types.ts'
 
 interface PolicySummaryReportProps {
-  status: PolicyDryRunStatus
+  status: PolicyDryRunStatus | undefined
   onOpenPublish?: () => void
   onClearPolicy?: () => void
 }
 
 const PolicySummaryReport: FC<PolicySummaryReportProps> = ({ status }) => {
   const { t } = useTranslation('datahub')
-  const alertStatus: AlertStatus = status === PolicyDryRunStatus.SUCCESS ? 'success' : 'warning'
+  const alertStatus: AlertStatus =
+    status === PolicyDryRunStatus.SUCCESS ? 'success' : status === PolicyDryRunStatus.FAILURE ? 'warning' : 'error'
 
   return (
     <Alert status={alertStatus} data-testid="toolbox-policy-check-status">
@@ -22,12 +22,7 @@ const PolicySummaryReport: FC<PolicySummaryReportProps> = ({ status }) => {
           <Text as="span">{t('workspace.dryRun.report.success.title', { context: alertStatus })}</Text>
         </AlertTitle>
         <AlertDescription>
-          <ConditionalWrapper
-            condition={status === PolicyDryRunStatus.SUCCESS}
-            wrapper={(children) => <Link aria-label={t('workspace.toolbox.navigation.goPublish')}>{children}</Link>}
-          >
-            <Text as="span">{t('workspace.dryRun.report.success.description', { context: alertStatus })}</Text>
-          </ConditionalWrapper>
+          <Text as="span">{t('workspace.dryRun.report.success.description', { context: alertStatus })}</Text>
         </AlertDescription>
       </Box>
     </Alert>
