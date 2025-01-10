fc531567d79fd7b8017a1ee3676240ff60df15a2
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-01-10 08:49:03 +0000

    test(27085): fix tests

---
+++ hivemq-edge/src/frontend/src/extensions/datahub/components/toolbar/ToolbarShowReport.spec.cy.tsx
@@ -0,0 +1,32 @@
+import { getPolicyPublishWrapper, MOCK_NODE_DATA_POLICY } from '@datahub/__test-utils__/react-flow.mocks.tsx'
+import { ToolbarShowReport } from '@datahub/components/toolbar/ToolbarShowReport.tsx'
+
+describe('ToolbarShowReport', () => {
+  beforeEach(() => {
+    cy.viewport(800, 250)
+  })
+
+  it('should render without report', () => {
+    cy.mountWithProviders(<ToolbarShowReport />, { wrapper: getPolicyPublishWrapper() })
+    cy.get('button').should('have.attr', 'aria-label', 'Show Validity Report').should('be.disabled')
+  })
+
+  it('should render with report', () => {
+    cy.mountWithProviders(<ToolbarShowReport />, {
+      wrapper: getPolicyPublishWrapper([{ node: MOCK_NODE_DATA_POLICY }]),
+    })
+    cy.get('button').should('have.attr', 'aria-label', 'Show Validity Report').should('not.be.disabled')
+    cy.getByTestId('test-dashboard').should('have.text', '/')
+    cy.get('button').click()
+    cy.getByTestId('test-dashboard').should('have.text', '/validation/')
+  })
+
+  it('should be accessible', () => {
+    cy.injectAxe()
+    cy.mountWithProviders(<ToolbarShowReport />, {
+      wrapper: getPolicyPublishWrapper([{ node: MOCK_NODE_DATA_POLICY }]),
+    })
+
+    cy.checkAccessibility()
+  })
+})
