badd4ab4eab9778fdbad155fa04559efced0d5e0
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-01-10 08:50:01 +0000

    test(27085): fix tests

--- hivemq-edge/src/frontend/src/extensions/datahub/components/toolbar/ToolbarDryRun.spec.cy.tsx
+++ hivemq-edge/src/frontend/src/extensions/datahub/components/toolbar/ToolbarDryRun.spec.cy.tsx
@@ -1,38 +1,14 @@
-/// <reference types="cypress" />
-
-import { Node } from 'reactflow'
-
-import { MOCK_DEFAULT_NODE } from '@/__test-utils__/react-flow/nodes.ts'
-
-import { MockChecksStoreWrapper } from '@datahub/__test-utils__/MockStoreWrapper.tsx'
+import { getPolicyPublishWrapper } from '@datahub/__test-utils__/react-flow.mocks.tsx'
 import { ToolbarDryRun } from '@datahub/components/toolbar/ToolbarDryRun.tsx'
-import { DataHubNodeType, DataPolicyData, PolicyDryRunStatus } from '@datahub/types.ts'
-
-const MOCK_NODE_DATA_POLICY: Node<DataPolicyData> = {
-  id: 'node-id',
-  type: DataHubNodeType.DATA_POLICY,
-  data: { id: 'my-policy-id' },
-  ...MOCK_DEFAULT_NODE,
-  position: { x: 0, y: 0 },
-}
-
-const wrapper: React.JSXElementConstructor<{ children: React.ReactNode }> = ({ children }) => (
-  <MockChecksStoreWrapper
-    config={{
-      node: MOCK_NODE_DATA_POLICY,
-    }}
-  >
-    {children}
-  </MockChecksStoreWrapper>
-)
+import { PolicyDryRunStatus } from '@datahub/types.ts'
 
 describe('ToolbarDryRun', () => {
   beforeEach(() => {
     cy.viewport(800, 600)
   })
 
-  it('should renders properly', () => {
-    cy.mountWithProviders(<ToolbarDryRun />, { wrapper })
+  it('should render properly', () => {
+    cy.mountWithProviders(<ToolbarDryRun />, { wrapper: getPolicyPublishWrapper() })
 
     cy.getByTestId('toolbox-policy-check')
       .should('have.text', 'Check')
@@ -46,16 +22,13 @@ describe('ToolbarDryRun', () => {
     cy.getByTestId('toolbox-policy-check')
       .should('have.text', 'Check')
       .should('have.attr', 'data-status', PolicyDryRunStatus.FAILURE)
-
-    cy.getByTestId('toolbox-policy-clear').click()
   })
 
   it('should be accessible', () => {
     cy.injectAxe()
-    cy.mountWithProviders(<ToolbarDryRun />, { wrapper })
+    cy.mountWithProviders(<ToolbarDryRun />, { wrapper: getPolicyPublishWrapper() })
     cy.getByTestId('toolbox-policy-check').click()
 
     cy.checkAccessibility()
-    cy.percySnapshot('Component: ToolbarDryRun')
   })
 })
