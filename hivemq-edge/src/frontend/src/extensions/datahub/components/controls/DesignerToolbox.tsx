30da3bbb56ba903b4015f8b479887f2e33243e2d
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-01-11 14:42:55 +0000

    refactor(27113): refactor the toolbox to utilise UI overlays

--- hivemq-edge/src/frontend/src/extensions/datahub/components/controls/DesignerToolbox.tsx
+++ hivemq-edge/src/frontend/src/extensions/datahub/components/controls/DesignerToolbox.tsx
@@ -1,9 +1,21 @@
-import { FC, useState } from 'react'
+import type { FC } from 'react'
 import { useTranslation } from 'react-i18next'
-import { motion } from 'framer-motion'
-import { Box, HStack, Icon, IconButton, useDisclosure } from '@chakra-ui/react'
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverBody,
+  PopoverArrow,
+  PopoverCloseButton,
+  Text,
+  VStack,
+  IconButton,
+  Icon,
+  HStack,
+} from '@chakra-ui/react'
+import { ChevronDownIcon, ChevronRightIcon } from '@chakra-ui/icons'
 import { FaTools } from 'react-icons/fa'
-import { LuPanelLeftOpen, LuPanelRightOpen } from 'react-icons/lu'
 
 import Panel from '@/components/react-flow/Panel.tsx'
 import { ToolboxNodes } from '@datahub/components/controls/ToolboxNodes.tsx'
@@ -11,41 +23,38 @@ import DraftStatus from '@datahub/components/helpers/DraftStatus.tsx'
 
 const DesignerToolbox: FC = () => {
   const { t } = useTranslation('datahub')
-  const { getButtonProps, getDisclosureProps, isOpen } = useDisclosure()
-  const [hidden, setHidden] = useState(!isOpen)
 
   return (
     <Panel position="top-left">
-      <HStack alignItems="center" userSelect="none">
-        <Box>
-          <IconButton
-            data-testid="toolbox-trigger"
-            aria-label={t('workspace.toolbox.trigger', { context: !isOpen ? 'open' : 'close' })}
-            icon={
-              <>
-                <Icon as={FaTools} />
-                <Icon as={isOpen ? LuPanelRightOpen : LuPanelLeftOpen} ml={2} boxSize="24px" />
-              </>
-            }
-            {...getButtonProps()}
-            px={2}
-          />
-        </Box>
-        <motion.div
-          {...getDisclosureProps()}
-          data-testid="toolbox-container"
-          hidden={hidden}
-          initial={false}
-          onAnimationStart={() => setHidden(false)}
-          onAnimationComplete={() => setHidden(!isOpen)}
-          animate={{ width: isOpen ? '100%' : 0 }}
-          style={{
-            overflow: 'hidden',
-            whiteSpace: 'nowrap',
-          }}
-        >
-          <ToolboxNodes />
-        </motion.div>
+      <HStack>
+        <Popover>
+          {({ isOpen }) => (
+            <>
+              <PopoverTrigger>
+                <IconButton
+                  data-testid="toolbox-trigger"
+                  aria-label={t('workspace.toolbox.trigger', { context: !isOpen ? 'open' : 'close' })}
+                  icon={
+                    <>
+                      <Icon as={FaTools} />
+                      <Icon as={isOpen ? ChevronDownIcon : ChevronRightIcon} ml={2} boxSize="24px" />
+                    </>
+                  }
+                  px={2}
+                />
+              </PopoverTrigger>
+              <PopoverContent width="unset">
+                <PopoverArrow />
+                <PopoverCloseButton />
+                <PopoverHeader>Policy Toolbox</PopoverHeader>
+                <PopoverBody as={VStack} alignItems="flex-start" maxWidth="12rem">
+                  <Text fontSize="sm">Drag elements on the canvas then connect them</Text>
+                  <ToolboxNodes direction="vertical" />
+                </PopoverBody>
+              </PopoverContent>
+            </>
+          )}
+        </Popover>
         <DraftStatus />
       </HStack>
     </Panel>
