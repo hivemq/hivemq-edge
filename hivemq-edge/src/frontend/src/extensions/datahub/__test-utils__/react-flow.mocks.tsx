7e0f5aac5fc3de6122c0094d330036b500701d6b
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-01-16 18:05:42 +0000

    test(29444): add mocks

--- hivemq-edge/src/frontend/src/extensions/datahub/__test-utils__/react-flow.mocks.tsx
+++ hivemq-edge/src/frontend/src/extensions/datahub/__test-utils__/react-flow.mocks.tsx
@@ -10,6 +10,7 @@ import {
   ClientFilterData,
   DataHubNodeType,
   DataPolicyData,
+  DesignerStatus,
   DryRunResults,
   OperationData,
   SchemaData,
@@ -22,7 +23,7 @@ import {
 } from '@/extensions/datahub/types.ts'
 import { styleDefaultEdge } from '@/extensions/datahub/utils/edge.utils.ts'
 import { MOCK_DEFAULT_NODE } from '@/__test-utils__/react-flow/nodes.ts'
-import { MockChecksStoreWrapper } from '@datahub/__test-utils__/MockStoreWrapper.tsx'
+import { MockChecksStoreWrapper, MockStoreWrapper } from '@datahub/__test-utils__/MockStoreWrapper.tsx'
 
 export const MOCK_INITIAL_POLICY = () => {
   const baseNode: Node<{ label: string }> = {
@@ -195,6 +196,21 @@ export const getPolicyPublishWrapper = (report?: DryRunResults<unknown, never>[]
           <CardBody data-testid="test-dashboard">{pathname}</CardBody>
         </Card>
       </MockChecksStoreWrapper>
+    )
+  }
+  return Wrapper
+}
+
+export const getPolicyWrapper = ({ status, nodes }: { status?: DesignerStatus; nodes?: Node[] }) => {
+  const Wrapper: FC<PropsWithChildren> = ({ children }) => {
+    return (
+      <MockStoreWrapper
+        config={{
+          initialState: { status: status, nodes: nodes || [] },
+        }}
+      >
+        {children}
+      </MockStoreWrapper>
     )
   }
   return Wrapper
