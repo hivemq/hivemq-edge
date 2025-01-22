587875efa260cba8144cb7cd0d957eb1a63cb3c9
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-01-22 21:08:28 +0000

    fix(29589): add position shit to compensate for source/target handle

--- hivemq-edge/src/frontend/src/extensions/datahub/designer/checks.utils.ts
+++ hivemq-edge/src/frontend/src/extensions/datahub/designer/checks.utils.ts
@@ -1,4 +1,10 @@
 import type { XYPosition } from 'reactflow'
+import { CANVAS_GRID } from '@datahub/utils/theme.utils.ts'
+
+export const CANVAS_DROP_DELTA: XYPosition = {
+  x: -CANVAS_GRID * 8,
+  y: -CANVAS_GRID * 2,
+}
 
 // The delta positions used to locate nodes on the canvas at loading time, as position is not serialised
 export const CANVAS_POSITION = {
