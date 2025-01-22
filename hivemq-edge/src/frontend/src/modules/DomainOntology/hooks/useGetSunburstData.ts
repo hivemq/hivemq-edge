bb78cf76a349da9a3529e1eea011405610dc0e98
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-01-22 10:15:04 +0000

    fix(29561): fix empty state

--- hivemq-edge/src/frontend/src/modules/DomainOntology/hooks/useGetSunburstData.ts
+++ hivemq-edge/src/frontend/src/modules/DomainOntology/hooks/useGetSunburstData.ts
@@ -1,13 +1,22 @@
 import { useMemo } from 'react'
+import { useTranslation } from 'react-i18next'
 
 import { stratifyTopicTree, toTreeMetadata } from '@/modules/Workspace/utils/topics-utils.ts'
 import { useGetDomainOntology } from '@/modules/DomainOntology/hooks/useGetDomainOntology.ts'
 
 export const useGetSunburstData = () => {
+  const { t } = useTranslation()
   const { topicFilters, tags, northMappings, bridgeSubscriptions, isLoading, isError } = useGetDomainOntology()
 
   const sunburstData = useMemo(() => {
-    if (isLoading) return stratifyTopicTree([{ label: 'root', count: 1 }])
+    // TODO[NVL] id and label needs to be added to datum when stratifying
+    // The Sunburst chart doesn't allow a single sector
+    const emptyStateData = stratifyTopicTree([
+      { label: t('ontology.error.noDataLoaded'), count: 1 },
+      { label: t('ontology.error.noDataLoaded'), count: 1 },
+    ])
+
+    if (isLoading) return emptyStateData
 
     const allTopicFilters = topicFilters.data?.items?.map<string>((filter) => filter.topicFilter) || []
     const allTags = tags.data?.items?.map<string>((filter) => filter.name) || []
@@ -20,12 +29,16 @@ export const useGetSunburstData = () => {
       ...bridgeSubscriptions.topics,
       ...bridgeSubscriptions.topicFilters,
     ])
+
+    if (!edgeTopics.length) return emptyStateData
+
     return stratifyTopicTree(edgeTopics)
   }, [
     bridgeSubscriptions.topicFilters,
     bridgeSubscriptions.topics,
     isLoading,
     northMappings.data?.items,
+    t,
     tags.data?.items,
     topicFilters.data?.items,
   ])
