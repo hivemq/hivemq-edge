5fcaea4e6dbbb8cd19eaf4ec434877e52cdb8e05
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-01-23 15:02:51 +0000

    fix(29589): fix initial position of loaded nodes

--- hivemq-edge/src/frontend/src/extensions/datahub/designer/checks.utils.ts
+++ hivemq-edge/src/frontend/src/extensions/datahub/designer/checks.utils.ts
@@ -8,7 +8,7 @@ export const CANVAS_DROP_DELTA: XYPosition = {
 
 // The delta positions used to locate nodes on the canvas at loading time, as position is not serialised
 export const CANVAS_POSITION = {
-  Client: { x: -300, y: 0 } as XYPosition,
+  Client: { x: -10 * CANVAS_GRID, y: 0 } as XYPosition,
   Topic: { x: -10 * CANVAS_GRID, y: 0 } as XYPosition,
   Function: { x: -10 * CANVAS_GRID, y: CANVAS_GRID * 10 } as XYPosition,
   Transition: { x: 10 * CANVAS_GRID, y: 4 * CANVAS_GRID } as XYPosition,
