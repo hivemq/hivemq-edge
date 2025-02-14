6806db1fd347f4cc8bf85ae01971e2b476167699
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-02-14 10:45:40 +0000

    feat(30341): add mocks and MSW handlers

---
+++ hivemq-edge/src/frontend/src/api/hooks/useCombiners/__handlers__/index.ts
@@ -0,0 +1,41 @@
+import { http, HttpResponse } from 'msw'
+import type { Combiner, CombinerList } from '../../../__generated__'
+
+interface CombinerParams {
+  combinerId: string
+}
+
+const mockCombinerId = '6991ff43-9105-445f-bce3-976720df40a3'
+
+export const mockCombiner: Combiner = {
+  id: mockCombinerId,
+  name: 'my-combiner',
+}
+
+export const handlers = [
+  http.get('*/management/combiners', () => {
+    return HttpResponse.json<CombinerList>({ items: [mockCombiner] }, { status: 200 })
+  }),
+
+  http.get<CombinerParams>('*/management/combiners/:combinerId', ({ params }) => {
+    const { combinerId } = params
+    return HttpResponse.json<Combiner>({ ...mockCombiner, id: combinerId }, { status: 200 })
+  }),
+
+  http.post<never, Combiner>('*/management/combiners', ({ request }) => {
+    const { body } = request
+    return HttpResponse.json({ created: body }, { status: 200 })
+  }),
+
+  http.delete<CombinerParams>('*/management/combiners/:combinerId', ({ params }) => {
+    const { combinerId } = params
+    return HttpResponse.json({ deleted: combinerId }, { status: 200 })
+  }),
+
+  http.put<CombinerParams, Combiner>('*/management/combiners/:combinerId', async ({ params, request }) => {
+    const { combinerId } = params
+    const data = await request.json()
+
+    return HttpResponse.json({ updated: data, id: combinerId }, { status: 200 })
+  }),
+]
