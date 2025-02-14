9d29f8ef71669f0ee58c73ac4daefd9c1c826d7e
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-02-14 10:43:52 +0000

    feat(30341): add react query hooks for the combiner API endpoints

---
+++ hivemq-edge/src/frontend/src/api/hooks/useCombiners/useCreateCombiner.ts
@@ -0,0 +1,25 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+
+import type { ApiError, Combiner } from '@/api/__generated__'
+import { useHttpClient } from '@/api/hooks/useHttpClient/useHttpClient.ts'
+import { QUERY_KEYS } from '@/api/utils.ts'
+
+interface CreateCombinerProps {
+  requestBody: Combiner
+}
+
+export const useCreateCombiner = () => {
+  const appClient = useHttpClient()
+  const queryClient = useQueryClient()
+
+  const createCombiner = ({ requestBody }: CreateCombinerProps) => {
+    return appClient.combiners.addCombiner(requestBody)
+  }
+
+  return useMutation<unknown, ApiError, CreateCombinerProps>({
+    mutationFn: createCombiner,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.COMBINING_MAPPINGS] })
+    },
+  })
+}
---
+++ hivemq-edge/src/frontend/src/api/hooks/useCombiners/useDeleteCombiner.ts
@@ -0,0 +1,25 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+
+import type { ApiError } from '../../__generated__'
+import { useHttpClient } from '@/api/hooks/useHttpClient/useHttpClient.ts'
+import { QUERY_KEYS } from '@/api/utils.ts'
+
+interface DeleteCombinerProps {
+  combinerId: string
+}
+
+export const useDeleteCombiner = () => {
+  const appClient = useHttpClient()
+  const queryClient = useQueryClient()
+
+  const deleteCombiner = ({ combinerId }: DeleteCombinerProps) => {
+    return appClient.combiners.deleteCombiner(combinerId)
+  }
+
+  return useMutation<unknown, ApiError, DeleteCombinerProps>({
+    mutationFn: deleteCombiner,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.COMBINING_MAPPINGS] })
+    },
+  })
+}
