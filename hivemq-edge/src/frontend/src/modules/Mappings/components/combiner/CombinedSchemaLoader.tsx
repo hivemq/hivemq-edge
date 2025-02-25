bf6aba6015821dcfe329d416a9c66eec93a34c54
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-02-25 12:55:06 +0000

    refactor(30678): fix the layout and rendering of the schema

--- hivemq-edge/src/frontend/src/modules/Mappings/components/combiner/CombinedSchemaLoader.tsx
+++ hivemq-edge/src/frontend/src/modules/Mappings/components/combiner/CombinedSchemaLoader.tsx
@@ -1,5 +1,6 @@
 import { type FC, useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
+import { Box, Heading } from '@chakra-ui/react'
 
 import type { DataCombining, DomainTag, TopicFilter } from '@/api/__generated__'
 import type { DataReference } from '@/api/hooks/useDomainModel/useGetCombinedDataSchemas'
@@ -46,7 +47,7 @@ export const CombinedSchemaLoader: FC<CombinedSchemaLoaderProps> = ({ formData, 
     const tags = formData?.sources?.tags || []
     const topicFilters = formData?.sources?.topicFilters || []
     const indexes = [...tags, ...topicFilters]
-    return allDataReferences?.filter((e) => indexes.includes(e.id)) || []
+    return allDataReferences?.filter((dataReference) => indexes.includes(dataReference.id)) || []
   }, [allDataReferences, formData?.sources?.tags, formData?.sources?.topicFilters])
 
   const queries = useGetCombinedDataSchemas(allSchemas)
@@ -54,8 +55,16 @@ export const CombinedSchemaLoader: FC<CombinedSchemaLoaderProps> = ({ formData, 
   const test = useMemo(() => {
     return allSchemas.map((dataReference, index) => {
       const { data } = queries[index]
+
+      // TODO[30744] Type of schema inconsistent between tag and topic filter
       if (typeof data === 'string') {
         dataReference.schema = validateSchemaFromDataURI(data)
+      } else if (typeof data === 'object') {
+        dataReference.schema = {
+          schema: data,
+          status: 'success',
+          message: t('topicFilter.schema.status.success'),
+        }
       } else {
         dataReference.schema = {
           status: 'warning',
@@ -64,16 +73,34 @@ export const CombinedSchemaLoader: FC<CombinedSchemaLoaderProps> = ({ formData, 
       }
       return dataReference
     })
-  }, [allSchemas, queries])
+  }, [allSchemas, queries, t])
 
   if (!test.length) return <ErrorMessage message={t('combiner.error.noSchemaLoadedYet')} status={'info'} />
 
   return (
     <>
-      {test.map((e) => {
-        const schema =
-          e.schema?.status === 'success' && e.schema.schema ? { ...e.schema.schema, title: e.id } : { title: e.id }
-        return <JsonSchemaBrowser key={e.id} schema={schema} isDraggable hasExamples />
+      {test.map((dataReference) => {
+        const hasSchema = dataReference.schema?.status === 'success' && dataReference.schema.schema
+
+        if (!hasSchema) {
+          // TODO[NVL] Duplication; integrate error message into the schema browser
+          return (
+            <Box key={dataReference.id}>
+              <Heading as="h4" size="sm">
+                {dataReference.id}
+              </Heading>
+              <ErrorMessage message={dataReference.schema?.message} status={dataReference.schema?.status} />
+            </Box>
+          )
+        }
+        return (
+          <JsonSchemaBrowser
+            key={dataReference.id}
+            schema={{ ...dataReference.schema?.schema, title: dataReference.id }}
+            isDraggable
+            hasExamples
+          />
+        )
       })}
     </>
   )
