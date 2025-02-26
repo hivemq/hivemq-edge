b118953f3a5786ff6be8cce31545c50d90645660
Sam Cao <sjtucaocao@gmail.com>
2025-02-26 15:26:06 +0100

    feat: Create scaffold

---
+++ modules/hivemq-edge-module-mtconnect/src/main/java/com/hivemq/edge/adapters/mtconnect/config/MtConnectAdapterConfig.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2023-present HiveMQ GmbH
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package com.hivemq.edge.adapters.mtconnect.config;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.hivemq.adapter.sdk.api.annotations.ModuleConfigField;
+import com.hivemq.adapter.sdk.api.config.ProtocolSpecificAdapterConfig;
+
+@SuppressWarnings({"unused", "FieldCanBeLocal", "FieldMayBeFinal"})
+public class MtConnectAdapterConfig implements ProtocolSpecificAdapterConfig {
+    @JsonProperty("pollingIntervalMillis")
+    @ModuleConfigField(title = "Polling Interval [ms]",
+                       description = "Time in millisecond that this endpoint will be polled",
+                       numberMin = 1,
+                       required = true,
+                       defaultValue = "1000")
+    private int pollingIntervalMillis = 1000;
+    @JsonProperty("maxPollingErrorsBeforeRemoval")
+    @ModuleConfigField(title = "Max. Polling Errors",
+                       description = "Max. errors polling the endpoint before the polling daemon is stopped",
+                       numberMin = 3,
+                       defaultValue = "10")
+    private int maxPollingErrorsBeforeRemoval = 10;
+
+    public MtConnectAdapterConfig() {
+    }
+
+    public int getPollingIntervalMillis() {
+        return pollingIntervalMillis;
+    }
+
+    public int getMaxPollingErrorsBeforeRemoval() {
+        return maxPollingErrorsBeforeRemoval;
+    }
+}
