9efa601f496f484829d3706a8aa6bac5d200cec4
Sam Cao <sjtucaocao@gmail.com>
2025-02-28 17:19:20 +0100

    feat: Add http client to get data from url and add test cases

--- modules/hivemq-edge-module-mtconnect/src/main/java/com/hivemq/edge/adapters/mtconnect/config/tag/MtConnectAdapterTagDefinition.java
+++ modules/hivemq-edge-module-mtconnect/src/main/java/com/hivemq/edge/adapters/mtconnect/config/tag/MtConnectAdapterTagDefinition.java
@@ -25,8 +25,13 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 public class MtConnectAdapterTagDefinition implements TagDefinition {
+    private static final int DEFAULT_HTTP_CONNECT_TIMEOUT_SECONDS = 5;
+    private static final int MIN_HTTP_CONNECT_TIMEOUT_SECONDS = 1;
+    private static final int MAX_HTTP_CONNECT_TIMEOUT_SECONDS = 60;
+
     @JsonProperty(value = "httpHeaders")
     @ModuleConfigField(title = "HTTP Headers", description = "HTTP headers to be added to your requests")
     private final @NotNull List<MtConnectAdapterHttpHeader> httpHeaders;
@@ -38,12 +43,38 @@ public class MtConnectAdapterTagDefinition implements TagDefinition {
                        required = true)
     private final @NotNull String url;
 
+    @JsonProperty("httpConnectTimeoutSeconds")
+    @ModuleConfigField(title = "HTTP Connection Timeout",
+                       description = "Timeout (in seconds) to allow the underlying HTTP connection to be established",
+                       defaultValue = DEFAULT_HTTP_CONNECT_TIMEOUT_SECONDS + "",
+                       numberMin = MIN_HTTP_CONNECT_TIMEOUT_SECONDS,
+                       numberMax = MAX_HTTP_CONNECT_TIMEOUT_SECONDS)
+    private final int httpConnectTimeoutSeconds;
+   
+    @JsonProperty(value = "schemaFile", required = true)
+    @ModuleConfigField(title = "Schema File",
+                       description = "The file that contains the MTConnect schema",
+                       format = ModuleConfigField.FieldType.UNSPECIFIED,
+                       required = true)
+    private final @NotNull String schemaFile;
+
     @JsonCreator
     public MtConnectAdapterTagDefinition(
             @JsonProperty(value = "url", required = true) final @NotNull String url,
+            @JsonProperty(value = "schemaFile", required = true) final @NotNull String schemaFile,
+            @JsonProperty(value = "httpConnectTimeoutSeconds") final @Nullable Integer httpConnectTimeoutSeconds,
             @JsonProperty(value = "httpHeaders") final @Nullable List<MtConnectAdapterHttpHeader> httpHeaders) {
         this.url = url;
+        this.schemaFile = schemaFile;
         this.httpHeaders = Objects.requireNonNullElseGet(httpHeaders, List::of);
+        this.httpConnectTimeoutSeconds = Optional.ofNullable(httpConnectTimeoutSeconds)
+                .map(s -> Math.min(s, MAX_HTTP_CONNECT_TIMEOUT_SECONDS))
+                .map(s -> Math.max(s, MIN_HTTP_CONNECT_TIMEOUT_SECONDS))
+                .orElse(DEFAULT_HTTP_CONNECT_TIMEOUT_SECONDS);
+    }
+
+    public @NotNull String getSchemaFile() {
+        return schemaFile;
     }
 
     public @NotNull List<MtConnectAdapterHttpHeader> getHttpHeaders() {
@@ -54,4 +85,7 @@ public class MtConnectAdapterTagDefinition implements TagDefinition {
         return url;
     }
 
+    public int getHttpConnectTimeoutSeconds() {
+        return httpConnectTimeoutSeconds;
+    }
 }
