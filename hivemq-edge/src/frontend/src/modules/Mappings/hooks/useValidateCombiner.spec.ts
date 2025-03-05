5d1523772405fffa6ee4ee9366ae3710e617d38b
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-03-05 14:29:53 +0000

    test(30977): add helpers for the hook tester

--- hivemq-edge/src/frontend/src/modules/Mappings/hooks/useValidateCombiner.spec.ts
+++ hivemq-edge/src/frontend/src/modules/Mappings/hooks/useValidateCombiner.spec.ts
@@ -1,24 +1,33 @@
 import { beforeEach, expect } from 'vitest'
 import { renderHook, waitFor } from '@testing-library/react'
+import { createErrorHandler, toErrorList } from '@rjsf/utils'
 
 import { server } from '@/__test-utils__/msw/mockServer.ts'
 import { SimpleWrapper as wrapper } from '@/__test-utils__/hooks/SimpleWrapper.tsx'
-import { mappingHandlers } from '@/api/hooks/useProtocolAdapters/__handlers__/mapping.mocks.ts'
+import type { Combiner } from '@/api/__generated__'
+import { handlers } from '@/api/hooks/useProtocolAdapters/__handlers__'
+import { mockEmptyCombiner } from '@/api/hooks/useCombiners/__handlers__'
+
 import { useValidateCombiner } from './useValidateCombiner'
 
 describe('useValidateCombiner', () => {
   beforeEach(() => {
-    server.use(...mappingHandlers)
+    server.use(...handlers)
   })
 
   afterEach(() => {
     server.resetHandlers()
   })
 
-  it('should do it', async () => {
+  const renderValidateHook = async (formData: Combiner | undefined) => {
+    const errors = createErrorHandler<Combiner>(mockEmptyCombiner)
+
     const { result } = renderHook(() => useValidateCombiner([], []), { wrapper })
     await waitFor(() => {
       expect(result.current).not.toBeUndefined()
     })
-  })
+
+    const formValidation = result.current(formData, errors)
+    return toErrorList(formValidation)
+  }
 })
