017fdbd65af888d38fd8811588c0594994d0c448
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-03-05 15:04:05 +0000

    test(30977): add tests

--- hivemq-edge/src/frontend/src/modules/Mappings/hooks/useValidateCombiner.spec.ts
+++ hivemq-edge/src/frontend/src/modules/Mappings/hooks/useValidateCombiner.spec.ts
@@ -5,8 +5,9 @@ import { createErrorHandler, toErrorList } from '@rjsf/utils'
 import { server } from '@/__test-utils__/msw/mockServer.ts'
 import { SimpleWrapper as wrapper } from '@/__test-utils__/hooks/SimpleWrapper.tsx'
 import type { Combiner } from '@/api/__generated__'
+import { EntityType } from '@/api/__generated__'
 import { handlers } from '@/api/hooks/useProtocolAdapters/__handlers__'
-import { mockEmptyCombiner } from '@/api/hooks/useCombiners/__handlers__'
+import { mockCombinerId, mockEmptyCombiner } from '@/api/hooks/useCombiners/__handlers__'
 
 import { useValidateCombiner } from './useValidateCombiner'
 
@@ -20,7 +21,7 @@ describe('useValidateCombiner', () => {
   })
 
   const renderValidateHook = async (formData: Combiner | undefined) => {
-    const errors = createErrorHandler<Combiner>(mockEmptyCombiner)
+    const errors = createErrorHandler<Combiner>(formData || mockEmptyCombiner)
 
     const { result } = renderHook(() => useValidateCombiner([], []), { wrapper })
     await waitFor(() => {
@@ -30,4 +31,39 @@ describe('useValidateCombiner', () => {
     const formValidation = result.current(formData, errors)
     return toErrorList(formValidation)
   }
+
+  it('should validate an empty payload', async () => {
+    const errors = await renderValidateHook(undefined)
+    expect(errors).toStrictEqual([
+      expect.objectContaining({
+        message: 'The combiner payload must be defined',
+      }),
+    ])
+  })
+
+  it('should validate capability', async () => {
+    const errors = await renderValidateHook({
+      id: mockCombinerId,
+      name: 'my-combiner',
+      sources: {
+        items: [
+          {
+            id: 'dd',
+            type: EntityType.ADAPTER,
+          },
+        ],
+      },
+      mappings: {
+        items: [],
+      },
+    })
+    expect(errors).toStrictEqual([
+      expect.objectContaining({
+        message: "The Edge broker must be connected to the combiner's sources",
+      }),
+      expect.objectContaining({
+        message: 'This is not a valid reference to a Workspace entity',
+      }),
+    ])
+  })
 })
