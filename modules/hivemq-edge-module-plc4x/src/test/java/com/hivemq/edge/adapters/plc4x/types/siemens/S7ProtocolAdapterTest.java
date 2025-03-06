7d09245879d67a7069a2a01c42b9dd91ef46a6dc
Jochen Mader <jochen.mader@hivemq.com>
2025-03-06 18:26:42 +0100

    Fix tests

--- modules/hivemq-edge-module-plc4x/src/test/java/com/hivemq/edge/adapters/plc4x/types/siemens/S7ProtocolAdapterTest.java
+++ modules/hivemq-edge-module-plc4x/src/test/java/com/hivemq/edge/adapters/plc4x/types/siemens/S7ProtocolAdapterTest.java
@@ -16,12 +16,16 @@
 package com.hivemq.edge.adapters.plc4x.types.siemens;
 
 import com.hivemq.adapter.sdk.api.config.MessageHandlingOptions;
+import com.hivemq.adapter.sdk.api.data.DataPoint;
+import com.hivemq.adapter.sdk.api.factories.AdapterFactories;
+import com.hivemq.adapter.sdk.api.factories.DataPointFactory;
 import com.hivemq.adapter.sdk.api.model.ProtocolAdapterInput;
 import com.hivemq.adapter.sdk.api.services.ModuleServices;
 import com.hivemq.edge.adapters.plc4x.config.Plc4xDataType;
 import com.hivemq.edge.adapters.plc4x.config.Plc4xToMqttMapping;
 import com.hivemq.edge.adapters.plc4x.config.tag.Plc4xTag;
 import com.hivemq.edge.adapters.plc4x.config.tag.Plc4xTagDefinition;
+import com.hivemq.edge.modules.adapters.data.DataPointImpl;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -40,7 +44,23 @@ public class S7ProtocolAdapterTest {
 
     @BeforeEach
     void setUp() {
+
+        var adapterFactories = mock(AdapterFactories.class);
+        when(adapterFactories.dataPointFactory()).thenReturn(new DataPointFactory() {
+            @Override
+            public @NotNull DataPoint create(final @NotNull String tagName, final @NotNull Object tagValue) {
+                return new DataPointImpl(tagName, tagValue);
+            }
+
+            @Override
+            public @NotNull DataPoint createJsonDataPoint(
+                    final @NotNull String tagName,
+                    final @NotNull Object tagValue) {
+                return new DataPointImpl(tagName, tagValue);
+            }
+        });
         when(protocolAdapterInput.moduleServices()).thenReturn(moduleServices);
+        when(protocolAdapterInput.adapterFactories()).thenReturn(adapterFactories);
         adapter = new TestS7ProtocolAdapter(protocolAdapterInput);
     }
 
