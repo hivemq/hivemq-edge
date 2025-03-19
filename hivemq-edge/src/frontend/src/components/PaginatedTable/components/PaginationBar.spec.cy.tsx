dabf0b6c86aff275fc0fa6c57d00e8163eacacc3
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-03-19 16:21:13 +0000

    Merge pull request #859
    * fix(30988): hide controls when there is no page
    
    * tests(30988): add tests

--- hivemq-edge/src/frontend/src/components/PaginatedTable/components/PaginationBar.spec.cy.tsx
+++ hivemq-edge/src/frontend/src/components/PaginatedTable/components/PaginationBar.spec.cy.tsx
@@ -63,10 +63,93 @@ describe('PaginationBar', () => {
     cy.get('@nextPage').should('have.been.calledOnce')
   })
 
+  it('should render properly', () => {
+    cy.mountWithProviders(
+      <PaginationBar
+        // @ts-ignore force mocked partial object
+        table={MOCK_TABLE}
+        pageSizes={MOCK_PAGE_SIZES}
+        options={{ enablePaginationSizes: true, enablePaginationGoTo: true }}
+      />
+    )
+
+    cy.getByTestId('table-pagination-navigation').within(() => {
+      cy.get('button').should('have.length', 4)
+      cy.get('button').eq(0).should('have.attr', 'aria-label', 'Go to the first page').should('not.be.disabled')
+      cy.get('button').eq(1).should('have.attr', 'aria-label', 'Go to the previous page').should('not.be.disabled')
+      cy.get('button').eq(2).should('have.attr', 'aria-label', 'Go to the next page').should('not.be.disabled')
+      cy.get('button').eq(3).should('have.attr', 'aria-label', 'Go to the last page').should('not.be.disabled')
+    })
+
+    cy.getByTestId('table-pagination-currentPage').should('have.text', 'Page 3 of 10')
+    cy.getByTestId('table-pagination-goto').within(() => {
+      cy.get('label').should('have.text', 'Go to page')
+      cy.get('input')
+        .should('have.value', 3)
+        .should('have.attr', 'role', 'spinbutton')
+        .should('have.attr', 'inputmode', 'decimal')
+        .should('have.attr', 'aria-valuemin', '1')
+        .should('have.attr', 'aria-valuemax', '10')
+    })
+
+    cy.getByTestId('table-pagination-perPages').within(() => {
+      cy.get('label').should('have.text', 'Items per page')
+      cy.get('select').should('have.value', 5)
+    })
+  })
+
+  it('should hide page controls if no data', () => {
+    const mockEmptyTable = { ...MOCK_TABLE, getPageCount: () => 0 }
+    cy.mountWithProviders(
+      <PaginationBar
+        // @ts-ignore force mocked partial object
+        table={mockEmptyTable}
+        pageSizes={MOCK_PAGE_SIZES}
+        options={{ enablePaginationSizes: true, enablePaginationGoTo: true }}
+      />
+    )
+
+    cy.getByTestId('table-pagination-navigation').should('be.visible')
+    cy.getByTestId('table-pagination-currentPage').should('not.exist')
+    cy.getByTestId('table-pagination-goto').should('not.exist')
+  })
+
+  it('should hide pagination size', () => {
+    cy.mountWithProviders(
+      <PaginationBar
+        // @ts-ignore force mocked partial object
+        table={MOCK_TABLE}
+        pageSizes={MOCK_PAGE_SIZES}
+        options={{ enablePaginationSizes: false, enablePaginationGoTo: true }}
+      />
+    )
+
+    cy.getByTestId('table-pagination-perPage').should('not.exist')
+  })
+
+  it('should hide page go to', () => {
+    cy.mountWithProviders(
+      <PaginationBar
+        // @ts-ignore force mocked partial object
+        table={MOCK_TABLE}
+        pageSizes={MOCK_PAGE_SIZES}
+        options={{ enablePaginationSizes: true, enablePaginationGoTo: false }}
+      />
+    )
+
+    cy.getByTestId('table-pagination-goto').should('not.exist')
+  })
+
   it('should be accessible', () => {
     cy.injectAxe()
-    // @ts-ignore force mocked partial object
-    cy.mountWithProviders(<PaginationBar table={MOCK_TABLE} pageSizes={MOCK_PAGE_SIZES} />)
+    cy.mountWithProviders(
+      <PaginationBar
+        // @ts-ignore force mocked partial object
+        table={MOCK_TABLE}
+        pageSizes={MOCK_PAGE_SIZES}
+        options={{ enablePaginationSizes: true, enablePaginationGoTo: true }}
+      />
+    )
     cy.checkAccessibility()
     cy.percySnapshot('Component: PaginationBar')
   })
