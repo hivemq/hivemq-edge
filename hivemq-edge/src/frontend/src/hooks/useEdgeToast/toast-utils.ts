fd5f0f4c20c901bfd07393bf64e710f1cf7c923b
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-03-19 15:12:04 +0000

    fix(30954): change structure of defaults for toast

--- hivemq-edge/src/frontend/src/hooks/useEdgeToast/toast-utils.ts
+++ hivemq-edge/src/frontend/src/hooks/useEdgeToast/toast-utils.ts
@@ -1,8 +1,12 @@
 import type { UseToastOptions } from '@chakra-ui/react'
 
-export const DEFAULT_TOAST_OPTION: UseToastOptions = {
-  status: 'success',
+export const BASE_TOAST_OPTION: UseToastOptions = {
   duration: 3000,
   isClosable: true,
+}
+
+export const DEFAULT_TOAST_OPTION: UseToastOptions = {
+  ...BASE_TOAST_OPTION,
+  status: 'success',
   position: 'top-right',
 }
