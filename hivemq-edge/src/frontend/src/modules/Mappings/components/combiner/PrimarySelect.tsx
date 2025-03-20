2564a57d11b529a4e6cbdf450457306944d21f10
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-03-20 15:00:21 +0000

    refactor(30987): add the key visual cue to the selector

--- hivemq-edge/src/frontend/src/modules/Mappings/components/combiner/PrimarySelect.tsx
+++ hivemq-edge/src/frontend/src/modules/Mappings/components/combiner/PrimarySelect.tsx
@@ -1,6 +1,8 @@
 import { useMemo, type FC } from 'react'
 import { useTranslation } from 'react-i18next'
-import { type SingleValue, Select } from 'chakra-react-select'
+import { type SingleValue, Select, chakraComponents } from 'chakra-react-select'
+import { Icon } from '@chakra-ui/react'
+import { FaKey } from 'react-icons/fa'
 
 import type { DataCombining } from '@/api/__generated__'
 import { DataIdentifierReference } from '@/api/__generated__'
@@ -54,6 +56,14 @@ export const PrimarySelect: FC<PrimarySelectProps> = ({ id, formData, onChange }
       value={primaryValue}
       onChange={onChange}
       isClearable
+      components={{
+        Control: ({ children, ...props }) => (
+          <chakraComponents.Control {...props}>
+            <Icon as={FaKey} ml={3} />
+            {children}
+          </chakraComponents.Control>
+        ),
+      }}
       placeholder={t('combiner.schema.mapping.primary.placeholder')}
       aria-label={t('combiner.schema.mapping.primary.label')}
     />
