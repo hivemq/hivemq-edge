01b162c22e720d6482c813749cd2bb80ec71c18e
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-03-21 11:43:03 +0000

    fix(30982): fix layout

--- hivemq-edge/src/frontend/src/modules/Mappings/components/combiner/DataCombiningEditorField.tsx
+++ hivemq-edge/src/frontend/src/modules/Mappings/components/combiner/DataCombiningEditorField.tsx
@@ -7,10 +7,10 @@ import {
   FormErrorMessage,
   FormHelperText,
   FormLabel,
+  Grid,
+  GridItem,
   HStack,
   Icon,
-  Stack,
-  VStack,
 } from '@chakra-ui/react'
 import { FaRightFromBracket } from 'react-icons/fa6'
 
@@ -50,162 +50,162 @@ export const DataCombiningEditorField: FC<FieldProps<DataCombining, RJSFSchema, 
   // TODO[RJSF] Create a custom error message, ensuring tag + topic filters >= 1
 
   return (
-    <VStack alignItems="stretch" gap={4}>
-      <Stack gap={2} flexDirection="row">
-        <VStack flex={1} alignItems="stretch" maxW="40vw" gap={5}>
-          <Box>
-            {sourceOptions.title && (
-              <TitleFieldTemplate id={'FIXME'} title={sourceOptions.title} schema={props.schema} registry={registry} />
-            )}
-            {sourceOptions.description && (
-              <DescriptionFieldTemplate
-                id={'FIXME'}
-                description={sourceOptions.description}
-                schema={props.schema}
-                registry={registry}
-              />
-            )}
-          </Box>
-          <Box>
-            <FormControl isInvalid={Boolean(sourceError)}>
-              <FormLabel>{t('combiner.schema.mappings.sources.combinedData.title')}</FormLabel>
-              <CombinedEntitySelect
-                tags={formData?.sources?.tags}
-                topicFilters={formData?.sources?.topicFilters}
-                formContext={formContext}
-                onChange={(values) => {
-                  if (!formData) return
-                  const tag: string[] = []
-                  const filter: string[] = []
-
-                  let isPrimary = false
-                  values.forEach((entity) => {
-                    if (entity.type === DataIdentifierReference.type.TAG) tag.push(entity.value)
-                    if (entity.type === DataIdentifierReference.type.TOPIC_FILTER) filter.push(entity.value)
-                    if (formData.sources.primary?.type === entity.type && formData.sources.primary?.id === entity.value)
-                      isPrimary = true
-                  })
-
-                  props.onChange({
-                    ...formData,
-                    sources: {
-                      ...formData.sources,
-                      tags: tag,
-                      topicFilters: filter,
-                      // @ts-ignore TODO[30935] check for type clash on primary
-                      primary: isPrimary ? formData.sources.primary : undefined,
-                    },
-                  })
-                }}
-              />
-              {!sourceError && (
-                <FormHelperText>{t('combiner.schema.mappings.sources.combinedData.description')}</FormHelperText>
-              )}
-              <FormErrorMessage>{sourceError?.join(' ')}</FormErrorMessage>
-            </FormControl>
-          </Box>
-          <VStack overflow={'auto'} alignItems={'flex-start'} justifyContent={'center'} tabIndex={0} gap={5}>
-            <FormControl>
-              <FormLabel>{t('combiner.schema.mappings.sources.combinedSchema.title')}</FormLabel>
-              <CombinedSchemaLoader formData={props.formData} formContext={formContext} />
-              {!sourceError && (
-                <FormHelperText>{t('combiner.schema.mappings.sources.combinedSchema.description')}</FormHelperText>
-              )}
-            </FormControl>
-          </VStack>
-          <Box maxW={'25vw'}>
-            <FormControl isInvalid={Boolean(primaryError)}>
-              <FormLabel>{primaryOptions.title}</FormLabel>
-              <PrimarySelect
-                formData={formData}
-                onChange={(values) => {
-                  if (!props.formData) return
-
-                  props.onChange({
-                    ...props.formData,
-                    sources: {
-                      ...props.formData.sources,
-                      // @ts-ignore TODO[30935] check for type clash on primary
-                      primary: values
-                        ? {
-                            id: values.value,
-                            type: values.type,
-                          }
-                        : undefined,
-                    },
-                  })
-                }}
-              />
-              {!primaryError && <FormHelperText>{primaryOptions.description}</FormHelperText>}
-              <FormErrorMessage>{props.errorSchema?.sources?.primary?.__errors}</FormErrorMessage>
-            </FormControl>
-          </Box>
-        </VStack>
-        <VStack justifyContent="center">
-          <HStack height={38}>
-            <Icon as={FaRightFromBracket} />
-          </HStack>
-        </VStack>
-        <VStack flex={1} alignItems="stretch" maxW="50vw">
-          <Box>
-            {destOptions.title && (
-              <TitleFieldTemplate id={'FIXME'} title={destOptions.title} schema={props.schema} registry={registry} />
-            )}
-            {destOptions.description && (
-              <DescriptionFieldTemplate
-                id={'FIXME'}
-                description={destOptions.description}
-                schema={props.schema}
-                registry={registry}
-              />
-            )}
-          </Box>
-          <Box maxW={'25vw'}>
-            <FormControl isInvalid={Boolean(destinationError)}>
-              <FormLabel>{destTopicOptions.title}</FormLabel>
-              <SelectTopic
-                isMulti={false}
-                isCreatable={true}
-                id={'destination'}
-                value={formData?.destination?.topic || null}
-                onChange={(topic) => {
-                  if (!props.formData) return
-
-                  if (topic && typeof topic === 'string') props.onChange({ ...props.formData, destination: { topic } })
-                  else if (!topic) props.onChange({ ...props.formData, destination: { topic: '' } })
-                }}
-              />
-              {!destinationError && <FormHelperText>{destTopicOptions.description}</FormHelperText>}
-
-              <FormErrorMessage>{destinationError}</FormErrorMessage>
-            </FormControl>
-          </Box>
-          <FormControl isInvalid={Boolean(destinationError)}>
-            <FormLabel>{destSchemaOptions.title}</FormLabel>
-            <DestinationSchemaLoader
-              formData={props.formData}
-              onChange={(schema) => {
-                if (!props.formData) return
-
-                props.onChange({
-                  ...props.formData,
-                  destination: { topic: props.formData.destination.topic, schema },
-                })
-              }}
-              onChangeInstructions={(v: Instruction[]) => {
-                if (!props.formData) return
-                if (!v.length) return
-
-                props.onChange({
-                  ...props.formData,
-                  instructions: v,
-                })
-              }}
+    <Grid templateColumns="1fr repeat(2, 1px) 1fr" gap={6}>
+      <GridItem colSpan={2}>
+        <Box>
+          {sourceOptions.title && (
+            <TitleFieldTemplate id={'FIXME'} title={sourceOptions.title} schema={props.schema} registry={registry} />
+          )}
+          {sourceOptions.description && (
+            <DescriptionFieldTemplate
+              id={'FIXME'}
+              description={sourceOptions.description}
+              schema={props.schema}
+              registry={registry}
             />
-            <FormHelperText>{destSchemaOptions.description}</FormHelperText>
-          </FormControl>
-        </VStack>
-      </Stack>
-    </VStack>
+          )}
+        </Box>
+      </GridItem>
+      <GridItem colSpan={2}>
+        <Box>
+          {destOptions.title && (
+            <TitleFieldTemplate id={'FIXME'} title={destOptions.title} schema={props.schema} registry={registry} />
+          )}
+          {destOptions.description && (
+            <DescriptionFieldTemplate
+              id={'FIXME'}
+              description={destOptions.description}
+              schema={props.schema}
+              registry={registry}
+            />
+          )}
+        </Box>
+      </GridItem>
+      <GridItem colSpan={2}>
+        <FormControl isInvalid={Boolean(sourceError)}>
+          <FormLabel>{t('combiner.schema.mappings.sources.combinedData.title')}</FormLabel>
+          <CombinedEntitySelect
+            tags={formData?.sources?.tags}
+            topicFilters={formData?.sources?.topicFilters}
+            formContext={formContext}
+            onChange={(values) => {
+              if (!formData) return
+              const tag: string[] = []
+              const filter: string[] = []
+
+              let isPrimary = false
+              values.forEach((entity) => {
+                if (entity.type === DataIdentifierReference.type.TAG) tag.push(entity.value)
+                if (entity.type === DataIdentifierReference.type.TOPIC_FILTER) filter.push(entity.value)
+                if (formData.sources.primary?.type === entity.type && formData.sources.primary?.id === entity.value)
+                  isPrimary = true
+              })
+
+              props.onChange({
+                ...formData,
+                sources: {
+                  ...formData.sources,
+                  tags: tag,
+                  topicFilters: filter,
+                  // @ts-ignore TODO[30935] check for type clash on primary
+                  primary: isPrimary ? formData.sources.primary : undefined,
+                },
+              })
+            }}
+          />
+          {!sourceError && (
+            <FormHelperText>{t('combiner.schema.mappings.sources.combinedData.description')}</FormHelperText>
+          )}
+          <FormErrorMessage>{sourceError?.join(' ')}</FormErrorMessage>
+        </FormControl>
+      </GridItem>
+      <GridItem colSpan={2}>
+        <FormControl isInvalid={Boolean(destinationError)}>
+          <FormLabel>{destTopicOptions.title}</FormLabel>
+          <SelectTopic
+            isMulti={false}
+            isCreatable={true}
+            id={'destination'}
+            value={formData?.destination?.topic || null}
+            onChange={(topic) => {
+              if (!props.formData) return
+
+              if (topic && typeof topic === 'string') props.onChange({ ...props.formData, destination: { topic } })
+              else if (!topic) props.onChange({ ...props.formData, destination: { topic: '' } })
+            }}
+          />
+          {!destinationError && <FormHelperText>{destTopicOptions.description}</FormHelperText>}
+
+          <FormErrorMessage>{destinationError}</FormErrorMessage>
+        </FormControl>
+      </GridItem>
+      <GridItem>
+        <FormControl>
+          <FormLabel>{t('combiner.schema.mappings.sources.combinedSchema.title')}</FormLabel>
+          <CombinedSchemaLoader formData={props.formData} formContext={formContext} />
+          {!sourceError && (
+            <FormHelperText>{t('combiner.schema.mappings.sources.combinedSchema.description')}</FormHelperText>
+          )}
+        </FormControl>
+      </GridItem>
+      <GridItem>
+        <HStack height={'100%'} justifyContent={'center'}>
+          <Icon as={FaRightFromBracket} />
+        </HStack>
+      </GridItem>
+      <GridItem colSpan={2}>
+        <FormControl isInvalid={Boolean(destinationError)}>
+          <FormLabel>{destSchemaOptions.title}</FormLabel>
+          <DestinationSchemaLoader
+            formData={props.formData}
+            onChange={(schema) => {
+              if (!props.formData) return
+
+              props.onChange({
+                ...props.formData,
+                destination: { topic: props.formData.destination.topic, schema },
+              })
+            }}
+            onChangeInstructions={(v: Instruction[]) => {
+              if (!props.formData) return
+              if (!v.length) return
+
+              props.onChange({
+                ...props.formData,
+                instructions: v,
+              })
+            }}
+          />
+          <FormHelperText>{destSchemaOptions.description}</FormHelperText>
+        </FormControl>
+      </GridItem>
+      <GridItem colSpan={2}>
+        <FormControl isInvalid={Boolean(primaryError)}>
+          <FormLabel>{primaryOptions.title}</FormLabel>
+          <PrimarySelect
+            formData={formData}
+            onChange={(values) => {
+              if (!props.formData) return
+
+              props.onChange({
+                ...props.formData,
+                sources: {
+                  ...props.formData.sources,
+                  // @ts-ignore TODO[30935] check for type clash on primary
+                  primary: values
+                    ? {
+                        id: values.value,
+                        type: values.type,
+                      }
+                    : undefined,
+                },
+              })
+            }}
+          />
+          {!primaryError && <FormHelperText>{primaryOptions.description}</FormHelperText>}
+          <FormErrorMessage>{props.errorSchema?.sources?.primary?.__errors}</FormErrorMessage>
+        </FormControl>
+      </GridItem>
+    </Grid>
   )
 }
