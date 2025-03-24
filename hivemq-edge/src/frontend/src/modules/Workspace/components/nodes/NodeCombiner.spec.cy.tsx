dc6a6a1ef221483859396a152bd115d7d9ceee13
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-03-24 03:08:58 +0000

    test(30538): fix tests

--- hivemq-edge/src/frontend/src/modules/Workspace/components/nodes/NodeCombiner.spec.cy.tsx
+++ hivemq-edge/src/frontend/src/modules/Workspace/components/nodes/NodeCombiner.spec.cy.tsx
@@ -29,6 +29,15 @@ describe('NodeCombiner', () => {
     cy.getByTestId('test-navigate-pathname').should('have.text', `/workspace/combiner/${MOCK_NODE_COMBINER.id}`)
   })
 
+  it('should render the mapping flags', () => {
+    cy.mountWithProviders(
+      <CustomNodeTesting
+        nodes={[{ ...MOCK_NODE_COMBINER, position: { x: 90, y: 100 }, selected: true }]}
+        nodeTypes={{ [NodeTypes.COMBINER_NODE]: NodeCombiner }}
+      />
+    )
+  })
+
   it('should be accessible', () => {
     cy.injectAxe()
     cy.mountWithProviders(mockReactFlow(<NodeCombiner {...MOCK_NODE_COMBINER} />))
