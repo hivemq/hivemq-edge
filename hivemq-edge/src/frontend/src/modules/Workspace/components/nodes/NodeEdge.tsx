26e292c33656806d0af37e1f74678ac331dc6ab3
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-03-24 03:08:40 +0000

    refactor(30538): refactor the edge node - add instance name - change layout - add topic filter flags

--- hivemq-edge/src/frontend/src/modules/Workspace/components/nodes/NodeEdge.tsx
+++ hivemq-edge/src/frontend/src/modules/Workspace/components/nodes/NodeEdge.tsx
@@ -1,22 +1,33 @@
 import type { FC } from 'react'
+import { useMemo } from 'react'
 import { useNavigate } from 'react-router-dom'
 import type { NodeProps } from 'reactflow'
 import { Handle, Position } from 'reactflow'
 import { useTranslation } from 'react-i18next'
-import { Icon, Image } from '@chakra-ui/react'
+import { Icon, Image, Text, VStack } from '@chakra-ui/react'
 
 import logo from '@/assets/edge/05-icon-industrial-hivemq-edge.svg'
+
+import { useListTopicFilters } from '@/api/hooks/useTopicFilters/useListTopicFilters'
+import { SelectEntityType } from '@/components/MQTT/types'
 import ToolbarButtonGroup from '@/components/react-flow/ToolbarButtonGroup.tsx'
 import IconButton from '@/components/Chakra/IconButton.tsx'
 import { TopicIcon } from '@/components/Icons/TopicIcon.tsx'
 import NodeWrapper from '@/modules/Workspace/components/parts/NodeWrapper.tsx'
 import { useContextMenu } from '@/modules/Workspace/hooks/useContextMenu.ts'
 import ContextualToolbar from '@/modules/Workspace/components/nodes/ContextualToolbar.tsx'
+import { CONFIG_ADAPTER_WIDTH } from '../../utils/nodes-utils'
+import MappingBadge from '../parts/MappingBadge'
 
 const NodeEdge: FC<NodeProps> = (props) => {
   const { t } = useTranslation()
   const { onContextMenu } = useContextMenu(props.id, props.selected, '/workspace/edge')
   const navigate = useNavigate()
+  const { data } = useListTopicFilters()
+
+  const topicFilters = useMemo(() => {
+    return data?.items.map((e) => e.topicFilter) || []
+  }, [data?.items])
 
   return (
     <>
@@ -38,12 +49,19 @@ const NodeEdge: FC<NodeProps> = (props) => {
         isSelected={props.selected}
         onDoubleClick={onContextMenu}
         onContextMenu={onContextMenu}
-        variant="unstyled"
-        borderWidth={0}
-        px={2}
-        py={0}
+        flexDirection="row"
+        w={CONFIG_ADAPTER_WIDTH}
+        p={2}
+        h={120}
       >
-        <Image data-testid="edge-node" src={logo} alt={t('workspace.node.edge')} boxSize="96px" />
+        <VStack h={'100%'} justifyContent={'center'}>
+          <Image data-testid="edge-node-icon" src={logo} alt={t('workspace.node.edge')} boxSize="96px" />
+        </VStack>
+        <VStack h={'100%'} flex={1} justifyContent={'space-between'}>
+          <Text data-testid="edge-node-title">{t('branding.appName')}</Text>
+          <MappingBadge destinations={topicFilters} type={SelectEntityType.TOPIC_FILTER} />
+        </VStack>
+        .
         <Handle
           type="target"
           position={Position.Bottom}
