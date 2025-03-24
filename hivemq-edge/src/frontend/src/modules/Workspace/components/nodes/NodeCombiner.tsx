635064ea0cde2577149933e7302c49cc0a22bc41
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-03-24 03:07:57 +0000

    refactor(30538): refactor the combiner node - add mapping flags - change layout

--- hivemq-edge/src/frontend/src/modules/Workspace/components/nodes/NodeCombiner.tsx
+++ hivemq-edge/src/frontend/src/modules/Workspace/components/nodes/NodeCombiner.tsx
@@ -1,18 +1,26 @@
 import type { FC } from 'react'
+import { useMemo } from 'react'
 import type { NodeProps } from 'reactflow'
 import { Handle, Position } from 'reactflow'
-import { Box, Icon, Text } from '@chakra-ui/react'
-import { MdScheduleSend } from 'react-icons/md'
+import { Icon, Text, useColorModeValue, VStack } from '@chakra-ui/react'
 
 import type { Combiner } from '@/api/__generated__'
 
+import { HqCombiner } from '@/components/Icons'
+import { SelectEntityType } from '@/components/MQTT/types'
 import NodeWrapper from '@/modules/Workspace/components/parts/NodeWrapper.tsx'
-
 import { useContextMenu } from '@/modules/Workspace/hooks/useContextMenu.ts'
 import ContextualToolbar from '@/modules/Workspace/components/nodes/ContextualToolbar.tsx'
+import { CONFIG_ADAPTER_WIDTH } from '@/modules/Workspace/utils/nodes-utils'
+import MappingBadge from '../parts/MappingBadge'
 
 const NodeCombiner: FC<NodeProps<Combiner>> = ({ id, selected, data, dragging }) => {
   const { onContextMenu } = useContextMenu(id, selected, '/workspace/combiner')
+  const bgColour = useColorModeValue('gray.300', 'gray.900')
+
+  const topics = useMemo(() => {
+    return data.mappings.items.map((e) => e.destination.topic as string)
+  }, [data.mappings.items])
 
   return (
     <>
@@ -27,15 +35,26 @@ const NodeCombiner: FC<NodeProps<Combiner>> = ({ id, selected, data, dragging })
         borderBottomRadius={30}
         flexDirection="row"
         p={0}
-        w={150}
+        w={CONFIG_ADAPTER_WIDTH}
         alignItems="center"
+        h={120}
       >
-        <Box p={4} backgroundColor="gray.300" borderTopLeftRadius={30} borderBottomLeftRadius={30}>
-          <Icon as={MdScheduleSend} boxSize={6} />
-        </Box>
-        <Box flex={1} p={1} data-testid="combiner-description">
-          <Text noOfLines={2}>{data.name}</Text>
-        </Box>
+        <VStack
+          h={'100%'}
+          p={4}
+          backgroundColor={bgColour}
+          borderTopLeftRadius={30}
+          borderBottomLeftRadius={30}
+          justifyContent={'center'}
+        >
+          <Icon as={HqCombiner} boxSize={10} />
+        </VStack>
+        <VStack p={2} h={'100%'} justifyContent={'space-evenly'}>
+          <Text data-testid="combiner-description" noOfLines={1}>
+            {data.name}
+          </Text>
+          <MappingBadge destinations={topics} type={SelectEntityType.TOPIC} />
+        </VStack>
       </NodeWrapper>
       <Handle type="source" position={Position.Bottom} />
       <Handle type="target" position={Position.Top} />
