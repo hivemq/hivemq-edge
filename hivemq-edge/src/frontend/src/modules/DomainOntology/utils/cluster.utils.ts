0213a5f0f0f76cb212083b5721474ae6eeb51598
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-03-24 04:09:51 +0000

    test(31384): add tests

--- hivemq-edge/src/frontend/src/modules/DomainOntology/utils/cluster.utils.ts
+++ hivemq-edge/src/frontend/src/modules/DomainOntology/utils/cluster.utils.ts
@@ -28,7 +28,11 @@ export interface ClusterFunctionCatalog<T> {
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const groupCatalog: ClusterFunctionCatalog<any>[] = [
   { key: 'runtime', name: 'Runtime Status', keyFunction: (e) => e.payload.status?.runtime },
-  { key: 'type', name: 'Started at', keyFunction: (e) => DateTime.fromISO(e.payload.status?.startedAt).toRelative() },
+  {
+    key: 'startedAt',
+    name: 'Started at',
+    keyFunction: (e) => DateTime.fromISO(e.payload.status?.startedAt).toRelative(),
+  },
   { key: 'type', name: 'Adapter type', keyFunction: (e) => e.payload.type },
   { key: 'cat', name: 'ssss', keyFunction: (e) => e.cat },
 ]
