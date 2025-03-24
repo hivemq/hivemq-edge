11d3242431472d763041d4df7f440692ed011954
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-03-24 04:09:05 +0000

    test(31384): add tests and fix deprecated code

--- hivemq-edge/src/frontend/src/modules/Mappings/hooks/useNorthboundMappingManager.spec.ts
+++ hivemq-edge/src/frontend/src/modules/Mappings/hooks/useNorthboundMappingManager.spec.ts
@@ -1,12 +1,13 @@
 import { beforeEach, expect } from 'vitest'
-import { renderHook, waitFor } from '@testing-library/react'
+import { act, renderHook, waitFor } from '@testing-library/react'
 
 import { server } from '@/__test-utils__/msw/mockServer.ts'
-import { SimpleWrapper as wrapper } from '@/__test-utils__/hooks/SimpleWrapper.tsx'
-import { useNorthboundMappingManager } from '@/modules/Mappings/hooks/useNorthboundMappingManager.ts'
-import { mappingHandlers } from '@/api/hooks/useProtocolAdapters/__handlers__/mapping.mocks.ts'
-import type { NorthboundMappingList } from '@/api/__generated__'
 import { MOCK_MAX_QOS } from '@/__test-utils__/adapters/mqtt.ts'
+import { SimpleWrapper as wrapper } from '@/__test-utils__/hooks/SimpleWrapper.tsx'
+import { mappingHandlers, MOCK_NORTHBOUND_MAPPING } from '@/api/hooks/useProtocolAdapters/__handlers__/mapping.mocks.ts'
+import type { NorthboundMappingList } from '@/api/__generated__'
+
+import { useNorthboundMappingManager } from '@/modules/Mappings/hooks/useNorthboundMappingManager.ts'
 
 describe('useNorthboundMappingManager', () => {
   beforeEach(() => {
@@ -36,6 +37,24 @@ describe('useNorthboundMappingManager', () => {
           topic: 'my/topic',
         }),
       ],
+    })
+  })
+
+  it('should do it properly', async () => {
+    const { result } = renderHook(() => useNorthboundMappingManager('my-adapter'), { wrapper })
+
+    expect(result.current.isLoading).toBeTruthy()
+    await waitFor(() => {
+      expect(result.current.isLoading).toBeFalsy()
+    })
+
+    // TODO[TEST] Needs to test the effect of calling the functions
+    act(() => {
+      result.current.onUpdateCollection({ items: [MOCK_NORTHBOUND_MAPPING] })
+    })
+
+    act(() => {
+      result.current.onClose()
     })
   })
 })
