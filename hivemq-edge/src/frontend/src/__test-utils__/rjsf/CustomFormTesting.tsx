92cd21f32cb550ddcab7bdba5a21b024c1bb22d4
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-03-25 11:38:40 +0000

    test(31384): fix templates for the wrapper

--- hivemq-edge/src/frontend/src/__test-utils__/rjsf/CustomFormTesting.tsx
+++ hivemq-edge/src/frontend/src/__test-utils__/rjsf/CustomFormTesting.tsx
@@ -3,6 +3,12 @@ import validator from '@rjsf/validator-ajv8'
 
 import Form from '@rjsf/chakra-ui'
 import type { FormProps } from '@rjsf/core'
+import { ArrayFieldItemTemplate } from '@/components/rjsf/ArrayFieldItemTemplate'
+import { ArrayFieldTemplate } from '@/components/rjsf/ArrayFieldTemplate'
+import { BaseInputTemplate } from '@/components/rjsf/BaseInputTemplate'
+import { FieldTemplate } from '@/components/rjsf/FieldTemplate'
+import { ObjectFieldTemplate } from '@/components/rjsf/ObjectFieldTemplate'
+import { DescriptionFieldTemplate, ErrorListTemplate, TitleFieldTemplate } from '@/components/rjsf/Templates'
 
 interface CustomFormTestingProps
   extends Pick<FormProps<unknown>, 'schema' | 'uiSchema' | 'formData' | 'onChange' | 'onSubmit' | 'onError'> {
@@ -28,6 +34,16 @@ export const CustomFormTesting: FC<CustomFormTestingProps> = ({
       onSubmit={onSubmit}
       liveValidate
       showErrorList="bottom"
+      templates={{
+        ObjectFieldTemplate,
+        FieldTemplate,
+        BaseInputTemplate,
+        ArrayFieldTemplate,
+        ArrayFieldItemTemplate,
+        DescriptionFieldTemplate,
+        ErrorListTemplate,
+        TitleFieldTemplate,
+      }}
     />
   )
 }
