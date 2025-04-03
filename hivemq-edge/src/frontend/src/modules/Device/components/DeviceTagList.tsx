4c075d08cbaed040aef54ce8f285294a3b06f239
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-04-03 12:35:40 +0100

    fix(28801): fix props

--- hivemq-edge/src/frontend/src/modules/Device/components/DeviceTagList.tsx
+++ hivemq-edge/src/frontend/src/modules/Device/components/DeviceTagList.tsx
@@ -11,7 +11,7 @@ import type { DeviceTagListContext } from '../types'
 
 interface DeviceTagListProps {
   adapter: Adapter
-  onClose: () => void
+  onClose?: () => void
 }
 
 const DeviceTagList: FC<DeviceTagListProps> = ({ adapter, onClose }) => {
@@ -21,7 +21,7 @@ const DeviceTagList: FC<DeviceTagListProps> = ({ adapter, onClose }) => {
   const onHandleSubmit = (data: IChangeEvent) => {
     if (!data.formData) return
     onupdateCollection(data.formData as DomainTagList)
-    onClose()
+    onClose?.()
   }
 
   const formContext: DeviceTagListContext = {
@@ -34,7 +34,7 @@ const DeviceTagList: FC<DeviceTagListProps> = ({ adapter, onClose }) => {
       {isError && <ErrorMessage message={t('device.errors.noTagLoaded')} />}
 
       {!context.schema && <ErrorMessage message={t('device.errors.noFormSchema')} status="error" />}
-      {context.schema && (
+      {!isLoading && !isError && context.schema && (
         <ChakraRJSForm
           id="tag-listing-form"
           schema={context.schema}
