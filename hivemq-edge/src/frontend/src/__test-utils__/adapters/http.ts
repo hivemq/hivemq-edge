9054ccf8b3fa9855b3d8a6cbbabe3c6baa424be6
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-04-07 22:05:05 +0100

    refactor(29697): add mock for http adapter

--- hivemq-edge/src/frontend/src/__test-utils__/adapters/http.ts
+++ hivemq-edge/src/frontend/src/__test-utils__/adapters/http.ts
@@ -1,4 +1,6 @@
-import type { ProtocolAdapter } from '@/api/__generated__'
+/* generated from Edge version 2025.5 -- do no edit */
+import type { Adapter, ProtocolAdapter, TagSchema } from '@/api/__generated__'
+import { Status } from '@/api/__generated__'
 
 export const MOCK_PROTOCOL_HTTP: ProtocolAdapter = {
   id: 'http',
@@ -6,12 +8,12 @@ export const MOCK_PROTOCOL_HTTP: ProtocolAdapter = {
   name: 'HTTP(s) to MQTT Protocol Adapter',
   description:
     'Connects HiveMQ Edge to arbitrary web endpoint URLs via HTTP(s), consuming structured JSON or plain data.',
-  url: 'https://github.com/hivemq/hivemq-edge/wiki/Protocol-adapters#http',
-  version: 'Development Snapshot (BETA)',
-  logoUrl: 'http://localhost:8080/images/http-icon.png',
+  url: 'https://docs.hivemq.com/hivemq-edge/protocol-adapters.html#http-adapter',
+  version: 'Development Version',
+  logoUrl: '/module/images/http-icon.png',
   author: 'HiveMQ',
   installed: true,
-  // capabilities: ['READ'],
+  capabilities: ['READ'],
   category: {
     name: 'CONNECTIVITY',
     displayName: 'Connectivity',
@@ -22,131 +24,255 @@ export const MOCK_PROTOCOL_HTTP: ProtocolAdapter = {
     $schema: 'https://json-schema.org/draft/2020-12/schema',
     type: 'object',
     properties: {
-      url: {
-        type: 'string',
-        title: 'URL',
-        description: 'The url of the http request you would like to make',
-        pattern:
-          'https?:\\/\\/(?:w{1,3}\\.)?[^\\s.]+(?:\\.[a-z]+)*(?::\\d+)?((?:\\/\\w+)|(?:-\\w+))*\\/?(?![^<]*(?:<\\/\\w+>|\\/?>))',
-      },
-      destination: {
-        type: 'string',
-        title: 'Destination Topic',
-        description: 'The topic to publish data on',
-        format: 'mqtt-topic',
-      },
-      qos: {
-        type: 'integer',
-        title: 'QoS',
-        description: 'MQTT Quality of Service level',
-        default: 0,
-        minimum: 0,
-        maximum: 2,
-      },
-      httpRequestMethod: {
-        type: 'string',
-        enum: ['GET', 'POST', 'PUT'],
-        title: 'Http Method',
-        description: 'Http method associated with the request',
-        default: 'GET',
+      allowUntrustedCertificates: {
+        type: 'boolean',
+        title: 'Allow Untrusted Certificates',
+        description: 'Allow the adapter to connect to untrusted SSL sources (for example expired certificates).',
+        default: false,
+        format: 'boolean',
       },
       httpConnectTimeoutSeconds: {
         type: 'integer',
-        title: 'Http Connection Timeout',
-        description: 'Timeout (in second) to apply to the HTTP Request',
-        default: 10,
+        title: 'HTTP Connection Timeout',
+        description: 'Timeout (in seconds) to allow the underlying HTTP connection to be established',
+        default: 5,
+        minimum: 1,
+        maximum: 60,
       },
-      httpRequestBodyContentType: {
-        type: 'string',
-        enum: ['JSON', 'PLAIN', 'HTML', 'XML', 'YAML'],
-        title: 'Http Request Content Type',
-        description: 'Content Type associated with the request',
-        default: 'JSON',
-      },
-      httpRequestBody: {
-        type: 'string',
-        title: 'Http Request Body',
-        description: 'The body to include in the HTTP request',
-      },
-      httpPublishSuccessStatusCodeOnly: {
-        type: 'boolean',
-        title: 'Only publish data when HTTP response code is successful ( 200 - 299 )',
-        default: true,
-        format: 'boolean',
-      },
-      httpHeaders: {
-        title: 'HTTP Headers',
-        description: 'HTTP headers to be added to your requests',
-        type: 'array',
-        items: {
-          type: 'object',
-          properties: {
-            name: {
-              type: 'string',
-              title: 'Http Header Name',
-              description: 'The name of the HTTP header',
-            },
-            value: {
-              type: 'string',
-              title: 'Http Header Value',
-              description: 'The value of the HTTP header',
-            },
+      httpToMqtt: {
+        type: 'object',
+        properties: {
+          assertResponseIsJson: {
+            type: 'boolean',
+            title: 'Assert JSON Response?',
+            description:
+              'Always attempt to parse the body of the response as JSON data, regardless of the Content-Type on the response.',
+            default: false,
+            format: 'boolean',
           },
-          title: 'HTTP Headers',
-          description: 'HTTP headers to be added to your requests',
+          httpPublishSuccessStatusCodeOnly: {
+            type: 'boolean',
+            title: 'Publish Only On Success Codes',
+            description: 'Only publish data when HTTP response code is successful ( 200 - 299 )',
+            default: true,
+            format: 'boolean',
+          },
+          maxPollingErrorsBeforeRemoval: {
+            type: 'integer',
+            title: 'Max. Polling Errors',
+            description: 'Max. errors polling the endpoint before the polling daemon is stopped',
+            default: 10,
+            minimum: 3,
+          },
+          pollingIntervalMillis: {
+            type: 'integer',
+            title: 'Polling Interval [ms]',
+            description: 'Time in millisecond that this endpoint will be polled',
+            default: 1000,
+            minimum: 1,
+          },
         },
+        title: 'HTTP To MQTT Config',
+        description: 'The configuration for a data stream from HTTP to MQTT',
       },
       id: {
         type: 'string',
         title: 'Identifier',
         description: 'Unique identifier for this protocol adapter',
+        writeOnly: true,
         minLength: 1,
         maxLength: 1024,
         format: 'identifier',
-        pattern: '([a-zA-Z_0-9\\-])*',
-      },
-      pollingIntervalMillis: {
-        type: 'integer',
-        title: 'Polling interval [ms]',
-        description: 'Time in millisecond that this URL will be called',
-        default: 1000,
-        minimum: 1,
-      },
-      maxPollingErrorsBeforeRemoval: {
-        type: 'integer',
-        title: 'Max. Polling Errors',
-        description: 'Max. errors polling the endpoint before the polling daemon is stopped',
-        default: 10,
-        minimum: 3,
+        pattern: '^([a-zA-Z_0-9-_])*$',
       },
     },
-    required: ['url', 'destination', 'qos', 'httpConnectTimeoutSeconds', 'id', 'pollingIntervalMillis'],
+    required: ['id'],
+  },
+  uiSchema: {
+    'ui:tabs': [
+      {
+        id: 'coreFields',
+        title: 'Connection',
+        properties: ['id', 'httpConnectTimeoutSeconds', 'allowUntrustedCertificates'],
+      },
+      {
+        id: 'httpToMqtt',
+        title: 'HTTP to MQTT',
+        properties: ['httpToMqtt'],
+      },
+      {
+        id: 'mqttToHttp',
+        title: 'MQTT to HTTP',
+        properties: ['mqttToHttp'],
+      },
+    ],
+    id: {
+      'ui:disabled': true,
+    },
+    'ui:order': ['id', 'url', '*'],
+    httpToMqtt: {
+      'ui:batchMode': true,
+      'ui:order': [
+        'httpToMqttMappings',
+        'pollingIntervalMillis',
+        'maxPollingErrorsBeforeRemoval',
+        'assertResponseIsJson',
+        'httpPublishSuccessStatusCodeOnly',
+        '*',
+      ],
+      httpToMqttMappings: {
+        'ui:batchMode': true,
+        items: {
+          'ui:order': [
+            'url',
+            'mqttTopic',
+            'mqttQos',
+            'mqttUserProperties',
+            'httpRequestMethod',
+            'httpRequestTimeoutSeconds',
+            'httpRequestBodyContentType',
+            'httpRequestBody',
+            'httpHeaders',
+            'includeTimestamp',
+            '*',
+          ],
+          'ui:collapsable': {
+            titleKey: 'mqttTopic',
+          },
+          httpRequestBody: {
+            'ui:widget': 'textarea',
+          },
+        },
+      },
+    },
+    mqttToHttp: {
+      'ui:batchMode': true,
+      'ui:order': ['mqttToHttpMappings', '*'],
+      mqttToHttpMappings: {
+        'ui:batchMode': true,
+        items: {
+          'ui:order': [
+            'url',
+            'mqttTopicFilter',
+            'mqttMaxQos',
+            'mqttUserProperties',
+            'httpRequestMethod',
+            'httpRequestTimeoutSeconds',
+            'httpHeaders',
+            '*',
+          ],
+          'ui:collapsable': {
+            titleKey: 'mqttTopicFilter',
+          },
+        },
+      },
+    },
   },
 }
 
-export const MOCK_ADAPTER_HTTP = {
-  id: '678',
-  type: 'http',
+export const MOCK_ADAPTER_HTTP: Adapter = {
+  id: 'test-http',
   config: {
-    url: 'http://s',
-    destination: 'a/valid/topic/http/1',
-    qos: 0,
-    httpRequestMethod: 'GET',
-    httpConnectTimeoutSeconds: 10,
-    httpRequestBodyContentType: 'JSON',
-    httpPublishSuccessStatusCodeOnly: true,
-    httpHeaders: [],
-    id: '678',
-    pollingIntervalMillis: 1000,
-    maxPollingErrorsBeforeRemoval: 10,
-  },
-  adapterRuntimeInformation: {
-    lastStartedAttemptTime: '2023-09-13T09:52:07.554+01',
-    numberOfDaemonProcesses: 1,
-    connectionStatus: {
-      status: 'CONNECTED',
-      id: '678',
-      type: 'adapter',
+    httpConnectTimeoutSeconds: 5,
+    httpToMqtt: {
+      pollingIntervalMillis: 5000,
+      maxPollingErrorsBeforeRemoval: 10,
+      assertResponseIsJson: false,
+      httpPublishSuccessStatusCodeOnly: true,
     },
+    allowUntrustedCertificates: false,
+    id: 'test-http',
   },
+  status: {
+    connection: Status.connection.STATELESS,
+    id: 'test-http',
+    runtime: Status.runtime.STARTED,
+    startedAt: '2025-04-05T20:40:59.871Z',
+    type: 'adapter',
+  },
+  type: 'http',
+}
+
+export const MOCK_SCHEMA_HTTP: TagSchema = {
+  configSchema: {
+    $schema: 'https://json-schema.org/draft/2020-12/schema',
+    type: 'object',
+    properties: {
+      definition: {
+        type: 'object',
+        properties: {
+          httpHeaders: {
+            title: 'HTTP Headers',
+            description: 'HTTP headers to be added to your requests',
+            type: 'array',
+            items: {
+              type: 'object',
+              properties: {
+                name: {
+                  type: 'string',
+                  title: 'Name',
+                  description: 'The name of the HTTP header',
+                },
+                value: {
+                  type: 'string',
+                  title: 'Value',
+                  description: 'The value of the HTTP header',
+                },
+              },
+              required: ['name', 'value'],
+            },
+          },
+          httpRequestBody: {
+            type: 'string',
+            title: 'Http Request Body',
+            description: 'The body to include in the HTTP request',
+          },
+          httpRequestBodyContentType: {
+            type: 'string',
+            enum: ['JSON', 'PLAIN', 'HTML', 'XML', 'YAML'],
+            title: 'Http Request Content Type',
+            description: 'Content Type associated with the request',
+            default: 'JSON',
+          },
+          httpRequestMethod: {
+            type: 'string',
+            enum: ['GET', 'POST', 'PUT'],
+            title: 'Http Method',
+            description: 'Http method associated with the request',
+            default: 'GET',
+          },
+          httpRequestTimeoutSeconds: {
+            type: 'integer',
+            title: 'Http Request Timeout',
+            description: 'Timeout (in seconds) to wait for the HTTP Request to complete',
+            default: 5,
+            minimum: 1,
+            maximum: 60,
+          },
+          url: {
+            type: 'string',
+            title: 'URL',
+            description: 'The url of the HTTP request you would like to make',
+            format: 'uri',
+          },
+        },
+        required: ['url'],
+        title: 'definition',
+        description: 'The actual definition of the tag on the device',
+      },
+      description: {
+        type: 'string',
+        title: 'description',
+        description: 'A human readable description of the tag',
+      },
+      name: {
+        type: 'string',
+        title: 'name',
+        description: 'name of the tag to be used in mappings',
+        format: 'mqtt-tag',
+      },
+    },
+    required: ['definition', 'name'],
+  },
+  protocolId: 'http',
 }
