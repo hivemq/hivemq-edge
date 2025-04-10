2b1f7cfe455f72929dd185a2a032a639404d6fc7
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-04-10 13:57:18 +0100

    test(31400): fix tests

--- hivemq-edge/src/frontend/src/modules/Workspace/utils/group.utils.spec.ts
+++ hivemq-edge/src/frontend/src/modules/Workspace/utils/group.utils.spec.ts
@@ -1,13 +1,10 @@
 import { expect } from 'vitest'
-import { getGroupLayout } from './group.utils.ts'
 import type { Rect } from '@xyflow/react'
-import { MOCK_NODE_ADAPTER } from '@/__test-utils__/react-flow/nodes.ts'
+import { getGroupBounds } from './group.utils.ts'
 
-describe('getGroupLayout', () => {
+describe('getGroupBounds', () => {
   it('should return the layout characteristics of a group', async () => {
-    expect(
-      getGroupLayout([{ ...MOCK_NODE_ADAPTER, position: { x: 0, y: 0 }, width: 50, height: 100 }])
-    ).toStrictEqual<Rect>({
+    expect(getGroupBounds({ x: 0, y: 0, width: 50, height: 100 })).toStrictEqual<Rect>({
       height: 164,
       width: 90,
       x: -20,
