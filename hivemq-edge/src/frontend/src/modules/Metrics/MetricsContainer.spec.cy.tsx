711cf6dc3958e9fe36044058e2b9637b7a2b2578
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-04-14 12:43:46 +0100

    Merge pull request #920
    * fix(32008): add link icons to the dynamic edge
    
    * fix(32008): fix panel header
    
    * fix(32008): fix types
    
    * fix(32008): fix empty list of metrics
    
    * fix(32008): fix error message component
    
    * fix(32008): fix translations
    
    * test(32008): fix tests
    
    * test(32008): fix tests
    
    * test(32008): fix tests
    
    * chore(32008): a bit of cleaning
    
    * chore(32008): ensure E2E videos are always available, for accountabil…
    
    * chore(32008): a bit of cleaning

--- hivemq-edge/src/frontend/src/modules/Metrics/MetricsContainer.spec.cy.tsx
+++ hivemq-edge/src/frontend/src/modules/Metrics/MetricsContainer.spec.cy.tsx
@@ -9,15 +9,16 @@ import { mockBridgeId } from '@/api/hooks/useGetBridges/__handlers__'
 
 describe('MetricsContainer', () => {
   beforeEach(() => {
-    cy.viewport(800, 800)
-    cy.intercept('/api/v1/metrics', { items: MOCK_METRICS } as MetricList)
+    cy.viewport(600, 800)
+    cy.intercept('/api/v1/metrics/**/*', { statusCode: 404, log: false })
+    cy.intercept('/api/v1/metrics', { items: MOCK_METRICS }).as('getMetrics')
   })
 
   it('should render the collapsible component', () => {
     cy.mountWithProviders(
       <MetricsContainer
         nodeId="bridge@bridge-id-01"
-        initMetrics={[]}
+        initMetrics={[MOCK_METRICS[0].name as string, MOCK_METRICS[1].name as string]}
         filters={[
           {
             id: mockBridgeId,
@@ -36,5 +37,47 @@ describe('MetricsContainer', () => {
 
     cy.getByTestId('metrics-toggle').click()
     cy.get('div#metrics-select-container').should('not.be.visible')
+  })
+
+  it('should render message when no metrics', () => {
+    cy.intercept('/api/v1/metrics**', { items: [] } as MetricList).as('getMetrics')
+
+    cy.mountWithProviders(
+      <MetricsContainer
+        nodeId="bridge@bridge-id-02"
+        initMetrics={[]}
+        filters={[
+          {
+            id: 'bridge@bridge-id-02',
+            type: 'com.hivemq.edge.bridge',
+          },
+        ]}
+        type={NodeTypes.BRIDGE_NODE}
+      />
+    )
+
+    cy.wait('@getMetrics')
+    cy.getByTestId('metrics-toggle').should('be.disabled')
+    cy.get('[role="alert"]')
+      .should('have.attr', 'data-status', 'info')
+      .should('have.text', 'There is no metrics available for this entity')
+  })
+
+  it('should be accessible', () => {
+    cy.injectAxe()
+    cy.mountWithProviders(
+      <MetricsContainer
+        nodeId="bridge@bridge-id-01"
+        initMetrics={[MOCK_METRICS[0].name as string, MOCK_METRICS[1].name as string]}
+        filters={[
+          {
+            id: mockBridgeId,
+            type: 'com.hivemq.edge.bridge',
+          },
+        ]}
+        type={NodeTypes.BRIDGE_NODE}
+      />
+    )
+    cy.checkAccessibility()
   })
 })
