1bd8d1ffaa6687a8092ce9e87f5b2ebd1e36c98c
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-04-14 07:19:01 +0100

    fix(32008): add link icons to the dynamic edge

--- hivemq-edge/src/frontend/src/modules/Workspace/components/edges/DynamicEdge.tsx
+++ hivemq-edge/src/frontend/src/modules/Workspace/components/edges/DynamicEdge.tsx
@@ -1,11 +1,53 @@
+import { HStack } from '@chakra-ui/react'
 import type { FC } from 'react'
-import { type EdgeProps, getBezierPath, useInternalNode } from '@xyflow/react'
+import { useMemo } from 'react'
+import {
+  BaseEdge,
+  EdgeLabelRenderer,
+  type EdgeProps,
+  getBezierPath,
+  useInternalNode,
+  useNodesData,
+} from '@xyflow/react'
 
 import { getEdgeParams } from '@/modules/Workspace/utils/edge.utils'
+import { useNavigate } from 'react-router-dom'
+import { DataHubNodeType } from '../../../../extensions/datahub/types'
+import { useGetPoliciesMatching } from '../../hooks/useGetPoliciesMatching'
+import { NodeTypes } from '../../types'
+import DataPolicyEdgeCTA from './DataPolicyEdgeCTA'
+import ObservabilityEdgeCTA from './ObservabilityEdgeCTA'
 
 export const DynamicEdge: FC<EdgeProps> = ({ id, source, target, markerEnd, style }) => {
   const sourceNode = useInternalNode(source)
   const targetNode = useInternalNode(target)
+  const policies = useGetPoliciesMatching(source)
+  const navigate = useNavigate()
+  const nodeData = useNodesData(target)
+
+  const isObservable = useMemo(() => {
+    if (!nodeData) return false
+    const { type } = nodeData
+    return type === NodeTypes.EDGE_NODE
+  }, [nodeData])
+
+  const policyRoutes = useMemo(() => {
+    if (!policies) return undefined
+
+    return policies.map((policy) => `/datahub/${DataHubNodeType.DATA_POLICY}/${policy.id}`)
+  }, [policies])
+
+  const handleOpenObservability = () => {
+    navigate(`/workspace/link/${id}`)
+  }
+
+  const handleShowPolicy = (route: string) => {
+    navigate(route)
+  }
+
+  const handleShowAllPolicies = () => {
+    navigate('/datahub')
+  }
 
   if (!sourceNode || !targetNode) {
     return null
@@ -13,7 +55,7 @@ export const DynamicEdge: FC<EdgeProps> = ({ id, source, target, markerEnd, styl
 
   const { sx, sy, tx, ty, sourcePos, targetPos } = getEdgeParams(sourceNode, targetNode)
 
-  const [edgePath] = getBezierPath({
+  const [edgePath, labelX, labelY] = getBezierPath({
     sourceX: sx,
     sourceY: sy,
     sourcePosition: sourcePos,
@@ -22,5 +64,33 @@ export const DynamicEdge: FC<EdgeProps> = ({ id, source, target, markerEnd, styl
     targetY: ty,
   })
 
-  return <path id={id} className="react-flow__edge-path" d={edgePath} markerEnd={markerEnd} style={style} />
+  return (
+    <>
+      <BaseEdge id={id} path={edgePath} markerEnd={markerEnd} style={style} />
+      {isObservable && (
+        <EdgeLabelRenderer>
+          <HStack
+            sx={{
+              position: 'absolute',
+              transform: `translate(-50%, -50%) translate(${labelX}px,${labelY}px)`,
+              pointerEvents: 'all',
+            }}
+            className="nodrag nopan"
+          >
+            <ObservabilityEdgeCTA source={source} style={style} onClick={handleOpenObservability} />
+            {policyRoutes && (
+              <DataPolicyEdgeCTA
+                data-testid="policy-panel-trigger"
+                style={style}
+                policyRoutes={policyRoutes}
+                onClickPolicy={handleShowPolicy}
+                onClickAll={handleShowAllPolicies}
+              />
+            )}
+          </HStack>
+        </EdgeLabelRenderer>
+      )}
+    </>
+  )
+  // return <path id={id} className="react-flow__edge-path" d={edgePath} markerEnd={markerEnd} style={style} />
 }
