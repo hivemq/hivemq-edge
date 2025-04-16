11c8663bc30054093abc26d879b682aa2dc0df13
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-04-16 15:43:07 +0100

    refactor(32144): add visual cues to the focus lock

--- hivemq-edge/src/frontend/src/hooks/useAccessibleDraggable/AccessibleDraggableLock.tsx
+++ hivemq-edge/src/frontend/src/hooks/useAccessibleDraggable/AccessibleDraggableLock.tsx
@@ -1,8 +1,21 @@
-import { FocusLock } from '@chakra-ui/react'
 import { type FC, type PropsWithChildren } from 'react'
+import { FocusLock, chakra as Chakra, type BoxProps } from '@chakra-ui/react'
+
+import config from '@/config'
+
 import { useAccessibleDraggable } from './useAccessibleDraggable'
 
 export const AccessibleDraggableLock: FC<PropsWithChildren> = ({ children }) => {
   const { isDragging } = useAccessibleDraggable()
-  return <FocusLock isDisabled={!isDragging}>{children}</FocusLock>
+
+  const devStyle: Partial<BoxProps> = {
+    backgroundColor: 'red',
+    borderColor: 'red',
+    borderWidth: 2,
+  }
+  return (
+    <FocusLock isDisabled={!isDragging}>
+      <Chakra.div {...(isDragging && config.isDevMode && devStyle)}>{children}</Chakra.div>
+    </FocusLock>
+  )
 }
