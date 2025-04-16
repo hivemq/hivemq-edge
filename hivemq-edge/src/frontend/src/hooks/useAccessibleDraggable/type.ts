3722d9e91db5cbec4e6328ae0d52f138da74166c
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-04-16 15:42:02 +0100

    refactor(32144): update types

--- hivemq-edge/src/frontend/src/hooks/useAccessibleDraggable/type.ts
+++ hivemq-edge/src/frontend/src/hooks/useAccessibleDraggable/type.ts
@@ -1,5 +1,16 @@
+import type { DataReference } from '@/api/hooks/useDomainModel/useGetCombinedDataSchemas'
+import type { FlatJSONSchema7 } from '@/components/rjsf/MqttTransformation/utils/json-schema.utils'
+
 export interface AccessibleDraggableProps {
   isDragging: boolean
-  startDragging: () => void
+  startDragging: (data: { property: FlatJSONSchema7; dataReference?: DataReference | undefined }) => void
   endDragging: () => void
+}
+
+export const EDGE_HOTKEY = {
+  ENTER: 'Enter',
+  BACKSPACE: 'Backspace',
+  DELETE: 'Delete',
+  ESC: 'ESC',
+  ESCAPE: 'Escape',
 }
