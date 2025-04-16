b3dbb69e6eb8c6fff9b8c87d8183664a20272695
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-04-16 12:26:05 +0100

    feat(32144): add activation/deactivation of focus trapping

--- hivemq-edge/src/frontend/src/hooks/useAccessibleDraggable/AccessibleDraggableProvider.tsx
+++ hivemq-edge/src/frontend/src/hooks/useAccessibleDraggable/AccessibleDraggableProvider.tsx
@@ -1,13 +1,46 @@
-import { type FC, type PropsWithChildren, useState } from 'react'
+import { type FC, type PropsWithChildren, useCallback, useEffect, useState } from 'react'
+import { useToast } from '@chakra-ui/react'
+import { useHotkeys } from 'react-hotkeys-hook'
+
+import { DATAHUB_HOTKEY } from '@/extensions/datahub/utils/datahub.utils'
 import { AccessibleDraggableContext, type AccessibleDraggableProps } from './index'
+
+const TOAST_DRAGGABLE = 'TOAST_DRAGGABLE'
 
 export const AccessibleDraggableProvider: FC<PropsWithChildren> = ({ children }) => {
   const [isDragging, setIsDragging] = useState(false)
+  const toast = useToast({ id: TOAST_DRAGGABLE })
+
+  const onStartDragging = useCallback(() => {
+    setIsDragging(true)
+    if (!toast.isActive(TOAST_DRAGGABLE)) toast({ duration: null, isClosable: true, description: `Dragging is active` })
+  }, [toast])
+
+  const onEndDragging = useCallback(() => {
+    setIsDragging(false)
+    toast.close(TOAST_DRAGGABLE)
+  }, [toast])
+
+  // TODO[NVL] ESCAPE is caught by Chakra (for closing the modals) and cannot be intercepted yet; reverting to BACKSPACE
+  useHotkeys([DATAHUB_HOTKEY.ESCAPE, DATAHUB_HOTKEY.BACKSPACE], () => {
+    onEndDragging()
+  })
+
+  useEffect(() => {
+    const handleMouseDown = () => {
+      onEndDragging()
+    }
+
+    document.addEventListener('mousedown', handleMouseDown)
+    return () => {
+      document.removeEventListener('mousedown', handleMouseDown)
+    }
+  }, [onEndDragging])
 
   const defaultAccessibleDraggableValues: AccessibleDraggableProps = {
     isDragging: isDragging,
-    startDragging: () => setIsDragging(true),
-    endDragging: () => setIsDragging(false),
+    startDragging: onStartDragging,
+    endDragging: onEndDragging,
   }
 
   return (
