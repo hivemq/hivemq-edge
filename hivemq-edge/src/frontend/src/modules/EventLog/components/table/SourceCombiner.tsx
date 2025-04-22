dd9d7fb46f9ad27105d8047661578f68efab14df
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-04-22 16:58:31 +0100

    feat(32398): add a custom renderer for combiner sources

---
+++ hivemq-edge/src/frontend/src/modules/EventLog/components/table/SourceCombiner.tsx
@@ -0,0 +1,31 @@
+import type { FC } from 'react'
+import { Link as ReactRouterLink } from 'react-router-dom'
+import { Icon, Link as ChakraLink, Text } from '@chakra-ui/react'
+
+import type { TypeIdentifier } from '@/api/__generated__'
+import { useGetCombiner } from '@/api/hooks/useCombiners'
+import { HqCombiner } from '@/components/Icons'
+
+interface SourceLinkProps {
+  source: TypeIdentifier
+}
+
+export const SourceCombiner: FC<SourceLinkProps> = ({ source }) => {
+  const { data, isSuccess } = useGetCombiner(source.identifier as string)
+
+  const isExisting = isSuccess && Boolean(data)
+  if (!isExisting) return <Text>{source?.identifier}</Text>
+
+  const visibleName = isSuccess && data.name ? data?.name : source?.identifier
+  return (
+    <ChakraLink
+      as={ReactRouterLink}
+      to={`/workspace/combiner/${source.identifier}`}
+      whiteSpace="nowrap"
+      display="inline-flex"
+    >
+      <Icon as={HqCombiner} mr={2} fontSize="1rem" />
+      <Text>{visibleName}</Text>
+    </ChakraLink>
+  )
+}
