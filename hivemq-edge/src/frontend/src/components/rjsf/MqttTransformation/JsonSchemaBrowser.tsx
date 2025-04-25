393bf74007644b8c2662e53a01236489586866e5
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-04-25 11:37:39 +0100

    Merge pull request #941
    * fix(31371): add custom rendering for the selected tags/topic filters
    
    * fix(31371): improve layout of the schemas and the messages
    
    * fix(31371): add light variant for the alerts
    
    * test(31371): fix tests
    
    * fix(31371): fic header of schemas

--- hivemq-edge/src/frontend/src/components/rjsf/MqttTransformation/JsonSchemaBrowser.tsx
+++ hivemq-edge/src/frontend/src/components/rjsf/MqttTransformation/JsonSchemaBrowser.tsx
@@ -3,7 +3,9 @@ import type { JSONSchema7 } from 'json-schema'
 import type { ListProps } from '@chakra-ui/react'
 import { Heading, List, ListItem } from '@chakra-ui/react'
 
+import { DataIdentifierReference } from '@/api/__generated__'
 import type { DataReference } from '@/api/hooks/useDomainModel/useGetCombinedDataSchemas'
+import { PLCTag, TopicFilter } from '@/components/MQTT/EntityTag'
 import { getPropertyListFrom } from '@/components/rjsf/MqttTransformation/utils/json-schema.utils.ts'
 import PropertyItem from '@/components/rjsf/MqttTransformation/components/schema/PropertyItem.tsx'
 
@@ -11,6 +13,7 @@ interface JsonSchemaBrowserProps extends ListProps {
   schema: JSONSchema7
   isDraggable?: boolean
   hasExamples?: boolean
+  isTagShown?: boolean
   dataReference?: DataReference
 }
 
@@ -18,6 +21,7 @@ const JsonSchemaBrowser: FC<JsonSchemaBrowserProps> = ({
   schema,
   isDraggable = false,
   hasExamples = false,
+  isTagShown = false,
   dataReference,
   ...props
 }) => {
@@ -29,7 +33,13 @@ const JsonSchemaBrowser: FC<JsonSchemaBrowserProps> = ({
     <>
       {schema.title && (
         <Heading as="h3" size="sm">
-          {schema.title}
+          {!isTagShown && schema.title}
+          {isTagShown && dataReference?.type === DataIdentifierReference.type.TAG && (
+            <PLCTag tagTitle={dataReference?.id} mr={3} />
+          )}
+          {isTagShown && dataReference?.type === DataIdentifierReference.type.TOPIC_FILTER && (
+            <TopicFilter tagTitle={dataReference?.id} mr={3} />
+          )}
         </Heading>
       )}
       <List {...props}>
