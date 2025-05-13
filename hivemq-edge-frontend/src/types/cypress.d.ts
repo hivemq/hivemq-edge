4398cb040bf9390b4119109c41e3515833025595
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-05-13 16:26:34 +0100

    chore(30540): add duplicate for testing

---
+++ hivemq-edge-frontend/src/types/cypress.d.ts
@@ -0,0 +1,27 @@
+/// <reference types="cypress" />
+import type { ContextObject } from 'axe-core'
+import type { Options } from 'cypress-axe'
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      getByTestId(value: string): Chainable<JQuery<HTMLElement>>
+      getByAriaLabel(value: string): Chainable<JQuery<HTMLElement>>
+      checkAccessibility(
+        context?: string | Node | ContextObject | undefined,
+        options?: Options | undefined,
+        skipFailures?: boolean
+      ): Chainable<JQuery<HTMLElement>>
+      clearInterceptList(interceptAlias: string): Chainable<JQuery<HTMLElement>>
+      mount(jsx: React__default.ReactNode, options?: MountOptions, rerenderKey?: string): Cypress.Chainable<MountReturn>
+      mountWithProviders(
+        component: React.ReactNode,
+        options?: MountOptions & { routerProps?: MemoryRouterProps } & {
+          wrapper?: React.JSXElementConstructor<{ children: React.ReactNode }>
+        }
+      ): Cypress.Chainable<MountReturn>
+    }
+  }
+}
+
+export {}
