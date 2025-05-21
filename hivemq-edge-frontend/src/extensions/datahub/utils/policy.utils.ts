5a3c983706d256c9f35eb0845d768b1347600c3b
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-05-21 11:14:40 +0100

    refactor(32894): refactor the grouping utility as generic

--- hivemq-edge-frontend/src/extensions/datahub/utils/policy.utils.ts
+++ hivemq-edge-frontend/src/extensions/datahub/utils/policy.utils.ts
@@ -1,15 +1,19 @@
-import type { PolicySchema, SchemaList } from '../../../api/__generated__'
-
-export interface PolicySchemaExpanded extends PolicySchema {
-  children?: PolicySchema[]
+// TODO[OpenAPI] This MUST be the common properties of DataHub resources, i.e. Script and Schema
+export interface ResourceBase {
+  id: string
+  readonly version?: number
 }
 
-export const groupResourceItems = (data: SchemaList | undefined) => {
+export type ExpandableGroupedResource<T extends ResourceBase> = T & {
+  children?: T[]
+}
+
+export const groupResourceItems = <T extends { items?: Array<U> }, U extends ResourceBase>(data: T | undefined) => {
   if (!data || !data.items || !data.items.length) return []
 
   // TODO[33008] Update Node to 21; Object.groupBy is not supported in Node 18 or 20
   // const schemasGroupedById = Object.groupBy(data.items, (schema) => schema.id)
-  const schemasGroupedById = data.items.reduce<Record<string, PolicySchema[]>>((acc, schema) => {
+  const schemasGroupedById = data.items.reduce<Record<string, U[]>>((acc, schema) => {
     if (!acc[schema.id]) {
       acc[schema.id] = []
     }
@@ -19,19 +23,19 @@ export const groupResourceItems = (data: SchemaList | undefined) => {
 
   const schemaGroups = Object.entries(schemasGroupedById).filter(([, schemas]) => schemas !== undefined) as [
     string,
-    PolicySchema[],
+    U[],
   ][]
 
   return schemaGroups.map(([, schemas]) => {
-    if (schemas.length === 1) return schemas.at(0) as PolicySchemaExpanded
+    if (schemas.length === 1) return schemas.at(0) as ExpandableGroupedResource<U>
 
-    const firstSchema = schemas.at(0) as PolicySchema
-    const lastSchema = schemas.at(-1) as PolicySchema
+    const firstSchema = schemas.at(0) as U
+    const lastSchema = schemas.at(-1) as U
 
     return {
       ...firstSchema,
       version: lastSchema.version, // replace the version with the latest one
       children: schemas,
-    } as PolicySchemaExpanded
+    } as ExpandableGroupedResource<U>
   })
 }
