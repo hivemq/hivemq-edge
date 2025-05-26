603edf60b5c14325915167a9cdf821f6d7118053
Sam Cao <sjtucaocao@gmail.com>
2025-05-26 16:39:17 +0200

    feat: Add equals(), hashCode(), toString()

--- hivemq-edge/src/main/java/com/hivemq/api/errors/common/RequestBodyMissingError.java
+++ hivemq-edge/src/main/java/com/hivemq/api/errors/common/RequestBodyMissingError.java
@@ -1,14 +1,14 @@
 package com.hivemq.api.errors.common;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.hivemq.api.errors.Error;
+import com.hivemq.api.errors.ErrorBase;
 import com.hivemq.http.HttpStatus;
 import io.swagger.v3.oas.annotations.media.Schema;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
 
-public class RequestBodyMissingError extends Error<RequestBodyMissingError> {
+public class RequestBodyMissingError extends ErrorBase<RequestBodyMissingError> {
     @JsonProperty("parameterName")
     @Schema(description = "Correlation id")
     protected @NotNull String parameterName;
@@ -29,5 +29,45 @@ public class RequestBodyMissingError extends Error<RequestBodyMissingError> {
     public @NotNull RequestBodyMissingError setParameterName(final @NotNull String parameterName) {
         this.parameterName = Objects.requireNonNull(parameterName);
         return this;
+    }
+
+    @Override
+    public @NotNull String toString() {
+        return "RequestBodyMissingError{" +
+                "parameterName='" +
+                parameterName +
+                '\'' +
+                ", code='" +
+                code +
+                '\'' +
+                ", detail='" +
+                detail +
+                '\'' +
+                ", status=" +
+                status +
+                ", title='" +
+                title +
+                '\'' +
+                ", type='" +
+                type +
+                '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        if (!super.equals(o)) {
+            return false;
+        }
+        final RequestBodyMissingError that = (RequestBodyMissingError) o;
+        return Objects.equals(parameterName, that.parameterName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), parameterName);
     }
 }
