d35fe1af9e2b2a9affbac1cce2d242849fd9694d
Sam Cao <sjtucaocao@gmail.com>
2025-05-26 09:59:18 +0200

    feat: Add path, rule, title to ErrorDataPolicyValidation

--- hivemq-edge/src/main/java/com/hivemq/http/error/ErrorDataPolicyValidation.java
+++ hivemq-edge/src/main/java/com/hivemq/http/error/ErrorDataPolicyValidation.java
@@ -16,10 +16,24 @@
 
 package com.hivemq.http.error;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class ErrorDataPolicyValidation extends Error {
+    private static final String REQUIRED_FIELD_MISSING = "Required field missing";
+    @JsonProperty("path")
+    @Schema(description = "The path of the issue")
+    protected @Nullable String path;
+    @JsonProperty("rule")
+    @Schema(description = "The rule of the issue")
+    protected @Nullable ErrorDataPolicyValidationRule rule;
+    @JsonProperty("title")
+    @Schema(description = "The title of the issue")
+    protected @Nullable String title;
+
     public ErrorDataPolicyValidation(final @NotNull String detail, final @Nullable String parameter) {
         super(detail, parameter);
     }
@@ -29,6 +43,50 @@ public class ErrorDataPolicyValidation extends Error {
     }
 
     public static @NotNull ErrorDataPolicyValidation missingField(final @NotNull String field) {
-        return new ErrorDataPolicyValidation(String.format("Required field '%s' is missing", field), field);
+        return new ErrorDataPolicyValidation(String.format("Required field '%s' is missing", field), field).setTitle(
+                REQUIRED_FIELD_MISSING).setRule(ErrorDataPolicyValidationRule.MissingField);
+    }
+
+    public @Nullable String getPath() {
+        return path;
+    }
+
+    public @NotNull ErrorDataPolicyValidation setPath(@Nullable final String path) {
+        this.path = path;
+        return this;
+    }
+
+    public @Nullable ErrorDataPolicyValidationRule getRule() {
+        return rule;
+    }
+
+    public @NotNull ErrorDataPolicyValidation setRule(@Nullable final ErrorDataPolicyValidationRule rule) {
+        this.rule = rule;
+        return this;
+    }
+
+    public @Nullable String getTitle() {
+        return title;
+    }
+
+    public @NotNull ErrorDataPolicyValidation setTitle(@Nullable final String title) {
+        this.title = title;
+        return this;
+    }
+
+    public enum ErrorDataPolicyValidationRule {
+        MissingField("data-policy-validation-missing-field"),
+        ;
+
+        private final @NotNull String displayName;
+
+        ErrorDataPolicyValidationRule(@NotNull final String displayName) {
+            this.displayName = displayName;
+        }
+
+        @JsonValue
+        public @NotNull String getDisplayName() {
+            return displayName;
+        }
     }
 }
