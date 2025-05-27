d3e105187c776a951350809da6dea1b012e25dae
Sam Cao <sjtucaocao@gmail.com>
2025-05-27 09:31:36 +0200

    refactor: Add type auto-generation

---
+++ hivemq-edge/src/main/java/com/hivemq/api/errors/datahub/DataPolicyValidationErrors.java
@@ -0,0 +1,52 @@
+package com.hivemq.api.errors.datahub;
+
+import com.hivemq.api.errors.validation.ValidationError;
+import com.hivemq.api.errors.validation.ValidationErrors;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class DataPolicyValidationErrors extends ValidationErrors<DataPolicyValidationErrors> {
+    protected DataPolicyValidationErrors(
+            final @NotNull String title,
+            final @Nullable String detail,
+            final @Nullable List<ValidationError<?>> errors,
+            final int status,
+            final @Nullable String code) {
+        super(title, detail, errors, status, code);
+    }
+
+    public static DataPolicyValidationErrors of(final @NotNull String title, final @Nullable String detail) {
+        return new DataPolicyValidationErrors(title, detail, null, 400, null);
+    }
+
+    public static DataPolicyValidationErrors of(
+            final @NotNull String title,
+            final @Nullable String detail,
+            final @Nullable List<ValidationError<?>> errors) {
+        return new DataPolicyValidationErrors(title, detail, errors, 400, null);
+    }
+
+    @Override
+    public @NotNull String toString() {
+        return "DataPolicyValidationErrors{" +
+                "errors=" +
+                errors +
+                ", code='" +
+                code +
+                '\'' +
+                ", detail='" +
+                detail +
+                '\'' +
+                ", status=" +
+                status +
+                ", title='" +
+                title +
+                '\'' +
+                ", type='" +
+                type +
+                '\'' +
+                '}';
+    }
+}
