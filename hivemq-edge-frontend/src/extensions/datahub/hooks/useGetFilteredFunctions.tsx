19f28f6a3d1e858d93f671af57c23363c869c2e8
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-05-27 17:57:29 +0100

    refactor(29945): Add the DataHub.transform function to the list

--- hivemq-edge-frontend/src/extensions/datahub/hooks/useGetFilteredFunctions.tsx
+++ hivemq-edge-frontend/src/extensions/datahub/hooks/useGetFilteredFunctions.tsx
@@ -1,4 +1,3 @@
-import { useGetAllFunctions } from '@datahub/api/hooks/DataHubFunctionsService/useGetAllFunctions.ts'
 import { useMemo } from 'react'
 
 import { BehaviorPolicyTransitionEvent, type FunctionSpecs } from '@/api/__generated__'
@@ -44,15 +43,8 @@ export const useGetFilteredFunction = (
   transition?: BehaviorPolicyTransitionEvent
 ) => {
   const { isError, error, isLoading, isSuccess, data } = useGetAllFunctionSpecs()
-  const { data: xxxxxx } = useGetAllFunctions()
 
   const filteredFunctions = useMemo(() => {
-    console.log(
-      'XXXX filteredFunctions 1',
-      xxxxxx,
-      data?.items?.map((e) => e.metadata)
-    )
-
     if (!data || !data.items?.length) return []
 
     return data.items.filter((functionSpec) => {
