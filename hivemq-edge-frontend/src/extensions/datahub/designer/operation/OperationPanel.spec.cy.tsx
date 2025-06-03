962fc05d2106df537d115b9efd0ed45311b66918
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-06-03 10:52:30 +0100

    Merge pull request #998
    * refactor(29945): update openAPI specs
    
    * refactor(29945): fix mocks
    
    * feat(29945): add filtering hook for functions
    
    * refactor(29945): Add the DataHub.transform function to the list
    
    * chore(29945): fix translations
    
    * test(29945): fix tests
    
    * chore(29945): revert fix
    
    * fix(24800): fix the format of the topic property
    
    * fix(24800): fix the widget for the topic property
    
    * test(24800): fix tests
    
    * fix(24800): fix rebase

--- hivemq-edge-frontend/src/extensions/datahub/designer/operation/OperationPanel.spec.cy.tsx
+++ hivemq-edge-frontend/src/extensions/datahub/designer/operation/OperationPanel.spec.cy.tsx
@@ -195,7 +195,7 @@ describe('OperationPanel', () => {
       },
     }
 
-    it('should render the form', () => {
+    it.only('should render the form', () => {
       cy.mountWithProviders(<OperationPanel selectedNode="my-node" />, {
         wrapper: getWrapperWith([node]),
       })
