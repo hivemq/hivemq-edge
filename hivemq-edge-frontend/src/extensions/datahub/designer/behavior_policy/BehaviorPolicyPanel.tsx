6564266fb1f4692f056967a24e56618e632b5544
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-06-04 21:05:35 +0100

    Merge pull request #999
    * fix(24801): fix warnings with unsupported formats from AJV
    
    * fix(24801): fix warnings with unsupported formats from AJV
    
    * fix(24801): redesign the schema for the maxPublish option
    
    * fix(24801): refactor the min and max publishes
    
    * fix(24801): add types for the Publish Quota model
    
    * fix(24801): add custom validation for publish arguments
    
    * test(24801): add tests
    
    * test(24801): fix tests
    
    * fix(24801): fix translations
    
    * fix(24801): fix constant

--- hivemq-edge-frontend/src/extensions/datahub/designer/behavior_policy/BehaviorPolicyPanel.tsx
+++ hivemq-edge-frontend/src/extensions/datahub/designer/behavior_policy/BehaviorPolicyPanel.tsx
@@ -5,13 +5,17 @@ import { useTranslation } from 'react-i18next'
 import { Card, CardBody } from '@chakra-ui/react'
 import type { CustomValidator } from '@rjsf/utils'
 
-import type { BehaviorPolicyData, PanelProps } from '@datahub/types.ts'
+import ErrorMessage from '@/components/ErrorMessage.tsx'
+
 import { useGetAllBehaviorPolicies } from '@datahub/api/hooks/DataHubBehaviorPoliciesService/useGetAllBehaviorPolicies.ts'
 import { ReactFlowSchemaForm } from '@datahub/components/forms/ReactFlowSchemaForm.tsx'
 import { MOCK_BEHAVIOR_POLICY_SCHEMA } from '@datahub/designer/behavior_policy/BehaviorPolicySchema.ts'
 import useDataHubDraftStore from '@datahub/hooks/useDataHubDraftStore.ts'
 import { usePolicyGuards } from '@datahub/hooks/usePolicyGuards.ts'
-import ErrorMessage from '@/components/ErrorMessage.tsx'
+import type { BehaviorPolicyData, PanelProps, PublishQuotaArguments } from '@datahub/types.ts'
+import { BehaviorPolicyType } from '@datahub/types.ts'
+
+const UNLIMITED_PUBLISH = -1
 
 export const BehaviorPolicyPanel: FC<PanelProps> = ({ selectedNode, onFormSubmit }) => {
   const { t } = useTranslation('datahub')
@@ -29,6 +33,17 @@ export const BehaviorPolicyPanel: FC<PanelProps> = ({ selectedNode, onFormSubmit
     else {
       const isIdNotUnique = Boolean(allPolicies.items?.find((e) => e.id === formData?.id))
       if (isIdNotUnique) errors['id']?.addError(t('error.validation.behaviourPolicy.notUnique'))
+    }
+    if (formData?.model === BehaviorPolicyType.PUBLISH_QUOTA) {
+      const { maxPublishes, minPublishes } = formData.arguments as PublishQuotaArguments
+      if (maxPublishes !== UNLIMITED_PUBLISH && maxPublishes < minPublishes) {
+        errors?.['arguments']?.['maxPublishes']?.addError(
+          t('error.validation.behaviourPolicy.publishQuota.maxLessThanMin')
+        )
+        errors?.['arguments']?.['minPublishes']?.addError(
+          t('error.validation.behaviourPolicy.publishQuota.minMoreThanMax')
+        )
+      }
     }
     return errors
   }
