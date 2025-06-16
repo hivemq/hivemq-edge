db87d9bbd351f4e25d5d59411b09d458ee667a49
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-06-16 16:17:56 +0100

    test(33325): add tests

--- hivemq-edge-frontend/src/extensions/datahub/components/interpolation/SuggestionList.spec.cy.tsx
+++ hivemq-edge-frontend/src/extensions/datahub/components/interpolation/SuggestionList.spec.cy.tsx
@@ -11,19 +11,21 @@ const mockSuggestionListProps: SuggestionListProps = {
   query: '',
 }
 
-const wrapper: React.JSXElementConstructor<{ children: React.ReactNode }> = ({ children }) => (
-  <MockStoreWrapper
-    config={{
-      initialState: {
-        status: DesignerStatus.DRAFT,
-        type: DataHubNodeType.DATA_POLICY,
-      },
-    }}
-  >
-    {children}
-  </MockStoreWrapper>
-)
-
+const getWrapperFor = (type: DataHubNodeType.DATA_POLICY | DataHubNodeType.BEHAVIOR_POLICY) => {
+  const wrapper: React.JSXElementConstructor<{ children: React.ReactNode }> = ({ children }) => (
+    <MockStoreWrapper
+      config={{
+        initialState: {
+          status: DesignerStatus.DRAFT,
+          type: type || DataHubNodeType.DATA_POLICY,
+        },
+      }}
+    >
+      {children}
+    </MockStoreWrapper>
+  )
+  return wrapper
+}
 describe('SuggestionList', () => {
   beforeEach(() => {
     cy.viewport(400, 600)
@@ -31,7 +33,9 @@ describe('SuggestionList', () => {
 
   it('should render loading states', () => {
     cy.intercept('/api/v1/data-hub/interpolation-variables', { statusCode: 404 }).as('getVariables')
-    cy.mountWithProviders(<SuggestionList {...mockSuggestionListProps} command={cy.stub} />, { wrapper })
+    cy.mountWithProviders(<SuggestionList {...mockSuggestionListProps} command={cy.stub} />, {
+      wrapper: getWrapperFor(DataHubNodeType.DATA_POLICY),
+    })
 
     cy.wait('@getVariables')
 
@@ -47,7 +51,9 @@ describe('SuggestionList', () => {
 
   it('should render variables for DATA POLICY', () => {
     cy.intercept('/api/v1/data-hub/interpolation-variables', MOCK_INTERPOLATION_VARIABLES).as('getVariables')
-    cy.mountWithProviders(<SuggestionList {...mockSuggestionListProps} command={cy.stub} />, { wrapper })
+    cy.mountWithProviders(<SuggestionList {...mockSuggestionListProps} command={cy.stub} />, {
+      wrapper: getWrapperFor(DataHubNodeType.DATA_POLICY),
+    })
 
     cy.wait('@getVariables')
 
@@ -66,11 +72,38 @@ describe('SuggestionList', () => {
     })
   })
 
+  it('should render variables for BEHAVIOUR POLICY', () => {
+    cy.intercept('/api/v1/data-hub/interpolation-variables', MOCK_INTERPOLATION_VARIABLES).as('getVariables')
+    cy.mountWithProviders(<SuggestionList {...mockSuggestionListProps} command={cy.stub} />, {
+      wrapper: getWrapperFor(DataHubNodeType.BEHAVIOR_POLICY),
+    })
+
+    cy.wait('@getVariables')
+
+    cy.getByTestId('interpolation-container').should('be.visible')
+    cy.getByTestId('interpolation-container').within(() => {
+      cy.get('button').as('getVariables')
+      cy.get('@getVariables').should('have.length', 6)
+      cy.get('@getVariables').eq(0).should('contain.text', 'clientId').should('have.attr', 'aria-selected', 'true')
+      cy.get('@getVariables')
+        .eq(0)
+        .within(() => {
+          cy.getByTestId('suggestion-description').should('contain.text', 'The MQTT client ID')
+        })
+
+      cy.get('@getVariables').eq(1).should('contain.text', 'policyId')
+      cy.get('@getVariables').eq(2).should('contain.text', 'fromState')
+      cy.get('@getVariables').eq(3).should('contain.text', 'toState')
+    })
+  })
+
   it('should select the variable', () => {
     const command = cy.stub().as('onCommand')
     cy.intercept('/api/v1/data-hub/interpolation-variables', MOCK_INTERPOLATION_VARIABLES).as('getVariables')
 
-    cy.mountWithProviders(<SuggestionList {...mockSuggestionListProps} command={command} />, { wrapper })
+    cy.mountWithProviders(<SuggestionList {...mockSuggestionListProps} command={command} />, {
+      wrapper: getWrapperFor(DataHubNodeType.DATA_POLICY),
+    })
     cy.wait('@getVariables')
 
     cy.getByTestId('interpolation-container').should('be.visible')
@@ -86,7 +119,9 @@ describe('SuggestionList', () => {
   it('should be accessible', () => {
     cy.injectAxe()
     cy.intercept('/api/v1/data-hub/interpolation-variables', MOCK_INTERPOLATION_VARIABLES).as('getVariables')
-    cy.mountWithProviders(<SuggestionList {...mockSuggestionListProps} command={cy.stub} />, { wrapper })
+    cy.mountWithProviders(<SuggestionList {...mockSuggestionListProps} command={cy.stub} />, {
+      wrapper: getWrapperFor(DataHubNodeType.DATA_POLICY),
+    })
     cy.wait('@getVariables')
 
     cy.checkAccessibility()
