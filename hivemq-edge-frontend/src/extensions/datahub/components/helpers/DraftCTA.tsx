7e49c69344ff1c67be68ffafd68e14db67d0f76f
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-06-18 17:13:02 +0100

    Merge pull request #1020
    * refactor(33990): add a custom footer to the modal
    
    * fix(33990): fix layout
    
    * refactor(33990): add a replace confirmation to the create new draft
    
    * refactor(33990): fix translations
    
    * test(33990): add tests
    
    * test(33990): add tests
    
    * test(33990): refactor the test wrapper
    
    * test(33990): fix the wrappers
    
    * test(33990): fix the wrappers
    
    * fix(33990): fix styling
    
    * fix(33990): fix translations

--- hivemq-edge-frontend/src/extensions/datahub/components/helpers/DraftCTA.tsx
+++ hivemq-edge-frontend/src/extensions/datahub/components/helpers/DraftCTA.tsx
@@ -1,8 +1,10 @@
 import type { FC } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useNavigate } from 'react-router-dom'
-import { Button } from '@chakra-ui/react'
+import { Button, useDisclosure } from '@chakra-ui/react'
 import { LuFilePlus } from 'react-icons/lu'
+
+import ConfirmationDialog from '@/components/Modal/ConfirmationDialog.tsx'
 
 import { DesignerStatus, DesignerPolicyType } from '@datahub/types.ts'
 import useDataHubDraftStore from '@datahub/hooks/useDataHubDraftStore.ts'
@@ -10,18 +12,45 @@ import useDataHubDraftStore from '@datahub/hooks/useDataHubDraftStore.ts'
 const DraftCTA: FC = () => {
   const { t } = useTranslation('datahub')
   const navigate = useNavigate()
-  const { setStatus, reset } = useDataHubDraftStore()
+  const { setStatus, reset, isDirty } = useDataHubDraftStore()
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
   const handleOnClick = () => {
+    if (isDirty()) onOpen()
+    else navigate(`/datahub/${DesignerPolicyType.CREATE_POLICY}`)
+  }
+
+  const handleReplaceDraft = () => {
+    onClose()
     reset()
     setStatus(DesignerStatus.DRAFT)
     navigate(`/datahub/${DesignerPolicyType.CREATE_POLICY}`)
   }
 
+  const handleGoDraft = () => {
+    onClose()
+    navigate(`/datahub/${DesignerPolicyType.CREATE_POLICY}`)
+  }
+
   return (
-    <Button leftIcon={<LuFilePlus />} onClick={handleOnClick} variant="primary">
-      {t('Listings.policy.action.create')}
-    </Button>
+    <>
+      <Button leftIcon={<LuFilePlus />} onClick={handleOnClick} variant="primary">
+        {t('Listings.policy.action.create')}
+      </Button>
+      <ConfirmationDialog
+        isOpen={isOpen}
+        onClose={onClose}
+        onSubmit={handleReplaceDraft}
+        message={t('workspace.toolbars.modal.replace.confirmation')}
+        header={t('workspace.toolbars.modal.replace.header')}
+        action={t('workspace.controls.replace')}
+        footer={
+          <Button data-testid="confirmation-navigate-draft" onClick={handleGoDraft}>
+            {t('workspace.controls.goDraft')}
+          </Button>
+        }
+      />
+    </>
   )
 }
 
