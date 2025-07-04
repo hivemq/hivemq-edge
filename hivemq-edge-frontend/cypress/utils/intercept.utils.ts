22203ba08678da70c0be838d835b44d024c5c713
Sam Cao <sjtucaocao@gmail.com>
2025-07-04 14:44:55 +0200

    fix: Fix VITE_API_BASE_URL for .env.test

--- hivemq-edge-frontend/cypress/utils/intercept.utils.ts
+++ hivemq-edge-frontend/cypress/utils/intercept.utils.ts
@@ -5,15 +5,15 @@ import { mockAdapter_OPCUA } from '@/api/hooks/useProtocolAdapters/__handlers__'
 export const cy_interceptCoreE2E = () => {
   // requests sent but not necessary to logic
   cy.intercept('https://api.github.com/repos/hivemq/hivemq-edge/releases', { statusCode: 202, log: false })
-  cy.intercept('../api/v1/frontend/notifications', { statusCode: 202, log: false })
-  cy.intercept('../api/v1/management/protocol-adapters/status', { statusCode: 202, log: false })
-  cy.intercept('../api/v1/management/bridges/status', { statusCode: 202, log: false })
-  cy.intercept('../api/v1/frontend/capabilities', { statusCode: 202, log: false })
+  cy.intercept('/api/v1/frontend/notifications', { statusCode: 202, log: false })
+  cy.intercept('/api/v1/management/protocol-adapters/status', { statusCode: 202, log: false })
+  cy.intercept('/api/v1/management/bridges/status', { statusCode: 202, log: false })
+  cy.intercept('/api/v1/frontend/capabilities', { statusCode: 202, log: false })
 
   // code business requests
-  cy.intercept('../api/v1/auth/authenticate', mockAuthApi(mockValidCredentials))
-  cy.intercept('../api/v1/frontend/configuration', mockGatewayConfiguration)
+  cy.intercept('/api/v1/auth/authenticate', mockAuthApi(mockValidCredentials))
+  cy.intercept('/api/v1/frontend/configuration', mockGatewayConfiguration)
 
   // Add a dummy element so we can check uniqueness
-  cy.intercept('../api/v1/management/protocol-adapters/adapters', { items: [mockAdapter_OPCUA] }).as('getAdapters')
+  cy.intercept('/api/v1/management/protocol-adapters/adapters', { items: [mockAdapter_OPCUA] }).as('getAdapters')
 }
