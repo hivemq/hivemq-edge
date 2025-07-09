3a3d9d92b5a597c22313bf89fe3613968ab1908c
Sam Cao <sjtucaocao@gmail.com>
2025-07-09 09:29:56 +0200

    fix: Exclude .properties from license check

--- hivemq-edge/build.gradle.kts
+++ hivemq-edge/build.gradle.kts
@@ -440,6 +440,7 @@ license {
     header = file("HEADER")
     mapping("java", "SLASHSTAR_STYLE")
     exclude("*.json")
+    exclude("**/*.properties")
     exclude("**/*.xml")
     exclude("**/RollingList.java")
     exclude("**/api/**/*.java")
