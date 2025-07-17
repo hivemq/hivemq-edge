7c76695d19f48017af6ea58d0db23dd8c9a068c0
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-07-17 09:32:08 +0100

    refactor(34332): add support for create/edit bridge and persistence

--- hivemq-edge-frontend/src/modules/Bridges/components/BridgeEditorDrawer.tsx
+++ hivemq-edge-frontend/src/modules/Bridges/components/BridgeEditorDrawer.tsx
@@ -1,4 +1,5 @@
 import type { FC, ReactNode } from 'react'
+import { useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
 import {
   Button,
@@ -14,17 +15,44 @@ import {
   DrawerOverlay,
   useDisclosure,
 } from '@chakra-ui/react'
-import ChakraRJSForm from '@/components/rjsf/Form/ChakraRJSForm'
+
+import { Capability } from '@/api/__generated__'
 import { bridgeSchema, bridgeUISchema } from '@/api/schemas'
+import { useGetCapability } from '@/api/hooks/useFrontendServices/useGetCapability.ts'
+import { useListBridges } from '@/api/hooks/useGetBridges/useListBridges.ts'
+
+import ChakraRJSForm from '@/components/rjsf/Form/ChakraRJSForm'
+import { customUniqueBridgeValidate } from '@/modules/Bridges/utils/validation-utils.ts'
 
 interface BridgeEditorDrawerProps {
   isNew?: boolean
   children?: ReactNode
 }
 
-const BridgeEditorDrawer: FC<BridgeEditorDrawerProps> = () => {
+const BridgeEditorDrawer: FC<BridgeEditorDrawerProps> = ({ isNew }) => {
   const { t } = useTranslation()
   const { onClose } = useDisclosure()
+  const hasPersistence = useGetCapability(Capability.id.MQTT_PERSISTENCE)
+  const { data: allBridges } = useListBridges()
+
+  const uiSchemaPersistence = useMemo(() => {
+    const { id, ['ui:tabs']: uiTabs, persist } = bridgeUISchema
+    const optional = {
+      'ui:tabs': (uiTabs as { id: string }[]).filter((tab) => tab.id !== 'bridgePersist'),
+      persist: {
+        ...persist,
+        'ui:widget': 'hidden',
+      },
+    }
+    return {
+      ...bridgeUISchema,
+      id: {
+        ...id,
+        'ui:disabled': !isNew,
+      },
+      ...(!hasPersistence && optional),
+    }
+  }, [hasPersistence, isNew])
 
   return (
     <Drawer isOpen={true} placement="right" size="xl" onClose={onClose} variant="hivemq">
@@ -40,7 +68,8 @@ const BridgeEditorDrawer: FC<BridgeEditorDrawerProps> = () => {
                 showNativeWidgets={false}
                 id="bridge-form"
                 schema={bridgeSchema}
-                uiSchema={bridgeUISchema}
+                uiSchema={uiSchemaPersistence}
+                customValidate={customUniqueBridgeValidate(bridgeSchema, allBridges)}
                 onSubmit={() => {
                   console.log('XXXX')
                 }}
