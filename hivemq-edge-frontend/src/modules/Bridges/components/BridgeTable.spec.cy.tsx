fbc2669e12a346747456f454f4912ad93aaf6181
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-07-18 10:38:39 +0100

    test(34332): fix tests

--- hivemq-edge-frontend/src/modules/Bridges/components/BridgeTable.spec.cy.tsx
+++ hivemq-edge-frontend/src/modules/Bridges/components/BridgeTable.spec.cy.tsx
@@ -80,47 +80,4 @@ describe('BridgeTable', () => {
       },
     })
   })
-
-  //   cy.get('header h1').should('have.text', 'MQTT bridges')
-  //   cy.get('header h1 + p').should(
-  //     'have.text',
-  //     'MQTT bridges let you connect multiple MQTT brokers to enable seamless data sharing between different networks or systems.'
-  //   )
-  //
-  //   cy.getByTestId('page-container-cta').within(() => {
-  //     cy.get('button').should('have.text', 'Add bridge connection')
-  //   })
-  //   cy.get('table').should('have.attr', 'aria-label', 'List of bridges')
-  //   cy.get('table tbody tr td').eq(0).should('have.text', 'bridge-id-01')
-  // })
-  //
-  // it.only('should create a new bridge by the main CTA', () => {
-  //   cy.mountWithProviders(<BridgePage />, {
-  //     wrapper: WrapperTestRoute,
-  //     routerProps: { initialEntries: [`/mqtt-bridge`] },
-  //   })
-  //   cy.wait('@getBridges')
-  //
-  //   cy.getByTestId('test-pathname').should('have.text', '/mqtt-bridge')
-  //   cy.getByTestId('page-container-cta').within(() => {
-  //     cy.get('button').should('have.text', 'Add bridge connection')
-  //     cy.get('button').click()
-  //   })
-  //   cy.getByTestId('test-pathname').should('have.text', '/mqtt-bridge/new')
-  // })
-  //
-  // it('should be accessible', () => {
-  //   cy.injectAxe()
-  //
-  //   cy.mountWithProviders(<BridgeTable />)
-  //   cy.wait('@getBridges')
-  //
-  //   cy.get('table tbody tr td').eq(0).should('have.text', 'bridge-id-01')
-  //
-  //   cy.checkAccessibility(undefined, {
-  //     rules: {
-  //       // TODO[#111] Skeleton are not accessible
-  //       'color-contrast': { enabled: false },
-  //     },
-  //   })
 })
