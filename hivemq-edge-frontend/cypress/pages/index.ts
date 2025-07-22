dfe5b308fad7f1539f996cd1331292334bdc14ad
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-07-22 16:56:53 +0100

    refactor(35016): add page object for the event log

--- hivemq-edge-frontend/cypress/pages/index.ts
+++ hivemq-edge-frontend/cypress/pages/index.ts
@@ -3,5 +3,6 @@ import { adapterPage } from './Protocols/AdapterPage.ts'
 import { rjsf } from './RJSF/RJSFomField.ts'
 import { workspacePage } from './Workspace/WorkspacePage'
 import { bridgePage } from './Bridges/BridgePage.ts'
+import { eventLogPage } from './EventLog/EventLogPage.ts'
 
-export { loginPage, workspacePage, adapterPage, rjsf, bridgePage }
+export { loginPage, workspacePage, adapterPage, rjsf, bridgePage, eventLogPage }
