76f4a1b82ac928eb76b02f4bda155ba6f971d46d
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-07-30 17:13:39 +0100

    refactor(34972): remove deprecated code

--- hivemq-edge-frontend/src/hooks/usePrivateMqttClient/PrivateMqttClientContext.ts
+++
@@ -1,4 +0,0 @@
-import { createContext } from 'react'
-import type { PrivateMqttClientType } from '@/hooks/usePrivateMqttClient/type.ts'
-
-export const PrivateMqttClientContext = createContext<PrivateMqttClientType>({ state: undefined, actions: undefined })
