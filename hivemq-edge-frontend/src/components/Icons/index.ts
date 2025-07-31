caaf48f5fb2606868ac8acce4d01110906505322
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-07-31 11:17:18 +0100

    feat(34972): add route for assets

--- hivemq-edge-frontend/src/components/Icons/index.ts
+++ hivemq-edge-frontend/src/components/Icons/index.ts
@@ -1,3 +1,5 @@
 import { TbArrowsJoin } from 'react-icons/tb'
+import { WorkspaceIcon } from './TopicIcon.tsx'
+import { PulseAgentIcon } from './PulseAgentIcon.tsx'
 
-export { TbArrowsJoin as HqCombiner }
+export { TbArrowsJoin as HqCombiner, WorkspaceIcon, PulseAgentIcon }
