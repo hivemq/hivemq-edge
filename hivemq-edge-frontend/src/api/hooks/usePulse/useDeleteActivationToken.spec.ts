afe555bc8e8cf311e2856112cf76152fdfa62761
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-07-31 10:19:24 +0100

    test(34972): add tests

---
+++ hivemq-edge-frontend/src/api/hooks/usePulse/useDeleteActivationToken.spec.ts
@@ -0,0 +1,28 @@
+import { expect } from 'vitest'
+import { act, renderHook, waitFor } from '@testing-library/react'
+
+import { server } from '@/__test-utils__/msw/mockServer.ts'
+import { SimpleWrapper as wrapper } from '@/__test-utils__/hooks/SimpleWrapper.tsx'
+import { useDeleteActivationToken } from '@/api/hooks/usePulse/useDeleteActivationToken.ts'
+import { handlers } from './__handlers__'
+
+describe('useDeleteActivationToken', () => {
+  afterEach(() => {
+    server.resetHandlers()
+  })
+
+  it('should execute the mutation', async () => {
+    server.use(...handlers)
+
+    const { result } = renderHook(() => useDeleteActivationToken(), { wrapper })
+
+    expect(result.current.isSuccess).toBeFalsy()
+    act(() => {
+      result.current.mutateAsync()
+    })
+    await waitFor(() => {
+      expect(result.current.isSuccess).toBeTruthy()
+      expect(result.current.data).toStrictEqual('Token deleted')
+    })
+  })
+})
