9d939e90783488dd48379a630306c29a17b52ece
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-08-05 16:58:30 +0100

    feat(34972): update icons

--- hivemq-edge-frontend/src/components/Icons/index.ts
+++ hivemq-edge-frontend/src/components/Icons/index.ts
@@ -1,5 +1,13 @@
-import { TbArrowsJoin } from 'react-icons/tb'
+import { TbArrowsJoin, TbArrowsShuffle } from 'react-icons/tb'
+import { MdOutlineCloudDone, MdOutlineCloudOff } from 'react-icons/md'
 import { WorkspaceIcon } from './TopicIcon.tsx'
 import { PulseAgentIcon } from './PulseAgentIcon.tsx'
 
-export { TbArrowsJoin as HqCombiner, WorkspaceIcon, PulseAgentIcon }
+export {
+  TbArrowsJoin as HqCombiner,
+  TbArrowsShuffle as HqAssets,
+  MdOutlineCloudDone as HqPulseActivated,
+  MdOutlineCloudOff as HqPulseNotActivated,
+  WorkspaceIcon,
+  PulseAgentIcon,
+}
