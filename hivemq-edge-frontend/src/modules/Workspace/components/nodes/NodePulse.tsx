57679d897a8eafbc90f23a252b64f2e6f23f0676
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-08-07 10:30:05 +0100

    fix(34972): fix status

--- hivemq-edge-frontend/src/modules/Workspace/components/nodes/NodePulse.tsx
+++ hivemq-edge-frontend/src/modules/Workspace/components/nodes/NodePulse.tsx
@@ -11,10 +11,13 @@ import ContextualToolbar from '@/modules/Workspace/components/nodes/ContextualTo
 import NodeWrapper from '@/modules/Workspace/components/parts/NodeWrapper.tsx'
 import { CONFIG_ADAPTER_WIDTH } from '@/modules/Workspace/utils/nodes-utils.ts'
 import type { NodePulseType } from '@/modules/Workspace/types'
+import { PulseStatus } from '@/modules/Pulse/types.ts'
 
 const NodePulse: FC<NodeProps<NodePulseType>> = ({ id, data, selected, dragging }) => {
   const { t } = useTranslation()
   const { data: hasPulseCapability, isSuccess } = useGetCapability(Capability.id.PULSE_ASSET_MANAGEMENT)
+
+  const pulseStatus: PulseStatus = hasPulseCapability ? PulseStatus.ACTIVATED : PulseStatus.DEACTIVATED
 
   return (
     <>
@@ -27,19 +30,20 @@ const NodePulse: FC<NodeProps<NodePulseType>> = ({ id, data, selected, dragging 
         w={CONFIG_ADAPTER_WIDTH}
         borderTopRadius={30}
       >
-        <HStack w="100%" justifyContent="flex-end" gap={1} data-testid="pulse-client-capability">
+        <HStack w="100%" justifyContent="flex-end" gap={1} data-testid="pulse-client-capabilities">
           {isSuccess && (
             <Icon
               boxSize={4}
               as={hasPulseCapability ? HqPulseActivated : HqPulseNotActivated}
-              data-type="pulse-client-capability"
+              data-type={pulseStatus}
+              aria-label={t('pulse.workspace.nodeClient.status', { context: pulseStatus })}
             />
           )}
         </HStack>
         <VStack>
-          <HStack w="100%" data-testid="device-description">
+          <HStack w="100%" data-testid="pulse-client-description">
             <PulseAgentIcon boxSize={10} />
-            <Text>{t('pulse.client.title')}</Text>
+            <Text>{t('pulse.workspace.nodeClient.title')}</Text>
           </HStack>
         </VStack>
       </NodeWrapper>
