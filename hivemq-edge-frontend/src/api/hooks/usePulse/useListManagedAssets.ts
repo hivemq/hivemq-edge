09c511050032e277a7b59f0b05279110fb7297e9
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-08-08 16:45:26 +0100

    feat(35447): add react query hooks for assets

---
+++ hivemq-edge-frontend/src/api/hooks/usePulse/useListManagedAssets.ts
@@ -0,0 +1,14 @@
+import { useQuery } from '@tanstack/react-query'
+
+import type { ApiError, ManagedAssetList } from '@/api/__generated__'
+import { useHttpClient } from '@/api/hooks/useHttpClient/useHttpClient.ts'
+import { QUERY_KEYS } from '@/api/utils.ts'
+
+export const useListManagedAssets = () => {
+  const appClient = useHttpClient()
+
+  return useQuery<ManagedAssetList, ApiError>({
+    queryKey: [QUERY_KEYS.PULSE_ASSETS],
+    queryFn: () => appClient.pulse.getManagedAssets(),
+  })
+}
