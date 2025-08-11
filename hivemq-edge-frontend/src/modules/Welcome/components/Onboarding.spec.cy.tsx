6b9c56361e2ec8ac6a564c4be3089475da696bd9
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-08-11 18:43:23 +0100

    Merge pull request #1069
    * a bit of cleaning
    
    * Merge pull request #1062
    
    * feat(34972): add openAPI specs
    
    * feat(34972): add openAPI specs
    
    * refactor(34972): refactor the dev-only startup
    
    * refactor(34972): update mocks
    
    * refactor(34972): remove deprecated code
    
    * refactor(34972): update openAPI specs
    
    * feat(34972): add react0query hooks for the activation token
    
    * test(34972): add mocks
    
    * test(34972): add tests
    
    * test(34972): fix tests
    
    * fix(34972): fix flaw in the capability hook
    
    * test(34972): add tests
    
    * feat(34972): add route for assets
    
    * refactor(34972): add optional content to the task specs
    
    * feat(34972): add Pulse tasks on the main page
    
    * refactor(34972): add optional content to the task specs
    
    * refactor(34972): fix layout
    
    * feat(34972): add activation trigger
    
    * refactor(34972): refactor onboarding to handle errors
    
    * test(34972): fix tests
    
    * refactor(34972): add simple RJSF validation for JWT
    
    * test(34972): add tests
    
    * refactor(34972): add support for onError
    
    * feat(34972): add the activation panel
    
    * feat(34972): update translations
    
    * test(34972): update mocks and intercepts
    
    * fix(34972): fix import
    
    * test(34972): add tests
    
    * test(34972): add tests
    
    * test(34972): fix tests
    
    * test(34972): fix tests
    
    * fix(34972): fix translations
    
    * feat(34972): add a landing page for the pulse assets
    
    * test(34972): add tests
    
    * feat(34972): add node types for pulse client and assets
    
    * feat(34972): add node types for pulse client and assets
    
    * feat(34972): add node for the pulse client
    
    * feat(34972): add node for the assets
    
    * feat(34972): add node types for pulse client and assets
    
    * feat(34972): add default drawer for the pulse nodes
    
    * feat(34972): update icons
    
    * feat(34972): update translations
    
    * test(34972): add tests
    
    * test(34972): add tests
    
    * test(34972): add tests
    
    * test(34972): add tests
    
    * fix(34972): fix status
    
    * fix(34972): fix aria label
    
    * test(34972): add tests
    
    * fix(34972): fix translations
    
    * test(34972): fix mocks
    
    * test(34972): add Page Object for the Pulse Activation and Assets
    
    * test(34972): add Page Object for the home page
    
    * test(34972): fix imports
    
    * test(34972): add onboarding enum
    
    * fix(34972): fix the computation of id for non-object root
    
    * fix(34972): add toast getters to home page
    
    * fix(34972): add submit button getter
    
    * test(34972): add e2e tests for pulse activation
    
    * fix(34972): a bit of cleaning
    
    * fix(34972): fix bug with E2E test

--- hivemq-edge-frontend/src/modules/Welcome/components/Onboarding.spec.cy.tsx
+++ hivemq-edge-frontend/src/modules/Welcome/components/Onboarding.spec.cy.tsx
@@ -1,41 +1,39 @@
 /// <reference types="cypress" />
+import type { OnboardingTask } from '@/modules/Welcome/types.ts'
 import { IoLinkOutline } from 'react-icons/io5'
 
 import Onboarding from './Onboarding.tsx'
-import type { OnboardingFetchType } from '@/modules/Welcome/hooks/useOnboarding.tsx'
 
-const MOCK_ONBOARDING: OnboardingFetchType = {
-  data: [
-    {
-      header: 'Heading 1',
-      sections: [
-        {
-          title: 'Task 1',
-          label: 'Get Started',
-          to: '/link1',
-          leftIcon: <IoLinkOutline />,
-        },
-        {
-          title: 'Task  2',
-          label: 'Get Started',
-          to: '/link2',
-          leftIcon: <IoLinkOutline />,
-        },
-      ],
-    },
-    {
-      header: 'Heading 2',
-      sections: [
-        {
-          title: 'Task 3',
-          label: 'Get Started',
-          to: '/link3',
-          leftIcon: <IoLinkOutline />,
-        },
-      ],
-    },
-  ],
-}
+const MOCK_ONBOARDING: OnboardingTask[] = [
+  {
+    header: 'Heading 1',
+    sections: [
+      {
+        title: 'Task 1',
+        label: 'Get Started',
+        to: '/link1',
+        leftIcon: <IoLinkOutline />,
+      },
+      {
+        title: 'Task  2',
+        label: 'Get Started',
+        to: '/link2',
+        leftIcon: <IoLinkOutline />,
+      },
+    ],
+  },
+  {
+    header: 'Heading 2',
+    sections: [
+      {
+        title: 'Task 3',
+        label: 'Get Started',
+        to: '/link3',
+        leftIcon: <IoLinkOutline />,
+      },
+    ],
+  },
+]
 
 describe('Onboarding', () => {
   beforeEach(() => {
--- hivemq-edge-frontend/src/modules/Welcome/types.ts
+++ hivemq-edge-frontend/src/modules/Welcome/types.ts
@@ -3,13 +3,15 @@ import type React from 'react'
 export interface OnboardingAction {
   title: string
   label: string
-  to: string
-  leftIcon: React.ReactElement
+  to?: string
   isExternal?: boolean
+  content?: React.ReactElement
+  leftIcon: React.ReactElement
 }
 
 export interface OnboardingTask {
   header: string
   sections: OnboardingAction[]
   isLoading?: boolean
+  error?: Error | null
 }
