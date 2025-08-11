6f2ed9d9c754541546c5979bc81468e0f3d62571
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-08-11 18:43:23 +0100

    Merge pull request #1069
    * a bit of cleaning
    
    * Merge pull request #1062
    
    * feat(34972): add openAPI specs
    
    * feat(34972): add openAPI specs
    
    * refactor(34972): refactor the dev-only startup
    
    * refactor(34972): update mocks
    
    * refactor(34972): remove deprecated code
    
    * refactor(34972): update openAPI specs
    
    * feat(34972): add react0query hooks for the activation token
    
    * test(34972): add mocks
    
    * test(34972): add tests
    
    * test(34972): fix tests
    
    * fix(34972): fix flaw in the capability hook
    
    * test(34972): add tests
    
    * feat(34972): add route for assets
    
    * refactor(34972): add optional content to the task specs
    
    * feat(34972): add Pulse tasks on the main page
    
    * refactor(34972): add optional content to the task specs
    
    * refactor(34972): fix layout
    
    * feat(34972): add activation trigger
    
    * refactor(34972): refactor onboarding to handle errors
    
    * test(34972): fix tests
    
    * refactor(34972): add simple RJSF validation for JWT
    
    * test(34972): add tests
    
    * refactor(34972): add support for onError
    
    * feat(34972): add the activation panel
    
    * feat(34972): update translations
    
    * test(34972): update mocks and intercepts
    
    * fix(34972): fix import
    
    * test(34972): add tests
    
    * test(34972): add tests
    
    * test(34972): fix tests
    
    * test(34972): fix tests
    
    * fix(34972): fix translations
    
    * feat(34972): add a landing page for the pulse assets
    
    * test(34972): add tests
    
    * feat(34972): add node types for pulse client and assets
    
    * feat(34972): add node types for pulse client and assets
    
    * feat(34972): add node for the pulse client
    
    * feat(34972): add node for the assets
    
    * feat(34972): add node types for pulse client and assets
    
    * feat(34972): add default drawer for the pulse nodes
    
    * feat(34972): update icons
    
    * feat(34972): update translations
    
    * test(34972): add tests
    
    * test(34972): add tests
    
    * test(34972): add tests
    
    * test(34972): add tests
    
    * fix(34972): fix status
    
    * fix(34972): fix aria label
    
    * test(34972): add tests
    
    * fix(34972): fix translations
    
    * test(34972): fix mocks
    
    * test(34972): add Page Object for the Pulse Activation and Assets
    
    * test(34972): add Page Object for the home page
    
    * test(34972): fix imports
    
    * test(34972): add onboarding enum
    
    * fix(34972): fix the computation of id for non-object root
    
    * fix(34972): add toast getters to home page
    
    * fix(34972): add submit button getter
    
    * test(34972): add e2e tests for pulse activation
    
    * fix(34972): a bit of cleaning
    
    * fix(34972): fix bug with E2E test

---
+++ hivemq-edge-frontend/src/modules/Pulse/types.ts
@@ -0,0 +1,7 @@
+/**
+ * @deprecated This should be replaced by a dedicated API schema
+ */
+export enum PulseStatus {
+  ACTIVATED = 'ACTIVATED',
+  DEACTIVATED = 'DEACTIVATED',
+}
--- hivemq-edge-frontend/src/modules/Welcome/components/Onboarding.tsx
+++ hivemq-edge-frontend/src/modules/Welcome/components/Onboarding.tsx
@@ -18,66 +18,68 @@ import {
 import { BsClipboardCheck } from 'react-icons/bs'
 
 import ErrorMessage from '@/components/ErrorMessage.tsx'
-import type { OnboardingFetchType } from '@/modules/Welcome/hooks/useOnboarding.tsx'
+import type { OnboardingTask } from '@/modules/Welcome/types.ts'
 
 interface OnboardingProps extends BoxProps {
-  tasks: OnboardingFetchType
+  tasks: OnboardingTask[]
 }
 
 const Onboarding: FC<OnboardingProps> = ({ tasks, ...props }) => {
   const { t } = useTranslation()
-  const { data, error } = tasks
 
   return (
     <Box mt={6} {...props}>
       <Heading>{t('welcome.onboarding.title')}</Heading>
       <SimpleGrid spacing={6} templateColumns="repeat(auto-fill, minmax(33vw, 10fr))">
-        {data &&
-          data.map((e, i) => (
-            <Card flex={1} key={e.header} as="aside" aria-labelledby={`heading-task-${i}`}>
-              <CardHeader>
-                <Skeleton isLoaded={!e.isLoading}>
-                  <Heading as="h3" size="md" id={`heading-task-${i}`}>
-                    {e.header}
-                  </Heading>
-                </Skeleton>
-              </CardHeader>
+        {tasks?.map((task, index) => (
+          <Card flex={1} key={task.header} as="aside" aria-labelledby={`heading-task-${index}`}>
+            <CardHeader>
+              <Skeleton isLoaded={!task.isLoading}>
+                <Heading as="h3" size="md" id={`heading-task-${index}`}>
+                  {task.header}
+                </Heading>
+              </Skeleton>
+            </CardHeader>
 
-              <CardBody pt={0}>
-                <Stack divider={<StackDivider />} spacing="4">
-                  {e.sections.map((s) => (
-                    <Stack as="section" key={`${s.title}-${s.label}`} spacing={8} direction="row" gap={4}>
-                      <Skeleton isLoaded={!e.isLoading}>
+            <CardBody pt={0}>
+              <Stack divider={<StackDivider />} spacing="4">
+                {task.error && (
+                  <Card flex={1} m={1}>
+                    <ErrorMessage type={task.error?.message} message={t('welcome.onboarding.errorLoading')} />
+                  </Card>
+                )}
+                {!task.error &&
+                  task.sections.map((section) => (
+                    <Stack as="section" key={`${section.title}-${section.label}`} spacing={8} direction="row" gap={4}>
+                      <Skeleton isLoaded={!task.isLoading}>
                         <Box>
                           <BsClipboardCheck />
                         </Box>
                       </Skeleton>
-                      <Skeleton isLoaded={!e.isLoading}>
+                      <Skeleton isLoaded={!task.isLoading}>
                         <Box>
-                          <Text>{s.title}</Text>
-                          <Button
-                            variant="link"
-                            as={RouterLink}
-                            to={s.to}
-                            target={s.isExternal ? '_blank' : undefined}
-                            aria-label={s.label}
-                            leftIcon={s.leftIcon}
-                          >
-                            {s.label}
-                          </Button>
+                          <Text>{section.title}</Text>
+                          {!!section.to && (
+                            <Button
+                              variant="link"
+                              as={RouterLink}
+                              to={section.to}
+                              target={section.isExternal ? '_blank' : undefined}
+                              aria-label={section.label}
+                              leftIcon={section.leftIcon}
+                            >
+                              {section.label}
+                            </Button>
+                          )}
+                          {!!section.content && section.content}
                         </Box>
                       </Skeleton>
                     </Stack>
                   ))}
-                </Stack>
-              </CardBody>
-            </Card>
-          ))}
-        {!!error && (
-          <Card flex={1} m={1}>
-            <ErrorMessage type={error?.message} message={t('welcome.onboarding.errorLoading')} />
+              </Stack>
+            </CardBody>
           </Card>
-        )}
+        ))}
       </SimpleGrid>
     </Box>
   )
