ea3c45df7a0fe713136a0f436f1946b64a8bec2f
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-08-11 08:59:53 +0100

    feat(35447): add custom renderer for searched text in global filters

---
+++ hivemq-edge-frontend/src/modules/Pulse/components/assets/FilteredCell.tsx
@@ -0,0 +1,36 @@
+import type { FC } from 'react'
+import { Skeleton, Highlight, Text } from '@chakra-ui/react'
+
+interface FilteredCellProps {
+  value: string
+  isLoading?: boolean
+  canGlobalFilter?: boolean
+  globalFilter?: string
+}
+
+const FilteredCell: FC<FilteredCellProps> = ({
+  value,
+  isLoading = false,
+  canGlobalFilter = false,
+  globalFilter = '',
+}) => {
+  return (
+    <Skeleton isLoaded={!isLoading}>
+      <Text data-testid="cell-content">
+        {canGlobalFilter && globalFilter ? (
+          <Highlight
+            data-testid="cell-content"
+            query={globalFilter}
+            styles={{ px: '2', py: '1', rounded: 'full', bg: 'teal.50' }}
+          >
+            {value}
+          </Highlight>
+        ) : (
+          value
+        )}
+      </Text>
+    </Skeleton>
+  )
+}
+
+export default FilteredCell
