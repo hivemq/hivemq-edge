133903c6d0b89b192040f3015fe3ca7bf16e10c6
Lukas Brand <lukas.brand@hivemq.com>
2025-08-21 11:45:24 +0200

    Remove Develocity from hivemq-edge as it was never configured

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,11 +6,6 @@ pluginManagement {
     }
 }
 
-plugins {
-    id("com.gradle.develocity") version "3.19.2"
-    id("com.gradle.common-custom-user-data-gradle-plugin") version "2.1"
-}
-
 includeBuild("./hivemq-edge")
 includeBuild("./hivemq-edge-frontend")
 
