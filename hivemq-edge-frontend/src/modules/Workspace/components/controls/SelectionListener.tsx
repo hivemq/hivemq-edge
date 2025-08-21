caf0ff0d37c8b71eaefb4441cf9b3da6cbc3d500
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-08-21 13:48:10 +0100

    Merge pull request #1090
    * refactor(35506): add assets to entities
    
    * refactor(35506): refactor the Pulse node to add asset badges
    
    * refactor(35506): refactor the Asset node to add asset badges
    
    * refactor(35506): refactor the property panel
    
    * feat(35506): add assets to the node panel
    
    * feat(35506): add dynamic columns to the asset table
    
    * fix(35506): fix zIndex with all select widgets
    
    * fix(35506): fix creation of pulse/asset nodes
    
    * fix(35506): fix handles
    
    * fix(35506): fix bug with unmapped detection
    
    * fix(35506): fix side panel for pulse node
    
    * fix(35506): fix default type for assets and pulse nodes
    
    * refactor(35506): refactor the workspace command
    
    * refactor(35506): add support for pulse nodes
    
    * fix(35506): fix default id for pulse nodes
    
    * refactor(35506): update the action menu from the asset list
    
    * refactor(35506): update translations
    
    * test(35506): fix tests
    
    * test(35506): add tests
    
    * test(35506): add tests
    
    * test(35506): add tests
    
    * test(35506): fix tests
    
    * test(35506): add tests
    
    * fix(35506): add state to the test wrapper
    
    * test(35506): add tests
    
    * test(35506): add tests
    
    * test(35506): add tests
    
    * test(35506): fix test
    
    * chore(35506): a bit of cleaning
    
    * fix(35506): fix translations
    
    * test(35506): add tests
    
    * refactor(35506): add summary version of the actions
    
    * test(35506): add tests
    
    * fix(35506): a bit of cleaning

--- hivemq-edge-frontend/src/modules/Workspace/components/controls/SelectionListener.tsx
+++ hivemq-edge-frontend/src/modules/Workspace/components/controls/SelectionListener.tsx
@@ -6,7 +6,7 @@ import { useStore, useReactFlow } from '@xyflow/react'
 import useWorkspaceStore from '@/modules/Workspace/hooks/useWorkspaceStore.ts'
 import type { DeviceMetadata } from '@/modules/Workspace/types.ts'
 import { NodeTypes } from '@/modules/Workspace/types.ts'
-import { WorkspaceAdapterCommand } from '@/modules/ProtocolAdapters/types.ts'
+import { WorkspaceNavigationCommand } from '@/modules/Workspace/types.ts'
 
 const addSelectedNodesState = (state: ReactFlowState) => (nodeIds: string[]) => state.addSelectedNodes(nodeIds)
 
@@ -28,14 +28,14 @@ const SelectionListener = () => {
     }
 
     switch (command) {
-      case WorkspaceAdapterCommand.TOPIC_FILTERS: {
+      case WorkspaceNavigationCommand.TOPIC_FILTERS: {
         // Topic filters are on the Edge node
         const edgeFound = nodes.find((e) => e.type === NodeTypes.EDGE_NODE)
         if (edgeFound) focusOnNodes([edgeFound.id])
 
         return
       }
-      case WorkspaceAdapterCommand.TAGS: {
+      case WorkspaceNavigationCommand.TAGS: {
         // Tags are on the Device node connected to the adapter
         const found = nodes.find((e) => adapterId === e.data.id)
         if (found?.type === NodeTypes.ADAPTER_NODE) {
@@ -46,11 +46,20 @@ const SelectionListener = () => {
         }
         return
       }
-      case WorkspaceAdapterCommand.MAPPINGS: {
+      case WorkspaceNavigationCommand.MAPPINGS: {
         // N and S mappings are on the adapter node
         const found = nodes.find((e) => adapterId === e.data.id)
         if (found) {
           focusOnNodes([found.id])
+        }
+        return
+      }
+      case WorkspaceNavigationCommand.ASSET_MAPPER: {
+        const found = nodes.filter(
+          (e) => e.type === NodeTypes.PULSE_NODE || (e.type === NodeTypes.ASSETS_NODE && e.data.id === adapterId)
+        )
+        if (found.length > 0) {
+          focusOnNodes(found.map((e) => e.id))
         }
         return
       }
@@ -74,7 +83,8 @@ const SelectionListener = () => {
         return
       }
     }
-  }, [addSelectedNodes, fitView, navigate, nodes, pathname, state?.selectedAdapter])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [pathname, state?.selectedAdapter])
 
   return null
 }
