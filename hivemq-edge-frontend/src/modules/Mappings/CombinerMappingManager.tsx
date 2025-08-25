64a32acc0c8ea6124d4085a6463d1890d8495e10
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-08-25 10:32:52 +0100

    refactor(35452): add detection of asset manager

--- hivemq-edge-frontend/src/modules/Mappings/CombinerMappingManager.tsx
+++ hivemq-edge-frontend/src/modules/Mappings/CombinerMappingManager.tsx
@@ -1,8 +1,8 @@
 import { type FC, useEffect, useMemo } from 'react'
-import type { Node, NodeRemoveChange } from '@xyflow/react'
-import { useNavigate, useParams } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
+import { useNavigate, useParams } from 'react-router-dom'
 import type { IChangeEvent } from '@rjsf/core'
+import type { Node, NodeRemoveChange } from '@xyflow/react'
 import {
   Button,
   ButtonGroup,
@@ -22,25 +22,23 @@ import {
   useToast,
 } from '@chakra-ui/react'
 
-import config from '@/config'
-
-import type { CombinerContext } from './types'
-import { MappingType } from './types'
 import type { Combiner } from '@/api/__generated__'
 import { EntityType } from '@/api/__generated__'
+import { useDeleteCombiner, useUpdateCombiner } from '@/api/hooks/useCombiners/'
+import { useGetCombinedEntities } from '@/api/hooks/useDomainModel/useGetCombinedEntities'
 import { combinerMappingJsonSchema } from '@/api/schemas/combiner-mapping.json-schema'
 import { combinerMappingUiSchema } from '@/api/schemas/combiner-mapping.ui-schema'
-import { useUpdateCombiner, useDeleteCombiner } from '@/api/hooks/useCombiners/'
-import { useGetCombinedEntities } from '@/api/hooks/useDomainModel/useGetCombinedEntities'
 import ChakraRJSForm from '@/components/rjsf/Form/ChakraRJSForm'
-import ErrorMessage from '@/components/ErrorMessage'
+import DangerZone from '@/modules/Mappings/components/DangerZone.tsx'
 import { BASE_TOAST_OPTION } from '@/hooks/useEdgeToast/toast-utils'
-import type { NodeTypes } from '@/modules/Workspace/types.ts'
-import { IdStubs } from '@/modules/Workspace/types.ts'
-import useWorkspaceStore from '@/modules/Workspace/hooks/useWorkspaceStore.ts'
+import type { CombinerContext } from '@/modules/Mappings/types.ts'
+import { useValidateCombiner } from '@/modules/Mappings/hooks/useValidateCombiner.ts'
+import { MappingType } from '@/modules/Mappings/types.ts'
 import NodeNameCard from '@/modules/Workspace/components/parts/NodeNameCard.tsx'
-import DangerZone from './components/DangerZone'
-import { useValidateCombiner } from './hooks/useValidateCombiner'
+import useWorkspaceStore from '@/modules/Workspace/hooks/useWorkspaceStore.ts'
+import { IdStubs, NodeTypes } from '@/modules/Workspace/types.ts'
+
+import config from '@/config'
 
 const CombinerMappingManager: FC = () => {
   const { t } = useTranslation()
@@ -64,6 +62,11 @@ const CombinerMappingManager: FC = () => {
     if (!isBridgeIn) entities.push({ id: IdStubs.EDGE_NODE, type: EntityType.EDGE_BROKER })
     return entities
   }, [selectedNode.data.sources.items])
+
+  const isAssetManager = useMemo(() => {
+    // TODO[35769] This is a hack; PULSE_AGENT needs to be supported as a valid EntityType
+    return entities?.some((e) => e.type === EntityType.DEVICE)
+  }, [entities])
 
   const sources = useGetCombinedEntities(entities)
   const validator = useValidateCombiner(sources, entities)
@@ -123,8 +126,8 @@ const CombinerMappingManager: FC = () => {
         <DrawerHeader>
           <Text>{t('protocolAdapter.mapping.manager.header', { context: MappingType.COMBINING })}</Text>
           <NodeNameCard
-            name={selectedNode?.data.name}
-            type={selectedNode?.type as NodeTypes}
+            name={selectedNode.data.name}
+            type={isAssetManager ? NodeTypes.ASSETS_NODE : NodeTypes.COMBINER_NODE}
             description={t('combiner.type')}
           />
         </DrawerHeader>
@@ -133,7 +136,7 @@ const CombinerMappingManager: FC = () => {
             showNativeWidgets={showNativeWidgets}
             id="combiner-main-form"
             schema={combinerMappingJsonSchema}
-            uiSchema={combinerMappingUiSchema}
+            uiSchema={combinerMappingUiSchema(isAssetManager)}
             formData={selectedNode.data}
             onSubmit={handleOnSubmit}
             formContext={{ queries: sources, entities } as CombinerContext}
