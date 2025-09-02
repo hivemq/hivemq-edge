007c52356fc0a99e07177faad3cc9a2d6b8b17af
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-09-02 09:27:30 +0100

    chore(35558): linting

--- hivemq-edge-frontend/src/api/hooks/useCombiners/useSelectCombinerFromMapping.ts
+++ hivemq-edge-frontend/src/api/hooks/useCombiners/useSelectCombinerFromMapping.ts
@@ -7,8 +7,6 @@ import { QUERY_KEYS } from '@/api/utils.ts'
 export const useSelectCombinerFromMapping = (mappingId: string | undefined) => {
   const appClient = useHttpClient()
 
-  const gg = appClient.combiners.httpRequest
-
   return useQuery<CombinerList, ApiError, Combiner>({
     queryKey: [QUERY_KEYS.ADAPTERS, 'all', mappingId],
     queryFn: () => appClient.combiners.getCombiners(),
@@ -17,7 +15,6 @@ export const useSelectCombinerFromMapping = (mappingId: string | undefined) => {
       const combiner = combiners.items.find((c) => c.mappings.items.some((m) => m.id === mappingId))
 
       if (!combiner) {
-        console.log('xxxxx dd', gg)
         throw new Error('No matching Combiner found')
       }
       return combiner
