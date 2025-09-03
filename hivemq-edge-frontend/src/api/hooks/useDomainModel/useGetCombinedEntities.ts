00047e568e6b3d4e0b3657851a5911ce4f53296f
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-09-03 11:48:28 +0100

    fix(35995): fix bug with ReactQuery requests combining

--- hivemq-edge-frontend/src/api/hooks/useDomainModel/useGetCombinedEntities.ts
+++ hivemq-edge-frontend/src/api/hooks/useDomainModel/useGetCombinedEntities.ts
@@ -1,26 +1,35 @@
+import type { QueryFunction, QueryKey } from '@tanstack/react-query'
 import { useQueries } from '@tanstack/react-query'
+
 import type { EntityReference } from '@/api/__generated__'
 import { EntityType } from '@/api/__generated__'
-
-import { QUERY_KEYS } from '@/api/utils.ts'
 import { useHttpClient } from '@/api/hooks/useHttpClient/useHttpClient.ts'
+import { QUERY_KEYS } from '@/api/utils.ts'
 
 export const useGetCombinedEntities = (entities: EntityReference[]) => {
   const appClient = useHttpClient()
 
   // TODO[NVL] Likely too many duplicates; needs filtering
+  const combinedQueries = entities.reduce(
+    (acc, entity) => {
+      if (entity.type === EntityType.ADAPTER) {
+        acc.push({
+          queryKey: [QUERY_KEYS.ADAPTERS, entity.id, QUERY_KEYS.DISCOVERY_TAGS],
+          queryFn: () => appClient.protocolAdapters.getAdapterDomainTags(entity.id),
+        })
+      } else if (entity.type === EntityType.BRIDGE || entity.type === EntityType.EDGE_BROKER) {
+        // TODO[NVL] Certainly s hack: Bridges are not supported yet
+        acc.push({
+          queryKey: [QUERY_KEYS.DISCOVERY_TOPIC_FILTERS],
+          queryFn: () => appClient.topicFilters.getTopicFilters(),
+        })
+      }
+      return acc
+    },
+    [] as Array<{ queryKey: QueryKey; queryFn: QueryFunction }>
+  )
+
   return useQueries({
-    queries: entities.map((entity) => {
-      return entity.type === EntityType.ADAPTER
-        ? {
-            queryKey: [QUERY_KEYS.ADAPTERS, entity.id, QUERY_KEYS.DISCOVERY_TAGS],
-            queryFn: () => appClient.protocolAdapters.getAdapterDomainTags(entity.id),
-          }
-        : {
-            // TODO[NVL] Certainly s hack: everything else returns topic filters. Bridges are not supported yet
-            queryKey: [QUERY_KEYS.DISCOVERY_TOPIC_FILTERS],
-            queryFn: () => appClient.topicFilters.getTopicFilters(),
-          }
-    }),
+    queries: combinedQueries,
   })
 }
