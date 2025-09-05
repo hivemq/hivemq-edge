4e0cd5900d5f8994a13ad5bb2b55f290490a658d
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-09-05 10:40:11 +0100

    feat(35449): add custom todo task for asset monitoring

---
+++ hivemq-edge-frontend/src/modules/Pulse/components/assets/AssetMonitoringOnboardingTask.tsx
@@ -0,0 +1,75 @@
+import type { FC } from 'react'
+import { useMemo } from 'react'
+import { useTranslation } from 'react-i18next'
+import { Link as RouterLink } from 'react-router-dom'
+import { Badge, Button, List, ListItem, Text } from '@chakra-ui/react'
+import { CheckIcon } from '@chakra-ui/icons'
+
+import { AssetMapping } from '@/api/__generated__'
+import { useListManagedAssets } from '@/api/hooks/usePulse/useListManagedAssets.ts'
+import LoaderSpinner from '@/components/Chakra/LoaderSpinner.tsx'
+import ErrorMessage from '@/components/ErrorMessage.tsx'
+
+interface AssetMonitoringOnboardingTaskContent {
+  [key: string]: { name: string; color?: string }
+}
+
+const AssetMonitoringOnboardingTask: FC = () => {
+  const { t } = useTranslation()
+  const { data: assets, isLoading: isAssetLoading, error } = useListManagedAssets()
+
+  // TODO[NVL] Colors need to be sync with other Pulse/asset components and the theme
+  const categories: AssetMonitoringOnboardingTaskContent = {
+    unmapped: { name: t('New asset(s) requiring mapping'), color: 'status.disconnected' },
+    remapping: { name: t('Existing asset(s) requiring a different mapping'), color: 'status.error' },
+    errors: { name: t('Existing asset(s) with errors'), color: 'status.error' },
+  }
+
+  const aggregate = useMemo(() => {
+    if (!assets?.items) return undefined
+    const aggregate = {
+      unmapped: 0,
+      remapping: 0,
+      errors: 0,
+    }
+
+    return assets.items.reduce((acc, asset) => {
+      if (asset.mapping.status === AssetMapping.status.UNMAPPED) acc.unmapped++
+      if (asset.mapping.status === AssetMapping.status.REQUIRES_REMAPPING) acc.remapping++
+      if (asset.mapping.status === AssetMapping.status.MISSING) acc.errors++
+      return acc
+    }, aggregate)
+  }, [assets?.items])
+
+  const isTaskClear = aggregate?.unmapped === 0 && aggregate?.remapping === 0 && aggregate?.errors === 0
+
+  if (isAssetLoading) return <LoaderSpinner />
+  if (error) return <ErrorMessage message={error.message} />
+
+  return (
+    <List>
+      {!isTaskClear && (
+        <ListItem display="flex" gap={4} alignItems="center" mb={1}>
+          <Badge colorScheme="green">
+            <CheckIcon />
+          </Badge>
+          <Text>{t('All assets are streaming')}</Text>
+        </ListItem>
+      )}
+      {!isTaskClear &&
+        Object.entries(aggregate || {}).map(([key, value]) => (
+          <ListItem key={key} display="flex" gap={4} alignItems="center" mb={1}>
+            <Badge colorScheme={value ? categories[key].color || 'gray' : undefined}>{value}</Badge>
+            {Boolean(value) && (
+              <Button variant="link" as={RouterLink} to="/pulse-assets">
+                {categories[key].name}
+              </Button>
+            )}
+            {!value && <Text>{categories[key].name}</Text>}
+          </ListItem>
+        ))}
+    </List>
+  )
+}
+
+export default AssetMonitoringOnboardingTask
