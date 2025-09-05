06a1c81805305d9a4acdd80b6bf390084eaca7cd
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-09-05 11:38:39 +0100

    fix(35449): handle errors

--- hivemq-edge-frontend/src/modules/Pulse/components/assets/AssetMonitoringBadge.tsx
+++ hivemq-edge-frontend/src/modules/Pulse/components/assets/AssetMonitoringBadge.tsx
@@ -6,21 +6,27 @@ import { AssetMapping } from '@/api/__generated__'
 import { useListManagedAssets } from '@/api/hooks/usePulse/useListManagedAssets.ts'
 import LoaderSpinner from '@/components/Chakra/LoaderSpinner.tsx'
 
+const BADGE_ERROR_CONTENT = '?'
+
 const AssetMonitoringBadge: FC = () => {
-  const { data: assets, isLoading } = useListManagedAssets()
+  const { data: assets, isLoading, error } = useListManagedAssets()
 
   const unattendedAssets = useMemo(() => {
-    if (!assets?.items) return undefined
+    if (!assets?.items || error) return undefined
     return assets.items.filter(
       (asset) =>
         asset.mapping.status === AssetMapping.status.UNMAPPED ||
         asset.mapping.status === AssetMapping.status.REQUIRES_REMAPPING
     )?.length
-  }, [assets?.items])
+  }, [assets?.items, error])
 
-  if (isLoading || !unattendedAssets) return <LoaderSpinner boxSize={4} />
+  if (isLoading) return <LoaderSpinner boxSize={4} />
 
-  return <Badge>{unattendedAssets}</Badge>
+  return (
+    <Badge data-testid="asset-monitoring-unattended" colorScheme={unattendedAssets ? undefined : 'red'}>
+      {unattendedAssets || BADGE_ERROR_CONTENT}
+    </Badge>
+  )
 }
 
 export default AssetMonitoringBadge
