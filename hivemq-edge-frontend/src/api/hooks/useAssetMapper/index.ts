1183b27cd01eceb64c2f25be8d7b0b99f700dfa4
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-09-08 19:14:30 +0100

    fix(36051): fix imports

---
+++ hivemq-edge-frontend/src/api/hooks/useAssetMapper/index.ts
@@ -0,0 +1,7 @@
+import { useCreateAssetMapper } from './useCreateAssetMapper'
+import { useDeleteAssetMapper } from './useDeleteAssetMapper'
+import { useGetAssetMapper } from './useGetAssetMapper'
+import { useListAssetMappers } from './useListAssetMappers'
+import { useUpdateAssetMapper } from './useUpdateAssetMapper'
+
+export { useCreateAssetMapper, useDeleteAssetMapper, useGetAssetMapper, useListAssetMappers, useUpdateAssetMapper }
