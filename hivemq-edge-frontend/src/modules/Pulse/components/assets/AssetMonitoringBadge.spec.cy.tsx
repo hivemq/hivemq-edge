bc06e9a6b5d8696d5c317853748fc81d0971ba92
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-09-08 09:02:33 +0100

    Merge pull request #1131
    * refactor(35449): add provision for a right-hand-side addon
    
    * feat(35449): add custom notification badge for asset monitoring
    
    * feat(35449): add asset monitoring badge into the navlink for Pulse
    
    * feat(35449): add custom todo task for asset monitoring
    
    * feat(35449): add asset monitoring into the onboarding list
    
    * fix(35449): fix type
    
    * fix(35449): fix translations
    
    * fix(35449): fix bug
    
    * feat(35449): add search param to the navigation route
    
    * fix(35449): handle errors
    
    * fix(35449): fix accessibility
    
    * test(35449): add tests
    
    * test(35449): add tests
    
    * test(35449): add tests
    
    * fix(35449): fix type
    
    * fix(35449): fix datatestid
    
    * test(35449): add tests
    
    * chore(35449): update dependencies
    
    * fix(35449): fix typos

---
+++ hivemq-edge-frontend/src/modules/Pulse/components/assets/AssetMonitoringBadge.spec.cy.tsx
@@ -0,0 +1,33 @@
+import { MOCK_PULSE_ASSET_LIST } from '@/api/hooks/usePulse/__handlers__'
+import AssetMonitoringBadge from '@/modules/Pulse/components/assets/AssetMonitoringBadge.tsx'
+
+describe('AssetMonitoringBadge', () => {
+  beforeEach(() => {
+    cy.viewport(350, 600)
+  })
+
+  it('should render properly', () => {
+    cy.intercept('/api/v1/management/pulse/managed-assets', MOCK_PULSE_ASSET_LIST).as('getAssets')
+    cy.mountWithProviders(<AssetMonitoringBadge />)
+
+    cy.getByTestId('loading-spinner').should('be.visible')
+    cy.wait('@getAssets')
+    cy.getByTestId('asset-monitoring-unattended').should('have.text', '3')
+  })
+
+  it('should handle errors', () => {
+    cy.intercept('/api/v1/management/pulse/managed-assets', { statusCode: 404 }).as('getAssets')
+    cy.mountWithProviders(<AssetMonitoringBadge />)
+
+    cy.getByTestId('loading-spinner').should('be.visible')
+    cy.wait('@getAssets')
+    cy.getByTestId('asset-monitoring-unattended').should('have.text', '?')
+  })
+
+  it('should be accessible', () => {
+    cy.intercept('/api/v1/management/pulse/managed-assets', MOCK_PULSE_ASSET_LIST)
+    cy.injectAxe()
+    cy.mountWithProviders(<AssetMonitoringBadge />)
+    cy.checkAccessibility()
+  })
+})
