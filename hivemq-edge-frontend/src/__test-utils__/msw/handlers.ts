b8ef5869f5494a455be00b1f45ce84354a7d0955
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-09-09 10:19:04 +0100

    test(36051): update mocks and handlers

--- hivemq-edge-frontend/src/__test-utils__/msw/handlers.ts
+++ hivemq-edge-frontend/src/__test-utils__/msw/handlers.ts
@@ -1,4 +1,7 @@
 import { handlerCapabilities, MOCK_CAPABILITIES } from '@/api/hooks/useFrontendServices/__handlers__'
-import { handlers } from '@/api/hooks/usePulse/__handlers__'
+import { handlers as pulseHandlers } from '@/api/hooks/usePulse/__handlers__'
+import { persistHandlers } from '@/api/hooks/useAssetMapper/__handlers__'
 
-export const createInterceptHandlers = () => [...handlerCapabilities(MOCK_CAPABILITIES), ...handlers]
+export const createInterceptHandlers = () => {
+  return [...handlerCapabilities(MOCK_CAPABILITIES), ...pulseHandlers, ...persistHandlers()]
+}
--- hivemq-edge-frontend/src/api/hooks/useAssetMapper/__handlers__/index.ts
+++ hivemq-edge-frontend/src/api/hooks/useAssetMapper/__handlers__/index.ts
@@ -1,3 +1,5 @@
+import { NODE_PULSE_AGENT_DEFAULT_ID } from '@/modules/Workspace/utils/nodes-utils.ts'
+import { factory, primaryKey } from '@mswjs/data'
 import { http, HttpResponse } from 'msw'
 import { DataIdentifierReference } from '@/api/__generated__'
 import type { Combiner, CombinerList } from '@/api/__generated__'
@@ -16,7 +18,7 @@ export const MOCK_ASSET_MAPPER: Combiner = {
     items: [
       {
         type: EntityType.ADAPTER,
-        id: 'my-adapter',
+        id: 'opcua-boiler1',
       },
       {
         type: EntityType.ADAPTER,
@@ -24,7 +26,7 @@ export const MOCK_ASSET_MAPPER: Combiner = {
       },
       {
         type: EntityType.PULSE_AGENT,
-        id: 'my-pulse-agent',
+        id: NODE_PULSE_AGENT_DEFAULT_ID,
       },
     ],
   },
@@ -97,3 +99,98 @@ export const handlers = [
     return HttpResponse.json({ updated: data, id: combinerId }, { status: 200 })
   }),
 ]
+
+export const persistHandlers = () => {
+  const mswDB = factory({
+    assetMapper: {
+      id: primaryKey(String),
+      mapper: String,
+    },
+  })
+
+  // Create a fake one
+  mswDB.assetMapper.create({
+    id: MOCK_ASSET_MAPPER.id,
+    mapper: JSON.stringify(MOCK_ASSET_MAPPER),
+  })
+
+  return [
+    /**
+     * @see useListAssetMappers
+     */
+    http.get('*/management/pulse/asset-mappers', () => {
+      const data = mswDB.assetMapper.getAll()
+      const hhhh = data.map((e) => JSON.parse(e.mapper))
+      return HttpResponse.json<CombinerList>(
+        {
+          items: hhhh,
+        },
+        { status: 200 }
+      )
+    }),
+
+    /**
+     * @see useGetAssetMapper
+     */
+    http.get<CombinerParams>('*/management/pulse/asset-mappers/:combinerId', ({ params }) => {
+      const { combinerId } = params
+      const data = mswDB.assetMapper.findFirst({
+        where: {
+          id: {
+            equals: combinerId,
+          },
+        },
+      })
+      if (data?.mapper)
+        return HttpResponse.json<Combiner>({ ...JSON.parse(data?.mapper), id: combinerId }, { status: 200 })
+      else HttpResponse.json({ error: 'not found' }, { status: 400 })
+    }),
+
+    /**
+     * @see useCreateAssetMapper
+     */
+    http.post<never, Combiner>('*/management/pulse/asset-mappers', async ({ request }) => {
+      const data = await request.json()
+      mswDB.assetMapper.create({
+        id: data.id,
+        mapper: JSON.stringify(data),
+      })
+      return HttpResponse.json({ created: data }, { status: 200 })
+    }),
+
+    /**
+     * @see useDeleteAssetMapper
+     */
+    http.delete<CombinerParams>('*/management/pulse/asset-mappers/:combinerId', ({ params }) => {
+      const { combinerId } = params
+      const data = mswDB.assetMapper.delete({
+        where: {
+          id: {
+            equals: combinerId,
+          },
+        },
+      })
+      if (data?.mapper) return HttpResponse.json({ deleted: combinerId }, { status: 200 })
+      else HttpResponse.json({ error: 'not found' }, { status: 400 })
+    }),
+
+    /**
+     * @see useUpdateAssetMapper
+     */
+    http.put<CombinerParams, Combiner>('*/management/pulse/asset-mappers/:combinerId', async ({ params, request }) => {
+      const { combinerId } = params
+      const data = await request.json()
+
+      const updatedData = mswDB.assetMapper.update({
+        where: {
+          id: {
+            equals: combinerId,
+          },
+        },
+        data,
+      })
+      if (updatedData?.mapper) return HttpResponse.json({ updated: updatedData, id: combinerId }, { status: 200 })
+      else HttpResponse.json({ error: 'not found' }, { status: 400 })
+    }),
+  ]
+}
