390ea603ab179c724f884ac674d1f6f14193c4d3
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-09-15 15:51:40 +0100

    test(36221): fix tests

--- hivemq-edge-frontend/src/modules/Pulse/components/assets/AssetActionMenu.spec.cy.tsx
+++ hivemq-edge-frontend/src/modules/Pulse/components/assets/AssetActionMenu.spec.cy.tsx
@@ -10,13 +10,13 @@ describe('AssetActionMenu', () => {
     cy.mountWithProviders(<AssetActionMenu asset={MOCK_PULSE_ASSET} />)
 
     cy.getByAriaLabel('Actions').click()
-    cy.getByTestId('assets-action-edit').should('have.text', 'View details')
+    cy.getByTestId('assets-action-view').should('have.text', 'View details')
     cy.getByTestId('assets-action-map').should('have.text', 'Map the asset')
     cy.getByTestId('assets-action-delete').should('have.text', 'Delete mapping')
     cy.getByTestId('assets-action-mapper').should('have.text', 'Open asset mapper')
 
     cy.get('body').click(0, 0)
-    cy.getByTestId('assets-action-edit').should('not.exist')
+    cy.getByTestId('assets-action-view').should('not.exist')
   })
 
   it('should trigger edit action', () => {
@@ -26,9 +26,9 @@ describe('AssetActionMenu', () => {
 
     cy.get('@onEdit').should('not.have.been.called')
     cy.getByAriaLabel('Actions').click()
-    cy.getByTestId('assets-action-edit').click()
+    cy.getByTestId('assets-action-view').click()
     cy.get('@onEdit').should('have.been.calledWith', '3b028f58-f949-4de1-9b8b-c1a35b1643a4')
-    cy.getByTestId('assets-action-edit').should('not.exist')
+    cy.getByTestId('assets-action-view').should('not.exist')
   })
 
   it('should trigger delete action', () => {
