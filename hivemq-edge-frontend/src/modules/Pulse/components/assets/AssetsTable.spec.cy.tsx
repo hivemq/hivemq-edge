90e2ba3fa67379aab019204b7a16c0ddb7776349
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-09-16 16:25:08 +0100

    test(36221): add tests

--- hivemq-edge-frontend/src/modules/Pulse/components/assets/AssetsTable.spec.cy.tsx
+++ hivemq-edge-frontend/src/modules/Pulse/components/assets/AssetsTable.spec.cy.tsx
@@ -1,5 +1,6 @@
 import { WrapperTestRoute } from '@/__test-utils__/hooks/WrapperTestRoute.tsx'
 
+import type { ManagedAsset } from '@/api/__generated__'
 import { AssetMapping } from '@/api/__generated__'
 import { mockAdapter, mockProtocolAdapter } from '@/api/hooks/useProtocolAdapters/__handlers__'
 import {
@@ -7,13 +8,14 @@ import {
   MOCK_PULSE_EXT_ASSETS_LIST,
 } from '@/api/hooks/usePulse/__handlers__/pulse-mocks.ts'
 import AssetsTable from '@/modules/Pulse/components/assets/AssetsTable.tsx'
+import { NodeTypes, WorkspaceNavigationCommand } from '@/modules/Workspace/types.ts'
 
 const cy_getCell = (row: number, column: number) => cy.get('table tbody tr').eq(row).find('td').eq(column)
 const cy_getHeader = (column: number) => cy.get('table thead th').eq(column)
 
 describe('AssetsTable', () => {
   beforeEach(() => {
-    cy.viewport(1000, 600)
+    cy.viewport(1000, 700)
   })
 
   it('should render errors', () => {
@@ -163,7 +165,7 @@ describe('AssetsTable', () => {
       cy.intercept('/api/v1/management/pulse/managed-assets', MOCK_PULSE_EXT_ASSETS_LIST).as('getStatus')
       cy.intercept('GET', '/api/v1/management/pulse/asset-mappers', MOCK_PULSE_EXT_ASSET_MAPPERS_LIST)
       cy.intercept('/api/v1/management/protocol-adapters/types', { items: [mockProtocolAdapter] })
-      cy.intercept('api/v1/management/protocol-adapters/adapters', { items: [mockAdapter] })
+      cy.intercept('/api/v1/management/protocol-adapters/adapters', { items: [mockAdapter] })
     })
 
     it('should handle navigation and initial filters', () => {
@@ -179,6 +181,94 @@ describe('AssetsTable', () => {
       cy.get('table tbody tr').should('have.length', 2)
       cy_getCell(0, 3).should('have.text', 'Unmapped')
       cy_getCell(1, 3).should('have.text', 'Unmapped')
+    })
+
+    it('should handle view detail', () => {
+      cy.mountWithProviders(<AssetsTable />, {
+        wrapper: WrapperTestRoute,
+        routerProps: { initialEntries: ['/pulse-assets'] },
+      })
+
+      cy.getByTestId('test-pathname').should('have.text', '/pulse-assets')
+      cy_getCell(0, 5).find('button').click()
+      cy.get("[role='menu']#menu-list-asset-actions button").eq(0).click()
+
+      cy.getByTestId('test-pathname').should('have.text', '/pulse-assets/3b028f58-1111-4de1-9b8b-c1a35b1643a4')
+    })
+
+    it('should handle unmapping', () => {
+      cy.intercept('PUT', 'api/v1/management/pulse/managed-assets/*', (req) => {
+        req.reply({ statusCode: 404 })
+      }).as('unmapRequest')
+
+      cy.mountWithProviders(<AssetsTable />, {
+        wrapper: WrapperTestRoute,
+        routerProps: { initialEntries: ['/pulse-assets'] },
+      })
+
+      cy.getByTestId('test-pathname').should('have.text', '/pulse-assets')
+      cy_getCell(3, 5).find('button').click()
+      cy.get("[role='menu']#menu-list-asset-actions button").eq(2).click()
+
+      cy.get('[role="alertdialog"]').within(() => {
+        cy.get('header').should('have.text', 'Unmapping asset "Test asset remapping"')
+        cy.getByTestId('confirmation-message').should(
+          'have.text',
+          'Do you want to remove all mapping instructions from the asset? The action cannot be reversed.'
+        )
+        cy.get('footer button').eq(1).click()
+      })
+
+      cy.get('[role="status"] > div').should('have.attr', 'data-status', 'error')
+      cy.get('[role="status"] > div').should(
+        'contain.text',
+        'There was a problem trying to remove the mapping from the asset' + 'Managed asset not found'
+      )
+
+      cy.wait('@unmapRequest').then(({ request }) => {
+        const body = request.body as Partial<ManagedAsset>
+        expect(body).to.deep.include({
+          id: '3b028f58-3333-4de1-9b8b-c1a35b1643a4',
+        })
+        expect(body.mapping).to.deep.equal({
+          status: 'UNMAPPED',
+        })
+      })
+    })
+
+    it('should handle mapping', () => {
+      cy.intercept('PUT', 'api/v1/management/pulse/managed-assets/*', (req) => {
+        req.reply({ statusCode: 404 })
+      }).as('unmapRequest')
+
+      cy.mountWithProviders(<AssetsTable />, {
+        wrapper: WrapperTestRoute,
+        routerProps: { initialEntries: ['/pulse-assets'] },
+      })
+
+      cy.getByTestId('test-pathname').should('have.text', '/pulse-assets')
+      cy_getCell(0, 5).find('button').click()
+      cy.get("[role='menu']#menu-list-asset-actions button").eq(1).click()
+
+      cy.get('section[role="dialog"][aria-modal="true"]').within(() => {
+        cy.get('header').should('have.text', 'Mapping asset "Test asset unmapped"')
+
+        cy.getByTestId('wizard-mapper-selector-container').click()
+        cy.getByTestId('node-source-id').eq(0).click()
+
+        cy.get('footer button').should('have.text', 'Edit the asset mapper')
+        cy.get('footer button').click()
+      })
+
+      cy.getByTestId('test-pathname').should('have.text', '/workspace')
+      cy.getByTestId('test-state')
+        .invoke('text')
+        .then((text) => {
+          const state = JSON.parse(text)
+          expect(state).to.have.nested.property('selectedAdapter.adapterId', 'e9af7f82-aaaa-4d07-8c0f-e4591148af19')
+          expect(state).to.have.nested.property('selectedAdapter.type', NodeTypes.COMBINER_NODE)
+          expect(state).to.have.nested.property('selectedAdapter.command', WorkspaceNavigationCommand.ASSET_MAPPER)
+        })
     })
   })
 })
