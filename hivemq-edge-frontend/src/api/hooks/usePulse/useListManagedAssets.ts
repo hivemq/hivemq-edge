6c9f25a720cf8c5c3505353b21dca9c88f9ab347
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-09-17 18:39:32 +0100

    refactor(36401): add conditional enabling to the Pulse queries

--- hivemq-edge-frontend/src/api/hooks/usePulse/useListManagedAssets.ts
+++ hivemq-edge-frontend/src/api/hooks/usePulse/useListManagedAssets.ts
@@ -1,14 +1,24 @@
 import { useQuery } from '@tanstack/react-query'
 
 import type { ApiError, ManagedAssetList } from '@/api/__generated__'
+import { Capability } from '@/api/__generated__'
 import { useHttpClient } from '@/api/hooks/useHttpClient/useHttpClient.ts'
+import { useGetCapability } from '@/api/hooks/useFrontendServices/useGetCapability.ts'
 import { QUERY_KEYS } from '@/api/utils.ts'
 
 export const useListManagedAssets = () => {
   const appClient = useHttpClient()
+  const { data: hasPulse, isLoading } = useGetCapability(Capability.id.PULSE_ASSET_MANAGEMENT)
 
-  return useQuery<ManagedAssetList, ApiError>({
+  const query = useQuery<ManagedAssetList, ApiError>({
     queryKey: [QUERY_KEYS.PULSE_ASSETS],
     queryFn: () => appClient.pulse.getManagedAssets(),
+    enabled: hasPulse !== undefined,
   })
+
+  return {
+    // eslint-disable-next-line @tanstack/query/no-rest-destructuring
+    ...query,
+    isLoading: isLoading || query.isLoading,
+  }
 }
