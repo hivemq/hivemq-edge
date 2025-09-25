4e0ebd849938576eac63a127a7de3e3648c52e89
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-09-25 13:39:49 +0100

    refactor(36682): support clean/preloaded states

--- hivemq-edge-frontend/cypress/utils/intercept-pulse.utils.ts
+++ hivemq-edge-frontend/cypress/utils/intercept-pulse.utils.ts
@@ -1,15 +1,16 @@
-import { factory, primaryKey } from '@mswjs/data'
-import type { FactoryAPI } from '@mswjs/data/lib/glossary'
-import type { PrimaryKey } from '@mswjs/data/lib/primaryKey'
-
 import type { ManagedAssetList, ProtocolAdapter, PulseStatus } from '@/api/__generated__'
 import { MOCK_CAPABILITY_PERSISTENCE, MOCK_CAPABILITY_PULSE_ASSETS } from '@/api/hooks/useFrontendServices/__handlers__'
+import { mockAdapter_OPCUA } from '@/api/hooks/useProtocolAdapters/__handlers__'
 import { MOCK_PULSE_STATUS_CONNECTED, MOCK_PULSE_STATUS_DISCONNECTED } from '@/api/hooks/usePulse/__handlers__'
 import {
   MOCK_PULSE_EXT_ASSET_MAPPERS_LIST,
   MOCK_PULSE_EXT_ASSETS_LIST,
+  MOCK_PULSE_EXT_UNMAPPED_ASSETS_LIST,
 } from '@/api/hooks/usePulse/__handlers__/pulse-mocks.ts'
 import { QUERY_KEYS } from '@/api/utils.ts'
+import { factory, primaryKey } from '@mswjs/data'
+import type { FactoryAPI } from '@mswjs/data/lib/glossary'
+import type { PrimaryKey } from '@mswjs/data/lib/primaryKey'
 
 type PrimaryKeyGetter = {
   id: PrimaryKey<string>
@@ -89,8 +90,9 @@ const interceptStatus = (factory: PulseFactory, isActivated: boolean) => {
   }).as('getPulseStatus')
 }
 
-const interceptAssets = (factory: PulseFactory) => {
-  for (const asset of MOCK_PULSE_EXT_ASSETS_LIST.items) {
+const interceptAssets = (factory: PulseFactory, isPreLoaded: boolean) => {
+  const assetList = isPreLoaded ? MOCK_PULSE_EXT_ASSETS_LIST : MOCK_PULSE_EXT_UNMAPPED_ASSETS_LIST
+  for (const asset of assetList.items) {
     factory.assets.create({
       id: asset.id,
       json: JSON.stringify(asset),
@@ -103,12 +105,22 @@ const interceptAssets = (factory: PulseFactory) => {
   }).as('getAssets')
 }
 
-const interceptAssetMappers = (factory: PulseFactory) => {
-  for (const assetMapper of MOCK_PULSE_EXT_ASSET_MAPPERS_LIST.items) {
-    factory.assetMappers.create({
-      id: assetMapper.id,
-      json: JSON.stringify(assetMapper),
-    })
+const interceptAssetMappers = (factory: PulseFactory, isPreLoaded: boolean) => {
+  if (isPreLoaded) {
+    for (const assetMapper of MOCK_PULSE_EXT_ASSET_MAPPERS_LIST.items) {
+      factory.assetMappers.create({
+        id: assetMapper.id,
+        json: JSON.stringify(assetMapper),
+      })
+    }
+
+    // make sure we have the connected adapters
+    cy.intercept('/api/v1/management/protocol-adapters/adapters', {
+      items: [
+        { ...mockAdapter_OPCUA, is: 'my-adapter' },
+        { ...mockAdapter_OPCUA, is: 'my-other-adapter' },
+      ],
+    }).as('getAdapters')
   }
 
   cy.intercept<ManagedAssetList>('GET', '/api/v1/management/pulse/asset-mappers', (req) => {
@@ -117,9 +129,9 @@ const interceptAssetMappers = (factory: PulseFactory) => {
   }).as('getAssetMappers')
 }
 
-export const cy_interceptPulseWithMockDB = (factory: PulseFactory, isActivated = false) => {
+export const cy_interceptPulseWithMockDB = (factory: PulseFactory, isActivated = false, isPreLoaded = false) => {
   interceptCapabilities(factory, isActivated)
   interceptStatus(factory, isActivated)
-  interceptAssets(factory)
-  interceptAssetMappers(factory)
+  interceptAssets(factory, isPreLoaded)
+  interceptAssetMappers(factory, isPreLoaded)
 }
