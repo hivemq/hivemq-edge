b8b6202427a87bf803760ba13e25809a3e2099bc
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-09-30 08:58:30 +0100

    test(36682): add intercepts for PUT assets/asset mappers

--- hivemq-edge-frontend/cypress/utils/intercept-pulse.utils.ts
+++ hivemq-edge-frontend/cypress/utils/intercept-pulse.utils.ts
@@ -1,4 +1,5 @@
-import type { ManagedAssetList, ProtocolAdapter, PulseStatus } from '@/api/__generated__'
+import { MOCK_PROTOCOL_OPC_UA } from '@/__test-utils__/adapters/opc-ua.ts'
+import type { Combiner, ManagedAsset, ManagedAssetList, ProtocolAdapter, PulseStatus } from '@/api/__generated__'
 import { MOCK_CAPABILITY_PERSISTENCE, MOCK_CAPABILITY_PULSE_ASSETS } from '@/api/hooks/useFrontendServices/__handlers__'
 import { mockAdapter_OPCUA } from '@/api/hooks/useProtocolAdapters/__handlers__'
 import { MOCK_PULSE_STATUS_CONNECTED, MOCK_PULSE_STATUS_DISCONNECTED } from '@/api/hooks/usePulse/__handlers__'
@@ -103,6 +104,20 @@ const interceptAssets = (factory: PulseFactory, isPreLoaded: boolean) => {
     const data = factory.assets.getAll()
     req.reply(200, { items: data.map((e) => JSON.parse(e.json)) })
   }).as('getAssets')
+
+  cy.intercept<ManagedAsset>('PUT', '/api/v1/management/pulse/managed-assets/**', (req) => {
+    const asset = req.body
+    const updatedData = factory.assets.update({
+      where: {
+        id: {
+          equals: asset.id,
+        },
+      },
+
+      data: { json: JSON.stringify(asset) },
+    })
+    req.reply(200, { updated: JSON.parse(updatedData.json) })
+  }).as('updateAsset')
 }
 
 const interceptAssetMappers = (factory: PulseFactory, isPreLoaded: boolean) => {
@@ -113,20 +128,32 @@ const interceptAssetMappers = (factory: PulseFactory, isPreLoaded: boolean) => {
         json: JSON.stringify(assetMapper),
       })
     }
-
-    // make sure we have the connected adapters
-    cy.intercept('/api/v1/management/protocol-adapters/adapters', {
-      items: [
-        { ...mockAdapter_OPCUA, is: 'my-adapter' },
-        { ...mockAdapter_OPCUA, is: 'my-other-adapter' },
-      ],
-    }).as('getAdapters')
   }
+
+  // make sure we have the connected adapters
+  cy.intercept('/api/v1/management/protocol-adapters/types', { items: [MOCK_PROTOCOL_OPC_UA] }).as('getProtocols')
+  cy.intercept('/api/v1/management/protocol-adapters/adapters', {
+    items: [
+      { ...mockAdapter_OPCUA, id: 'my-adapter' },
+      { ...mockAdapter_OPCUA, id: 'my-other-adapter' },
+    ],
+  }).as('getAdapters')
+
+  cy.intercept('/api/v1/management/bridges', { items: [] }).as('getBridges')
 
   cy.intercept<ManagedAssetList>('GET', '/api/v1/management/pulse/asset-mappers', (req) => {
     const data = factory.assetMappers.getAll()
     req.reply(200, { items: data.map((e) => JSON.parse(e.json)) })
   }).as('getAssetMappers')
+
+  cy.intercept<Combiner>('POST', '/api/v1/management/pulse/asset-mappers', (req) => {
+    const combiner = req.body
+    const newCombinerData = factory.assetMappers.create({
+      id: combiner.id,
+      json: JSON.stringify(combiner),
+    })
+    req.reply(200, { created: JSON.parse(newCombinerData.json) })
+  }).as('createAssetMapper')
 }
 
 export const cy_interceptPulseWithMockDB = (factory: PulseFactory, isActivated = false, isPreLoaded = false) => {
