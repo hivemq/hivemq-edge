ff429cbf9a10886b7497a2e5afb7af541c81f781
Jochen Mader <jochen@codepitbull.de>
2025-09-30 12:18:27 +0200

    Added env and property to control pulse token location (#1197)

--- hivemq-edge/src/main/java/com/hivemq/configuration/SystemProperties.java
+++ hivemq-edge/src/main/java/com/hivemq/configuration/SystemProperties.java
@@ -34,6 +34,7 @@ public class SystemProperties {
     public static final String DATA_FOLDER = "hivemq.data.folder";
 
     public static final String EXTENSIONS_FOLDER = "hivemq.extensions.folder";
+    public static final String PULSE_TOKEN_FOLDER = "hivemq.pulse.token.folder";
     public static final String CORE_FOLDER = "hivemq.core.folder";
 
     public static final String MODULES_FOLDER = "hivemq.modules.folder";
