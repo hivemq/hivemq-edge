e71ff53a6ef70eb116e029d848f7aae24b7b80d3
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-10-02 15:35:50 +0100

    fix(36963): add PO for the mapper wizard

---
+++ hivemq-edge-frontend/cypress/pages/Workspace/AssetMapperFormPage.ts
@@ -0,0 +1,32 @@
+import { RJSFomField } from '../RJSF/RJSFomField.ts'
+
+export class AssetMapperFormPage extends RJSFomField {
+  get formTabs() {
+    return cy.get('[role="dialog"] form [role="tablist"]')
+  }
+
+  formTab(index: number) {
+    return this.formTabs.find('button').eq(index)
+  }
+
+  get submit() {
+    return cy.get('[role="dialog"][aria-label="Managing Pulse Asset Mappings"] footer button[type="submit"]')
+  }
+
+  get assetMappings() {
+    return {
+      get table() {
+        return cy.get('table[aria-label="The list of mappings created for this combiner"]')
+      },
+
+      action(row: number, action: 'edit' | 'delete') {
+        return cy
+          .get('table[aria-label="The list of mappings created for this combiner"] tbody tr')
+          .eq(row)
+          .find(`[data-testid="combiner-mapping-list-${action}"]`)
+      },
+    }
+  }
+}
+
+export const assetMapperForm = new AssetMapperFormPage()
