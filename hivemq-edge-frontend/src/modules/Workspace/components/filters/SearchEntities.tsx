10400eedb2a10d3206db8583c21bb4e50c9e3294
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-10-07 10:03:09 +0200

    feat(37055): add the search toolbox

---
+++ hivemq-edge-frontend/src/modules/Workspace/components/filters/SearchEntities.tsx
@@ -0,0 +1,119 @@
+import type { FC } from 'react'
+import { useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import { useReactFlow, useStore } from '@xyflow/react'
+import {
+  ButtonGroup,
+  FormControl,
+  FormLabel,
+  Icon,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+  Text,
+} from '@chakra-ui/react'
+import { SearchIcon } from '@chakra-ui/icons'
+import { MdArrowBack, MdArrowForward, MdClear } from 'react-icons/md'
+
+import IconButton from '@/components/Chakra/IconButton.tsx'
+import useWorkspaceStore from '@/modules/Workspace/hooks/useWorkspaceStore.ts'
+import { addSelectedNodesState, resetSelectedNodesState } from '@/modules/Workspace/utils/react-flow.utils.ts'
+
+interface SearchEntitiesProps {
+  id?: string
+}
+
+const SearchEntities: FC<SearchEntitiesProps> = () => {
+  const { t } = useTranslation()
+  const [search, setSearch] = useState('')
+  const [selectedNodes, setSelectedNodes] = useState<string[]>([])
+  const [current, setCurrent] = useState<number | null>(null)
+  const { fitView } = useReactFlow()
+  const { nodes } = useWorkspaceStore()
+  const resetSelectedNodes = useStore(resetSelectedNodesState)
+  const addSelectedNodes = useStore(addSelectedNodesState)
+
+  const handleNavigate = (direction: 'next' | 'prev') => {
+    if (current === null) return
+    let newIndex = direction === 'next' ? current + 1 : current - 1
+    if (newIndex < 0) newIndex = selectedNodes.length - 1
+    if (newIndex >= selectedNodes.length) newIndex = 0
+    setCurrent(newIndex)
+    fitView({ nodes: [{ id: selectedNodes[newIndex] }], padding: 0.25, duration: 750 })
+  }
+
+  const handleClear = (clearValue = false) => {
+    resetSelectedNodes()
+    fitView()
+    setSelectedNodes([])
+    setCurrent(null)
+    if (clearValue) setSearch('')
+  }
+
+  const handleChange = (value: string) => {
+    if (!value) {
+      handleClear(true)
+      return
+    }
+    const foundNodes = nodes.filter((node) => {
+      return new RegExp(value, 'i').test(node.id) && node.hidden === false
+    })
+    const ids = foundNodes.map((node) => node.id)
+    setSearch(value)
+    setSelectedNodes(ids)
+
+    console.log('foundNodes', value, foundNodes)
+    if (foundNodes.length === 0) {
+      handleClear()
+      return
+    }
+
+    addSelectedNodes(ids)
+    setCurrent(0)
+    fitView({ nodes: foundNodes, padding: 0.25, duration: 750 })
+  }
+
+  return (
+    <FormControl variant="horizontal">
+      <FormLabel fontSize="sm" htmlFor="workspace-search" whiteSpace="nowrap" hidden>
+        {t('workspace.searchToolbox.search.label')}
+      </FormLabel>
+      <InputGroup size="sm">
+        <InputLeftElement>
+          <Icon as={SearchIcon} boxSize="3" />
+        </InputLeftElement>
+        <Input
+          placeholder={t('workspace.searchToolbox.search.placeholder')}
+          size="sm"
+          id="workspace-search"
+          value={search}
+          onChange={(e) => handleChange(e.target.value)}
+        />
+        {search && (
+          <InputRightElement>
+            <IconButton
+              size="sm"
+              variant="ghost"
+              aria-label={t('workspace.searchToolbox.search.clear')}
+              icon={<MdClear />}
+              onClick={() => {
+                handleClear(true)
+              }}
+            />
+          </InputRightElement>
+        )}
+      </InputGroup>
+      <ButtonGroup size="sm" isAttached isDisabled={current === null}>
+        <IconButton icon={<MdArrowBack />} aria-label="Previous node" onClick={() => handleNavigate('prev')} />
+        <Text alignContent="center" marginX={2}>
+          {current !== null ? current + 1 : 0} of {selectedNodes.length}
+        </Text>
+
+        <IconButton icon={<MdArrowForward />} aria-label="Next node" onClick={() => handleNavigate('next')} />
+      </ButtonGroup>
+    </FormControl>
+  )
+}
+
+export default SearchEntities
