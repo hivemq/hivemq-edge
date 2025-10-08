dd5c0dd33269c4cc53a16e7f52a69881fff99fd7
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-10-08 15:40:01 +0200

    feat(37057): add filter for topics

---
+++ hivemq-edge-frontend/src/modules/Workspace/components/filters/FilterTopics.tsx
@@ -0,0 +1,84 @@
+import type { FC } from 'react'
+import { useMemo } from 'react'
+import { useTranslation } from 'react-i18next'
+import { FormControl, FormLabel } from '@chakra-ui/react'
+import { chakraComponents, type MultiValue, type OptionProps, Select } from 'chakra-react-select'
+
+import { PLCTag, Topic, TopicFilter } from '@/components/MQTT/EntityTag.tsx'
+import { SelectEntityType } from '@/components/MQTT/types.ts'
+import { useGetDomainOntology } from '@/modules/DomainOntology/hooks/useGetDomainOntology.ts'
+
+import { filterContainerStyle } from '@/modules/Workspace/components/filters/filters.utils.ts'
+import type { FilterTopicsOption } from '@/modules/Workspace/components/filters/types.ts'
+
+interface FilterTopicsProps {
+  onChange?: (values: MultiValue<FilterTopicsOption>) => void
+}
+
+const Option = (props: OptionProps<FilterTopicsOption>) => {
+  const { type, value } = props.data
+  return (
+    <chakraComponents.Option {...props}>
+      {type === SelectEntityType.TOPIC && <Topic tagTitle={value} mr={3} />}
+      {type === SelectEntityType.TAG && <PLCTag tagTitle={value} mr={3} />}
+      {type === SelectEntityType.TOPIC_FILTER && <TopicFilter tagTitle={value} mr={3} />}{' '}
+    </chakraComponents.Option>
+  )
+}
+
+const FilterTopics: FC<FilterTopicsProps> = ({ onChange }) => {
+  const { t } = useTranslation()
+
+  const { tags, northMappings, isLoading, isError } = useGetDomainOntology()
+
+  const options = useMemo<FilterTopicsOption[]>(() => {
+    const allTags =
+      tags.data?.items?.map<FilterTopicsOption>((filter) => ({
+        label: filter.name,
+        value: filter.name,
+        type: SelectEntityType.TAG,
+      })) || []
+    const allTopics =
+      northMappings.data?.items?.map<FilterTopicsOption>((filter) => ({
+        label: filter.topic,
+        value: filter.topic,
+        type: SelectEntityType.TOPIC,
+      })) || []
+
+    return [...allTopics, ...allTags].sort((a, b) => a.value.localeCompare(b.value))
+  }, [northMappings.data?.items, tags.data?.items])
+
+  const handleChange = (values: MultiValue<FilterTopicsOption>) => {
+    console.log('XXXXXXX', values)
+    onChange?.(values)
+  }
+
+  return (
+    <FormControl variant="horizontal">
+      <FormLabel fontSize="sm" htmlFor="workspace-filter-topics">
+        {t('workspace.searchToolbox.byTopics.label')}
+      </FormLabel>
+      <Select<FilterTopicsOption, true>
+        isClearable
+        isMulti
+        isLoading={isLoading}
+        isInvalid={isError}
+        inputId="workspace-filter-topics"
+        options={options}
+        getOptionValue={(option) => option.value}
+        onChange={handleChange}
+        placeholder={t('workspace.searchToolbox.byTopics.placeholder')}
+        size="sm"
+        noOptionsMessage={() => t('workspace.searchToolbox.byTopics.noOptions')}
+        chakraStyles={{
+          container: filterContainerStyle,
+        }}
+        components={{
+          Option,
+        }}
+      />
+    </FormControl>
+  )
+}
+
+export default FilterTopics
