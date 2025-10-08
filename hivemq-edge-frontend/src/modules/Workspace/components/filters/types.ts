482770ba5058f3fc9051b4b16a54d86ae2607531
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-10-08 15:34:31 +0200

    feat(37057): add filter types

---
+++ hivemq-edge-frontend/src/modules/Workspace/components/filters/types.ts
@@ -0,0 +1,30 @@
+import type { SelectEntityType } from '@/components/MQTT/types.ts'
+import type { EdgeTypes, NodeTypes } from '@/modules/Workspace/types.ts'
+
+export const KEY_FILTER_CONFIGURATION = 'workspace:filters'
+
+export interface FilterConfigurationOption {
+  label: string
+  config: string
+}
+
+export interface FilterSelectionOption {
+  id: string
+  type: NodeTypes | EdgeTypes
+}
+
+export interface FilterEntitiesOption {
+  label: string
+  value: NodeTypes
+}
+
+export interface FilterTopicsOption {
+  label: string
+  value: string
+  type: SelectEntityType
+}
+
+export interface FilterStatusOption {
+  label: string
+  status: string
+}
