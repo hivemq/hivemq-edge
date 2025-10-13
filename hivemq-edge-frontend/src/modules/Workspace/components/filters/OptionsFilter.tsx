fa800796629f81c06feb27dec1fd62faf90fc03f
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-10-13 08:55:55 +0200

    refactor(37057): change layout

--- hivemq-edge-frontend/src/modules/Workspace/components/filters/OptionsFilter.tsx
+++ hivemq-edge-frontend/src/modules/Workspace/components/filters/OptionsFilter.tsx
@@ -1,8 +1,20 @@
 import type { FC } from 'react'
 import { useTranslation } from 'react-i18next'
-import { FormControl, FormHelperText, FormLabel, Radio, RadioGroup, Stack, Switch, VStack } from '@chakra-ui/react'
+import {
+  Card,
+  CardBody,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Radio,
+  RadioGroup,
+  Stack,
+  Switch,
+  VStack,
+} from '@chakra-ui/react'
 
-const FILTER_JOIN_OPTIONS = ['OR', 'AND'] as const
+// TODO[NVL] AND is not yet supported
+const FILTER_JOIN_OPTIONS = ['OR' /* 'AND' */] as const
 type FilterJoinOptions = (typeof FILTER_JOIN_OPTIONS)[number]
 
 interface FilterDynamicProps {
@@ -18,38 +30,45 @@ const OptionsFilter: FC<FilterDynamicProps> = ({ onChangeDynamic, onChangeJoin }
   }
 
   return (
-    <>
-      <FormControl variant="horizontal">
-        <FormLabel fontSize="sm" htmlFor="workspace-filter-join">
-          {t('workspace.searchToolbox.join.label')}
-        </FormLabel>
-        <VStack alignItems="flex-start">
-          <RadioGroup
-            onChange={(value: FilterJoinOptions) => onChangeJoin?.(value)}
-            defaultValue={FILTER_JOIN_OPTIONS[0]}
-            id="workspace-filter-join"
-          >
-            <Stack direction="row">
-              {FILTER_JOIN_OPTIONS.map((option) => (
-                <Radio key={option} value={option}>
-                  {t('workspace.searchToolbox.join.option', { context: option })}
-                </Radio>
-              ))}
-            </Stack>
-          </RadioGroup>
-          <FormHelperText>{t('workspace.searchToolbox.join.helper')}</FormHelperText>
-        </VStack>
-      </FormControl>
-      <FormControl variant="horizontal">
-        <FormLabel fontSize="sm" htmlFor="workspace-filter-dynamic-update">
-          {t('workspace.searchToolbox.liveUpdate.label')}
-        </FormLabel>
-        <VStack alignItems="flex-start">
-          <Switch id="workspace-filter-dynamic-update" onChange={handleChange} />
-          <FormHelperText>{t('workspace.searchToolbox.liveUpdate.helper')}</FormHelperText>
-        </VStack>
-      </FormControl>
-    </>
+    <Card
+      size="sm"
+      alignContent="space-between"
+      width="-webkit-fill-available"
+      data-testid="workspace-filter-options-container"
+    >
+      <CardBody gap={3} as={VStack}>
+        <FormControl variant="horizontal">
+          <FormLabel fontSize="sm" htmlFor="workspace-filter-join">
+            {t('workspace.searchToolbox.join.label')}
+          </FormLabel>
+          <VStack alignItems="flex-start">
+            <RadioGroup
+              onChange={(value: FilterJoinOptions) => onChangeJoin?.(value)}
+              defaultValue={FILTER_JOIN_OPTIONS[0]}
+              id="workspace-filter-join"
+            >
+              <Stack direction="row">
+                {FILTER_JOIN_OPTIONS.map((option) => (
+                  <Radio key={option} value={option}>
+                    {t('workspace.searchToolbox.join.option', { context: option })}
+                  </Radio>
+                ))}
+              </Stack>
+            </RadioGroup>
+            <FormHelperText>{t('workspace.searchToolbox.join.helper')}</FormHelperText>
+          </VStack>
+        </FormControl>
+        <FormControl variant="horizontal">
+          <FormLabel fontSize="sm" htmlFor="workspace-filter-dynamic-update">
+            {t('workspace.searchToolbox.liveUpdate.label')}
+          </FormLabel>
+          <VStack alignItems="flex-start">
+            <Switch id="workspace-filter-dynamic-update" onChange={handleChange} />
+            <FormHelperText>{t('workspace.searchToolbox.liveUpdate.helper')}</FormHelperText>
+          </VStack>
+        </FormControl>
+      </CardBody>
+    </Card>
   )
 }
 
