5df525a0655bd39ec8eb2ec58b769e25901779ec
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-10-13 08:48:22 +0200

    refactor(37057): refactor the CTAs

--- hivemq-edge-frontend/src/modules/Workspace/components/filters/ApplyFilter.tsx
+++ hivemq-edge-frontend/src/modules/Workspace/components/filters/ApplyFilter.tsx
@@ -1,76 +1,24 @@
 import type { FC } from 'react'
 import { useTranslation } from 'react-i18next'
-import { Button, FormControl, FormLabel } from '@chakra-ui/react'
-import type { NodeReplaceChange } from '@xyflow/react'
-import { useReactFlow } from '@xyflow/react'
-import type { MultiValue } from 'chakra-react-select'
-
-import type {
-  FilterEntitiesOption,
-  FilterSelectionOption,
-  FilterStatusOption,
-  FilterTopicsOption,
-} from '@/modules/Workspace/components/filters/types.ts'
-import useWorkspaceStore from '@/modules/Workspace/hooks/useWorkspaceStore.ts'
-import { NodeTypes } from '@/modules/Workspace/types.ts'
+import { Button, HStack } from '@chakra-ui/react'
 
 interface ApplyFilterProps {
-  id?: string
-  topics?: MultiValue<FilterTopicsOption>
-  status?: MultiValue<FilterStatusOption>
-  entities?: MultiValue<FilterEntitiesOption>
-  selection?: MultiValue<FilterSelectionOption>
-  isDynamic?: boolean
-  join?: string
+  onSubmit?: () => void
+  onClear?: () => void
 }
 
-const ApplyFilter: FC<ApplyFilterProps> = ({ topics, status, entities, selection, isDynamic, join }) => {
+const ApplyFilter: FC<ApplyFilterProps> = ({ onSubmit, onClear }) => {
   const { t } = useTranslation()
-  const { nodes, onNodesChange } = useWorkspaceStore()
-  const { fitView } = useReactFlow()
-
-  const handleClearFilters = () => {
-    const changeContent = nodes.map<NodeReplaceChange>((node) => ({
-      id: node.id,
-      item: { ...node, hidden: false },
-      type: 'replace',
-    }))
-    onNodesChange(changeContent)
-    fitView({ padding: 0.25, duration: 750 })
-  }
-
-  const handleFilter = () => {
-    // if (!selection?.length) {
-    //   // handleClearFilters()
-    //   return
-    // }
-
-    const changeContent = nodes.map<NodeReplaceChange>((node) => {
-      const isSelection = selection?.some((e) => e.id === node.id)
-      const isWithinEntities = entities?.some((e) => e.value === node.type)
-      console.log('XXXX', isWithinEntities)
-      const isEdge = node.type === NodeTypes.EDGE_NODE
-      return {
-        id: node.id,
-        item: { ...node, hidden: !isSelection && !isEdge && !isWithinEntities },
-        type: 'replace',
-      }
-    })
-    onNodesChange(changeContent)
-    fitView({ padding: 0.25, duration: 750 })
-  }
-
-  const handleApply = () => {
-    console.log('XXXXXXX', { topics, status, entities, selection, isDynamic, join })
-    handleFilter()
-  }
 
   return (
-    <FormControl variant="horizontal">
-      <FormLabel fontSize="sm" htmlFor="workspace-filter-apply"></FormLabel>
-      <Button onClick={() => handleApply()}>{t('workspace.searchToolbox.action.apply')} </Button>
-      <Button onClick={() => handleClearFilters()}>{t('workspace.searchToolbox.action.clear')} </Button>
-    </FormControl>
+    <HStack>
+      <Button onClick={onSubmit} data-testid="filter-apply">
+        {t('workspace.searchToolbox.action.apply')}
+      </Button>
+      <Button onClick={onClear} data-testid="filter-clearAll">
+        {t('workspace.searchToolbox.action.clear')}
+      </Button>
+    </HStack>
   )
 }
 
