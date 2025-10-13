71ef077358db6a6aa1f564f0fe123bea09143fd7
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-10-13 08:52:09 +0200

    refactor(37057): add types

--- hivemq-edge-frontend/src/modules/Workspace/components/filters/types.ts
+++ hivemq-edge-frontend/src/modules/Workspace/components/filters/types.ts
@@ -1,3 +1,5 @@
+import type { FC } from 'react'
+import type { MultiValue, PropsValue } from 'chakra-react-select'
 import type { SelectEntityType } from '@/components/MQTT/types.ts'
 import type { EdgeTypes, NodeTypes } from '@/modules/Workspace/types.ts'
 
@@ -19,6 +21,11 @@ export interface FilterEntitiesOption {
   value: NodeTypes
 }
 
+export interface FilterAdapterOption {
+  label: string
+  type: string
+}
+
 export interface FilterTopicsOption {
   label: string
   value: string
@@ -28,4 +35,40 @@ export interface FilterTopicsOption {
 export interface FilterStatusOption {
   label: string
   status: string
+}
+
+export interface FilterOperationOption {
+  isLiveUpdate: string
+  joinOperator: 'OR' | 'AND'
+}
+
+export interface ActiveFilter<T extends PropsValue<any>> {
+  isActive: boolean
+  filter?: T
+}
+
+export interface Filter {
+  entities?: ActiveFilter<MultiValue<FilterEntitiesOption>>
+  protocols?: ActiveFilter<MultiValue<FilterAdapterOption>>
+  selection?: ActiveFilter<MultiValue<FilterSelectionOption>>
+  topic?: ActiveFilter<MultiValue<FilterTopicsOption>>
+  status?: ActiveFilter<MultiValue<FilterStatusOption>>
+}
+
+export interface FilerConfig extends Filter {
+  quickFilters?: Array<Filter>
+  options?: FilterOperationOption
+}
+
+export interface FilterEditorProps {
+  id: keyof Filter
+  label: string
+  // TODO Fix the type for the filter component
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  editor: FC<any>
+}
+
+export interface FilterCriteriaEditorProps<T> {
+  onChange?: (values: PropsValue<T>) => void
+  selection?: PropsValue<FilterSelectionOption>
 }
