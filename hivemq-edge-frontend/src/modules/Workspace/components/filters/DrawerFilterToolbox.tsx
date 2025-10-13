c68ff2dc92052135a88245d848e4e343af4a12e5
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-10-13 08:52:49 +0200

    refactor(37057): refactor the drawer

---
+++ hivemq-edge-frontend/src/modules/Workspace/components/filters/DrawerFilterToolbox.tsx
@@ -0,0 +1,157 @@
+import type { FC } from 'react'
+import { useTranslation } from 'react-i18next'
+import { useLocalStorage } from '@uidotdev/usehooks'
+import type { NodeReplaceChange } from '@xyflow/react'
+import { useReactFlow } from '@xyflow/react'
+import type { MultiValue } from 'chakra-react-select'
+import {
+  Button,
+  ButtonGroup,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  Icon,
+  useDisclosure,
+  VStack,
+} from '@chakra-ui/react'
+import { GoSidebarExpand } from 'react-icons/go'
+
+import {
+  FilterEntities,
+  FilterSelection,
+  FilterProtocol,
+  FilterStatus,
+  FilterTopics,
+  OptionsFilter,
+  WrapperCriteria,
+  QuickFilters,
+  ApplyFilter,
+} from '@/modules/Workspace/components/filters/index.ts'
+import { hideNodeWithFilters } from '@/modules/Workspace/components/filters/filters.utils.ts'
+
+import type {
+  ActiveFilter,
+  FilerConfig,
+  Filter,
+  FilterEditorProps,
+  FilterEntitiesOption,
+  FilterSelectionOption,
+  FilterStatusOption,
+  FilterTopicsOption,
+} from '@/modules/Workspace/components/filters/types.ts'
+import { KEY_FILTER_CURRENT } from '@/modules/Workspace/components/filters/types.ts'
+import useWorkspaceStore from '@/modules/Workspace/hooks/useWorkspaceStore.ts'
+
+const DrawerFilterToolbox: FC = () => {
+  const { t } = useTranslation()
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  // const [currentState, setCurrentState] = useState<Filter>({})
+  const [currentState, setCurrentState] = useLocalStorage<FilerConfig>(KEY_FILTER_CURRENT, {})
+  const { nodes, onNodesChange } = useWorkspaceStore()
+  const { fitView } = useReactFlow()
+
+  const filterEditors: FilterEditorProps[] = [
+    { id: 'selection', label: t('workspace.searchToolbox.bySelection.criteria'), editor: FilterSelection },
+    { id: 'entities', label: t('workspace.searchToolbox.byEntity.criteria'), editor: FilterEntities },
+    { id: 'protocols', label: t('workspace.searchToolbox.byProtocol.criteria'), editor: FilterProtocol },
+    { id: 'topic', label: t('workspace.searchToolbox.byTopics.criteria'), editor: FilterTopics },
+    { id: 'status', label: t('workspace.searchToolbox.byStatus.criteria'), editor: FilterStatus },
+  ]
+
+  const isAnyFilterActive = Object.values(currentState).some((e: ActiveFilter<never>) => e.isActive)
+
+  const handleClearFilters = () => {
+    const changeContent = nodes.map<NodeReplaceChange>((node) => ({
+      id: node.id,
+      item: { ...node, hidden: false },
+      type: 'replace',
+    }))
+    onNodesChange(changeContent)
+    fitView({ padding: 0.25, duration: 750 })
+    setCurrentState({})
+  }
+
+  const handleFilter = (state: Filter) => {
+    const changeContent = nodes.map<NodeReplaceChange>((node) => {
+      return {
+        id: node.id,
+        item: {
+          ...node,
+          hidden: hideNodeWithFilters(node, state),
+        },
+        type: 'replace',
+      }
+    })
+    onNodesChange(changeContent)
+    fitView({ padding: 0.25, duration: 750 })
+  }
+
+  const handleChange = (id: keyof Filter) => {
+    return (
+      value:
+        | MultiValue<FilterSelectionOption>
+        | MultiValue<FilterEntitiesOption>
+        | MultiValue<FilterTopicsOption>
+        | MultiValue<FilterStatusOption>
+    ) => {
+      const rnew = { ...currentState, [id]: { isActive: true, filter: value } }
+      setCurrentState(rnew)
+      handleFilter(rnew)
+    }
+  }
+
+  const handleActive = (id: string) => (value: boolean) => {
+    const current = currentState[id as keyof Filter]
+    const rnew = { ...currentState, [id]: { isActive: value, filter: current?.filter } }
+    setCurrentState(rnew)
+    handleFilter(rnew)
+  }
+
+  return (
+    <ButtonGroup variant="outline" isAttached size="sm" m={1} data-testid="toolbox-filter">
+      <Button
+        size="sm"
+        leftIcon={<Icon as={GoSidebarExpand} boxSize="24px" />}
+        // rightIcon={<Icon as={TbFilter} boxSize="24px" />}
+        onClick={onOpen}
+        data-testid="toolbox-filter-open"
+      >
+        {t('workspace.searchToolbox.trigger.aria-label')}
+      </Button>
+      <Button onClick={handleClearFilters} isDisabled={!isAnyFilterActive} data-testid="toolbox-filter-clearAll">
+        {t('workspace.searchToolbox.action.clear')}
+      </Button>
+
+      <Drawer isOpen={isOpen} placement="right" size="sm" onClose={onClose} id="filter-workspace">
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>{t('workspace.searchToolbox.title')}</DrawerHeader>
+
+          <DrawerBody as={VStack} gap={4}>
+            <QuickFilters />
+            {filterEditors.map((criteria) => (
+              <WrapperCriteria
+                key={criteria.id}
+                label={criteria.label}
+                id={criteria.id}
+                isActive={currentState[criteria.id]?.isActive || false}
+                onChange={handleActive(criteria.id)}
+              >
+                <criteria.editor onChange={handleChange(criteria.id)} value={currentState[criteria.id]?.filter} />
+              </WrapperCriteria>
+            ))}
+            <OptionsFilter />
+          </DrawerBody>
+          <DrawerFooter mt={4}>
+            <ApplyFilter onClear={handleClearFilters} />
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </ButtonGroup>
+  )
+}
+
+export default DrawerFilterToolbox
