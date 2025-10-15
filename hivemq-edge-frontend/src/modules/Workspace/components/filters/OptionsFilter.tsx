d9b97581ea85e28f6580246d132f30d50d6dc34d
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-10-15 09:09:05 +0100

    test(37057): add tests

--- hivemq-edge-frontend/src/modules/Workspace/components/filters/OptionsFilter.tsx
+++ hivemq-edge-frontend/src/modules/Workspace/components/filters/OptionsFilter.tsx
@@ -15,7 +15,7 @@ import {
 } from '@chakra-ui/react'
 
 // TODO[NVL] AND is not yet supported
-const FILTER_JOIN_OPTIONS = ['OR' /* 'AND' */] as const
+const FILTER_JOIN_OPTIONS = ['OR', 'AND'] as const
 type FilterJoinOptions = (typeof FILTER_JOIN_OPTIONS)[number]
 
 interface FilterDynamicProps {
@@ -37,15 +37,15 @@ const OptionsFilter: FC<FilterDynamicProps> = ({ value, onChange }) => {
       data-testid="workspace-filter-options-container"
     >
       <CardBody gap={3} as={VStack}>
-        <FormControl variant="horizontal">
-          <FormLabel fontSize="sm" htmlFor="workspace-filter-join">
+        <FormControl variant="horizontal" id="workspace-filter-join">
+          <FormLabel fontSize="sm" htmlFor="workspace-filter-join-input">
             {t('workspace.searchToolbox.join.label')}
           </FormLabel>
-          <VStack alignItems="flex-start">
+          <VStack alignItems="flex-start" gap={0}>
             <RadioGroup
               onChange={(value: FilterJoinOptions) => onChange?.('joinOperator', value)}
-              defaultValue={value?.joinOperator || 'OR'}
-              id="workspace-filter-join"
+              value={value?.joinOperator || 'OR'}
+              id="workspace-filter-join-input"
             >
               <Stack direction="row">
                 {FILTER_JOIN_OPTIONS.map((option) => (
@@ -58,13 +58,13 @@ const OptionsFilter: FC<FilterDynamicProps> = ({ value, onChange }) => {
             <FormHelperText>{t('workspace.searchToolbox.join.helper')}</FormHelperText>
           </VStack>
         </FormControl>
-        <FormControl variant="horizontal">
-          <FormLabel fontSize="sm" htmlFor="workspace-filter-dynamic-update">
+        <FormControl variant="horizontal" id="workspace-filter-dynamic-update">
+          <FormLabel fontSize="sm" htmlFor="workspace-filter-dynamic-update-input">
             {t('workspace.searchToolbox.liveUpdate.label')}
           </FormLabel>
-          <VStack alignItems="flex-start">
+          <VStack alignItems="flex-start" gap={0}>
             <Switch
-              id="workspace-filter-dynamic-update"
+              id="workspace-filter-dynamic-update-input"
               onChange={(e) => onChange?.('isLiveUpdate', e.target.checked)}
               isChecked={value?.isLiveUpdate || false}
             />
