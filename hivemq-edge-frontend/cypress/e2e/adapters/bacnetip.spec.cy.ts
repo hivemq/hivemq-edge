45929ab03f29e886291187c2025534f68253f885
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-10-22 11:10:13 +0100

    test(37074): add baseline E2E tests for missing adapters

---
+++ hivemq-edge-frontend/cypress/e2e/adapters/bacnetip.spec.cy.ts
@@ -0,0 +1,25 @@
+import { MOCK_PROTOCOL_BACNET_IP } from '@/__test-utils__/adapters/bacnetip.ts'
+import { loginPage, adapterPage } from 'cypress/pages'
+
+import { cy_interceptCoreE2E } from 'cypress/utils/intercept.utils.ts'
+
+describe('BACnet/IP', () => {
+  beforeEach(() => {
+    cy_interceptCoreE2E()
+
+    cy.intercept('/api/v1/management/protocol-adapters/types', { items: [MOCK_PROTOCOL_BACNET_IP] })
+
+    // TODO[E2E] This doesn't work: JWT needs mocking
+    loginPage.visit('/app/protocol-adapters/catalog/new/bacnetip')
+    loginPage.loginButton.click()
+    adapterPage.navLink.click()
+  })
+
+  it('should be accessible', () => {
+    cy.injectAxe()
+    cy.checkAccessibility()
+
+    adapterPage.addNewAdapter.click()
+    cy.checkAccessibility()
+  })
+})
---
+++ hivemq-edge-frontend/cypress/e2e/adapters/mtconnect.spec.cy.ts
@@ -0,0 +1,24 @@
+import { MOCK_PROTOCOL_MTCONNECT } from '@/__test-utils__/adapters/mtconnect.ts'
+import { loginPage, adapterPage } from 'cypress/pages'
+
+import { cy_interceptCoreE2E } from 'cypress/utils/intercept.utils.ts'
+
+describe('Databases Protocol Adapter', () => {
+  beforeEach(() => {
+    cy_interceptCoreE2E()
+
+    cy.intercept('/api/v1/management/protocol-adapters/types', { items: [MOCK_PROTOCOL_MTCONNECT] }).as('getProtocols')
+
+    loginPage.visit('/app/protocol-adapters/catalog/new/mtconnect')
+    loginPage.loginButton.click()
+    adapterPage.navLink.click()
+  })
+
+  it('should be accessible', () => {
+    cy.injectAxe()
+    cy.checkAccessibility()
+
+    adapterPage.addNewAdapter.click()
+    cy.checkAccessibility()
+  })
+})
