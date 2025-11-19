dc2740072cb27efeacd1393ef37a134d43c3abcc
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-11-19 17:36:21 +0000

    refactor(37761): refactor the main domain lists

--- hivemq-edge-frontend/src/api/hooks/useDomainModel/useListDomainSouthboundMappings.ts
+++ hivemq-edge-frontend/src/api/hooks/useDomainModel/useListDomainSouthboundMappings.ts
@@ -1,13 +1,13 @@
 import { useQuery } from '@tanstack/react-query'
 import { QUERY_KEYS } from '@/api/utils.ts'
 
-import type { ApiError, SouthboundMappingList } from '@/api/__generated__'
+import type { ApiError, SouthboundMappingOwnerList } from '@/api/__generated__'
 import { useHttpClient } from '@/api/hooks/useHttpClient/useHttpClient.ts'
 
 export const useListDomainSouthboundMappings = () => {
   const appClient = useHttpClient()
 
-  return useQuery<SouthboundMappingList, ApiError>({
+  return useQuery<SouthboundMappingOwnerList, ApiError>({
     queryKey: [QUERY_KEYS.SOUTHBOUND_MAPPINGS],
     queryFn: () => appClient.protocolAdapters.getSouthboundMappings(),
   })
