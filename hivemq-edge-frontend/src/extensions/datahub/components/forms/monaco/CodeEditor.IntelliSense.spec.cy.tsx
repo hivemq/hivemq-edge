2b2642cc5d9f65cf9b116737680fd1056257ac19
Nicolas Van Labeke <nicolas.vanlabeke@hivemq.com>
2025-12-03 09:23:59 +0000

    test(37937) fix tests

--- hivemq-edge-frontend/src/extensions/datahub/components/forms/monaco/CodeEditor.IntelliSense.spec.cy.tsx
+++ hivemq-edge-frontend/src/extensions/datahub/components/forms/monaco/CodeEditor.IntelliSense.spec.cy.tsx
@@ -28,7 +28,8 @@ describe('Monaco IntelliSense - Integration Tests (Configuration Verification)',
     cy.viewport(1200, 800)
 
     // Ignore Monaco worker loading errors
-    cy.on('uncaught:exception', (err) => {
+    cy.on('uncaught:exception', (err, _, pro) => {
+      console.log('XXXXXXX', { err, pro })
       return !(err.message.includes('importScripts') || err.message.includes('ts.worker'))
     })
   })
