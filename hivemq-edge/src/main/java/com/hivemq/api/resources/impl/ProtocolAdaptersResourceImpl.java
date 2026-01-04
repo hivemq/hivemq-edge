787fc6309fa3e7364cd6b0d8378f317db810189a
martin Schoenert <martin.schoenert@hivemq.com>
2026-01-04 15:58:15 +0100

    added roles to transitionAdapterStatus

--- hivemq-edge/src/main/java/com/hivemq/api/resources/impl/ProtocolAdaptersResourceImpl.java
+++ hivemq-edge/src/main/java/com/hivemq/api/resources/impl/ProtocolAdaptersResourceImpl.java
@@ -27,6 +27,8 @@ import com.hivemq.api.AbstractApi;
 import com.hivemq.api.errors.BadRequestError;
 import com.hivemq.api.errors.ConfigWritingDisabled;
 import com.hivemq.api.errors.InternalServerError;
+import com.hivemq.api.auth.ApiRoles;
+import jakarta.annotation.security.RolesAllowed;
 import com.hivemq.api.errors.adapters.AdapterCannotBeUpdatedError;
 import com.hivemq.api.errors.adapters.AdapterFailedSchemaValidationError;
 import com.hivemq.api.errors.adapters.AdapterFailedValidationError;
@@ -366,6 +368,7 @@ public class ProtocolAdaptersResourceImpl extends AbstractApi implements Protoco
     }
 
     @Override
+    @RolesAllowed({ApiRoles.ADMIN, ApiRoles.SUPER})
     public @NotNull Response transitionAdapterStatus(
             final @NotNull String adapterId,
             final @NotNull StatusTransitionCommand command) {
