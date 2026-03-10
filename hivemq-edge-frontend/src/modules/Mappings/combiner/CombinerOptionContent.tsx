075f0981b7fd45257a5de7f823a05e7aa90f9aa8
Nicolas Van Labeke <nicolas.github@calques3d.org>
2026-03-10 15:31:50 +0000

    Merge pull request #1439
    * Add scope to DataIdentifierReference (#1367)
    
    * refactor(EDG-176): add custom renderers to the primary selector
    
    * test(EDG-176): fix tests
    
    * chore(EDG-176): add context
    
    * chore(EDG-176): linting
    
    * test(EDG-176): fix tests
    
    * refactor(EDG-176): fix code duplication
    
    * chore(EDG-176): linting
    
    * chore(EDG-176): linting
    
    * Revert "Add scope to DataIdentifierReference (#1367)"
    
    This reverts commit 05efa588f01ba5ae0aa253e4c25f9fec58b16732.
    
    ---------
    
    Co-authored-by: Sam Cao <sam.cao@hivemq.com>

---
+++ hivemq-edge-frontend/src/modules/Mappings/combiner/CombinerOptionContent.tsx
@@ -0,0 +1,36 @@
+import type { FC } from 'react'
+import { useTranslation } from 'react-i18next'
+import { HStack, Text, VStack } from '@chakra-ui/react'
+
+import type { DataIdentifierReference } from '@/api/__generated__'
+
+interface CombinerOptionContentProps {
+  label: string
+  type: DataIdentifierReference.type
+  adapterId?: string
+  description?: string
+}
+
+export const CombinerOptionContent: FC<CombinerOptionContentProps> = ({ label, adapterId, type, description }) => {
+  const { t } = useTranslation()
+  return (
+    <VStack gap={0} alignItems="stretch" w="100%">
+      <HStack>
+        <Text flex={1}>{label}</Text>
+        {adapterId && (
+          <Text fontSize="sm" color="gray.500">
+            {adapterId}
+          </Text>
+        )}
+        <Text fontSize="sm" fontWeight="bold">
+          {t('combiner.schema.mapping.combinedSelector.type', { context: type })}
+        </Text>
+      </HStack>
+      {description && (
+        <Text fontSize="sm" noOfLines={3} ml={4} lineHeight="normal" textAlign="justify">
+          {description}
+        </Text>
+      )}
+    </VStack>
+  )
+}
