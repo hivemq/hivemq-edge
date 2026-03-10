b6cc5bf715b5f23e6d1a2daf4eed75bcd51ad7e9
Nicolas Van Labeke <nicolas.github@calques3d.org>
2026-03-10 15:31:50 +0000

    Merge pull request #1439
    * Add scope to DataIdentifierReference (#1367)
    
    * refactor(EDG-176): add custom renderers to the primary selector
    
    * test(EDG-176): fix tests
    
    * chore(EDG-176): add context
    
    * chore(EDG-176): linting
    
    * test(EDG-176): fix tests
    
    * refactor(EDG-176): fix code duplication
    
    * chore(EDG-176): linting
    
    * chore(EDG-176): linting
    
    * Revert "Add scope to DataIdentifierReference (#1367)"
    
    This reverts commit 05efa588f01ba5ae0aa253e4c25f9fec58b16732.
    
    ---------
    
    Co-authored-by: Sam Cao <sam.cao@hivemq.com>

---
+++ hivemq-edge-frontend/src/modules/Mappings/combiner/CombinerOptionContent.spec.cy.tsx
@@ -0,0 +1,65 @@
+import { DataIdentifierReference } from '@/api/__generated__'
+import { CombinerOptionContent } from './CombinerOptionContent'
+
+describe('CombinerOptionContent', () => {
+  beforeEach(() => {
+    cy.viewport(400, 200)
+  })
+
+  it('should render the label and type badge', () => {
+    cy.mountWithProviders(<CombinerOptionContent label="boiler/temperature" type={DataIdentifierReference.type.TAG} />)
+
+    cy.get('p').should('contain.text', 'boiler/temperature')
+    cy.get('p').should('contain.text', 'Tag')
+  })
+
+  it('should render the adapter name when adapterId is provided', () => {
+    cy.mountWithProviders(
+      <CombinerOptionContent
+        label="boiler/temperature"
+        type={DataIdentifierReference.type.TAG}
+        adapterId="muc-opc-server"
+      />
+    )
+
+    cy.get('p').should('contain.text', 'muc-opc-server')
+  })
+
+  it('should not render the adapter name when adapterId is absent', () => {
+    cy.mountWithProviders(
+      <CombinerOptionContent label="my/topic/+/temp" type={DataIdentifierReference.type.TOPIC_FILTER} />
+    )
+
+    cy.get('p').should('contain.text', 'Topic Filter')
+    cy.get('p').should('have.length', 2) // label + type badge only
+  })
+
+  it('should render the description when provided', () => {
+    cy.mountWithProviders(
+      <CombinerOptionContent
+        label="boiler/temperature"
+        type={DataIdentifierReference.type.TAG}
+        adapterId="muc-opc-server"
+        description="Current boiler temperature in Celsius"
+      />
+    )
+
+    cy.get('p').should('contain.text', 'Current boiler temperature in Celsius')
+  })
+
+  it('should not render a description element when description is absent', () => {
+    cy.mountWithProviders(<CombinerOptionContent label="boiler/temperature" type={DataIdentifierReference.type.TAG} />)
+
+    cy.get('p').should('not.contain.text', 'Celsius')
+    cy.get('p').should('have.length', 2) // label + type badge only
+  })
+
+  it('should render the correct type badge for topic filters', () => {
+    cy.mountWithProviders(
+      <CombinerOptionContent label="my/topic/+/temp" type={DataIdentifierReference.type.TOPIC_FILTER} />
+    )
+
+    cy.get('p').should('contain.text', 'Topic Filter')
+    cy.get('p').should('not.contain.text', 'Tag')
+  })
+})
