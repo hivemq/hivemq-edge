3f86d955786b3186a03e91bfbb3d5256ea4f89c9
Jochen Mader <jochen.mader@hivemq.com>
2026-03-13 09:19:04 +0100

    Add try-catch to executor usage to avoid errors on shutdown

--- hivemq-edge/src/main/java/com/hivemq/protocols/ProtocolAdapterManager.java
+++ hivemq-edge/src/main/java/com/hivemq/protocols/ProtocolAdapterManager.java
@@ -55,6 +55,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.function.Function;
@@ -164,7 +165,8 @@ public class ProtocolAdapterManager {
     }
 
     public void refresh(final @NotNull List<ProtocolAdapterEntity> configs) {
-        executorService.submit(() -> {
+        try {
+            executorService.submit(() -> {
             log.info("Refreshing adapters");
 
             final Map<String, ProtocolAdapterConfig> protocolAdapterConfigs = configs.stream()
@@ -269,6 +271,13 @@ public class ProtocolAdapterManager {
                         .fire();
             }
         });
+        } catch (final RejectedExecutionException e) {
+            if (executorService.isShutdown()) {
+                log.debug("Adapter refresh task submission rejected, executor is shutting down.");
+            } else {
+                throw e;
+            }
+        }
     }
 
     // INTERNAL
