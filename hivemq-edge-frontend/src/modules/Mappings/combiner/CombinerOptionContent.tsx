5d0e51bd69e92ef1834115747f3b7d5c7afe2ae8
Nicolas Van Labeke <nicolas.github@calques3d.org>
2026-03-19 10:07:48 +0000

    Merge pull request #1449
    * fix(EDG-222): change rendering of the tag elements
    
    * fix(EDG-222): change ordering of the options
    
    * test(EDG-222): fix tests
    
    * chore(EDG-222): add context
    
    * chore(EDG-222): linting
    
    * fix(EDG-222): fix sorting
    
    * fix(EDG-222): fix formatting and layout
    
    * fix(EDG-222): fix tests
    
    * fix(EDG-222): fix tests
    
    * fix(EDG-222): fix layout

--- hivemq-edge-frontend/src/modules/Mappings/combiner/CombinerOptionContent.tsx
+++ hivemq-edge-frontend/src/modules/Mappings/combiner/CombinerOptionContent.tsx
@@ -2,7 +2,9 @@ import type { FC } from 'react'
 import { useTranslation } from 'react-i18next'
 import { HStack, Text, VStack } from '@chakra-ui/react'
 
-import type { DataIdentifierReference } from '@/api/__generated__'
+import { DataIdentifierReference } from '@/api/__generated__'
+import { PLCTagIcon, TopicFilterIcon } from '@/components/Icons/TopicIcon'
+import { formatOwnershipString } from '@/components/MQTT/topic-utils'
 
 interface CombinerOptionContentProps {
   label: string
@@ -13,21 +15,23 @@ interface CombinerOptionContentProps {
 
 export const CombinerOptionContent: FC<CombinerOptionContentProps> = ({ label, adapterId, type, description }) => {
   const { t } = useTranslation()
+  // const displayLabel = adapterId ? `${adapterId} :: ${label}` : label
+  const displayLabel = formatOwnershipString({ id: label, type, scope: adapterId })
+
   return (
     <VStack gap={0} alignItems="stretch" w="100%">
       <HStack>
-        <Text flex={1}>{label}</Text>
-        {adapterId && (
-          <Text fontSize="sm" color="gray.500">
-            {adapterId}
-          </Text>
-        )}
+        <HStack flex={1} gap={2} overflow="hidden" minW={0}>
+          {type === DataIdentifierReference.type.TAG && <PLCTagIcon boxSize="12px" flexShrink={0} />}
+          {type === DataIdentifierReference.type.TOPIC_FILTER && <TopicFilterIcon boxSize="12px" flexShrink={0} />}
+          <Text isTruncated>{displayLabel}</Text>
+        </HStack>
         <Text fontSize="sm" fontWeight="bold">
           {t('combiner.schema.mapping.combinedSelector.type', { context: type })}
         </Text>
       </HStack>
       {description && (
-        <Text fontSize="sm" noOfLines={3} ml={4} lineHeight="normal" textAlign="justify">
+        <Text fontSize="sm" noOfLines={3} ml={5} lineHeight="normal" textAlign="justify">
           {description}
         </Text>
       )}
