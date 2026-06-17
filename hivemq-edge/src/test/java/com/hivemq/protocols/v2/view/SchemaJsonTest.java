399f6b50625b9004029f34455562efb099b5d1d4
Sam Cao <sam.cao@hivemq.com>
2026-06-17 10:36:16 +0200

    EDG-722: Move the Nevsky framework to the protocols.v2 package (#1568)

--- hivemq-edge/src/test/java/com/hivemq/protocols/v2/view/SchemaJsonTest.java
+++ hivemq-edge/src/test/java/com/hivemq/protocols/v2/view/SchemaJsonTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.hivemq.protocols2.view;
+package com.hivemq.protocols.v2.view;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
